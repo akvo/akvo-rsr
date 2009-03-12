0fc2244aae1bb634113ff8ce188e1ecc9887440a
Gabriel von Heijne <gabriel@dev-head>
2009-03-12 21:20:40 +0000

    Dutch and German translation of widgets and paypal donations.

--- akvo/settings.py
+++ akvo/settings.py
@@ -11,22 +11,22 @@ from settings_base import *
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'rsrdb' # Or path to database file if using sqlite3.
 DATABASE_USER = 'rsruser'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'qQGfrcb5JWrPmwS9'         # Not used with sqlite3.
+DATABASE_PASSWORD = 'u5r6vKSBwUxE4EMB'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # has to be defined _after_ DATABASE_ENGINE
 #from django.contrib.sites.models import Site
 
-DEBUG = False
+DEBUG = True
 
 SITE_ID = 1
 try:
     DOMAIN_NAME = Site.objects.get(pk=SITE_ID)
 except:
-    DOMAIN_NAME = 'www.akvo.org'
+    DOMAIN_NAME = 'newdev.akvo.org'
 if not DOMAIN_NAME:
-    DOMAIN_NAME = 'www.akvo.org'
+    DOMAIN_NAME = 'newdev.akvo.org'
     
 DEFAULT_FROM_EMAIL = 'noreply@%s' % DOMAIN_NAME
 
@@ -36,7 +36,7 @@ DEFAULT_FROM_EMAIL = 'noreply@%s' % DOMAIN_NAME
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/var/www/akvo/mediaroot/'
+MEDIA_ROOT = '/var/dev/akvo/mediaroot/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -46,7 +46,7 @@ MEDIA_URL = 'http://%s/rsr/media/' % DOMAIN_NAME
 TEMPLATE_DEBUG = DEBUG
 
 # PayPal settings
-PAYPAL_DEBUG = False # Set to False in production
+PAYPAL_DEBUG = True # Set to False in production
 #PAYPAL_COMMAND = '_donations' # This defaults to '_donations'
 #PAYPAL_CURRENCY_CODE = 'EUR' # This defaults to 'EUR'
 PAYPAL_BUSINESS = 'thomas@akvo.org'
