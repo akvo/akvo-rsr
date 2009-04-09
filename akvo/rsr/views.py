aa9f4693852bbf5f446fb131f6ea9eff7d75c66c
Gabriel von Heijne <gabriel@akvo.org>
2009-04-09 16:27:24 +0200

    User profile checkbox for activating users. Email notification when user validates email to SUs and org admins. User profile fields User and Org fields read-only for org users.

--- akvo/rsr/views.py
+++ akvo/rsr/views.py
@@ -203,7 +203,8 @@ def index(request):
         }
     p = Project.objects.published()        
     if bandwidth == 'low':
-        grid_projects = p.filter(current_image__startswith='img').order_by('?')[:12]
+        #TODO: better filtering criteria, we want to find all projects that have an image
+        grid_projects = p.filter(current_image__startswith='db').order_by('?')[:12]
     else:
         grid_projects = None
     stats = akvo_at_a_glance(p)
@@ -424,26 +425,58 @@ def activate(request, activation_key,
     registration/activate.html or ``template_name`` keyword argument.
     
     """
+    
+    #amalgamation of registration.views.activate and registration.models.RegistrationManager.activate_user
+    #however, we don't actually acivate! Instead we use signals.
+
+    from registration.signals import user_activated
+    import re
+
+    SHA1_RE = re.compile('^[a-f0-9]{40}$')
+    
     activation_key = activation_key.lower() # Normalize before trying anything with it.
-    user = RegistrationProfile.objects.activate_user(activation_key)
-    if user:
-        #Since we want to verify the user before letting anyone in we set is_active
-        #to False (it is set to True by RegistrationProfile.objects.activate_user)
-        user.is_active = False
-        user.save()
-        current_site = Site.objects.get_current()
-        subject = 'Akvo user email confirmed'                
-        message = 'A user, %s, has confirmed her email. Check it out!' % user.username
-        send_mail(subject, message, 'noreply@%s' % current_site, REGISTRATION_RECEIVERS)
+
+    if SHA1_RE.search(activation_key):
+        try:
+            profile = RegistrationProfile.objects.get(activation_key=activation_key)
+        except RegistrationProfile.DoesNotExist:
+            account = False
+        else:
+            account = profile.user
+            if not profile.activation_key_expired():
+                profile.activation_key = RegistrationProfile.ACTIVATED
+                profile.save()
+                user_activated.send(sender=RegistrationProfile, user=account)
     if extra_context is None:
         extra_context = {}
     context = RequestContext(request)
     for key, value in extra_context.items():
         context[key] = callable(value) and value() or value
     return render_to_response(template_name,
-                              { 'account': user,
+                              { 'account': account,
                                 'expiration_days': settings.ACCOUNT_ACTIVATION_DAYS },
-                              context_instance=context)
+                              context_instance=context)    
+    
+    #activation_key = activation_key.lower() # Normalize before trying anything with it.
+    #user = RegistrationProfile.objects.activate_user(activation_key)
+    #if user:
+    #    #Since we want to verify the user before letting anyone in we set is_active
+    #    #to False (it is set to True by RegistrationProfile.objects.activate_user)
+    #    user.is_active = False
+    #    user.save()
+    #    current_site = Site.objects.get_current()
+    #    subject = 'Akvo user email confirmed'                
+    #    message = 'A user, %s, has confirmed her email. Check it out!' % user.username
+    #    send_mail(subject, message, 'noreply@%s' % current_site, REGISTRATION_RECEIVERS)
+    #if extra_context is None:
+    #    extra_context = {}
+    #context = RequestContext(request)
+    #for key, value in extra_context.items():
+    #    context[key] = callable(value) and value() or value
+    #return render_to_response(template_name,
+    #                          { 'account': user,
+    #                            'expiration_days': settings.ACCOUNT_ACTIVATION_DAYS },
+    #                          context_instance=context)
 
 
 #copied from django.contrib.auth.views to be able to customize the form widget attrs
@@ -539,8 +572,8 @@ class UpdateForm(ModelForm):
     #status          = forms.CharField(widget=forms.RadioSelect(choices=STATUSES, attrs={'class':'radio'}))
     photo           = forms.ImageField(required=False, widget=forms.FileInput(attrs={'class':'input', 'size':'15', 'style':'height: 2em'}))
     photo_location  = forms.CharField(required=False, widget=forms.RadioSelect(choices=PHOTO_LOCATIONS, attrs={'class':'radio'}))
-    photo_caption   = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'input', 'size':'25',}))
-    photo_credit    = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'input', 'size':'25',}))
+    photo_caption   = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'input', 'size':'25', 'maxlength': '75'}))
+    photo_credit    = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'input', 'size':'25', 'maxlength': '25'}))
     
     class Meta:
         model = ProjectUpdate
