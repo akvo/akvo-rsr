178501f1cc7a948a8a8f0e87b16af37339be1015
Daniel Karlsson <daniel@fraction4.com>
2009-09-18 12:00:24 +0200

    FIxed a bug with the jquery update

--- akvo/mediaroot/js/machinery2.js
+++ akvo/mediaroot/js/machinery2.js
@@ -10,13 +10,15 @@ function getHeight(widget_type)
 	{
 		case 'feature-side': 		return 840; break; 
 		case 'project-contribute':  return 570; break; 
+		case 'project-small':       return 570; break; 
 		case 'project-updates': 	return 900; break; 
-		case 'project-list': 		return 730; break; 
+		case 'project-list': 		return 730; break;
 		case 'project-narrow': 		return 840; break; 
-		case 'cobranded-narrow': 	return 911; break; 
+		case 'cobranded-narrow':    return 911; break;
 		case 'cobranded-short':     return 627; break;
 		case 'cobranded-banner':    return 234; break;
 		case 'cobranded-leader':    return 207; break;
+
 		default: 					return 840;
 	}	
 }
@@ -27,16 +29,18 @@ function getWidth(widget_type)
 	{
 		case 'feature-side': 		return 202; break; 
 		case 'project-contribute': 	return 202; break; 
+		case 'project-small':       return 170; break;
 		case 'project-updates': 	return 202; break; 
-		case 'project-list': 		return 745; break;
-		case 'project-narrow': 		return 170; break;
-		case 'cobranded-narrow': 	return 170; break;
-		case 'cobranded-short': 	return 170; break;
+		case 'project-list': 		return 745; break; 
+		case 'project-narrow': 		return 170; break; 
+		case 'cobranded-narrow': 	return 170; break; 
+		case 'cobranded-short': 	return 170; break; 
 		case 'cobranded-banner':    return 468; break;
 		case 'cobranded-leader':    return 728; break;
 		default: 					return 202;
 	}	
 }
+
 
 
 $(document).ready(function() 
