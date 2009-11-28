81b89156c3dfcf126f39beeab1e3a9f0f4c9f123
Paul Burt <paul.burt@gmail.com>
2009-11-28 13:17:53 +0100

    Moved DONATION_NOTIFICATION_EMAILS check condition into models.py.

--- akvo/rsr/utils.py
+++ akvo/rsr/utils.py
@@ -97,17 +97,16 @@ def model_and_instance_based_filename(object_name, pk, field_name, img_name):
 
 
 def send_donation_confirmation_emails(invoice_id):
-    if settings.DONATION_EMAIL_NOTIFICATIONS:
-        invoice = get_model('rsr', 'invoice').objects.get(pk=invoice_id)
-        t = loader.get_template('rsr/donation_confirmation_email.html')
-        c = Context({'invoice': invoice, 'domain_name': settings.DOMAIN_NAME})
-        message_body = t.render(c)
-        subject_field, from_field = _(u'Thank you from Akvo.org!'), settings.DEFAULT_FROM_EMAIL
-        bcc_field = invoice.notification_email
-        if invoice.user:
-            to_field = invoice.user.email
-        else:
-            to_field = invoice.email
-        msg = EmailMessage(subject_field, message_body, from_field, [to_field], [bcc_field])
-        msg.content_subtype = "html"
-        msg.send()
+    invoice = get_model('rsr', 'invoice').objects.get(pk=invoice_id)
+    t = loader.get_template('rsr/donation_confirmation_email.html')
+    c = Context({'invoice': invoice, 'domain_name': settings.DOMAIN_NAME})
+    message_body = t.render(c)
+    subject_field, from_field = _(u'Thank you from Akvo.org!'), settings.DEFAULT_FROM_EMAIL
+    bcc_field = invoice.notification_email
+    if invoice.user:
+        to_field = invoice.user.email
+    else:
+        to_field = invoice.email
+    msg = EmailMessage(subject_field, message_body, from_field, [to_field], [bcc_field])
+    msg.content_subtype = "html"
+    msg.send()
