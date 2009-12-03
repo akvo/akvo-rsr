f90ab946f514a96bd6da5a72b1c58859e7b730fc
Paul Burt <paul.burt@gmail.com>
2009-12-03 11:30:19 +0100

    removed DONATION_NOTIFICATION_EMAILS check from models.py

--- akvo/rsr/models.py
+++ akvo/rsr/models.py
@@ -1337,8 +1337,7 @@ post_save.connect(create_organisation_account, sender=Organisation)
 post_save.connect(create_publishing_status, sender=Project)
 post_save.connect(create_payment_gateway_selector, sender=Project)
 
-if settings.DONATION_NOTIFICATION_EMAILS:
-    post_save.connect(donation_completed, sender=Invoice)
+post_save.connect(donation_completed, sender=Invoice)
 
 post_save.connect(change_name_of_file_on_create, sender=Organisation)
 post_save.connect(change_name_of_file_on_create, sender=Project)
