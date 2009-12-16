465ad9bf38698130e7631fe7cd31144dc001c29c
Daniel Karlsson <daniel@fraction4.com>
2009-12-16 14:06:16 +0100

    Changed the base_settings to use a prefix for akvo

--- akvo/settings_base.py
+++ akvo/settings_base.py
@@ -117,13 +117,16 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 #TEMPLATE_DEBUG = DEBUG
+SITE = 'akvo'
+TEMPLATEDIR = 'templates/%s' % SITE
 
 import os.path
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
+    #os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
+    os.path.join(os.path.dirname(__file__), TEMPLATEDIR).replace('\\','/'),
 )
 
 # List of callables that know how to import templates from various sources.
