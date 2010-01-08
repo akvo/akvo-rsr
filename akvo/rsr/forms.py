c24d1fc042d9e6505ba3bc6672e5180bbd9724cf
Daniel Karlsson <daniel@fraction4.com>
2010-01-08 15:38:43 +0100

    Removed WYMEditor code from forms.py

--- akvo/rsr/forms.py
+++ akvo/rsr/forms.py
@@ -197,32 +197,7 @@ class RSR_SetPasswordForm(SetPasswordForm):
 class RSR_PasswordResetForm(PasswordResetForm):
     email = forms.EmailField(label=_("E-mail"), max_length=75, widget=forms.TextInput(attrs={'class': 'input'}))
 #        self.fields['email'].widget.attrs = {'class': 'input'}
-        
-############################### WYMEditor support ############################### 
 
-class WYMEditor(forms.Textarea):
-    class Media:
-        js = (
-            'js/jquery-1.3.2.min.js',
-            'js/wymeditor/jquery.wymeditor.pack.js',
-        )
-
-    def __init__(self, language=None, attrs=None):
-        self.language = language or settings.LANGUAGE_CODE[:2]
-        self.attrs = {'class': 'wymeditor'}
-        if attrs:
-            self.attrs.update(attrs)
-        super(WYMEditor, self).__init__(attrs)
-
-    def render(self, name, value, attrs=None):
-        rendered = super(WYMEditor, self).render(name, value, attrs)
-        return rendered + mark_safe(u'''<script type="text/javascript">
-            jQuery('#id_%s').wymeditor({
-                updateSelector: '.submit-row input[type=submit]',
-                updateEvent: 'click',
-                lang: '%s',
-            });
-            </script>''' % (name, self.language))
 
 class InvoiceForm(forms.ModelForm):
     def __init__(self, user, project, engine, add_extra_fields=True, *args, **kwargs): 
