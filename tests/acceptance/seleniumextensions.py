8399dd9262d7914fdf365c9e81d4869125d2823e
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-01-27 15:56:28 +0000

    [#2027887] Added click_element_at_path(), click_tab(), click_javascript_tab() and verify_element_size_at_path() methods.

--- tests/acceptance/seleniumextensions.py
+++ tests/acceptance/seleniumextensions.py
@@ -54,20 +54,31 @@ class SeleniumTestCase(TestCase):
     def open_project_page(self, project_number):
         self.open_page("/rsr/project/%i/" % (project_number))
 
-    def click_link(self, link_text):
-        self.selenium.click("link=%s" % (link_text))
+    def click_element_at_path(self, element_path):
+        self.selenium.click(element_path)
         self.wait_for_page_to_load()
 
+    def click_link(self, link_text):
+        self.click_element_at_path("link=%s" % (link_text))
+
+    def click_tab(self, tab_element_path):
+        self.click_element_at_path(tab_element_path)
+
+    def click_javascript_tab(self, tab_element_path):
+        self.selenium.click(tab_element_path) # client side, so no page load wait needed
+
     def click_button(self, button_name_or_path):
-        self.selenium.click(button_name_or_path)
-        self.wait_for_page_to_load()
+        self.click_element_at_path(button_name_or_path)
 
     def click_submit_button(self):
         self.click_button("//button[@type='submit']")
 
     def click_submit_button_with_text(self, button_text):
-        self.selenium.click("//input[@value=\"%s\"]" % (button_text))
-        self.wait_for_page_to_load()
+        self.click_element_at_path("//input[@value=\"%s\"]" % (button_text))
+
+    def assert_location_contains(self, expected_text):
+        self.failIf(self.selenium.get_location().find(expected_text) == -1,
+                    "Page URL should contain: %s" % (expected_text))
 
     def assert_title_is(self, expected_title):
         self.failUnlessEqual(expected_title, self.selenium.get_title(),
@@ -101,8 +112,14 @@ class SeleniumTestCase(TestCase):
         actual_attribute_value = self.selenium.get_attribute(attribute_xpath)
         self.failUnlessEqual(expected_attribute_value, actual_attribute_value,
             "\nExpected attribute value at %s: %s\nActual attribute value: %s" %
-            (attribute_xpath, expected_attribute_value, actual_attribute_value))
+                (attribute_xpath, expected_attribute_value, actual_attribute_value))
 
-    def assert_location_contains(self, expected_text):
-        self.failIf(self.selenium.get_location().find(expected_text) == -1,
-                    "Page URL should contain: %s" % (expected_text))
+    def verify_element_size_at_path(self, expected_element_width, expected_element_height, element_path):
+        actual_element_width = int(self.selenium.get_element_width(element_path))
+        actual_element_height = int(self.selenium.get_element_height(element_path))
+        self.failUnlessEqual(expected_element_width, actual_element_width,
+            "\nExpected element width at %s: %i\nActual element width: %i" %
+                (element_path, expected_element_width, actual_element_width))
+        self.failUnlessEqual(expected_element_height, actual_element_height,
+            "\nExpected element height at %s: %i\nActual element height: %i" %
+                (element_path, expected_element_height, actual_element_height))
