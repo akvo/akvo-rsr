6c12e5cf30bf72d58021d5c17c561d7122aa469d
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-02-23 19:44:13 +0000

    [#2497123] Removed unused import statement.

--- tests/acceptance/helpers/rsruseradmin.py
+++ tests/acceptance/helpers/rsruseradmin.py
@@ -2,8 +2,6 @@
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
-from test_settings import *
-
 class RSRUser:
 
     def __init__(self, selenium_client):
