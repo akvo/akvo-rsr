c5e67b24cafcce64d754c357d10ccd25399b6d27
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-05-15 00:23:17 +0100

    [#3345188] Added xml_api_test_suite and all_test_suites modules. Added nosetestloaders module for common test loading and running functions.

--- tests/acceptance/api/xml/all_projects_test.py
+++ tests/acceptance/api/xml/all_projects_test.py
@@ -4,10 +4,9 @@
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
-import nose
-
 from extensions.xmlextensions import *
 from extensions.xmltestcase import XMLTestCase
+from helpers.nosetestloaders import *
 from helpers.rsrapi import *
 
 from expectedelements import *
@@ -48,7 +47,7 @@ class AllProjectsTest(XMLTestCase):
 
 
 def suite():
-    return nose.loader.TestLoader().loadTestsFromTestCase(AllProjectsTest)
+    return load_tests_from(AllProjectsTest)
 
 if __name__ == "__main__":
-    nose.core.TextTestRunner(verbosity=2).run(suite())
+    run_test_suite(suite())
--- tests/acceptance/api/xml/single_project_update_test.py
+++ tests/acceptance/api/xml/single_project_update_test.py
@@ -4,10 +4,11 @@
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
-import nose, urllib
+import urllib
 
 from extensions.xmlextensions import *
 from extensions.xmltestcase import XMLTestCase
+from helpers.nosetestloaders import *
 from helpers.rsrapi import *
 
 from expectedelements import *
@@ -70,7 +71,7 @@ class SingleProjectUpdateTest(XMLTestCase):
 
 
 def suite():
-    return nose.loader.TestLoader().loadTestsFromTestCase(SingleProjectUpdateTest)
+    return load_tests_from(SingleProjectUpdateTest)
 
 if __name__ == "__main__":
-    nose.core.TextTestRunner(verbosity=2).run(suite())
+    run_test_suite(suite())
