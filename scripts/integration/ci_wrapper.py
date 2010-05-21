b60ac7c66d34b1cebfa968c6814bcf62b63ddfca
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-05-21 15:54:01 +0100

    [#3345224] The ci_wrapper script now configures the test environment before running acceptance tests.

--- scripts/integration/ci_wrapper.py
+++ scripts/integration/ci_wrapper.py
@@ -6,15 +6,17 @@
 
 import os, sys
 
+from ci_cleanup import *
+from ci_environment import *
+
+
 if len(sys.argv) <= 1:
     print 'Usage: ci_wrapper <virtualenv_path>'
     sys.exit(1)
 
 os.chdir(os.path.realpath(os.path.dirname(__file__)))
-
 os.system("bash ../testing/run_django_tests %s ci_mode" % (sys.argv[1]))
-os.system("bash ../testing/run_acceptance_tests")
-
-from ci_cleanup import *
-
 remove_project_links_to_prevent_subsequent_build_failure()
+
+setup_acceptance_test_environment()
+os.system("bash ../testing/run_acceptance_tests")
