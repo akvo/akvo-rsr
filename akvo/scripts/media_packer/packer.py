e65a88cf89c3f69a947960ef1888af4918045c00
Daniel Karlsson <daniel@fraction4.com>
2010-05-25 11:32:29 +0200

    Updated packer script to test packer script.

--- akvo/scripts/media_packer/packer.py
+++ akvo/scripts/media_packer/packer.py
@@ -13,7 +13,7 @@ import sys, os, hashlib
 import cleaner, generator
 
 def main():
-    print 'Running packer, with a change :-)'
+    print 'Running packer...'
     
     # 1. Clean up old files (bundle and map in the cleaner script)
     # if (cleaner.main()):
