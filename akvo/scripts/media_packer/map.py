f99532cc01545559073b4d3bebb0f376559f5165
Daniel Karlsson <daniel@fraction4.com>
2010-05-27 20:08:56 +0200

    Added simple css3 animation to the main_nav

--- akvo/scripts/media_packer/map.py
+++ akvo/scripts/media_packer/map.py
@@ -1,1 +1,1 @@
-BUNDLE_MAP = {'akvo_js': {'path': 'akvo/js/', 'hash': 'f5480e21e2bc215e254f3522773f414f31ab3492', 'type': 'js'}, 'akvo_style': {'path': 'akvo/css/', 'hash': '06e880c7e5bacab6b3d0493678c5d7507b02ba69', 'type': 'css'}}
+BUNDLE_MAP = {'akvo_js': {'path': 'akvo/js/', 'hash': 'f5480e21e2bc215e254f3522773f414f31ab3492', 'type': 'js'}, 'akvo_style': {'path': 'akvo/css/', 'hash': '3cd1ca791c640e3700a8c5aba31d8ce18f38faac', 'type': 'css'}}
