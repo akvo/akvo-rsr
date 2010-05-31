b85fd60a2fc809b6b96ddea6257a572942c7bbf3
Daniel Karlsson <daniel@fraction4.com>
2010-05-31 18:56:41 +0200

    First cut on simple search

--- akvo/rsr/views.py
+++ akvo/rsr/views.py
@@ -254,6 +254,43 @@ def walking_for_water(request):
         'walking_for_water': wfw,
         'RSR_CACHE_SECONDS': get_setting('RSR_CACHE_SECONDS', default=300),
     }
+
+# http://www.julienphalip.com/blog/2008/08/16/adding-search-django-site-snap/
+import re
+from django.db.models import Q
+def normalize_query(query_string,
+                    findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+                    normspace=re.compile(r'\s{2,}').sub):
+    ''' Splits the query string in invidual keywords, getting rid of unecessary spaces
+        and grouping quoted words together.
+        Example:
+        
+        >>> normalize_query('  some random  words "with   quotes  " and   spaces')
+        ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
+    
+    '''
+    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)] 
+
+def get_query(query_string, search_fields):
+    ''' Returns a query, that is a combination of Q objects. That combination
+        aims to search keywords within a model by testing the given search fields.
+    
+    '''
+    query = None # Query to search for every search term        
+    terms = normalize_query(query_string)
+    for term in terms:
+        or_query = None # Query to search for a given term in each field
+        for field_name in search_fields:
+            q = Q(**{"%s__icontains" % field_name: term})
+            if or_query is None:
+                or_query = q
+            else:
+                or_query = or_query | q
+        if query is None:
+            query = or_query
+        else:
+            query = query & or_query
+    return query
     
 @render_to('rsr/project_directory.html')
 def projectlist(request):
@@ -267,10 +304,13 @@ def projectlist(request):
     projs = Project.objects.published().funding().select_related()
     fundable = projs.need_funding()
     showcases = get_random_from_qs(fundable, 3)
-
-    #qs_list = list(projs.need_funding().values_list('pk', flat=True))
-    #random.shuffle(qs_list)
-    #showcases = projs.filter(pk__in=qs_list[:3])
+    
+    query_string = ''
+    found_entries = None
+    if ('q' in request.GET) and request.GET['q'].strip():
+        query_string = request.GET['q']
+        project_query = get_query(query_string, ['name', 'subtitle','country__country_name','city','state','goals_overview','current_status_detail','project_plan_detail','sustainability','context','notes',])
+        projs = Project.objects.filter(project_query)
     
     page = project_list_data(request, projs)
     return {
@@ -280,6 +320,7 @@ def projectlist(request):
         'showcases': showcases,
         'RSR_CACHE_SECONDS': get_setting('RSR_CACHE_SECONDS', default=300),
         'site_section': 'projects',
+        'query_string': query_string,
     }
 
 @render_to('rsr/project_directory.html')
