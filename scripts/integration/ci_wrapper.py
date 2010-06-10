9e54410b174464cfceb9c7f72ac5bb848f1c1872
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-06-10 21:14:15 +0100

    [#3345224] Refactored ci_wrapper script by moving test running steps into separate functions and added a main method.

--- scripts/integration/ci_wrapper.py
+++ scripts/integration/ci_wrapper.py
@@ -10,13 +10,22 @@ from ci_cleanup import *
 from ci_environment import *
 
 
-if len(sys.argv) <= 1:
-    print 'Usage: ci_wrapper <virtualenv_path>'
-    sys.exit(1)
+def verify_script_parameters():
+    if len(sys.argv) <= 1:
+        print 'Usage: ci_wrapper <virtualenv_path>'
+        sys.exit(1)
 
-os.chdir(os.path.realpath(os.path.dirname(__file__)))
-os.system("bash ../testing/run_django_tests %s ci_mode" % (sys.argv[1]))
-remove_project_links_to_prevent_subsequent_build_failure()
+def run_django_tests():
+    os.chdir(os.path.realpath(os.path.dirname(__file__)))
+    os.system("bash ../testing/run_django_tests %s ci_mode" % (sys.argv[1]))
+    remove_project_links_to_prevent_subsequent_build_failure()
 
-setup_acceptance_test_environment()
-os.system("bash ../testing/run_acceptance_tests")
+def run_acceptance_tests():
+    setup_acceptance_test_environment()
+    os.system("bash ../testing/run_acceptance_tests")
+
+
+if __name__ == "__main__":
+    verify_script_parameters()
+    run_django_tests()
+    run_acceptance_tests()
