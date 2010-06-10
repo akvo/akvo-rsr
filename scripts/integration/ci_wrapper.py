c464434c68c697cde3cd5e401870b5cd6c8b4767
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-06-10 22:56:19 +0100

    [#3345224] The ci_wrapper script now uses the newer subprocess module instead of os.system.

--- scripts/integration/ci_wrapper.py
+++ scripts/integration/ci_wrapper.py
@@ -4,10 +4,11 @@
 # See more details in the license.txt file located at the root folder of the Akvo RSR module. 
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
-import os, sys
+import os, subprocess, sys
 
 from ci_cleanup import *
 from ci_environment import *
+from commandline import *
 
 
 def verify_script_parameters():
@@ -16,12 +17,12 @@ def verify_script_parameters():
         sys.exit(1)
 
 def run_django_tests():
-    os.system("bash ../testing/run_django_tests %s ci_mode" % (sys.argv[1]))
+    shell_call("bash ../testing/run_django_tests %s ci_mode" % (sys.argv[1]))
     remove_project_links_to_prevent_subsequent_build_failure()
 
 def run_acceptance_tests():
     setup_acceptance_test_environment()
-    os.system("bash ../testing/run_acceptance_tests")
+    shell_call("bash ../testing/run_acceptance_tests")
 
 
 if __name__ == "__main__":
