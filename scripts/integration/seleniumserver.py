6d62af0d02a4ed40f872fcf271dffa0457581a95
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-06-14 15:04:32 +0100

    [#3345224] SeleniumRCServer.stop() method now first tries to stop the server via the Selenium API and then falls back to using an interrupt signal if necessary. Moved java version logging into a log_java_version() method.

--- scripts/integration/seleniumserver.py
+++ scripts/integration/seleniumserver.py
@@ -4,6 +4,8 @@
 
 import os, signal, subprocess, time
 
+from selenium import selenium
+
 from ci_paths import *
 
 
@@ -14,12 +16,14 @@ class SeleniumRCServer:
 
     def __init__(self):
         self.log_file_path = self.server_log_file_path()
+        self.log_java_version()
+
+    def log_java_version(self):
         log_file = open(self.log_file_path, 'w')
 
         java_version_call = subprocess.Popen(["java", "-version"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
         log_file.writelines(java_version_call.stdout.readlines())
-        log_file.write("\n")
         log_file.close()
 
         java_version_call.wait()
@@ -41,9 +45,29 @@ class SeleniumRCServer:
             os.mkdir(logging_path, 0775)
 
     def start(self):
+        # The exec call with the shell=True parameter is a hack so that we can use the OS's output
+        # redirection process (which runs in its own thread) rather than having to write the necessary
+        # threading and redirection plumbing ourselves.  This is because the RC server keeps running
+        # until being shut down so we can't use any of the subprocess.Popen methods that wait for the
+        # server process to terminate otherwise this method will lock.
+
+        print "Starting Selenium RC server..."
         self.server_process = subprocess.Popen("exec java -jar %s >> %s" % (SELENIUM_SERVER_JAR_PATH, self.log_file_path),
                                                shell=True)
+        print "Server started successfully\n"
 
     def stop(self):
-        os.kill(self.server_process.pid, signal.SIGINT) # send interrupt signal
+        selenium_server_host = "localhost"
+        selenium_server_port = 4444
+        browser_environment = "*firefox"
+
+        try:
+            print "\nStopping Selenium RC server..."
+            selenium(selenium_server_host, selenium_server_port, browser_environment, '').shut_down_selenium_server()
+        except Exception, exception:
+            print "Unable to stop Selenium RC server with shutdown command: %s" % (exception)
+            print "\nStopping server with interrupt signal instead"
+            os.kill(self.server_process.pid, signal.SIGINT) # send interrupt signal
+
         self.server_process.wait()
+        print "Server stopped successfully\n"
