f058cf145ef495dbeb7365db40debc13f35ff1d7
Paul Burt <paul.burt@gmail.com>
2010-06-15 11:29:49 +0100

    Modified paypal_thanks view function

--- akvo/rsr/views.py
+++ akvo/rsr/views.py
@@ -26,7 +26,6 @@ from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import Context, RequestContext, loader
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _, get_language
-from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.http import require_GET, require_POST
 
 from BeautifulSoup import BeautifulSoup
@@ -1133,13 +1132,15 @@ def mollie_report(request):
     return HttpResponseServerError
 
 @require_POST
-@csrf_protect
-@render_to('rsr/donate_thanks.html')
 def paypal_thanks(request):
     invoice_id = request.POST.get('invoice', None)
     if invoice_id:
         invoice = Invoice.objects.get(pk=invoice_id)
-        return {'invoice': invoice, 'p': invoice.project, 'user': invoice.user}
+        return render_to_response('rsr/donate_thanks.html',
+                                  {'invoice': invoice,
+                                   'p': invoice.project,
+                                   'user': invoice.user},
+                                  context_instance=RequestContext(request))
     return redirect('/')
 
 @require_GET
