ed60847b7b50a2d4f605dbcd783ee0ebe0d7f0e8
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-06-24 23:16:17 +0100

    [#3345224, #4031106] Switched to bash shell for xvfb init.d script instead of sh. Output from the start/stop process is now redirected to an output file in the Xvfb logging directory.

--- setup/integration/scripts/etc_init.d_xvfb.sh
+++ setup/integration/scripts/etc_init.d_xvfb.sh
@@ -1,23 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Xvfb auto-start script
 # 
 # description: Starts Xvfb automatically on server startup
 # processname: xvfb
 
-export DISPLAY=:108
-
+XVFB_DISPLAY=:108
 XVFB_SCRIPTS_PATH=/usr/local/ci/scripts
 XVFB_LOG_PATH=/var/tmp/log/xvfb
 
+UTC_LOG_TIMESTAMP=`date -u +%Y%m%d_%H%M%S`
+XVFB_PROCESS_OUTPUT_FILE=$XVFB_LOG_PATH/xvfb_process_$UTC_LOG_TIMESTAMP.out
+
 function start_xvfb
 {
-    su -p -s /bin/sh tomcat $XVFB_SCRIPTS_PATH/start_xvfb $XVFB_LOG_PATH $DISPLAY
+    su -p -s /bin/bash tomcat $XVFB_SCRIPTS_PATH/start_xvfb $XVFB_LOG_PATH $XVFB_DISPLAY > $XVFB_PROCESS_OUTPUT_FILE 2>&1
 }
 
 function stop_xvfb
 {
-    su -p -s /bin/sh tomcat $XVFB_SCRIPTS_PATH/stop_xvfb.py $XVFB_LOG_PATH
+    su -p -s /bin/bash tomcat $XVFB_SCRIPTS_PATH/stop_xvfb.py $XVFB_LOG_PATH > $XVFB_PROCESS_OUTPUT_FILE 2>&1
 }
 
 case $1 in
