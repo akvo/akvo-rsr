398caa6188647f54ac80a482b46400bf48e71194
Daniel Karlsson <daniel@fraction4.com>
2010-06-29 15:37:59 +0200

    Added 400 & 500 errorr pages. Added first cut on step 1 & 2 in account registration.

--- akvo/urls.py
+++ akvo/urls.py
@@ -23,9 +23,8 @@ feeds = {
 
 urlpatterns = patterns('',
     #(r'^rsr/', include('akvo.rsr.urls')),
-
-    url(r'^rsr/test_500/$', direct_to_template, {'template': 'rsr/error_500.html'}, name='error_500'),
     
+    url(r'^rsr/test_500/$', direct_to_template, {'template': '500.html'}, name='test_500'),
     # django_counter
     (r'^rsr/counter/', include('django_counter.urls')),
 
