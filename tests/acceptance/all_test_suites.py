914e2f81cebe5b72e4971c2ca53e1e8ea52350d5
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-07-20 15:35:19 +0100

    [#4292706] The helpers.testexecution.run_test_suite() function now returns the test results. The all_test_suites script will now run the any unit tests for the acceptance test helper modules and then run the acceptance test suites if all the helper unit tests pass.  This is a self-diagnostic step to ensure the integrity of any acceptance tests that depend on these helper modules.

--- tests/acceptance/all_test_suites.py
+++ tests/acceptance/all_test_suites.py
@@ -4,22 +4,29 @@
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
-import os
+import os, sys
 
 from helpers.seleniumclient import SeleniumClient
 from helpers.testexecution import *
 
 from test_settings import SITE_UNDER_TEST
 
+from internal.internal_unittests_suite import unittests_suite
 from api.xml.xml_api_test_suite import xml_api_suite
 from web.web_test_suite import web_suite
 
-def all_suites():
+def acceptance_test_suites():
     return create_test_suite_from_suites([xml_api_suite(), web_suite()])
 
 if __name__ == "__main__":
     acceptance_test_root_path = os.path.realpath(os.path.dirname(__file__))
-    print "Acceptance test suite root path: %s" % (acceptance_test_root_path)
-    print "Running tests for: %s\n" % (SITE_UNDER_TEST)
-    run_test_suite(all_suites())
-    SeleniumClient().stop()
+    print "Test suite root path: %s\n" % (acceptance_test_root_path)
+
+    print "Running unit tests for acceptance test helpers\n"
+    if run_test_suite(unittests_suite()).wasSuccessful():
+        print "\nRunning acceptance tests for: %s\n" % (SITE_UNDER_TEST)
+        run_test_suite(acceptance_test_suites())
+        SeleniumClient().stop()
+    else:
+        print "\nUnable to run acceptance tests until the unit test errors above have been resolved\n"
+        sys.exit(1)
