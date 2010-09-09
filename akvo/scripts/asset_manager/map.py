ef3cbd255c83ba08263ce0c4fff25655a01c6973
Daniel Karlsson <daniel@fraction4.com>
2010-09-09 19:58:37 +0800

    First start on the mainnav

--- akvo/scripts/asset_manager/map.py
+++ akvo/scripts/asset_manager/map.py
@@ -1,1 +1,1 @@
-BUNDLE_MAP = {'pvw_ie6_style': {'path': 'pvw-take2/css/', 'hash': '7590132c8e598b0159ae6ceb34fcf30bcb3f6d8b', 'type': 'css'}, 'pvw_style': {'path': 'pvw-take2/css/', 'hash': 'a25f5d46f5e2d52de31a5b745fecd150d7a40e36', 'type': 'css'}}
+BUNDLE_MAP = {'pvw_ie6_style': {'path': 'pvw-take2/css/', 'hash': '7590132c8e598b0159ae6ceb34fcf30bcb3f6d8b', 'type': 'css'}, 'pvw_style': {'path': 'pvw-take2/css/', 'hash': 'a6c34ae24db1cccff6ade4bca867110beb43afbc', 'type': 'css'}}
