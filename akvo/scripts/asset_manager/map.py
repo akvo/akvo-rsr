a766a8720569f3219250872029f9ead926e48e9f
Gabriel von Heijne <gabriel@akvo.org>
2010-09-10 14:05:51 +0200

    project_main.html: Project status and Page tools boxes

--- akvo/scripts/asset_manager/map.py
+++ akvo/scripts/asset_manager/map.py
@@ -1,1 +1,1 @@
-BUNDLE_MAP = {'pvw_ie6_style': {'path': 'pvw-take2/css/', 'hash': '7590132c8e598b0159ae6ceb34fcf30bcb3f6d8b', 'type': 'css'}, 'pvw_style': {'path': 'pvw-take2/css/', 'hash': 'a6c34ae24db1cccff6ade4bca867110beb43afbc', 'type': 'css'}}
+BUNDLE_MAP = {'pvw_ie6_style': {'path': 'pvw-take2/css/', 'hash': '7590132c8e598b0159ae6ceb34fcf30bcb3f6d8b', 'type': 'css'}, 'pvw_style': {'path': 'pvw-take2/css/', 'hash': 'eb534bd849cedb0323d510a8a6fc2116562db879', 'type': 'css'}}
