32a2ed6b90c74630b2f60300726df8cd232d531f
Daniel Karlsson <daniel@fraction4.com>
2010-09-10 16:04:18 +0800

    Updated the mainnav header to use smaller background.

--- akvo/scripts/asset_manager/map.py
+++ akvo/scripts/asset_manager/map.py
@@ -1,1 +1,1 @@
-BUNDLE_MAP = {'pvw_ie6_style': {'path': 'pvw-take2/css/', 'hash': '7590132c8e598b0159ae6ceb34fcf30bcb3f6d8b', 'type': 'css'}, 'pvw_style': {'path': 'pvw-take2/css/', 'hash': 'c5dda75ce0990b4b85ab8e2464596add6277fd6d', 'type': 'css'}}
+BUNDLE_MAP = {'pvw_ie6_style': {'path': 'pvw-take2/css/', 'hash': '7590132c8e598b0159ae6ceb34fcf30bcb3f6d8b', 'type': 'css'}, 'pvw_style': {'path': 'pvw-take2/css/', 'hash': '40d7bbcaec359264e5cc60b29be9ac202d42b366', 'type': 'css'}}
