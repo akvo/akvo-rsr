7acbd7c08b1808ff6e2ec68b5a9661cb41c428e8
Daniel Karlsson <daniel@fraction4.com>
2010-09-10 15:33:37 +0800

    Added new background to the mainnav

--- akvo/scripts/asset_manager/map.py
+++ akvo/scripts/asset_manager/map.py
@@ -1,1 +1,1 @@
-BUNDLE_MAP = {'pvw_ie6_style': {'path': 'pvw-take2/css/', 'hash': '7590132c8e598b0159ae6ceb34fcf30bcb3f6d8b', 'type': 'css'}, 'pvw_style': {'path': 'pvw-take2/css/', 'hash': 'a6c34ae24db1cccff6ade4bca867110beb43afbc', 'type': 'css'}}
+BUNDLE_MAP = {'pvw_ie6_style': {'path': 'pvw-take2/css/', 'hash': '7590132c8e598b0159ae6ceb34fcf30bcb3f6d8b', 'type': 'css'}, 'pvw_style': {'path': 'pvw-take2/css/', 'hash': '5bc13e6b1f3036eeb012de8310a6e285578ffa18', 'type': 'css'}}
