97869aec2f818e478057a2e6d7b1944bd0202146
Daniel Karlsson <daniel@fraction4.com>
2010-09-11 15:33:58 +0800

    Added the Welcome text located under mainnav.

--- akvo/scripts/asset_manager/map.py
+++ akvo/scripts/asset_manager/map.py
@@ -1,1 +1,1 @@
-BUNDLE_MAP = {'pvw_ie6_style': {'path': 'pvw-take2/css/', 'hash': '7590132c8e598b0159ae6ceb34fcf30bcb3f6d8b', 'type': 'css'}, 'pvw_style': {'path': 'pvw-take2/css/', 'hash': '40d7bbcaec359264e5cc60b29be9ac202d42b366', 'type': 'css'}}
+BUNDLE_MAP = {'pvw_ie6_style': {'path': 'pvw-take2/css/', 'hash': '7590132c8e598b0159ae6ceb34fcf30bcb3f6d8b', 'type': 'css'}, 'pvw_style': {'path': 'pvw-take2/css/', 'hash': '096d70dadb7552f598ffe3b367fce1151260da93', 'type': 'css'}}
