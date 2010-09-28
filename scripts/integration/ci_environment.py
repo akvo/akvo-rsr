87028afa760dc2e40e19c0afdce544f61180484a
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-09-29 00:58:33 +0100

    [#5399615] Corrected root path for PvW acceptance tests.

--- scripts/integration/ci_environment.py
+++ scripts/integration/ci_environment.py
@@ -7,7 +7,7 @@
 import os, shutil
 
 def configure_acceptance_test_settings_for_ci():
-    acceptance_test_root_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '../../tests/akvo/acceptance'))
+    acceptance_test_root_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '../../tests/pvw/acceptance'))
     
     shutil.copy(os.path.join(acceptance_test_root_path, 'test_settings_ci.py'),
                 os.path.join(acceptance_test_root_path, 'test_settings.py'))
