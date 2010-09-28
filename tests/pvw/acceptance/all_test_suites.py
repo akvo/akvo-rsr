ef007b5228526887131deed425677f2361d2631b
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-09-29 00:04:03 +0100

    [#5400121, #5399615] Added navigation_test_suite and web_test_suite and updated all_test_suites. Reworked all_test_suites to use new testexecution helper and TeamCity-friendly output.

--- tests/pvw/acceptance/all_test_suites.py
+++ tests/pvw/acceptance/all_test_suites.py
@@ -4,16 +4,21 @@
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
-import nose
+import os, sys
 
-from seleniumclient import SeleniumClient
+from helpers.seleniumclient import SeleniumClient
+from helpers.testexecution import *
 
-from users.user_admin_test_suite import user_admin_suite
+from test_settings import SITE_UNDER_TEST
 
-def all_suites():
-    print "All test suites:"
-    return nose.suite.LazySuite([user_admin_suite()])
+from web.web_test_suite import web_suite
+
+def acceptance_test_suites():
+    return create_test_suite_from_suites([web_suite()])
 
 if __name__ == "__main__":
-    nose.core.TextTestRunner(verbosity=2).run(all_suites())
+    acceptance_test_root_path = os.path.realpath(os.path.dirname(__file__))
+    print "Test suite root path: %s\n" % (acceptance_test_root_path)
+    print "Running acceptance tests for: %s\n" % (SITE_UNDER_TEST)
+    run_test_suite(acceptance_test_suites())
     SeleniumClient().stop()
