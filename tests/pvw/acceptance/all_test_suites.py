c947e6c658b3444ef4df063ac537072199a18527
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-10-01 16:30:59 +0100

    [#5400121] Updated all_test_suites script to run internal unit tests (to validate assumptions made in acceptance tests) before running the acceptance tests themselves.

--- tests/pvw/acceptance/all_test_suites.py
+++ tests/pvw/acceptance/all_test_suites.py
@@ -11,7 +11,9 @@ from helpers.testexecution import *
 
 from test_settings import SITE_UNDER_TEST
 
+from internal.internal_unittests_suite import unittests_suite
 from web.web_test_suite import web_suite
+
 
 def acceptance_test_suites():
     return create_test_suite_from_suites([web_suite()])
@@ -19,6 +21,12 @@ def acceptance_test_suites():
 if __name__ == "__main__":
     acceptance_test_root_path = os.path.realpath(os.path.dirname(__file__))
     print "Test suite root path: %s\n" % (acceptance_test_root_path)
-    print "Running acceptance tests for: %s\n" % (SITE_UNDER_TEST)
-    run_test_suite(acceptance_test_suites())
-    SeleniumClient().stop()
+
+    print "Running unit tests for acceptance test helpers\n"
+    if run_test_suite(unittests_suite()).wasSuccessful():
+        print "\nRunning acceptance tests for: %s\n" % (SITE_UNDER_TEST)
+        run_test_suite(acceptance_test_suites())
+        SeleniumClient().stop()
+    else:
+        print "\nUnable to run acceptance tests until the unit test errors above have been resolved\n"
+        sys.exit(1)
