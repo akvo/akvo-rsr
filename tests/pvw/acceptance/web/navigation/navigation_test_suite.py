23b0e4944531d47a326fd2256e1f0b031b91cba6
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-10-03 14:03:01 +0100

    [#5400121] Removed unused SeleniumClient call since the menu navigation tests don't rely on Selenium.

--- tests/pvw/acceptance/web/navigation/navigation_test_suite.py
+++ tests/pvw/acceptance/web/navigation/navigation_test_suite.py
@@ -4,14 +4,13 @@
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
-from helpers.seleniumclient import SeleniumClient
 from helpers.testexecution import *
 
 from web.navigation.menu_navigation_test import MenuNavigationTest
+
 
 def navigation_suite():
     return create_test_suite_from_classes([MenuNavigationTest])
 
 if __name__ == "__main__":
     run_test_suite(navigation_suite())
-    SeleniumClient().stop()
