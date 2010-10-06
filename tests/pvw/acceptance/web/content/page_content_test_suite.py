f38467a6221a9498ce036d423e59915224e5fa14
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-10-06 16:32:35 +0100

    [#5524542] Added education_page_content_test.

--- tests/pvw/acceptance/web/content/page_content_test_suite.py
+++ tests/pvw/acceptance/web/content/page_content_test_suite.py
@@ -11,11 +11,12 @@ from web.content.home_page_content_test import HomePageContentTest
 from web.content.focus_areas_page_content_test import FocusAreasPageContentTest
 from web.content.projects_page_content_test import ProjectsPageContentTest
 from web.content.netherlands_page_content_test import NetherlandsPageContentTest
+from web.content.education_page_content_test import EducationPageContentTest
 
 
 def page_content_suite():
     return create_test_suite_from_classes([HomePageContentTest, FocusAreasPageContentTest, ProjectsPageContentTest,
-                                           NetherlandsPageContentTest])
+                                           NetherlandsPageContentTest, EducationPageContentTest])
 
 if __name__ == "__main__":
     run_test_suite(page_content_suite())
