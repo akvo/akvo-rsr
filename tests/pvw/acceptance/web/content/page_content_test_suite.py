f56eabb9aaaf279fa73e5146d09e97c11c4a7154
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-10-06 09:09:57 +0100

    [#5524542] Added focus_areas_page_content_test.

--- tests/pvw/acceptance/web/content/page_content_test_suite.py
+++ tests/pvw/acceptance/web/content/page_content_test_suite.py
@@ -8,10 +8,11 @@ from helpers.seleniumclient import SeleniumClient
 from helpers.testexecution import *
 
 from web.content.home_page_content_test import HomePageContentTest
+from web.content.focus_areas_page_content_test import FocusAreasPageContentTest
 
 
 def page_content_suite():
-    return create_test_suite_from_classes([HomePageContentTest])
+    return create_test_suite_from_classes([HomePageContentTest, FocusAreasPageContentTest])
 
 if __name__ == "__main__":
     run_test_suite(page_content_suite())
