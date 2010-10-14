4a6e9e62d4ec5c3d96c1e57444df22c265d792b1
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-10-15 00:48:47 +0100

    [#5374644] Simplified the acceptance test directory parameterisation for the run_acceptance_tests script.  Now only the test project name needs to be specified as a parameter instead of the full test directory path. Removed the run_akvo/run_pvw/run_FORK convenience scripts as these are no longer necessary.

--- scripts/integration/ci_wrapper.py
+++ scripts/integration/ci_wrapper.py
@@ -11,6 +11,21 @@ from ci_environment import *
 from ci_settings import *
 
 
+def display_usage_and_exit():
+    print "Usage: ci_wrapper [acceptance_test_project_name]"
+    print "Optionally specify an acceptance test project name, e.g. akvo"
+    sys.exit(1)
+
+
+TEST_PROJECT_NAME = 'akvo'
+
+if len(sys.argv) > 2:
+    print ">> Unexpected number of parameters: %s" % (sys.argv[1:])
+    display_usage_and_exit()
+elif len(sys.argv) == 2:
+    TEST_PROJECT_NAME = sys.argv[1]
+
+
 def run_django_tests():
     print "Using virtualenv path: %s" % VIRTUAL_ENV_PATH
     subprocess.call(["./run_django_tests", VIRTUAL_ENV_PATH, "ci_mode"])
@@ -18,7 +33,8 @@ def run_django_tests():
 
 def run_acceptance_tests():
     configure_acceptance_test_settings_for_ci()
-    subprocess.call(["./run_akvo_acceptance_tests", SELENIUM_SERVER_LOG_PATH, XVFB_LOG_PATH])
+    print "Running acceptance tests for project: %s" % TEST_PROJECT_NAME
+    subprocess.call(["./run_acceptance_tests", TEST_PROJECT_NAME, SELENIUM_SERVER_LOG_PATH, XVFB_LOG_PATH])
 
 
 if __name__ == "__main__":
