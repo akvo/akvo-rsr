30b9c9c0f3d219bd97ce23fe4b550e4824fc306d
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-10-20 10:17:06 +0100

    [#5762847] Added further internal unit tests for expected footer link paths.

--- tests/akvo/acceptance/helpers/akvopaths.py
+++ tests/akvo/acceptance/helpers/akvopaths.py
@@ -32,6 +32,81 @@ def register_page():
 def sign_in_page():
     return rsr_path("signin/?next=%s" % all_projects_page())
 
+def terms_of_use_page():
+    return cms_path("terms_of_use")
+
+def donation_policy_page():
+    return cms_path("donation_policy")
+
+def partners_policy_page():
+    return cms_path("partners_and_projects_policy")
+
+def user_contribution_policy_page():
+    return cms_path("user_contribution_policy")
+
+def open_license_page():
+    return cms_path("open_license")
+
+def privacy_policy_page():
+    return cms_path("privacy_policy")
+
+def api_code_of_conduct_page():
+    return cms_path("akvo-rsr-api-code-of-conduct")
+
+def what_we_do_page():
+    return cms_path("what_we_do")
+
+def how_akvo_works_page():
+    return cms_path("how_akvo_works")
+
+def why_we_do_it_page():
+    return cms_path("why_we_do_it")
+
+def faq_page():
+    return cms_path("faq")
+
+def global_maps_page():
+    return cms_path("map_partners_projects")
+
+def akvo_team_page():
+    return cms_path("team")
+
+def akvo_jobs_page():
+    return cms_path("jobs")
+
+def press_page():
+    return cms_path("press")
+
+def annual_reports_page():
+    return cms_path("annual_reports")
+
+def organisations_page():
+    return rsr_path("organisations")
+
+def commercial_partners_page():
+    return cms_path("commercial_partners")
+
+def knowledge_institute_partners_page():
+    return cms_path("knowledge_institute_partners")
+
+def akvo_investors_page():
+    return cms_path("akvo_investors")
+
+def akvo_labs_page():
+    return "/labs"
+
+def contact_us_page():
+    return cms_path("contact_us")
+
+def rsr_admin_page():
+    return rsr_path("admin")
+
+def blog_admin_page():
+    return "/blog/wp-login.php"
+
+def cms_admin_page():
+    return cms_path("?q=user")
+
 def rsr_path(subpath):
     return "/rsr/%s" % subpath
 
