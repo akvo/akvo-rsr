b4aafd42de97f480f5a867f037c47d5ddf696f04
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2010-10-20 22:36:27 +0100

    [#5762847] Added AkvoPathsTest, AkvoURLsTest and ExternalURLsTest to the helpers unit test suite.

--- tests/akvo/acceptance/helpers/unittests/helpers_test_suite.py
+++ tests/akvo/acceptance/helpers/unittests/helpers_test_suite.py
@@ -6,11 +6,14 @@
 
 from helpers.testexecution import *
 
+from helpers.unittests.akvopaths_test import AkvoPathsTest
+from helpers.unittests.akvourls_test import AkvoURLsTest
+from helpers.unittests.externalurls_test import ExternalURLsTest
 from helpers.unittests.rsrpath_test import RSRPathTest
 
 
 def helpers_suite():
-    return create_test_suite_from_classes([RSRPathTest])
+    return create_test_suite_from_classes([AkvoPathsTest, AkvoURLsTest, ExternalURLsTest, RSRPathTest])
 
 if __name__ == "__main__":
     run_test_suite(helpers_suite())
