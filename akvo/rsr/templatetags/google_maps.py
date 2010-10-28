ae66c22e0b5383edf1c4fca4f4223f49689b832c
Paul Burt <paul.burt@gmail.com>
2010-10-28 11:15:39 +0100

    Modified google_map inclusion tag to check for type with isinstance.

--- akvo/rsr/templatetags/google_maps.py
+++ akvo/rsr/templatetags/google_maps.py
@@ -13,10 +13,11 @@ from akvo.rsr.models import Project, Organisation
 
 @register.inclusion_tag('inclusion_tags/google_map.html')
 def google_map(object, width, height, zoom, marker_icon=None):
-    object_type = object.__class__.__name__.lower()
-    if object_type == 'project':
+    is_project = isinstance(object, Project)
+    is_organisation = isinstance(object, Organisation)
+    if is_project:
         marker_icon = getattr(settings, 'GOOGLE_MAPS_PROJECT_MARKER_ICON', '')
-    elif object_type == 'organisation':
+    elif is_organisation:
         marker_icon = getattr(settings, 'GOOGLE_MAPS_ORGANISATION_MARKER_ICON', '')
     template_context = dict(object=object, width=width, height=height,
         zoom=zoom, marker_icon=marker_icon)
