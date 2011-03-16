3f512f148fe0aa607cd97751801a58618f885058
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-03-16 17:17:28 +0000

    [#6699037] Added clean_deployment_directories() function. Removed unused parameter in ensure_path_exists() function. Renamed 'zipball' references to archive for clarity.

--- scripts/deployment/integration_fabfile.py
+++ scripts/deployment/integration_fabfile.py
@@ -10,19 +10,28 @@ from fabric.contrib import files
 from fab_config_develop import load_config
 
 
-def ensure_path_exists(path_env_var, path):
+def ensure_path_exists(path):
     if not files.exists(path):
         sudo("mkdir %s" % path)
         sudo("chmod 775 %s" % path)
 
 def ensure_required_paths_exist():
-    ensure_path_exists('repo_checkout_root', env.repo_checkout_root)
-    ensure_path_exists('rsr_snapshots_dir', env.rsr_snapshots_dir)
+    ensure_path_exists(env.repo_checkout_root)
+    ensure_path_exists(env.rsr_snapshots_dir)
 
-def download_and_unpack_rsr_zipball():
-    print ">> Fetching akvo-rsr zipball for %s branch from Github" % env.rsr_branch
+def clean_deployment_directories():
+    if files.exists(env.rsr_snapshot_file):
+        print "\n>> Deleting previous snapshot file"
+        sudo("rm %s" % env.rsr_snapshot_file)
+    if files.exists(env.rsr_src_root):
+        print "\n>> Deleting previous deployment directory"
+        sudo("rm -r %s" % env.rsr_src_root)
+
+def download_and_unpack_rsr_archive():
+    clean_deployment_directories()
+    print "\n>> Fetching akvo-rsr archive for %s branch from Github" % env.rsr_branch
     sudo("wget -nv -O %s %s" % (env.rsr_snapshot_file, env.rsr_zipball_path))
-    print ">> Unpacking akvo-rsr zipball in %s" % env.repo_checkout_root
+    print "\n>> Unpacking akvo-rsr archive in %s" % env.repo_checkout_root
     sudo("unzip -q %s -d %s -x */.gitignore" % (env.rsr_snapshot_file, env.repo_checkout_root))
     sudo("mv %s/akvo-akvo-rsr-* %s/akvo-rsr_develop" % (env.repo_checkout_root, env.repo_checkout_root))
 
@@ -30,7 +39,8 @@ def rebuild_virtualenv():
     print "not implemented: rebuild_virtualenv()"
 
 def deploy_rsr():
+    print "\n>> Starting RSR deployment"
     load_config()
     ensure_required_paths_exist()
-    download_and_unpack_rsr_zipball()
+    download_and_unpack_rsr_archive()
     rebuild_virtualenv()
