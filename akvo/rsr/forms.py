36c3d40f0e09567c4abae100dc3a1de624c4363b
Paul Burt <paul.burt@gmail.com>
2011-03-21 11:39:22 +0000

    Renamed views.UpdateForm -> forms.ProjectUpdateForm and changed imports/references in forms and views modules accordingly.

--- akvo/rsr/forms.py
+++ akvo/rsr/forms.py
@@ -25,7 +25,7 @@ from registration.forms import RegistrationFormUniqueEmail
 
 from mollie.ideal.utils import get_mollie_banklist
 
-from akvo.rsr.models import (UserProfile, Organisation, Project,)
+from akvo.rsr.models import UserProfile, Organisation, Project, ProjectUpdate
 
 # I put this on all required fields, because it's easier to pick up
 # on them with CSS or JavaScript if they have a class of "required"
@@ -254,3 +254,23 @@ class ReadonlyFKAdminField(object):
                 if field_name in form.base_fields:
                     form.base_fields[field_name].widget = ReadonlyFKWidget(self.admin_site, getattr(obj, field_name, ''))
         return form
+
+
+class ProjectUpdateForm(forms.ModelForm):
+    """Form representing a ProjectUpdate."""
+    js_snippet = "return taCount(this,'myCounter')"
+    js_snippet = mark_safe(js_snippet)    
+    title = forms.CharField(widget=forms.TextInput(
+        attrs={'class':'input', 'maxlength':'50', 'size':'25', 'onKeyPress':'return taLimit(this)', 'onKeyUp':js_snippet}
+                      ))
+    text = forms.CharField(required=False, widget=forms.Textarea(attrs={'class':'textarea', 'cols':'50'}))
+    #status = forms.CharField(widget=forms.RadioSelect(choices=STATUSES, attrs={'class':'radio'}))
+    photo = forms.ImageField(required=False, widget=forms.FileInput(attrs={'class':'input', 'size':'15', 'style':'height: 2em'}))
+    photo_location = forms.CharField(required=False, widget=forms.RadioSelect(choices=PHOTO_LOCATIONS, attrs={'class':'radio'}))
+    photo_caption = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'input', 'size':'25', 'maxlength':'75',}))
+    photo_credit = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'input', 'size':'25', 'maxlength':'25',}))
+    video = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'input', 'site':'25', 'maxlength':'50'}))
+
+    class Meta:
+        model = ProjectUpdate
+        exclude = ('time', 'project', 'user', )
