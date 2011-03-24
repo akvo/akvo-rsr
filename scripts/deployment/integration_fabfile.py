95ee97bef58f3c4d1dea66098453b3089cab91c8
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-03-24 17:09:58 +0000

    [#6699037] Added install_akvo_modpython() function. Refactored renaming of unpacked archive directory so that destination directory uses env.rsr_src_root instead of an explicit directory name. Renamed env.rsr_archive_path to rsr_archive_url. Added missing if statement colon.

--- scripts/deployment/integration_fabfile.py
+++ scripts/deployment/integration_fabfile.py
@@ -32,16 +32,16 @@ def clean_deployment_directories():
 def download_and_unpack_rsr_archive():
     clean_deployment_directories()
     print "\n>> Fetching akvo-rsr archive for %s branch from Github" % env.rsr_branch
-    sudo("wget -nv -O %s %s" % (env.rsr_snapshot_file, env.rsr_archive_path))
+    sudo("wget -nv -O %s %s" % (env.rsr_snapshot_file, env.rsr_archive_url))
     print "\n>> Unpacking akvo-rsr archive in %s" % env.repo_checkout_root
     sudo("unzip -q %s -d %s -x */.gitignore" % (env.rsr_snapshot_file, env.repo_checkout_root))
-    sudo("mv %s/akvo-akvo-rsr-* %s/akvo-rsr_develop" % (env.repo_checkout_root, env.repo_checkout_root))
+    sudo("mv %s %s" % (env.rsr_unpacked_archive_match, env.rsr_src_root))
 
 def clean_virtualenv_directory():
     if files.exists(env.rsr_virtualenv_path):
         print "\n>> Deleting previous RSR virtualenv directory"
         sudo("rm -r %s" % env.rsr_virtualenv_path)
-    if files.exists(env.pip_install_log_file)
+    if files.exists(env.pip_install_log_file):
         print ">> Deleting pip install log file"
         sudo("rm %s" % env.pip_install_log_file)
 
@@ -54,9 +54,13 @@ def rebuild_virtualenv():
     sudo("virtualenv %s" % env.rsr_virtualenv_path)
     with_virtualenv("pip install -q -M -U -E %s -r %s --log=%s" % (env.rsr_virtualenv_path, env.pip_requirements_file, env.pip_install_log_file))
 
+def install_akvo_modpython():
+    sudo("cp -p %s %s" % (env.mod_python_template_file, env.mod_python_destination_path))
+
 def deploy_rsr():
     print "\n>> Starting RSR deployment"
     load_config()
     ensure_required_paths_exist()
     download_and_unpack_rsr_archive()
     rebuild_virtualenv()
+    install_akvo_modpython()
