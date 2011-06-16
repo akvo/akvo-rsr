13dc7d33b4649ebad27acb5a13a2824aaf6eaf5b
Paul Burt <paul.burt@gmail.com>
2011-06-16 14:23:31 +0100

    Added comment stubs to populate_time_last_updated.py.

--- akvo/populate_time_last_updated.py
+++ akvo/populate_time_last_updated.py
@@ -11,12 +11,16 @@ setup_environ(settings)
 from django.db.models import get_model
 
 
+
 def populate_project_update_time_last_updated():
+    # set time_last_updated field to NULL
     updates = get_model('rsr', 'projectupdate').objects.all()
     for update in updates:
         update.time_last_updated = update.time
         update.save()
         print 'Project Update %d last updated timestamp set.' % update.id
+    # set time_last_updated field to NOT NULL
+    print 'Success!'
 
 
 if __name__ == '__main__':
