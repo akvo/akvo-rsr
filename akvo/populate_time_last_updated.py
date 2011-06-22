2faf70dfb74b23f30286fc60cfb96a858908668d
Paul Burt <paul.burt@gmail.com>
2011-06-22 11:07:50 +0100

    Beefed up populate_time_last_updated.py script.

--- akvo/populate_time_last_updated.py
+++ akvo/populate_time_last_updated.py
@@ -4,6 +4,8 @@
 #to be run in the akvo rsr root folder. setting up all projects as published, if they have no status
 # and setting all orgs to free account if they have none
 
+import os
+
 from django.core.management import setup_environ
 import settings
 setup_environ(settings)
@@ -12,21 +14,34 @@ from django.db import connection
 from django.db.models import get_model
 
 
+cursor = connection.cursor()
+
+
+def set_null():
+    print 'Setting time_last_updated field to NULL...'
+    cursor.execute('ALTER TABLE `rsr_projectupdate` '
+                   'MODIFY COLUMN `time_last_updated` datetime NULL')
+
+def db_dump_load():
+    print 'Loading data into database...'
+    os.system('python db_dump.py load')
 
 def populate_project_update_time_last_updated():
-    cursor = connection.cursor()
-    cursor.execute('ALTER TABLE `rsr_projectupdate` '
-                   'MODIFY COLUMN `time_last_update` datetime NULL')
-    print 'Set time_last_updated field to NULL.'
     updates = get_model('rsr', 'projectupdate').objects.all()
     for update in updates:
         update.time_last_updated = update.time
         update.save()
-        print 'Project Update %d last updated timestamp set.' % update.id
+        print 'Project Update %d last updated timestamp set...' % update.id
+
+def set_not_null():
+    print 'Resetting time_last_updated field to NOT NULL...'
     cursor.execute('ALTER TABLE `rsr+projectupdate` '
-                   'MODIFY_COLUMN `time_last_updated` datetime NUL NULL')
-    print 'Reset time_last_updated field to NOT NULL.\nSuccess!'
+                   'MODIFY COLUMN `time_last_updated` datetime NOT NULL')
 
 
 if __name__ == '__main__':
+    set_null()
+    db_dump_load()
     populate_project_update_time_last_updated()
+    set_not_null()
+    print 'All done!'
