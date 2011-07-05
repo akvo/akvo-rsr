c343c831359136d0e6a1b3422bd45694df6d403e
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-07-05 13:39:08 +0100

    [#6699037] Added further implementation for fab.tasks.DeployRSRCode. Replaced helpers.paths with an OO implementation in fab.helpers.paths.PathsHelper. The PathsHelper now also gives execution feedback when expected paths are found instead of only when creating missing paths.

--- scripts/deployment/fab/tasks/deploy_rsr_code.py
+++ scripts/deployment/fab/tasks/deploy_rsr_code.py
@@ -8,6 +8,7 @@ import fabric.api
 import fabric.tasks
 
 import fab.config.deployer_config
+import fab.helpers.paths
 import fab.helpers.permissions
 
 
@@ -19,6 +20,7 @@ class DeployRSRCode(fabric.tasks.Task):
     def __init__(self, environment):
         self.env = environment
         self.permissions = fab.helpers.permissions.PermissionsHelper(self.env)
+        self.paths = fab.helpers.paths.PathsHelper(self.permissions)
 
     def run(self):
         print "\n>> Starting RSR codebase deployment"
@@ -27,10 +29,13 @@ class DeployRSRCode(fabric.tasks.Task):
 
     def ensure_required_paths_exist(self):
         self.ensure_user_is_member_of_akvo_permissions_group()
+        print "\n>> Ensuring expected paths exist"
+        self.paths.ensure_path_exists_with_akvo_group_permissions(self.env.repo_checkout_root)
+        self.paths.ensure_path_exists(self.env.rsr_snapshots_dir)
+        self.paths.ensure_path_exists_with_akvo_group_permissions(self.env.virtualenvs_root)
 
     def ensure_user_is_member_of_akvo_permissions_group(self):
-        print "\n>> Checking that user [%s] is a member of group [%s]" % (self.env.user,
-            self.env.akvo_permissions_group) + " (required for setting directory permissions later on)"
+        print "\n>> Checking group membership for user [%s]" % self.env.user + " (required for setting directory permissions later on)"
         self.permissions.ensure_user_is_member_of_group(self.env.akvo_permissions_group)
 
 
