f51db2a1ec73454e1d29962f506063d5f7349f1b
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-07-06 12:30:13 +0100

    [#6699037] Added the deployment pip requirements to the dev virtualenv rebuild script to ensure Fabric is available within the virtualenv as necessary. Added a versioned pip requirement for the distribute package in the 1_deployment.txt requirements file to ensure the installed distribute package is updated within the dev virtualenv (otherwise an old version of distribute gets installed).

--- scripts/deployment/pip/rebuild_osx_dev_env.sh
+++ scripts/deployment/pip/rebuild_osx_dev_env.sh
@@ -19,15 +19,18 @@ VIRTUALENV_NAME=$1
 source osx_build_flags_env_intel.config
 source virtualenv_paths.config
 
-function install_rsr_and_testing_packages
+function install_rsr_and_infrastructure_packages
 {
     printf "\n>> Fresh virtualenv should only contain the distribute and wsgiref packages:\n"
     pip freeze
 
+    printf "\n>> Installing deployment packages:\n"
+    pip install -M -r requirements/1_deployment.txt
+
     printf "\n>> Installing RSR packages:\n"
     pip install -M -r requirements/2_rsr.txt
 
-    printf "\n>> Installing further dev and testing packages:\n"
+    printf "\n>> Installing testing packages:\n"
     pip install -M -r requirements/3_testing.txt
 
     printf "\n>> Packages installed in the $VIRTUALENV_NAME virtualenv:\n"
@@ -41,7 +44,7 @@ function install_rsr_packages_within_virtualenv
 
     # proceed if no errors occur when switching to the named virtualenv
     if [ $? -eq 0 ]; then
-        install_rsr_and_testing_packages
+        install_rsr_and_infrastructure_packages
         printf "\n>> Deactivating virtualenv: $VIRTUALENV_NAME\n"
         deactivate
     else
