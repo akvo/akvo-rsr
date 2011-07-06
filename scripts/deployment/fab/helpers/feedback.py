2509dd377ec46a7147ad09b970b1a1167b652f06
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-07-06 19:19:09 +0100

    [#6699037] Added fab.helpers.FabricRunner and ExecutionFeedback classes for encapsulating common functionality that can be stubbed in tests of classes that depend on these.

---
+++ scripts/deployment/fab/helpers/feedback.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Akvo RSR is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+
+class ExecutionFeedback(object):
+
+    def comment(self, message):
+        print message
---
+++ scripts/deployment/fab/helpers/runner.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Akvo RSR is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+
+import fabric.api
+
+
+class FabricRunner(object):
+
+    def run(self, command):
+        return fabric.api.run(command)
+
+    def sudo(self, command):
+        return fabric.api.sudo(command)
