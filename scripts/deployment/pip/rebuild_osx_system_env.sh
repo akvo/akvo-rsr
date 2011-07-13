a11b2bc2a9b0521682ec69006d2534972ce471cb
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-07-13 22:31:33 +0100

    [#6699037] The latest Fabric 1.1.x now builds successfully with 64-bit build configurations.

--- scripts/deployment/pip/rebuild_osx_system_env.sh
+++ scripts/deployment/pip/rebuild_osx_system_env.sh
@@ -85,8 +85,8 @@ function install_system_packages
     printf "\n>> Installing/upgrading system packages: (with 64-bit architecture)\n"
     pip install -M -r requirements/0_system.txt
 
-    source osx_build_flags_env_intel.config
-    printf "\n>> Installing/upgrading deployment packages: (with universal 32-bit & 64-bit architecture)\n"
+    source osx_build_flags_env_64.config
+    printf "\n>> Installing/upgrading deployment packages: (with 64-bit architecture)\n"
     pip install -M -r requirements/1_deployment.txt
 
     printf "\n>> Installed system packages:\n"
