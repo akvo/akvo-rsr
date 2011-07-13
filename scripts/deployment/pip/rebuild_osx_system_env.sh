ca8dd238f71e18f3e802d626cb404abe771f39a0
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-07-13 22:06:04 +0100

    [#6699037] Added error handling in the rebuild_osx_system_env.sh script when the distribute or pip package installations fail. Added environment flags listings before each package installation for clearer build output when using 64-bit or universal build configurations. Added the distribute and pip URLs to the build comments for each package. The temp download directory is now cleared before each run of the script to prevent stale downloads accumulating.

--- scripts/deployment/pip/rebuild_osx_system_env.sh
+++ scripts/deployment/pip/rebuild_osx_system_env.sh
@@ -12,35 +12,41 @@ if [ $CURRENT_USER != "root" ]; then
     exit -1
 fi
 
-cd "$PIP_DIR"
-
 # exit if osx_system.config file does not exist
 if [ ! -e osx_system.config ]; then
-    printf "\n>> Expected osx_system.config file not found -- copy the osx_system.config.template file and edit as necessary\n"
+    printf "\n>> Expected $PIP_DIR/osx_system.config file not found -- copy the osx_system.config.template file and edit as necessary\n"
     exit -1
 fi
 
 source osx_system.config
-source osx_build_flags_env_64.config
 
 PY_PATH="$PY_BIN_PATH/python"
 
 function ensure_temp_dir_exists
 {
     if [ ! -d "$PYTHON_TEMP_DIR" ]; then
-        printf "\n>> Creating temp directory: $PYTHON_TEMP_DIR\n"
+        printf ">> Creating temp directory: $PYTHON_TEMP_DIR\n"
         mkdir -p "$PYTHON_TEMP_DIR"
     else
-        printf "\n>> Using existing temp directory: $PYTHON_TEMP_DIR\n"
+        # check if directory is empty
+        if [ "$(ls -A $PYTHON_TEMP_DIR)" ]; then
+            printf ">> Clearing existing temp directory: $PYTHON_TEMP_DIR\n"
+            rm -r "$PYTHON_TEMP_DIR"/*
+        else
+            printf ">> Using existing temp directory: $PYTHON_TEMP_DIR\n"
+        fi
     fi
 }
 
 function install_distribute_package
 {
     # See installation notes at http://pypi.python.org/pypi/distribute#distribute-setup-py
-    printf "\n>> Installing distribute package\n"
+    cd "$PIP_DIR"
+    source osx_build_flags_env_64.config
+    DISTRIBUTE_SETUP_URL=http://python-distribute.org/distribute_setup.py
+    printf "\n>> Installing distribute package from $DISTRIBUTE_SETUP_URL (with 64-bit architecture)\n\n"
     cd "$PYTHON_TEMP_DIR"
-    curl -L -O http://python-distribute.org/distribute_setup.py
+    curl -L -O $DISTRIBUTE_SETUP_URL
     $PY_PATH distribute_setup.py
 }
 
@@ -60,9 +66,12 @@ function link_easy_install
 function install_pip_package
 {
     # See installation notes at http://www.pip-installer.org/en/latest/installing.html
-    printf "\n>> Installing pip package\n"
+    cd "$PIP_DIR"
+    source osx_build_flags_env_64.config
+    GET_PIP_URL=https://raw.github.com/pypa/pip/1.0.1/contrib/get-pip.py
+    printf "\n>> Installing pip package from $GET_PIP_URL (with 64-bit architecture)\n"
     cd "$PYTHON_TEMP_DIR"
-    curl -L -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+    curl -L -O $GET_PIP_URL
     $PY_PATH get-pip.py
 }
 
@@ -72,11 +81,12 @@ function install_system_packages
     printf "\n>> Current system packages:\n"
     pip freeze
 
+    source osx_build_flags_env_64.config
     printf "\n>> Installing/upgrading system packages: (with 64-bit architecture)\n"
     pip install -M -r requirements/0_system.txt
 
-    printf "\n>> Installing/upgrading deployment packages: (with universal 32-bit & 64-bit architecture)\n"
     source osx_build_flags_env_intel.config
+    printf "\n>> Installing/upgrading deployment packages: (with universal 32-bit & 64-bit architecture)\n"
     pip install -M -r requirements/1_deployment.txt
 
     printf "\n>> Installed system packages:\n"
@@ -87,9 +97,21 @@ function build_system_environment
 {
     ensure_temp_dir_exists
     install_distribute_package
-    link_easy_install
-    install_pip_package
-    install_system_packages
+
+    # proceed if no errors occurred
+    if [ $? -eq 0 ]; then
+        link_easy_install
+        install_pip_package
+
+        # proceed if no errors occurred
+        if [ $? -eq 0 ]; then
+            install_system_packages
+        else
+            printf "\n>> Installation of pip package failed -- see error details above\n"
+        fi
+    else
+        printf "\n>> Installation of distribute package failed -- see error details above\n"
+    fi
 }
 
 
