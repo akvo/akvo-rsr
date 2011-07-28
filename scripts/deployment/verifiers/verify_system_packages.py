ab592240b537232114b5acb4713f1efcb36d8eab
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-07-28 13:13:37 +0100

    [#6698487] (pb, ogl) Moved the SystemPackageVerifier class into a separate verifiers.syspackages module.

--- scripts/deployment/verifiers/verify_system_packages.py
+++ scripts/deployment/verifiers/verify_system_packages.py
@@ -5,28 +5,7 @@
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
 
-from verifiers.helpers.dependencies import CommandDependency, DependencyVerifier, HeaderDirectoryDependency, HeaderFileDependency
-
-
-class SystemPackageVerifier(object):
-
-    def ensure_expected_system_components_are_installed(self):
-        # perhaps load these dependency assertions from a YAML file in future
-        verifier = DependencyVerifier()
-        verifier.add(CommandDependency("mysql_config", "MySQL", "MySQL-python"))
-        verifier.add(HeaderFileDependency("libiconv", "iconv.h", "lxml"))
-        verifier.add(HeaderDirectoryDependency("libxml2", "lxml"))
-        verifier.add(HeaderDirectoryDependency("libxslt", "lxml"))
-        verifier.add(HeaderFileDependency("libjpeg", "jpeglib.h", "PIL"))
-        verifier.add(HeaderFileDependency("zlib", "zlib.h", "PIL"))
-
-        print ">> Verifying expected system components for building Python modules:"
-        verifier.verify_all()
-        if verifier.not_all_dependencies_met():
-            print "\n>> Not all expected dependencies were met:"
-            verifier.display_dependency_warnings()
-            print
-            raise Exception("Missing dependencies for building required Python modules: %s" % verifier.missing_dependencies())
+from verifiers.syspackages import SystemPackageVerifier
 
 
 if __name__ == "__main__":
