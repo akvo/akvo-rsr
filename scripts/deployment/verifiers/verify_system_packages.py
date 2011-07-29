2641cab584a417d9342544ff6e4780f14dfd8234
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-07-29 13:04:29 +0100

    [#6699037] Added call to SysPathVerifier in verifiers/verify_system_packages.py script. The verification exception message is now displayed rather than reraising the exception. Added an explicit non-zero system exit call if any required system packages are missing.

--- scripts/deployment/verifiers/verify_system_packages.py
+++ scripts/deployment/verifiers/verify_system_packages.py
@@ -5,8 +5,21 @@
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
 
+import imp, os, sys
+
+imp.load_source("syspath", os.path.join(os.path.dirname(__file__), 'syspath.py'))
+
+from syspath import SysPathVerifier
+
+SysPathVerifier().exit_if_deployment_scripts_home_not_on_syspath()
+
+
 from verifiers.syspackages import SystemPackageVerifier
 
 
 if __name__ == "__main__":
-    SystemPackageVerifier().ensure_expected_system_components_are_installed()
+    try:
+        SystemPackageVerifier().ensure_expected_system_components_are_installed()
+    except Exception, exception:
+        print exception
+        sys.exit(1)
