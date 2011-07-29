5018e90d20f383e4f996381fac8265632a58362d
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-07-29 17:34:27 +0100

    [#6699037] Added gmp system package requirement -- used for building some pycrypto extensions.

--- scripts/deployment/verifiers/syspackages.py
+++ scripts/deployment/verifiers/syspackages.py
@@ -14,6 +14,7 @@ class SystemPackageVerifier(object):
         # perhaps load these dependency assertions from a YAML file in future
         verifier = DependencyVerifier()
         verifier.add(CommandDependency("mysql_config", "MySQL", "MySQL-python"))
+        verifier.add(HeaderFileDependency("gmp", "gmp.h", "pycrypto"))
         verifier.add(HeaderFileDependency("libiconv", "iconv.h", "lxml"))
         verifier.add(HeaderDirectoryDependency("libxml2", "lxml"))
         verifier.add(HeaderDirectoryDependency("libxslt", "lxml"))
