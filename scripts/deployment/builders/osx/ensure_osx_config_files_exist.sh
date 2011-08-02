57440fefcc5d14e4016e79e224d389ea1b6d03d2
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-08-02 19:13:08 +0100

    [#6699037] Added check for expected config files directory in ensure_osx_config_files_exist.sh script. Added feedback when an expected config file is found, in addition to when a config file is missing.

--- scripts/deployment/builders/osx/ensure_osx_config_files_exist.sh
+++ scripts/deployment/builders/osx/ensure_osx_config_files_exist.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-CONFIG_DIR="$(cd `dirname $0` && pwd)/config"
+if [ -z "$CONFIG_DIR" ]; then
+    export CONFIG_DIR="$(cd `dirname $0` && pwd)/config"
+fi
 
 function exit_if_expected_config_file_is_missing
 {
@@ -12,8 +14,11 @@ function exit_if_expected_config_file_is_missing
         printf ">> Copy the $1.template file and edit as necessary\n\n"
         exit -1
     fi
+
+    printf ">> Found expected $1 file\n"
 }
 
 exit_if_expected_config_file_is_missing "user.config"
 exit_if_expected_config_file_is_missing "python_system.config"
 exit_if_expected_config_file_is_missing "rsr_env.config"
+printf "\n"
