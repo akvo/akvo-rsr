b537899274e4f33d00f3faf3a373387bdd217b7c
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-08-16 14:06:41 +0100

    [#15222535] Added fix to ensure the RSR log file is writable to prevent failure when dumping data.

--- scripts/deployment/fab/helpers/dataretriever.py
+++ scripts/deployment/fab/helpers/dataretriever.py
@@ -20,6 +20,7 @@ class DataRetriever(object):
 
     def fetch_data_from_database(self):
         self.ensure_required_paths_exist()
+        self.ensure_rsr_log_file_is_writable()
         rsr_data_dump_path = self.config.rsr_data_dump_path
         self.feedback.comment("Fetching data from database at %s" % self.config.akvo_rsr_app_path)
         self.database_host.run_within_virtualenv("python %s -d %s dump" % (self.config.db_dump_script_path, rsr_data_dump_path))
@@ -30,3 +31,7 @@ class DataRetriever(object):
         self.database_host.ensure_directory_exists_with_sudo(self.config.data_dumps_home)
         self.database_host.exit_if_directory_does_not_exist(self.config.rsr_virtualenv_path)
         self.database_host.exit_if_file_does_not_exist(self.config.db_dump_script_path)
+
+    def ensure_rsr_log_file_is_writable(self):
+        self.feedback.comment("Ensuring RSR log file is writable")
+        self.database_host.make_file_writable_for_all_users(self.config.rsr_log_file_path)
--- scripts/deployment/fab/tests/helpers/data_retriever_test.py
+++ scripts/deployment/fab/tests/helpers/data_retriever_test.py
@@ -34,15 +34,20 @@ class DataRetrieverTest(mox.MoxTestBase):
         akvo_rsr_app_path = "/var/django_apps/rsr_1.0.9/akvo"
         db_dump_script_path = os.path.join(akvo_rsr_app_path, "db_dump.py")
         rsr_data_dump_path = os.path.join(data_dumps_home, "rsr_1.0.9_utc_timestamp")
+        rsr_log_file_path = os.path.join(akvo_rsr_app_path, "akvo.log")
+
         self.mock_config.data_dumps_home = data_dumps_home
         self.mock_config.rsr_virtualenv_path = rsr_virtualenv_path
         self.mock_config.akvo_rsr_app_path = akvo_rsr_app_path
         self.mock_config.db_dump_script_path = db_dump_script_path
         self.mock_config.rsr_data_dump_path = rsr_data_dump_path
+        self.mock_config.rsr_log_file_path = rsr_log_file_path
 
         self.mock_database_host.ensure_directory_exists_with_sudo(data_dumps_home)
         self.mock_database_host.exit_if_directory_does_not_exist(rsr_virtualenv_path)
         self.mock_database_host.exit_if_file_does_not_exist(db_dump_script_path)
+        self.mock_feedback.comment("Ensuring RSR log file is writable")
+        self.mock_database_host.make_file_writable_for_all_users(rsr_log_file_path)
         self.mock_feedback.comment(mox.StrContains("Fetching data from database"))
         self.mock_database_host.run_within_virtualenv("python %s -d %s dump" % (db_dump_script_path, rsr_data_dump_path))
         self.mock_database_host.compress_directory(rsr_data_dump_path)
