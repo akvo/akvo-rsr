2f70eb5d4f066b6404d62d6809d00aeb2daf0167
Paul Burt <paul.burt@me.com>
2011-08-22 12:31:35 +0100

    Updated sites_migrate.py script.

--- akvo/sites_migrate.py
+++ akvo/sites_migrate.py
@@ -6,11 +6,21 @@ setup_environ(settings)
 from django.contrib.sites.models import Site
 
 
-def main():
+def enable_existing_sites():
     sites = Site.objects.all()
     for site in sites:
         site.enabled = True
         site.save()
+    print 'Existing sites migrated.\n'
+
+def create_local_development_site():
+    site = Site.objects.get(name='localhost:8000')
+    site.name = 'Local development (vanilla RSR)'
+    site.domain = site.development_domain = 'akvo.dev'
+    site.enabled = True
+    site.save()
+    print 'Local development site created.\n'
 
 if __name__ == '__main__':
-    main()
+    enable_existing_sites()
+    create_local_development_site()
