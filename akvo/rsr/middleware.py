9f65e6fe0c92d7f03ed9d93df50067115c5fe754
Paul Burt <paul.burt@me.com>
2011-08-22 09:09:51 +0100

    Added PartnerSitesRouterMiddleware.

--- akvo/rsr/middleware.py
+++ akvo/rsr/middleware.py
@@ -7,7 +7,35 @@
 """
 from __future__ import absolute_import
 
+from django.conf.settings import RESERVED_HOSTNAMES
+from django.contrib.sites.models import Site
 
-class PartnerSitesRouterMiddleware:
-    def process_request(self, request):
-        return None
+
+class PartnerSitesRouterMiddleware(object):
+    def process_request(self, request, partner_site=None):
+        domain = request.get_host().split(':')[0]
+        if domain.endswith('.dev'):  # local development domain
+            try:
+                partner_site = Site.objects.get(development_domain=domain)
+            except:
+                pass
+        elif domain.endswith('.akvo.org'):  # akvo dev/production
+            parts = domain.split('.')
+            num_parts = len(parts)
+            if num_parts >= 3:
+                hostname = parts[-3]
+                if ((num_parts == 3 and not hostname in RESERVED_HOSTNAMES) or
+                    (num_parts >=4 and hostname in RESERVED_HOSTNAMES)):
+                    try:
+                        partner_site = Site.objects.get(domain=domain)
+                    except:
+                        pass
+        else:  # probably a partner-nominated domain
+            try:
+                partner_site = Site.objects.get(partner_domain=domain)
+            except:
+                pass
+        if partner_site is not None:
+            request.partner_id = partner_site.organisation.id
+            request.urlconf = 'akvo.urls_partnersites'
+        return
