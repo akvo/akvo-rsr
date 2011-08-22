0bf46b6929e0f8b74958324030fb7ef2f97b2baa
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-08-22 16:49:23 +0100

    [#17133413] Updated the main deploy_rsr.py script to pass host_controller_mode=remote parameters to the deploy_rsr_code and rebuild_rsr_virtualenv tasks.

--- scripts/deployment/deploy_rsr.py
+++ scripts/deployment/deploy_rsr.py
@@ -47,8 +47,8 @@ def run_fab_task(fully_qualified_task, host):
 def deploy_rsr():
     os.chdir(FABRIC_SCRIPTS_HOME)
     run_fab_task("fab.tasks.dataretriever.fetch_rsr_data", LIVE_DATABASE_HOST)
-    run_fab_task("fab.tasks.codedeployment.deploy_rsr_code", DEPLOYMENT_HOST)
-    run_fab_task("fab.tasks.virtualenv.rebuild_rsr_virtualenv", DEPLOYMENT_HOST)
+    run_fab_task("fab.tasks.codedeployment.deploy_rsr_code:host_controller_mode=remote", DEPLOYMENT_HOST)
+    run_fab_task("fab.tasks.virtualenv.rebuild_rsr_virtualenv:host_controller_mode=remote", DEPLOYMENT_HOST)
 
 
 if __name__ == "__main__":
