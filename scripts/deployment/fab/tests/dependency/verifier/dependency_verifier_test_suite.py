001d8b30ff5b1f4f7b5f47c58e6308d68a1f4ab2
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-09-06 13:08:19 +0100

    [#17289463] Added fab.dependency.verifier.packageverifier module with a SystemPackageVerifier class.

--- scripts/deployment/fab/tests/dependency/verifier/dependency_verifier_test_suite.py
+++ scripts/deployment/fab/tests/dependency/verifier/dependency_verifier_test_suite.py
@@ -8,10 +8,11 @@
 from testing.helpers.execution import TestSuiteLoader, TestRunner
 
 from fab.tests.dependency.verifier.dependency_collection_verifier_test import DependencyCollectionVerifierTest
+from fab.tests.dependency.verifier.system_package_verifier_test import SystemPackageVerifierTest
 
 
 def dependency_verifier_suite():
-    return TestSuiteLoader().create_suite_from_classes([DependencyCollectionVerifierTest])
+    return TestSuiteLoader().create_suite_from_classes([DependencyCollectionVerifierTest, SystemPackageVerifierTest])
 
 if __name__ == "__main__":
     from fab.tests.test_settings import TEST_MODE
