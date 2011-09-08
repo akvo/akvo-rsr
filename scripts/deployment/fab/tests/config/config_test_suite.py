bdcbe9cde3f784ba813b0c1ec47021da57df75cc
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-09-08 16:38:20 +0100

    [#17289463] Added fab.config.linux.systempackages module with package name and version specifications for compilation, core utils, package tools, locales and languages, Python, Python package dependencies, database and additional command-line tools used during deployment.

--- scripts/deployment/fab/tests/config/config_test_suite.py
+++ scripts/deployment/fab/tests/config/config_test_suite.py
@@ -9,10 +9,13 @@ from testing.helpers.execution import TestSuiteLoader, TestRunner
 
 from fab.tests.config.data_retriever_config_test import DataRetrieverConfigTest
 from fab.tests.config.deployer_config_test import DeployerConfigTest
+from fab.tests.config.linux.linux_config_test_suite import linux_config_suite
 
 
 def config_suite():
-    return TestSuiteLoader().create_suite_from_classes([DataRetrieverConfigTest, DeployerConfigTest])
+    config_suite = TestSuiteLoader().create_suite_from_classes([DataRetrieverConfigTest, DeployerConfigTest])
+
+    return TestSuiteLoader().create_suite_from_list([config_suite, linux_config_suite()])
 
 if __name__ == "__main__":
     from fab.tests.test_settings import TEST_MODE
