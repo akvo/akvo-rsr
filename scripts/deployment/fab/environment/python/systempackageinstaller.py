d3fb230e5c511b9f122d117566dee88708428541
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-09-14 23:08:35 +0100

    [#18043873] Added fab.environment.python.systempackageinstaller module with PackageInstallationPaths and SystemPythonPackageInstaller classes.

---
+++ scripts/deployment/fab/environment/python/systempackageinstaller.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+# Akvo RSR is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+
+import os
+
+from fab.config.environment.python.packagetools import PackageInstallationToolsConfig
+from fab.config.rsr.codebase import RSRCodebaseConfig
+from fab.config.values import SharedConfigValues, PythonConfigValues
+from fab.helpers.internet import Internet
+
+
+class PackageInstallationPaths(object):
+
+    def __init__(self, python_config_values, package_tools_config, codebase_config):
+        self.package_download_dir = python_config_values.python_package_download_dir
+        self.distribute_setup_url = package_tools_config.distribute_setup_url
+        self.pip_setup_url = package_tools_config.pip_setup_url
+        self.system_requirements_url = codebase_config.system_requirements_file_url
+
+
+class SystemPythonPackageInstaller(object):
+
+    def __init__(self, installation_paths, internet_helper, host_controller):
+        self.paths = installation_paths
+        self.internet = internet_helper
+        self.host_controller = host_controller
+        self.feedback = host_controller.feedback
+
+    @staticmethod
+    def create_instance(host_controller):
+        python_config_values = PythonConfigValues()
+        installation_paths = PackageInstallationPaths(python_config_values,
+                                                      PackageInstallationToolsConfig(python_config_values.pip_version),
+                                                      RSRCodebaseConfig(SharedConfigValues().repository_branch))
+
+        return SystemPythonPackageInstaller(installation_paths, Internet(host_controller), host_controller)
+
+    def install_package_tools(self):
+        self._download_and_install_package("distribute", self.paths.distribute_setup_url)
+        self._download_and_install_package("pip", self.paths.pip_setup_url)
+
+    def _download_and_install_package(self, package_name, setup_script_url):
+        self.feedback.comment("Installing %s package from %s" % (package_name, setup_script_url))
+        self.internet.download_file_to_directory(self.paths.package_download_dir, setup_script_url)
+
+        with self.host_controller.cd(self.paths.package_download_dir):
+            self.host_controller.sudo("python %s" % self._file_from_url(setup_script_url))
+
+    def install_system_packages(self):
+        self._list_installed_python_packages()
+        self.feedback.comment("Updating system Python packages:")
+        self._install_packages_with_pip(self.paths.system_requirements_file_url)
+        self._list_installed_python_packages()
+
+    def _list_installed_python_packages(self):
+        self.feedback.comment("Installed system packages:")
+        self.host_controller.run("pip freeze")
+
+    def _install_packages_with_pip(self, requirements_file_url):
+        self.internet.download_file_to_directory(self.paths.package_download_dir, requirements_file_url)
+
+        with self.host_controller.cd(self.paths.package_download_dir):
+            self.host_controller.run("pip install -M -r %s --log=pip_install.log" % self._file_from_url(requirements_file_url))
+
+    def _file_from_url(self, file_url):
+        return file_url.split('/')[-1]
--- scripts/deployment/fab/tests/all_test_suites.py
+++ scripts/deployment/fab/tests/all_test_suites.py
@@ -11,6 +11,7 @@ from testing.helpers.execution import TestSuiteLoader, TestRunner
 
 from fab.tests.config.config_test_suite import config_suite
 from fab.tests.dependency.dependency_test_suite import dependency_suite
+from fab.tests.environment.environment_test_suite import environment_suite
 from fab.tests.helpers.helpers_test_suite import helpers_suite
 from fab.tests.host.host_test_suite import host_suite
 from fab.tests.os.os_test_suite import os_suite
@@ -18,8 +19,8 @@ from fab.tests.tasks.tasks_test_suite import tasks_suite
 
 
 def unit_test_suites():
-    return TestSuiteLoader().create_suite_from_list([config_suite(), dependency_suite(), helpers_suite(), os_suite(),
-                                                     host_suite(), tasks_suite()])
+    return TestSuiteLoader().create_suite_from_list([config_suite(), dependency_suite(), environment_suite(), helpers_suite(),
+                                                     os_suite(), host_suite(), tasks_suite()])
 
 if __name__ == "__main__":
     print "Test suite root: %s\n" % os.path.realpath(os.path.dirname(__file__))
