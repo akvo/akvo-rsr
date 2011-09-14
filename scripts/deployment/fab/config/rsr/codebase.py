722bf5ce2e65395996ededd0d7808e8a92cf807d
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-09-14 16:00:54 +0100

    [#18243733] Added fab.config.rsr.codebase.RSRCodebaseConfig with code repository paths and pip requirements file paths.

---
+++ scripts/deployment/fab/config/rsr/codebase.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Akvo RSR is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+
+import os
+
+
+class RSRCodebaseConfig(object):
+
+    RSR_FILES_BASE_URL          = "https://raw.github.com/akvo/akvo-rsr"
+    PIP_REQUIREMENTS_PATH       = "scripts/deployment/pip/requirements"
+
+    SYSTEM_REQUIREMENTS_FILE    = "0_system.txt"
+    RSR_REQUIREMENTS_FILE       = "2_rsr.txt"
+
+    def __init__(self, repo_branch):
+        self.repo_branch        = repo_branch
+        self.repo_branch_name   = self._repository_branch_without_type()
+
+        self._set_repository_paths()
+        self._set_pip_requirements_paths()
+
+    def _repository_branch_without_type(self):
+        if self._repository_branch_includes_branch_type():
+            return self._repository_branch_name_only()
+
+        return self.repo_branch
+
+    def _repository_branch_includes_branch_type(self):
+        return self.repo_branch.find("/") > 0
+
+    def _repository_branch_name_only(self):
+        return self.repo_branch.split("/")[-1]
+
+    def _set_repository_paths(self):
+        self.rsr_code_branch_url = os.path.join(self.RSR_FILES_BASE_URL, self.repo_branch)
+
+    def _set_pip_requirements_paths(self):
+        pip_requirements_base_url           = os.path.join(self.rsr_code_branch_url, self.PIP_REQUIREMENTS_PATH)
+        self.system_requirements_file_url   = os.path.join(pip_requirements_base_url, self.SYSTEM_REQUIREMENTS_FILE)
+        self.rsr_requirements_file_url      = os.path.join(pip_requirements_base_url, self.RSR_REQUIREMENTS_FILE)
---
+++ scripts/deployment/fab/tests/config/rsr/codebase_config_test.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+# Akvo RSR is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+
+import os, unittest2
+
+from testing.helpers.execution import TestSuiteLoader, TestRunner
+
+from fab.config.rsr.codebase import RSRCodebaseConfig
+
+
+class CodebaseConfigTest(unittest2.TestCase):
+
+    def setUp(self):
+        super(CodebaseConfigTest, self).setUp()
+        
+        self.codebase_config = RSRCodebaseConfig("feature/sms")
+
+    def test_has_repository_branch(self):
+        """fab.tests.config.rsr.codebase_config_test  Has repository branch"""
+
+        self.assertEqual("feature/sms", self.codebase_config.repo_branch)
+
+    def test_can_set_repository_branch_name_when_branch_includes_branch_type(self):
+        """fab.tests.config.rsr.codebase_config_test  Can set repository branch name when branch includes branch type"""
+
+        self.assertEqual("sms", self.codebase_config.repo_branch_name)
+
+    def test_can_set_repository_branch_name_when_branch_does_not_include_branch_type(self):
+        """fab.tests.config.rsr.codebase_config_test  Can set repository branch name when branch does not include branch type"""
+
+        self.assertEqual("develop", RSRCodebaseConfig("develop").repo_branch_name)
+
+    def test_has_rsr_files_base_url(self):
+        """fab.tests.config.rsr.codebase_config_test  Has RSR files base URL"""
+
+        self.assertEqual("https://raw.github.com/akvo/akvo-rsr", RSRCodebaseConfig.RSR_FILES_BASE_URL)
+
+    def test_has_rsr_code_branch_url(self):
+        """fab.tests.config.rsr.codebase_config_test  Has RSR code branch URL"""
+
+        self.assertEqual(os.path.join(RSRCodebaseConfig.RSR_FILES_BASE_URL, "feature/sms"), self.codebase_config.rsr_code_branch_url)
+
+    def test_has_pip_requirements_path(self):
+        """fab.tests.config.rsr.codebase_config_test  Has pip requirements path"""
+
+        self.assertEqual("scripts/deployment/pip/requirements", RSRCodebaseConfig.PIP_REQUIREMENTS_PATH)
+
+    def test_has_system_requirements_file_name(self):
+        """fab.tests.config.rsr.codebase_config_test  Has system requirements file name"""
+
+        self.assertEqual("0_system.txt", RSRCodebaseConfig.SYSTEM_REQUIREMENTS_FILE)
+
+    def test_has_rsr_requirements_file_name(self):
+        """fab.tests.config.rsr.codebase_config_test  Has RSR requirements file name"""
+
+        self.assertEqual("2_rsr.txt", RSRCodebaseConfig.RSR_REQUIREMENTS_FILE)
+
+    def test_has_system_requirements_file_url(self):
+        """fab.tests.config.rsr.codebase_config_test  Has system requirements file URL"""
+
+        self.assertEqual(self._expected_requirements_file_url(RSRCodebaseConfig.SYSTEM_REQUIREMENTS_FILE), self.codebase_config.system_requirements_file_url)
+
+    def test_has_rsr_requirements_file_url(self):
+        """fab.tests.config.rsr.codebase_config_test  Has RSR requirements file URL"""
+
+        self.assertEqual(self._expected_requirements_file_url(RSRCodebaseConfig.RSR_REQUIREMENTS_FILE), self.codebase_config.rsr_requirements_file_url)
+
+    def _expected_requirements_file_url(self, requirements_file):
+        return os.path.join(self.codebase_config.rsr_code_branch_url, RSRCodebaseConfig.PIP_REQUIREMENTS_PATH, requirements_file)
+
+
+def suite():
+    return TestSuiteLoader().load_tests_from(CodebaseConfigTest)
+
+if __name__ == "__main__":
+    from fab.tests.test_settings import TEST_MODE
+    TestRunner(TEST_MODE).run_test_suite(suite())
