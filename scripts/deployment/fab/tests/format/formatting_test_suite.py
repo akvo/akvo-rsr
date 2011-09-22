62db802f0d4f1366f077d9da69137815e221310c
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-09-22 18:12:14 +0100

    [#15223285, #18243733] Moved file timestamp formatting into a fab.format.timestamp.TimeStampFormatter class so that this can be reused for time-stamping various deployment and log files.

---
+++ scripts/deployment/fab/tests/format/formatting_test_suite.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Akvo RSR is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+
+from testing.helpers.execution import TestSuiteLoader, TestRunner
+
+from fab.tests.format.time_stamp_formatter_test import TimeStampFormatterTest
+
+
+def formatting_suite():
+    return TestSuiteLoader().create_suite_from_classes([TimeStampFormatterTest])
+
+if __name__ == "__main__":
+    from fab.tests.test_settings import TEST_MODE
+    TestRunner(TEST_MODE).run_test_suite(formatting_suite())
