b8ed389486f8949313f1ef0e5cb2be9317a18b95
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-09-27 02:16:16 +0100

    [#15222535, #15223285, #18243733] Removed the old fab.config.dataretriever.DataRetrieverConfig and fab.config.deployer.DeployerConfig classes since these have now been replaced by the refactored fab.config.rsr.dataretriever.RSRDataRetrieverConfig and fab.config.rsr.deployment.RSRDeploymentConfig classes and supporting config values classes.

--- scripts/deployment/fab/tests/config/config_test_suite.py
+++ scripts/deployment/fab/tests/config/config_test_suite.py
@@ -7,18 +7,14 @@
 
 from testing.helpers.execution import TestSuiteLoader, TestRunner
 
-from fab.tests.config.config_values_test import ConfigValuesTest
-from fab.tests.config.data_retriever_config_test import DataRetrieverConfigTest
-from fab.tests.config.deployer_config_test import DeployerConfigTest
+from fab.tests.config.config_values_test import suite as config_values_suite
 
 from fab.tests.config.environment.environment_config_test_suite import environment_config_suite
 from fab.tests.config.rsr.rsr_config_test_suite import rsr_config_suite
 
 
 def config_suite():
-    config_suite = TestSuiteLoader().create_suite_from_classes([ConfigValuesTest, DataRetrieverConfigTest, DeployerConfigTest])
-
-    return TestSuiteLoader().create_suite_from_list([config_suite, environment_config_suite(), rsr_config_suite()])
+    return TestSuiteLoader().create_suite_from_list([config_values_suite(), environment_config_suite(), rsr_config_suite()])
 
 if __name__ == "__main__":
     from fab.tests.test_settings import TEST_MODE
