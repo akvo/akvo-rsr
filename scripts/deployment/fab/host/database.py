e94baec2cb7e6939780e50883891f9c57adc62a9
Oliver Galloway-Lunn <olivergl@happyappleboutique.com>
2011-09-27 13:10:52 +0100

    [#15222535, #18243733] Greatly simplified the DatabaseHost, which now uses the reworked DataRetriever.

--- scripts/deployment/fab/host/database.py
+++ scripts/deployment/fab/host/database.py
@@ -5,41 +5,19 @@
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
 
-from fab.environment.python.virtualenv import VirtualEnv
+from fab.helpers.dataretriever import DataRetriever
 from fab.host.controller import RemoteHostController
-from fab.host.neutral import NeutralHost
-from fab.os.filesystem import FileSystem
 
 
-class DatabaseHost(NeutralHost):
+class DatabaseHost(object):
     """DatabaseHost encapsulates common actions available when retrieving data from a remote database host"""
 
-    def __init__(self, file_system, virtualenv, feedback):
-        super(DatabaseHost, self).__init__(file_system, feedback)
-        self.virtualenv = virtualenv
+    def __init__(self, data_retriever):
+        self.data_retriever = data_retriever
 
     @staticmethod
-    def create_instance(virtualenv_path):
-        host_controller = RemoteHostController.create_instance()
-        file_system = FileSystem(host_controller)
-        virtualenv = VirtualEnv(virtualenv_path, host_controller, file_system)
+    def create_instance():
+        return DatabaseHost(DataRetriever.create_instance(RemoteHostController.create_instance()))
 
-        return DatabaseHost(file_system, virtualenv, host_controller.feedback)
-
-    def ensure_directory_exists(self, dir_path):
-        self.file_system.ensure_directory_exists(dir_path)
-
-    def ensure_directory_exists_with_sudo(self, dir_path):
-        self.file_system.ensure_directory_exists_with_sudo(dir_path)
-
-    def delete_directory(self, dir_path):
-        self.file_system.delete_directory(dir_path)
-
-    def compress_directory(self, full_path_to_compress):
-        self.file_system.compress_directory(full_path_to_compress)
-
-    def download_file_to_local_directory(self, remote_file_path, local_dir):
-        self.file_system.download_file(remote_file_path, local_dir)
-
-    def run_within_virtualenv(self, command):
-        self.virtualenv.run_within_virtualenv(command)
+    def fetch_data_from_host(self):
+        self.data_retriever.fetch_data_from_database()
