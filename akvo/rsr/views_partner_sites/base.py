35f3496fc4f2e00b92ba7a734a185ab1f6762bf7
Daniel Karlsson <daniel@kardans.com>
2011-10-24 13:31:58 +0200

    Updated the partner sites views with a new Mixin to prevent duplication.

--- akvo/rsr/views_partner_sites/base.py
+++ akvo/rsr/views_partner_sites/base.py
@@ -24,7 +24,10 @@ __all__ = [
     'BaseView',
     ]
 
+
 class DebugViewMixin(object):
+    """In debug mode show query information"""
+
     def get_context_data(self, **kwargs):
         context = super(DebugViewMixin, self).get_context_data(**kwargs)
         if settings.DEBUG:
@@ -32,20 +35,28 @@ class DebugViewMixin(object):
             context['queries'] = connection.queries
         return context
 
-class BaseView(DebugViewMixin, TemplateView):
+
+class PartnerSitesMixin(object):
+    """Add partner sites context info to the request"""
+
+    def get_context_data(self, **kwargs):
+        context = super(PartnerSitesMixin, self).get_context_data(**kwargs)
+        context['favicon'] = self.request.partner_site.favicon  
+        context['logo'] = self.request.partner_site.logo
+        context['organisation'] = \
+            get_object_or_404(Organisation, pk=self.request.organisation_id)
+        context['return_url'] = self.request.partner_site.return_url
+        context['stylesheet'] = self.request.partner_site.stylesheet
+        return context
+
+
+class BaseView(DebugViewMixin, PartnerSitesMixin, TemplateView):
     """Base view that adds current organisation to the template context or
     throws a 404. Also adds the return_url, favicon and stylesheet context
     variables."""
 
     def get_context_data(self, **kwargs):
-        context = super(BaseView, self).get_context_data(**kwargs)
-        context['organisation'] = \
-            get_object_or_404(Organisation, pk=self.request.organisation_id)
-        context['return_url'] = self.request.partner_site.return_url
-        context['favicon'] = self.request.partner_site.favicon
-        context['stylesheet'] = self.request.partner_site.stylesheet
-        # Queries should be removed for production
-        return context
+        return super(BaseView, self).get_context_data(**kwargs)
 
 
 class BaseProjectView(BaseView):
@@ -66,19 +77,13 @@ class BaseProjectView(BaseView):
         return context
 
 
-class BaseListView(DebugViewMixin, ListView):
+class BaseListView(DebugViewMixin, PartnerSitesMixin, ListView):
     """List view that are extended with the current organisation and the
     proejcts connected to the organisation available in the template context
     variable project_list"""
 
     def get_context_data(self, **kwargs):
-        context = super(BaseListView, self).get_context_data(**kwargs)
-        context['organisation'] = \
-            get_object_or_404(Organisation, pk=self.request.organisation_id)
-        context['return_url'] = self.request.partner_site.return_url
-        context['favicon'] = self.request.partner_site.favicon
-        context['stylesheet'] = self.request.partner_site.stylesheet
-        return context
+        return super(BaseListView, self).get_context_data(**kwargs)
 
 
 class BaseProjectListView(BaseListView):
@@ -88,27 +93,35 @@ class BaseProjectListView(BaseListView):
     def get_context_data(self, **kwargs):
         context = super(BaseProjectListView, self).get_context_data(**kwargs)
         if context['filtered_projects'].form.data:
-            continent_code = context['filtered_projects'].form.data.get('continent', '')
-            country_id = context['filtered_projects'].form.data.get('locations__country', '')
-            org_id = context['filtered_projects'].form.data.get('organisation', '')
-            context['search_country'] = Country.objects.get(pk=int(country_id)) if country_id else ''
-            context['search_continent'] = dict(CONTINENTS)[continent_code] if continent_code else ''
-            context['search_organisation'] = Organisation.objects.get(pk=int(org_id)) if org_id else ''
+            continent_code = \
+                context['filtered_projects'].form.data.get('continent', '')
+            country_id = context['filtered_projects'] \
+                .form.data.get('locations__country', '')
+            org_id = context['filtered_projects'] \
+                .form.data.get('organisation', '')
+            context['search_country'] = \
+                Country.objects.get(pk=int(country_id)) if country_id else ''
+            context['search_continent'] = \
+                dict(CONTINENTS)[continent_code] if continent_code else ''
+            context['search_organisation'] = \
+                Organisation.objects.get(pk=int(org_id)) if org_id else ''
         return context
 
     def render_to_response(self, context):
-        """here we sanitize the query string, removing empty variables, and then we add the continent
-        if the country is part of the query string
+        """here we sanitize the query string, removing empty variables, and
+        then we add the continent if the country is part of the query string
         """
         # remove empty query string variables
         query_dict = remove_empty_querydict_items(self.request.GET)
         # if filtering on country, set the correct continent
         country_id = query_dict.get('locations__country', '')
         if country_id:
-            continent = dict(COUNTRY_CONTINENTS)[Country.objects.get(pk=int(country_id)).iso_code]
+            continent = dict(COUNTRY_CONTINENTS)[Country \
+                .objects.get(pk=int(country_id)).iso_code]
             if not query_dict.get('continent', None) == continent:
                 query_dict['continent'] = continent
-                return redirect("%s?%s" % (reverse('home'), query_dict.urlencode()))
+                return redirect("%s?%s" % (reverse('home'), \
+                    query_dict.urlencode()))
 
         return super(BaseProjectListView, self).render_to_response(context)
 
