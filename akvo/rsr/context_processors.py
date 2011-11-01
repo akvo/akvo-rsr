0e2838c418f1d7bc939d5266f0294a2113b672c9
Paul Burt <paul.burt@me.com>
2011-11-01 08:35:09 +0000

    Added domain_name to context_processors.py and updated email templates to use domain_name instead of site.

--- akvo/rsr/context_processors.py
+++ akvo/rsr/context_processors.py
@@ -1,9 +1,13 @@
 import django
+from django.conf import settings
 from django.contrib.sites.models import Site
 
 def extra_context(request):
+    domain_name = settings.DOMAIN_NAME
     current_site = Site.objects.get_current()
     django_version = django.get_version()
-    template_context = dict(current_site=current_site,
+    template_context = dict(
+        domain_name=domain_name,
+        current_site=current_site,
         django_version=django_version)
     return template_context
