321e1440364c3c9cf7f7316f0d8a733431c293af
Oliver Galloway-Lunn <olivergl@akvo.org>
2011-11-07 08:02:55 +0000

    [#15222535] Added fab.database.mysql.command.DatabaseCopier class and removed the SchemaInformation and DatabaseConnection classes as there are no longer needed.

--- scripts/deployment/fab/database/mysql/command.py
+++ scripts/deployment/fab/database/mysql/command.py
@@ -5,19 +5,35 @@
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
 
-class SQLStatementExecutor(object):
+class CommandExecutor(object):
 
     def __init__(self, database_config, host_controller):
         self.admin_credentials = "--user='%s' --password='%s'" % (database_config.admin_user, database_config.admin_password)
         self.host_controller = host_controller
         self.feedback = host_controller.feedback
 
+    def _execute(self, command_with_credentials_and_parameters):
+        with self.host_controller.hide_command(): # so that we don't expose passwords in any logged output
+            self.host_controller.run(command_with_credentials_and_parameters)
+
+    def _command_with_credentials(self, command, parameters):
+        return "%s %s %s" % (command, self.admin_credentials, parameters)
+
+
+class SQLStatementExecutor(CommandExecutor):
+
     def execute(self, statement_list):
-        statement_sequence = self._create_statement_sequence(statement_list)
+        statement_sequence = "; ".join(statement_list)
 
         self.feedback.comment("Executing SQL: %s" % statement_sequence)
-        with self.host_controller.hide_command(): # so that we don't expose passwords in any logged output
-            self.host_controller.run('mysql %s -e "%s"' % (self.admin_credentials, statement_sequence))
+        self._execute(self._command_with_credentials('mysql', '-e "%s"' % statement_sequence))
 
-    def _create_statement_sequence(self, statement_list):
-        return "; ".join(statement_list)
+
+class DatabaseCopier(CommandExecutor):
+
+    def create_duplicate(self, original_database_name, duplicate_database_name):
+        dump_original_database = self._command_with_credentials("mysqldump", original_database_name)
+        import_into_new_database = self._command_with_credentials("mysql", duplicate_database_name)
+
+        self.feedback.comment("Copying database [%s] to [%s]" % (original_database_name, duplicate_database_name))
+        self._execute("%s | %s" % (dump_original_database, import_into_new_database))
