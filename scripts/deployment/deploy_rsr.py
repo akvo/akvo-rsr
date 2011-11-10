acb07e29d96896cee9b3bd70bf698dab9b51e8b8
Oliver Galloway-Lunn <olivergl@akvo.org>
2011-11-10 15:29:38 +0000

    [#15222535] Updated the main deploy_rsr.py script so that we can use a separate account for retrieving data from the data host rather than using the same credentials as for the deployment host.

--- scripts/deployment/deploy_rsr.py
+++ scripts/deployment/deploy_rsr.py
@@ -22,7 +22,8 @@ exit_if_config_file_is_missing("fab/config/values.py")
 print "\r"
 
 
-from deploy_rsr_config import LIVE_DATABASE_HOST, DEPLOYMENT_HOST, USERNAME, PASSWORD
+from deploy_rsr_config import DEPLOYMENT_HOST, DEPLOYMENT_HOST_USERNAME, DEPLOYMENT_HOST_PASSWORD
+from deploy_rsr_config import DATA_HOST, DATA_HOST_USERNAME, DATA_HOST_PASSWORD
 
 
 FABRIC_SCRIPTS_HOME = os.path.realpath(os.path.join(os.path.dirname(__file__), 'fab'))
@@ -35,31 +36,37 @@ def attempting_to_deploy_to_live_server():
     return deployment_host_contains("www.akvo.org") or (deployment_host_contains("akvo.org") and not deployment_host_contains(".akvo.org"))
 
 def verify_configuration():
-    if LIVE_DATABASE_HOST == DEPLOYMENT_HOST:
+    if DATA_HOST == DEPLOYMENT_HOST:
         print ">> Cannot deploy to the same host as the database host: %s" % DEPLOYMENT_HOST
         sys.exit(1)
     elif attempting_to_deploy_to_live_server():
         print ">> Deployment to live server not yet supported: %s" % DEPLOYMENT_HOST
         sys.exit(1)
     else:
-        print ">> Fetching data from: %s" % LIVE_DATABASE_HOST
+        print ">> Fetching data from: %s" % DATA_HOST
         print ">> Deploying RSR to:   %s\n" % DEPLOYMENT_HOST
 
-def run_fab_task(fully_qualified_task, host):
-    exit_code = subprocess.call(["fab", "-f", "fabfile.py", fully_qualified_task, "-H", host, "-u", USERNAME, "-p", PASSWORD])
+def run_fab_task(fully_qualified_task, host, username, password):
+    exit_code = subprocess.call(["fab", "-f", "fabfile.py", fully_qualified_task, "-H", host, "-u", username, "-p", password])
 
     if exit_code > 0:
         print "\n>> Deployment failed due to errors above.\n"
         sys.exit(1)
 
+def run_deployment_task(fully_qualified_task):
+    run_fab_task(fully_qualified_task, DEPLOYMENT_HOST, DEPLOYMENT_HOST_USERNAME, DEPLOYMENT_HOST_PASSWORD)
+
+def run_data_retrieval_task(fully_qualified_task):
+    run_fab_task(fully_qualified_task, DATA_HOST, DATA_HOST_USERNAME, DATA_HOST_PASSWORD)
+
 def deploy_rsr():
     os.chdir(FABRIC_SCRIPTS_HOME)
-    run_fab_task("fab.tasks.environment.linux.systempackages.verify_system_packages", DEPLOYMENT_HOST)
-    run_fab_task("fab.tasks.environment.python.systempackages.update_system_python_packages", DEPLOYMENT_HOST)
-    run_fab_task("fab.tasks.app.deployment.deploy_rsr_app:host_controller_mode=remote", DEPLOYMENT_HOST)
-    run_fab_task("fab.tasks.environment.python.virtualenv.rsr.rebuild_rsr_env:host_controller_mode=remote", DEPLOYMENT_HOST)
-    run_fab_task("fab.tasks.data.retrieval.fetch_rsr_data", LIVE_DATABASE_HOST)
-    run_fab_task("fab.tasks.database.backup.backup_rsr_database:host_controller_mode=remote", DEPLOYMENT_HOST)
+    run_deployment_task("fab.tasks.environment.linux.systempackages.verify_system_packages")
+    run_deployment_task("fab.tasks.environment.python.systempackages.update_system_python_packages")
+    run_deployment_task("fab.tasks.app.deployment.deploy_rsr_app:host_controller_mode=remote")
+    run_deployment_task("fab.tasks.environment.python.virtualenv.rsr.rebuild_rsr_env:host_controller_mode=remote")
+    run_data_retrieval_task("fab.tasks.data.retrieval.fetch_rsr_data")
+    run_deployment_task("fab.tasks.database.backup.backup_rsr_database:host_controller_mode=remote")
 
 
 if __name__ == "__main__":
