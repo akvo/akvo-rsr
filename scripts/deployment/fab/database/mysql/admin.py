f0a29b3b50d8ec8e812a755107d6ee9ab92cdd24
Oliver Galloway-Lunn <olivergl@akvo.org>
2011-11-10 22:40:27 +0000

    [#15222535] Reworked DatabaseAdmin to use the new DatabaseAdminCommand class and added a rebuild_database() method.

--- scripts/deployment/fab/database/mysql/admin.py
+++ scripts/deployment/fab/database/mysql/admin.py
@@ -6,55 +6,42 @@
 
 
 from fab.config.rsr.database import RSRDatabaseConfig
-from fab.database.mysql.command import DatabaseCopier, MySQLResponseData, SQLStatementExecutor
+from fab.database.mysql.admincommand import DatabaseAdminCommand
+from fab.database.mysql.command import DatabaseCopier, SQLStatementExecutor
 from fab.format.timestamp import TimeStampFormatter
 
 
 class DatabaseAdmin(object):
 
-    def __init__(self, statement_executor, database_copier, time_stamp_formatter, feedback):
-        self.statement_executor = statement_executor
+    def __init__(self, admin_command, database_copier, time_stamp_formatter, feedback):
+        self.admin_command = admin_command
         self.database_copier = database_copier
         self.time_stamp_formatter = time_stamp_formatter
         self.feedback = feedback
 
     @staticmethod
     def create_instance(database_config, host_controller):
-        return DatabaseAdmin(SQLStatementExecutor(database_config, host_controller),
+        return DatabaseAdmin(DatabaseAdminCommand.create_instance(database_config, host_controller),
                              DatabaseCopier(database_config, host_controller),
                              TimeStampFormatter(),
                              host_controller.feedback)
 
-    def database_exists(self, database_name):
-        database_search = self.statement_executor.execute_without_output(["SHOW DATABASES LIKE '%s'" % database_name])
-        if database_search.contains(database_name):
-            self.feedback.comment("Found database '%s'" % database_name)
-        else:
-            self.feedback.comment("Database '%s' does not exist" % database_name)
-
-        return database_search.contains(database_name)
-
-    def database_user_exists(self, user_name):
-        sql_to_find_user = ["USE mysql", "SELECT User FROM user WHERE User = '%s'" % user_name]
-        user_search = self.statement_executor.execute_without_output(sql_to_find_user)
-
-        if user_search.contains(user_name):
-            self.feedback.comment("Found user '%s'" % user_name)
-        else:
-            self.feedback.comment("User '%s' does not exist" % user_name)
-
-        return user_search.contains(user_name)
-
     def create_timestamped_backup_database(self, database_name):
-        if self.database_exists(database_name):
+        if self.admin_command.database_exists(database_name):
             duplicate_database_name = self.time_stamp_formatter.append_timestamp(database_name)
-            self.create_empty_database(duplicate_database_name)
+            self.admin_command.create_empty_database(duplicate_database_name)
             self.database_copier.create_duplicate(database_name, duplicate_database_name)
         else:
             self.feedback.comment("No backup required for database: %s" % database_name)
 
-    def create_empty_database(self, database_name):
-        self.statement_executor.execute(["CREATE DATABASE %s DEFAULT CHARACTER SET UTF8" % database_name])
+    def rebuild_database(self, database_name, user_name, user_password):
+        self.feedback.comment("Rebuild database '%s' and granting all permissions to user '%s'" % (database_name, user_name))
+        if self.admin_command.database_exists(database_name):
+            self.admin_command.drop_database(database_name)
 
-    def grant_all_database_permissions_for_user(self, database_user, database_name):
-        self.statement_executor.execute(["GRANT ALL ON %s.* TO %s@localhost" % (database_name, database_user)])
+        self.admin_command.create_empty_database(database_name)
+
+        if not self.admin_command.user_exists(user_name):
+            self.admin_command.create_user_account(user_name, user_password)
+
+        self.admin_command.grant_all_database_permissions_for_user(user_name, database_name)
