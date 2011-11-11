972687a590f1ca5ec9141897bfd6ff69619177f2
Oliver Galloway-Lunn <olivergl@akvo.org>
2011-11-11 22:41:10 +0000

    [#15222535] Added fab.app.command.DjangoManageCommand class for calling various manage.py commands within a given Django app environment.

---
+++ scripts/deployment/fab/app/command.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Akvo RSR is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+
+from fab.config.rsr.codebase import RSRCodebaseConfig
+
+
+class DjangoManageCommand(object):
+
+    CREATE_SUPERUSERS           = "no"
+    DELETE_STALE_CONTENT_TYPES  = "yes"
+
+    SYNCDB = "python %s syncdb" % RSRCodebaseConfig.MANAGE_SCRIPT_PATH
+    SYNCDB_WITHOUT_CREATING_SUPERUSERS = "echo %s | %s" % (CREATE_SUPERUSERS, SYNCDB)
+    SYNCDB_WITH_STALE_CONTENT_TYPE_DELETION = "echo %s | %s" % (DELETE_STALE_CONTENT_TYPES, SYNCDB)
