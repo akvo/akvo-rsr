e4606877f2630d3b27fec6ba3f73809ad1b75344
Oliver Galloway-Lunn <olivergl@akvo.org>
2011-11-14 16:07:38 +0000

    [#15222535] Added fab.app.command.DBDumpCommand for calling db_dump.py script commands.

--- scripts/deployment/fab/app/command.py
+++ scripts/deployment/fab/app/command.py
@@ -16,3 +16,24 @@ class DjangoManageCommand(object):
     SYNCDB = "python %s syncdb" % RSRCodebaseConfig.MANAGE_SCRIPT_PATH
     SYNCDB_WITHOUT_CREATING_SUPERUSERS = "echo %s | %s" % (CREATE_SUPERUSERS, SYNCDB)
     SYNCDB_WITH_STALE_CONTENT_TYPE_DELETION = "echo %s | %s" % (DELETE_STALE_CONTENT_TYPES, SYNCDB)
+
+
+class DBDumpAction(object):
+
+    DUMP_DATA   = "dump"
+    LOAD_DATA   = "load"
+
+
+class DBDumpCommand(object):
+
+    @staticmethod
+    def dump_to(data_archive_dir):
+        return DBDumpCommand.with_action(DBDumpAction.DUMP_DATA, data_archive_dir)
+
+    @staticmethod
+    def load_from(data_archive_dir):
+        return DBDumpCommand.with_action(DBDumpAction.LOAD_DATA, data_archive_dir)
+
+    @staticmethod
+    def with_action(action, data_archive_dir):
+        return "python %s -d %s %s" % (RSRCodebaseConfig.DB_DUMP_SCRIPT_PATH, data_archive_dir, action)
