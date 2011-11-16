472721e08b2099e8c46a9e8dd74fd55496872f40
Paul Burt <paul.burt@me.com>
2011-11-16 15:21:17 +0000

    Re-instated django.contrib.sites in INSTALLED_APPS. Added configure_sites.py script.

---
+++ akvo/configure_sites.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.core.management import setup_environ
+import settings
+setup_environ(settings)
+
+from django.contrib.sites.models import Site
+
+
+def main():
+    sites = ((1, 'www'), (2, 'test'), (3, 'test2'),
+             (4, 'gabriel'), (5, 'daniel'), (6, 'paul'),
+             (7, 'demo'), (8, 'uat'))
+    for id, hostname in SITES:
+        domain = '%s.akvo.org' % hostname
+        site = Site(id=id, domain=domain, name=domain)
+        site.save()
+        print 'Created site object for %d.' % domain
+
+
+if __name__ == '__main__':
+    main()
