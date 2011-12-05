03972838a4243e37590826a174af6b0ffd45baae
Oliver Galloway-Lunn <olivergl@akvo.org>
2011-12-05 12:16:34 +0000

    [#15222535] Updated RSRDataRetrieverConfig to use the revised config values.  Also updated paths based on the new deployment path structure and symlinks.

--- scripts/deployment/fab/tests/config/rsr/data_retriever_config_test.py
+++ scripts/deployment/fab/tests/config/rsr/data_retriever_config_test.py
@@ -15,77 +15,61 @@ from fab.format.timestamp import TimeStampFormatter
 CONFIG_VALUES_TEMPLATE_PATH = os.path.realpath(os.path.join(os.path.dirname(__file__), '../../../config/values.py.template'))
 imp.load_source('config_values', CONFIG_VALUES_TEMPLATE_PATH)
 
-from config_values import DataHostConfigValues
+from config_values import DataHostConfigValues, DeploymentHostConfigValues
 
 
 class RSRDataRetrieverConfigTest(mox.MoxTestBase):
 
     def setUp(self):
         super(RSRDataRetrieverConfigTest, self).setUp()
+        self.deployment_host_config_values = DeploymentHostConfigValues()
         self.data_host_config_values = DataHostConfigValues()
         self.mock_time_stamp_formatter = self.mox.CreateMock(TimeStampFormatter)
 
-        self.expected_virtualenvs_home = self.data_host_config_values.virtualenvs_home
-        self.expected_rsr_env_name = "rsr_%s" % self.data_host_config_values.deployed_rsr_version
-        self.expected_rsr_app_path = os.path.join(self.data_host_config_values.django_apps_home,
-                                                  self.data_host_config_values.deployed_rsr_dir_name, 'akvo')
+        self.expected_data_dumps_home = os.path.join(self.deployment_host_config_values.deployment_processing_home, 'data_dumps')
 
-        self.data_retriever_config = RSRDataRetrieverConfig(self.data_host_config_values, self.mock_time_stamp_formatter)
+        self.data_retriever_config = RSRDataRetrieverConfig(self.deployment_host_config_values,
+                                                            self.data_host_config_values,
+                                                            self.mock_time_stamp_formatter)
 
     def test_can_create_instance(self):
         """fab.tests.config.rsr.data_retriever_config_test  Can create RSRDataRetrieverConfig instance"""
 
         self.assertIsInstance(RSRDataRetrieverConfig.create_instance(), RSRDataRetrieverConfig)
 
-    def test_has_virtualenvs_home(self):
-        """fab.tests.config.rsr.data_retriever_config_test  Has virtualenvs home"""
-
-        self.assertEqual(self.expected_virtualenvs_home, self.data_retriever_config.virtualenvs_home)
-
     def test_has_data_dumps_home(self):
         """fab.tests.config.rsr.data_retriever_config_test  Has data dumps home"""
 
-        self.assertEqual(self.data_host_config_values.data_dumps_home, self.data_retriever_config.data_dumps_home)
-
-    def test_has_rsr_virtualenv_name(self):
-        """fab.tests.config.rsr.data_retriever_config_test  Has RSR virtualenv name"""
-
-        self.assertEqual(self.expected_rsr_env_name, self.data_retriever_config.rsr_env_name)
+        self.assertEqual(self.expected_data_dumps_home, self.data_retriever_config.data_dumps_home)
 
     def test_has_rsr_virtualenv_path(self):
         """fab.tests.config.rsr.data_retriever_config_test  Has RSR virtualenv path"""
 
-        expected_rsr_env_path = os.path.join(self.expected_virtualenvs_home, self.expected_rsr_env_name)
+        expected_rsr_env_path = os.path.join(self.data_host_config_values.virtualenvs_home, 'current')
 
         self.assertEqual(expected_rsr_env_path, self.data_retriever_config.rsr_env_path)
 
     def test_has_rsr_app_path(self):
         """fab.tests.config.rsr.data_retriever_config_test  Has RSR app path"""
 
-        self.assertEqual(self.expected_rsr_app_path, self.data_retriever_config.rsr_app_path)
+        expected_rsr_app_path = os.path.join(self.data_host_config_values.django_apps_home, 'current')
 
-    def test_has_db_dump_script_path(self):
-        """fab.tests.config.rsr.data_retriever_config_test  Has db_dump script path"""
-
-        expected_db_dump_script_path = os.path.join(self.expected_rsr_app_path, "db_dump.py")
-
-        self.assertEqual(expected_db_dump_script_path, self.data_retriever_config.db_dump_script_path)
+        self.assertEqual(expected_rsr_app_path, self.data_retriever_config.rsr_app_path)
 
     def test_has_rsr_log_file_path(self):
         """fab.tests.config.rsr.data_retriever_config_test  Has RSR log file path"""
 
-        expected_log_file_path = os.path.join(self.data_host_config_values.rsr_logs_home, "akvo.log")
+        expected_log_file_path = os.path.join(self.deployment_host_config_values.logging_home, "akvo.log")
 
         self.assertEqual(expected_log_file_path, self.data_retriever_config.rsr_log_file_path)
 
     def test_can_get_time_stamped_rsr_data_dump_path(self):
         """fab.tests.config.rsr.data_retriever_config_test  Has RSR log file path"""
 
-        data_dump_file_time_stamp = "20110923_153244"
-        rsr_data_dir_name = "%s_%s" % (self.expected_rsr_env_name, data_dump_file_time_stamp)
-        expected_data_dump_file_path = os.path.join(self.data_host_config_values.data_dumps_home, rsr_data_dir_name)
+        rsr_data_dir_with_timestamp = "rsrdb_20110923_153244"
+        expected_data_dump_file_path = os.path.join(self.expected_data_dumps_home, rsr_data_dir_with_timestamp)
 
-        self.mock_time_stamp_formatter.append_timestamp(self.expected_rsr_env_name).AndReturn(rsr_data_dir_name)
+        self.mock_time_stamp_formatter.append_timestamp("rsrdb").AndReturn(rsr_data_dir_with_timestamp)
         self.mox.ReplayAll()
 
         self.assertEqual(expected_data_dump_file_path, self.data_retriever_config.time_stamped_rsr_data_dump_path())
