05fd37af21f6f355a9af51aace43105ee7501483
Oliver Galloway-Lunn <olivergl@akvo.org>
2012-01-09 13:17:42 +0000

    [#21917553] Added missing self method parameters now that the SysPathVerifier.ensure_*() methods are no longer static.

--- scripts/deployment/verifiers/syspath.py
+++ scripts/deployment/verifiers/syspath.py
@@ -14,13 +14,13 @@ TEST_EXECUTION_HELPERS_HOME = os.path.realpath(os.path.join(DEPLOYMENT_SCRIPTS_H
 
 class SysPathVerifier(object):
 
-    def ensure_syspath_contains_deployment_scripts_home():
-        SysPath.ensure_syspath_contains(DEPLOYMENT_SCRIPTS_HOME)
+    def ensure_syspath_contains_deployment_scripts_home(self):
+        self.ensure_syspath_contains(DEPLOYMENT_SCRIPTS_HOME)
 
-    def ensure_syspath_contains_testing_helpers_home():
-        SysPath.ensure_syspath_contains(TEST_EXECUTION_HELPERS_HOME)
+    def ensure_syspath_contains_testing_helpers_home(self):
+        self.ensure_syspath_contains(TEST_EXECUTION_HELPERS_HOME)
 
-    def ensure_syspath_contains(expected_path):
+    def ensure_syspath_contains(self, expected_path):
         full_path = os.path.realpath(expected_path)
         if full_path not in sys.path:
             sys.path.insert(0, full_path)
