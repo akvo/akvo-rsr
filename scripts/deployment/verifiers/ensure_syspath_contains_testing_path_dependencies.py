9d7be7b87a52c19e0e4de4f513df6546b899af9c
Oliver Galloway-Lunn <olivergl@akvo.org>
2012-01-09 12:45:53 +0000

    [#21917553] Removed the obsolete verifiers.SysPathVerifier and renamed the verifiers.SysPath class to SysPathVerifier.  Also added convenience methods for ensuring the deployment scripts home and the testing execution helpers are on the syspath, which are used in the verifiers/ensure_* scripts.

--- scripts/deployment/verifiers/ensure_syspath_contains_testing_path_dependencies.py
+++ scripts/deployment/verifiers/ensure_syspath_contains_testing_path_dependencies.py
@@ -7,14 +7,9 @@
 
 import imp, os
 
-SCRIPT_DIR = os.path.dirname(__file__)
+imp.load_source("syspath_verifiers", os.path.join(os.path.dirname(__file__), 'syspath.py'))
+from syspath_verifiers import SysPathVerifier
 
-imp.load_source("syspath_verifiers", os.path.join(SCRIPT_DIR, 'syspath.py'))
 
-from syspath_verifiers import SysPath
-
-DEPLOYMENT_SCRIPTS_HOME = os.path.realpath(os.path.join(SCRIPT_DIR, '..'))
-TEST_EXECUTION_HELPERS_HOME = os.path.realpath(os.path.join(SCRIPT_DIR, '../../../tests/shared'))
-
-SysPath.ensure_syspath_contains(DEPLOYMENT_SCRIPTS_HOME)
-SysPath.ensure_syspath_contains(TEST_EXECUTION_HELPERS_HOME)
+SysPathVerifier().ensure_syspath_contains_deployment_scripts_home()
+SysPathVerifier().ensure_syspath_contains_testing_helpers_home()
--- scripts/deployment/verifiers/syspath.py
+++ scripts/deployment/verifiers/syspath.py
@@ -9,32 +9,18 @@ import os, sys
 
 
 DEPLOYMENT_SCRIPTS_HOME = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
-
-
-class SysPath(object):
-
-    @staticmethod
-    def ensure_syspath_contains(expected_path):
-        full_path = os.path.realpath(expected_path)
-        if full_path not in sys.path:
-            sys.path.insert(0, full_path)
+TEST_EXECUTION_HELPERS_HOME = os.path.realpath(os.path.join(DEPLOYMENT_SCRIPTS_HOME, '../../tests/shared'))
 
 
 class SysPathVerifier(object):
 
-    def exit_if_deployment_scripts_home_not_on_syspath(self):
-        self.exit_if_dir_not_on_syspath(DEPLOYMENT_SCRIPTS_HOME)
+    def ensure_syspath_contains_deployment_scripts_home():
+        SysPath.ensure_syspath_contains(DEPLOYMENT_SCRIPTS_HOME)
 
-    def exit_if_dir_not_on_syspath(self, expected_dir):
-        if expected_dir not in sys.path:
-            self.display_python_path_instructions_and_exit()
-        else:
-            try:
-                import verifiers.syspath
-            except ImportError:
-                self.display_python_path_instructions_and_exit()
+    def ensure_syspath_contains_testing_helpers_home():
+        SysPath.ensure_syspath_contains(TEST_EXECUTION_HELPERS_HOME)
 
-    def display_python_path_instructions_and_exit(self):
-        print ">> The [%s] directory should be on the system path" % DEPLOYMENT_SCRIPTS_HOME
-        print ">> Add the missing path to your PYTHONPATH environment variable to run this script"
-        sys.exit(1)
+    def ensure_syspath_contains(expected_path):
+        full_path = os.path.realpath(expected_path)
+        if full_path not in sys.path:
+            sys.path.insert(0, full_path)
