a9cb1cd9993ef99a8b71e27ab499143f9905bd0a
Paul Burt <paul.burt@me.com>
2012-01-09 11:57:30 +0000

    Improved robustness of PartnerSitesRouterMiddleware. Added PARTNER_SITES_* settings.

--- akvo/rsr/middleware.py
+++ akvo/rsr/middleware.py
@@ -20,36 +20,42 @@ from akvo.rsr.models import PartnerSite
 SITE_ID = settings.__class__.SITE_ID = make_tls_property()
 
 
+PARTNER_SITES_DEVELOPMENT_DOMAIN = getattr(settings, 'PARTNER_SITES_DEVELOPMENT_DOMAIN', 'akvoapp.dev')
+PARTNER_SITES_DOMAINS = getattr(settings, 'PARTNER_SITES_DOMAINS',
+        'akvoapp.org', 'akvotest.org', 'akvotest2.org', PARTNER_SITES_DEVELOPMENT_DOMAIN)
+PARTNER_SITES_MARKETING_SITE = getattr(settings, 'PARTNER_SITES_MARKETING_SITE', 'http://www.akvoapp.org/')
+
+
+def is_partner_site(domain):
+    domain_parts = domain.split('.')
+    if len(domain_parts) >= 3:
+        domain_name = domain_parts[-2:]
+        if domain_name in PARTNER_SITES_DOMAINS:
+            return True
+    return False
+
+
 class PartnerSitesRouterMiddleware(object):
     def process_request(self, request, partner_site=None):
         host = request.get_host().split(':')
         domain = host[0]
-        default_dev_domain = getattr(settings, 'PARTNER_SITES_DEVELOPMENT_DOMAIN', 'akvoapp.dev')
-        dev_domains = ('localhost', '127.0.0.1', 'akvo.dev', default_dev_domain)
-        partner_site_domains = getattr(settings, 'PARTNER_SITES_DOMAINS',
-            ('akvoapp.org', 'akvotest.org', 'akvotest2.org', default_dev_domain))
-        marketing_site = getattr(settings, 'PARTNER_SITES_MARKETING_SITE', 'http://www.akvoapp.org/')
+        dev_domains = ('localhost', '127.0.0.1', PARTNER_SITES_DEVELOPMENT_DOMAIN)
         if domain.endswith('akvo.org') or domain in dev_domains:  # Regular RSR instance
-            site = Site.objects.get(id=2) if domain in dev_domains else Site.objects.get(id=1)
+            site = Site.objects.get_or_create(domain=domain, name=domain)
             request.urlconf = 'akvo.urls.rsr'
-        elif domain == default_dev_domain:
-            return redirect(marketing_site)
-        elif (domain.endswith('.akvoapp.org') or
-              domain.endswith('.akvotest.org') or
-              domain.endswith('.akvotest2.org') or
-              domain.endswith('.akvoapp.dev')):  # Partner site instance
-            domain_parts = len(domain.split('.'))
-            if domain_parts >= 3:
-                hostname = domain_parts[-3]
-                try:
-                    partner_site = PartnerSite.objects.get(hostname=hostname)
-                    if partner_site is not None:
-                        site = Site.objects.get_or_create(domain=hostname, name=hostname)
-                except:
-                    pass
+        elif domain == PARTNER_SITES_DEVELOPMENT_DOMAIN:
+            return redirect(PARTNER_SITES_MARKETING_SITE)
+        elif is_partner_site(domain):  # Partner site instance
+            hostname = domain.split('.')[-3]
+            try:
+                partner_site = PartnerSite.objects.get(hostname=hostname)
+                if partner_site is not None:
+                    site = Site.objects.get_or_create(domain=hostname, name=hostname)
+            except:
+                pass
             if partner_site is None or not partner_site.enabled:
-                return redirect(marketing_site)
-        else:  # Partner site instance on partner-nominated domain (probably)
+                return redirect(PARTNER_SITES_MARKETING_SITE)
+        else:  # Partner site instance on partner-nominated domain
             try:
                 partner_site = PartnerSite.objects.get(cname=domain)
                 if partner_site is not None:
