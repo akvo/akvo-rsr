42eed800578f27f2f33ac1607e66bea3299fdc03
Oliver Galloway-Lunn <olivergl@akvo.org>
2012-01-16 10:07:30 +0000

    [#21406319] Updated the RSRDataPopulatorConfig to use the new config values classes.

--- scripts/deployment/fab/config/rsr/data/populator.py
+++ scripts/deployment/fab/config/rsr/data/populator.py
@@ -9,21 +9,22 @@ import os
 
 from fab.config.rsr.codebase import RSRCodebaseConfig
 from fab.config.rsr.deployment import RSRDeploymentConfig
-from fab.config.values import DeploymentHostConfigValues
 
 
 class RSRDataPopulatorConfig(object):
 
-    def __init__(self, deployment_config, deployment_host_config_values, codebase_config):
-        self.data_archives_home     = os.path.join(deployment_host_config_values.deployment_processing_home, 'data_archives')
+    def __init__(self, deployment_config, deployment_host_paths, codebase_config):
+        self.data_archives_home     = os.path.join(deployment_host_paths.deployment_processing_home, 'data_archives')
         self.rsr_deployment_home    = deployment_config.rsr_deployment_home
 
         rsr_env_name        = "rsr_%s" % codebase_config.repo_branch_without_type
-        self.rsr_env_path   = os.path.join(deployment_host_config_values.virtualenvs_home, rsr_env_name)
+        self.rsr_env_path   = os.path.join(deployment_host_paths.virtualenvs_home, rsr_env_name)
 
         self.django_apps_to_migrate = ["oembed", "ipn"]
         self.rsr_app_name           = RSRCodebaseConfig.RSR_APP_NAME
 
     @staticmethod
-    def create_instance():
-        return RSRDataPopulatorConfig(RSRDeploymentConfig.create_instance(), DeploymentHostConfigValues(), RSRCodebaseConfig.create_instance())
+    def create_with(deployment_host_config):
+        return RSRDataPopulatorConfig(RSRDeploymentConfig.create_with(deployment_host_config),
+                                      deployment_host_config.host_paths,
+                                      RSRCodebaseConfig(deployment_host_config.repository_branch))
