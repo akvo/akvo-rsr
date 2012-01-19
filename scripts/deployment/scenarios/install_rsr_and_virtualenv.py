0b8ce75ed74c0f3bb100b87840355e8d347e5358
Oliver Galloway-Lunn <olivergl@akvo.org>
2012-01-19 23:09:09 +0000

    [#23361761] Added deployment step wrapper scripts to install_rsr_codebase and rebuild_rsr_virtualenv, along with a scenario script to install_rsr_and_virtualenv.

---
+++ scripts/deployment/scenarios/install_rsr_and_virtualenv.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# Akvo RSR is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+
+import imp, os
+
+VERIFIERS_HOME = os.path.realpath(os.path.join(os.path.dirname(__file__), '../verifiers'))
+imp.load_source("syspath_verification", os.path.join(VERIFIERS_HOME, 'ensure_syspath_contains_deployment_scripts_home.py'))
+
+from scenarios.runner import ScenarioRunner
+
+
+def install_rsr_and_virtualenv(scenario_runner):
+    scenario_runner.run_step('install_rsr_codebase')
+    scenario_runner.run_step('rebuild_rsr_virtualenv')
+
+
+if __name__ == '__main__':
+    install_rsr_and_virtualenv(ScenarioRunner())
