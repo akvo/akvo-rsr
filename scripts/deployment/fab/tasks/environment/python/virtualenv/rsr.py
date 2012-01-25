dce7daeaad2e9082ab1cff1aae7899de3591bb2e
Oliver Galloway-Lunn <olivergl@akvo.org>
2012-01-25 17:48:17 +0000

    [#23361761] Updated the RebuildRSREnv task to inherit from the BaseDeploymentTask and refactored the task's run() method to use the new deployment config loading process.

--- scripts/deployment/fab/tasks/environment/python/virtualenv/rsr.py
+++ scripts/deployment/fab/tasks/environment/python/virtualenv/rsr.py
@@ -5,42 +5,33 @@
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
 
-import fabric.api
-import fabric.tasks
-
-import fab.config.loaders
 import fab.config.rsr.virtualenv
 import fab.host.virtualenv
+import fab.tasks.base
 
 
-class RebuildRSREnv(fabric.tasks.Task):
+class RebuildRSREnv(fab.tasks.base.BaseDeploymentTask):
     """Rebuilds an RSR virtualenv with the specified pip requirements"""
 
-    name = "rebuild_rsr_env"
+    name = 'rebuild_rsr_env'
 
-    def __init__(self, virtualenv_installer_config):
-        self.virtualenv_installer_config = virtualenv_installer_config
+    def run(self, host_controller_mode, config_type, host_alias=None, repository_branch=None, database_name=None, custom_config_module_path=None):
+        host_config = self.config_loader.host_config_for(config_type, host_alias, repository_branch, database_name, custom_config_module_path)
+        virtualenv_installer_config = fab.config.rsr.virtualenv.RSRVirtualEnvInstallerConfig.create_with(host_config, self.deployment_user)
+        virtualenv_deployment_host = self._configure_host_using(virtualenv_installer_config, host_controller_mode)
 
-    @staticmethod
-    def create_task():
-        return RebuildRSREnv(fab.config.rsr.virtualenv.RSRVirtualEnvInstallerConfig.create_with(fab.config.loaders.DeploymentConfigLoader.load(),
-                                                                                                fabric.api.env.user))
+        virtualenv_deployment_host.ensure_user_has_required_deployment_permissions()
+        virtualenv_deployment_host.ensure_virtualenv_exists()
+        virtualenv_deployment_host.remove_previously_downloaded_package_sources()
+        virtualenv_deployment_host.set_web_group_permissions_and_ownership_on_deployed_virtualenv()
+        virtualenv_deployment_host.install_virtualenv_packages(virtualenv_installer_config.rsr_requirements_path)
+        virtualenv_deployment_host.install_virtualenv_packages(virtualenv_installer_config.testing_requirements_path)
+        virtualenv_deployment_host.set_web_group_permissions_and_ownership_on_deployed_virtualenv()
+        virtualenv_deployment_host.ensure_virtualenv_symlinks_exist()
 
-    def run(self, host_controller_mode):
-        self._configure_host_using(host_controller_mode)
-
-        self.virtualenv_deployment_host.ensure_user_has_required_deployment_permissions()
-        self.virtualenv_deployment_host.ensure_virtualenv_exists()
-        self.virtualenv_deployment_host.remove_previously_downloaded_package_sources()
-        self.virtualenv_deployment_host.set_web_group_permissions_and_ownership_on_deployed_virtualenv()
-        self.virtualenv_deployment_host.install_virtualenv_packages(self.virtualenv_installer_config.rsr_requirements_path)
-        self.virtualenv_deployment_host.install_virtualenv_packages(self.virtualenv_installer_config.testing_requirements_path)
-        self.virtualenv_deployment_host.set_web_group_permissions_and_ownership_on_deployed_virtualenv()
-        self.virtualenv_deployment_host.ensure_virtualenv_symlinks_exist()
-
-    def _configure_host_using(self, host_controller_mode):
+    def _configure_host_using(self, virtualenv_installer_config, host_controller_mode):
         host_controller = fab.host.controller.HostController.create_from(host_controller_mode)
-        self.virtualenv_deployment_host = fab.host.virtualenv.VirtualEnvDeploymentHost.create_with(self.virtualenv_installer_config, host_controller)
+        return fab.host.virtualenv.VirtualEnvDeploymentHost.create_with(virtualenv_installer_config, host_controller)
 
 
-instance = RebuildRSREnv.create_task()
+instance = RebuildRSREnv()
