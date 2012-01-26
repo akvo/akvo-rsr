0cfb6771dbf7b72567518c6cc857f0429891cf77
Oliver Galloway-Lunn <olivergl@akvo.org>
2012-01-26 13:45:29 +0000

    [#23361761] Updated the ConvertRSRDatabaseForMigrations, RebuildRSRDatabase and RunDatabaseMigrations tasks to use the _perform_database_actions_with(database_host) base class method and the new config loading process.

--- scripts/deployment/fab/tests/tasks/database/run_database_migrations_test.py
+++ scripts/deployment/fab/tests/tasks/database/run_database_migrations_test.py
@@ -9,37 +9,37 @@ import mox
 
 from testing.helpers.execution import TestSuiteLoader, TestRunner
 
+from fab.config.loader import ConfigType
+from fab.config.values.host import HostAlias
 from fab.host.controller import HostControllerMode
 from fab.host.database import DatabaseHost
 from fab.tasks.database.migrate import RunDatabaseMigrations
+from fab.verifiers.config import ConfigFileVerifier
 
 
 class StubbedRunDatabaseMigrations(RunDatabaseMigrations):
 
-    def __init__(self, database_host):
-        self.database_host = database_host
+    def __init__(self, config_verifier, configured_database_host):
+        super(StubbedRunDatabaseMigrations, self).__init__(config_verifier)
+        self.configured_database_host = configured_database_host
 
-    def _create_database_host(self, host_controller_mode):
-        return self.database_host
+    def _configure_database_host_with(self, host_controller_mode, host_config):
+        return self.configured_database_host
 
 
 class RunDatabaseMigrationsTest(mox.MoxTestBase):
 
     def setUp(self):
         super(RunDatabaseMigrationsTest, self).setUp()
+        self.mock_config_verifier = self.mox.CreateMock(ConfigFileVerifier)
         self.mock_database_host = self.mox.CreateMock(DatabaseHost)
 
-        self.run_database_migrations_task = StubbedRunDatabaseMigrations(self.mock_database_host)
+        self.run_database_migrations_task = StubbedRunDatabaseMigrations(self.mock_config_verifier, self.mock_database_host)
 
     def test_has_expected_task_name(self):
         """fab.tests.tasks.database.run_database_migrations_test  Has expected task name"""
 
-        self.assertEqual("run_database_migrations", RunDatabaseMigrations.name)
-
-    def test_can_create_task_instance(self):
-        """fab.tests.tasks.database.run_database_migrations_test  Can create task instance"""
-
-        self.assertIsInstance(RunDatabaseMigrations.create_task(), RunDatabaseMigrations)
+        self.assertEqual('run_database_migrations', RunDatabaseMigrations.name)
 
     def test_can_run_database_migrations(self):
         """fab.tests.tasks.database.run_database_migrations_test  Can run database migrations"""
@@ -47,12 +47,12 @@ class RunDatabaseMigrationsTest(mox.MoxTestBase):
         self.mock_database_host.run_all_migrations()
         self.mox.ReplayAll()
 
-        self.run_database_migrations_task.run(HostControllerMode.REMOTE)
+        self.run_database_migrations_task.run(HostControllerMode.REMOTE, ConfigType.PRECONFIGURED, HostAlias.TEST)
 
 
 def suite():
     return TestSuiteLoader().load_tests_from(RunDatabaseMigrationsTest)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     from fab.tests.test_settings import TEST_MODE
     TestRunner(TEST_MODE).run_test_suite(suite())
