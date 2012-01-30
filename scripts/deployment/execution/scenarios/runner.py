17d33d23f18b5035ae5810bb0e2dafbe18e9256d
Oliver Galloway-Lunn <olivergl@akvo.org>
2012-01-30 10:42:15 +0000

    [#23361761] Moved the deployment scenarios to the execution.scenarios module and updated them to use host config specifications.

--- scripts/deployment/execution/scenarios/runner.py
+++ scripts/deployment/execution/scenarios/runner.py
@@ -12,13 +12,13 @@ DEPLOYMENT_STEPS_HOME = os.path.realpath(os.path.join(os.path.dirname(__file__),
 
 class ScenarioRunner(object):
 
-    def run_step(self, step_name):
-        exit_code = self._run_script(os.path.join(DEPLOYMENT_STEPS_HOME, step_name + '.py'))
+    def run_step(self, step_name, host_config_specification):
+        exit_code = self._run_script(os.path.join(DEPLOYMENT_STEPS_HOME, step_name + '.py'), host_config_specification)
 
         self._stop_scenario_execution_if_deployment_step_failed(exit_code)
 
-    def _run_script(self, script_path):
-        return subprocess.call(script_path)
+    def _run_script(self, script_path, host_config_specification):
+        return subprocess.call([script_path, host_config_specification])
 
     def _stop_scenario_execution_if_deployment_step_failed(self, exit_code):
         # we should already see a failure message when a deployment fails
---
+++ scripts/deployment/execution/scenarios/update_host_system.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# Akvo RSR is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+
+import imp, os, sys
+
+VERIFIERS_HOME = os.path.realpath(os.path.join(os.path.dirname(__file__), '../../verifiers'))
+imp.load_source("syspath_verification", os.path.join(VERIFIERS_HOME, 'ensure_syspath_contains_deployment_scripts_home.py'))
+
+from execution.scenarios.runner import ScenarioRunner
+
+import execution.verification
+
+
+def update_host_system(scenario_runner, host_config_specification):
+    scenario_runner.run_step('verify_system_packages', host_config_specification)
+    scenario_runner.run_step('update_system_python_packages', host_config_specification)
+
+
+if __name__ == '__main__':
+    execution.verification.display_usage_and_exit_if_host_config_spec_is_missing(os.path.basename(__file__))
+    update_host_system(ScenarioRunner(), sys.argv[1])
