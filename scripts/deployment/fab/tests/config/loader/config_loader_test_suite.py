204ed0280a1edd8447a55cbd8b32cbe04c61af93
Oliver Galloway-Lunn <olivergl@akvo.org>
2012-01-30 00:09:17 +0000

    [#23361761] Corrected the config loader test suite name.

--- scripts/deployment/fab/tests/config/loader/config_loader_test_suite.py
+++ scripts/deployment/fab/tests/config/loader/config_loader_test_suite.py
@@ -11,9 +11,9 @@ from fab.tests.config.loader.config_type_test import ConfigTypeTest
 from fab.tests.config.loader.deployment_config_loader_test import DeploymentConfigLoaderTest
 
 
-def config_loaders_suite():
+def config_loader_suite():
     return TestSuiteLoader().create_suite_from_classes([ConfigTypeTest, DeploymentConfigLoaderTest])
 
 if __name__ == "__main__":
     from fab.tests.test_settings import TEST_MODE
-    TestRunner(TEST_MODE).run_test_suite(config_loaders_suite())
+    TestRunner(TEST_MODE).run_test_suite(config_loader_suite())
