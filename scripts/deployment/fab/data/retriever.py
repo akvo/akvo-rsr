504e1c26f2db327a1a8becc3944461565760693b
Oliver Galloway-Lunn <olivergl@akvo.org>
2012-02-06 10:47:43 +0000

    [#24410959] Updated the RSRDataRetriever so that we now extract data in XML format with manage.py dumpdata since the db_dump.py script is no longer compatible with the migration-enabled RSR database.

--- scripts/deployment/fab/data/retriever.py
+++ scripts/deployment/fab/data/retriever.py
@@ -7,7 +7,7 @@
 
 import os
 
-from fab.app.admin import DBDump
+from fab.app.admin import DjangoAdmin
 from fab.config.rsr.data.retriever import RSRDataRetrieverConfig
 from fab.config.values.host import DataHostPaths
 from fab.environment.python.virtualenv import VirtualEnv
@@ -17,11 +17,11 @@ from fab.os.filesystem import FileSystem, LocalFileSystem
 
 class RSRDataRetriever(object):
 
-    def __init__(self, data_retriever_config, data_host_file_system, local_file_system, db_dump, feedback, time_stamp_formatter):
+    def __init__(self, data_retriever_config, data_host_file_system, local_file_system, django_admin, feedback, time_stamp_formatter):
         self.config = data_retriever_config
         self.data_host_file_system = data_host_file_system
         self.local_file_system = local_file_system
-        self.db_dump = db_dump
+        self.django_admin = django_admin
         self.feedback = feedback
         self.time_stamp_formatter = time_stamp_formatter
 
@@ -32,7 +32,7 @@ class RSRDataRetriever(object):
         return RSRDataRetriever(data_retriever_config,
                                 FileSystem(host_controller),
                                 LocalFileSystem(),
-                                DBDump(VirtualEnv(data_retriever_config.rsr_env_path, host_controller)),
+                                DjangoAdmin(VirtualEnv(data_retriever_config.rsr_env_path, host_controller)),
                                 host_controller.feedback,
                                 TimeStampFormatter())
 
@@ -40,12 +40,11 @@ class RSRDataRetriever(object):
         self._ensure_required_paths_exist()
         self._ensure_rsr_log_file_is_writable()
 
-        data_archive_dir = self.time_stamp_formatter.append_timestamp("rsrdb")
-        rsr_data_archive_path = os.path.join(self.config.data_archives_home, data_archive_dir)
+        data_fixture_file_name = '%s.xml' % self.time_stamp_formatter.append_timestamp('rsrdb')
+        rsr_data_fixture_path = os.path.join(self.config.data_archives_home, data_fixture_file_name)
 
-        self._create_data_extract(rsr_data_archive_path)
-        self._remove_extraneous_database_files(rsr_data_archive_path)
-        self._compress_and_download_data_archive(rsr_data_archive_path)
+        self._extract_data_to(rsr_data_fixture_path)
+        self._compress_and_download_data_fixture(rsr_data_fixture_path)
 
     def _ensure_required_paths_exist(self):
         self.local_file_system.ensure_directory_exists(self.config.data_archives_home)
@@ -55,18 +54,15 @@ class RSRDataRetriever(object):
         self.data_host_file_system.exit_if_file_does_not_exist(self.config.rsr_log_file_path)
 
     def _ensure_rsr_log_file_is_writable(self):
-        self.feedback.comment("Ensuring RSR log file is writable")
+        self.feedback.comment('Ensuring RSR log file is writable')
         self.data_host_file_system.make_file_writable_for_all_users(self.config.rsr_log_file_path)
 
-    def _create_data_extract(self, data_archive_path):
-        self.feedback.comment("Extracting latest data from database at %s" % self.config.rsr_app_path)
+    def _extract_data_to(self, data_fixture_file_path):
+        self.feedback.comment('Extracting latest data from database at %s' % self.config.rsr_app_path)
         with self.data_host_file_system.cd(self.config.rsr_app_path):
-            self.db_dump.extract_data_to(data_archive_path)
+            self.django_admin.extract_app_data_to(data_fixture_file_path, self.config.rsr_app_name)
 
-    def _remove_extraneous_database_files(self, data_archive_path):
-        self.data_host_file_system.delete_file(os.path.join(data_archive_path, "workflows_workflowpermissionrelation.py"))
-
-    def _compress_and_download_data_archive(self, data_archive_path):
-        self.data_host_file_system.compress_directory(data_archive_path)
-        self.data_host_file_system.delete_directory(data_archive_path)
-        self.data_host_file_system.download_file("%s.zip" % data_archive_path, self.config.data_archives_home)
+    def _compress_and_download_data_fixture(self, data_fixture_file_path):
+        self.data_host_file_system.compress_file(data_fixture_file_path)
+        self.data_host_file_system.delete_file(data_fixture_file_path)
+        self.data_host_file_system.download_file('%s.zip' % data_fixture_file_path, self.config.data_archives_home)
--- scripts/deployment/fab/tests/data/rsr_data_retriever_test.py
+++ scripts/deployment/fab/tests/data/rsr_data_retriever_test.py
@@ -8,9 +8,9 @@
 import fabric.api
 import mox, os
 
-from testing.helpers.execution import TestSuiteLoader, TestRunner
+from testing.helpers.execution import TestRunner, TestSuiteLoader
 
-from fab.app.admin import DBDump
+from fab.app.admin import DjangoAdmin
 from fab.config.rsr.data.retriever import RSRDataRetrieverConfig
 from fab.config.values.host import DataHostPaths
 from fab.data.retriever import RSRDataRetriever
@@ -27,12 +27,12 @@ class RSRDataRetrieverTest(mox.MoxTestBase):
         self.data_retriever_config = RSRDataRetrieverConfig(DataHostPaths())
         self.mock_data_host_file_system = self.mox.CreateMock(FileSystem)
         self.mock_local_file_system = self.mox.CreateMock(LocalFileSystem)
-        self.mock_db_dump = self.mox.CreateMock(DBDump)
+        self.mock_django_admin = self.mox.CreateMock(DjangoAdmin)
         self.mock_feedback = self.mox.CreateMock(ExecutionFeedback)
         self.mock_time_stamp_formatter = self.mox.CreateMock(TimeStampFormatter)
 
         self.data_retriever = RSRDataRetriever(self.data_retriever_config, self.mock_data_host_file_system,
-                                               self.mock_local_file_system, self.mock_db_dump,
+                                               self.mock_local_file_system, self.mock_django_admin,
                                                self.mock_feedback, self.mock_time_stamp_formatter)
 
     def test_can_create_instance_for_local_host(self):
@@ -55,13 +55,14 @@ class RSRDataRetrieverTest(mox.MoxTestBase):
     def test_can_fetch_data_from_database(self):
         """fab.tests.data.rsr_data_retriever_test  Can fetch data from database"""
 
-        rsr_data_archive_path = os.path.join(self.data_retriever_config.data_archives_home, "rsrdb_utc_timestamp")
+        time_stamped_fixture_name = 'rsrdb_utc_timestamp'
+        self.mock_time_stamp_formatter.append_timestamp('rsrdb').AndReturn(time_stamped_fixture_name)
+        rsr_data_fixture_path = os.path.join(self.data_retriever_config.data_archives_home, '%s.xml' % time_stamped_fixture_name)
 
         self._ensure_required_paths_exist()
         self._ensure_rsr_log_file_is_writable()
-        self._extract_latest_data(rsr_data_archive_path)
-        self._remove_extraneous_database_files(rsr_data_archive_path)
-        self._compress_and_download_data_archive(rsr_data_archive_path)
+        self._extract_data_to(rsr_data_fixture_path)
+        self._compress_and_download_data_fixture(rsr_data_fixture_path)
         self.mox.ReplayAll()
 
         self.data_retriever.fetch_data_from_database()
@@ -74,22 +75,18 @@ class RSRDataRetrieverTest(mox.MoxTestBase):
         self.mock_data_host_file_system.exit_if_file_does_not_exist(self.data_retriever_config.rsr_log_file_path)
 
     def _ensure_rsr_log_file_is_writable(self):
-        self.mock_feedback.comment("Ensuring RSR log file is writable")
+        self.mock_feedback.comment('Ensuring RSR log file is writable')
         self.mock_data_host_file_system.make_file_writable_for_all_users(self.data_retriever_config.rsr_log_file_path)
 
-    def _extract_latest_data(self, rsr_data_archive_path):
-        self.mock_time_stamp_formatter.append_timestamp("rsrdb").AndReturn(rsr_data_archive_path)
-        self.mock_feedback.comment("Extracting latest data from database at %s" % self.data_retriever_config.rsr_app_path)
+    def _extract_data_to(self, data_fixture_path):
+        self.mock_feedback.comment('Extracting latest data from database at %s' % self.data_retriever_config.rsr_app_path)
         self._change_dir_to(self.data_retriever_config.rsr_app_path)
-        self.mock_db_dump.extract_data_to(rsr_data_archive_path)
+        self.mock_django_admin.extract_app_data_to(data_fixture_path, self.data_retriever_config.rsr_app_name)
 
-    def _remove_extraneous_database_files(self, rsr_data_archive_path):
-        self.mock_data_host_file_system.delete_file(os.path.join(rsr_data_archive_path, "workflows_workflowpermissionrelation.py"))
-
-    def _compress_and_download_data_archive(self, rsr_data_archive_path):
-        self.mock_data_host_file_system.compress_directory(rsr_data_archive_path)
-        self.mock_data_host_file_system.delete_directory(rsr_data_archive_path)
-        self.mock_data_host_file_system.download_file("%s.zip" % rsr_data_archive_path, self.data_retriever_config.data_archives_home)
+    def _compress_and_download_data_fixture(self, data_fixture_path):
+        self.mock_data_host_file_system.compress_file(data_fixture_path)
+        self.mock_data_host_file_system.delete_file(data_fixture_path)
+        self.mock_data_host_file_system.download_file('%s.zip' % data_fixture_path, self.data_retriever_config.data_archives_home)
 
     def _change_dir_to(self, expected_directory):
         self.mock_data_host_file_system.cd(expected_directory).AndReturn(fabric.api.cd(expected_directory))
@@ -98,6 +95,6 @@ class RSRDataRetrieverTest(mox.MoxTestBase):
 def suite():
     return TestSuiteLoader().load_tests_from(RSRDataRetrieverTest)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     from fab.tests.test_settings import TEST_MODE
     TestRunner(TEST_MODE).run_test_suite(suite())
