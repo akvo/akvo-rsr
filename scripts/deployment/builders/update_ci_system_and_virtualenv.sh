c4704399e1d93cead103357ebd3c107fdd5f961a
Oliver Galloway-Lunn <olivergl@akvo.org>
2012-02-23 16:12:56 +0000

    [#23633289] Moved the source call from the rebuild_all.sh call to the rebuild_rsr_virtualenv.sh call.

--- scripts/deployment/builders/update_ci_system_and_virtualenv.sh
+++ scripts/deployment/builders/update_ci_system_and_virtualenv.sh
@@ -6,4 +6,4 @@ SHARED_SCRIPTS_HOME="$(cd `dirname "$THIS_SCRIPT"` && pwd)/shared"
 
 CI_EXECUTION_MODE='ci' # for the continuous integration server
 
-source "$SHARED_SCRIPTS_HOME/rebuild_all.sh" $CI_EXECUTION_MODE
+"$SHARED_SCRIPTS_HOME/rebuild_all.sh" $CI_EXECUTION_MODE
