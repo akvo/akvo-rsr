37ab5b8a77110f6dbe1ba4930236f0b7195ee379
Paul Burt <paul.burt@me.com>
2012-03-06 17:20:43 +0100

    Fleshed out home_page smoke test feature.

--- features/smoke_tests/home_page.feature
+++ features/smoke_tests/home_page.feature
@@ -7,3 +7,9 @@ Feature: Home page navigation
     Go to home page
     Then I should see the title "Akvo.org - See it happen"
     And I also see a "Projects" link
+
+  Scenario: Home page displays recent project updates
+    Go to home page
+    Then I should see "Most recent project updates"
+    Then I click on the first project update
+    Then I expect to see the page for the selected update
--- features/smoke_tests/home_page_steps.py
+++ features/smoke_tests/home_page_steps.py
@@ -3,7 +3,7 @@
 from lettuce import after, before, step, world
 from splinter.browser import Browser
 
-SITE_UNDER_TEST = 'test2.akvo.org'
+SITE_UNDER_TEST = 'test.akvo.org'
 
 @before.all
 def setUp():
@@ -28,3 +28,17 @@ def and_i_also_see_link(step, link_text):
 @step(u'And I also see a "([^"]*)" link')
 def and_i_also_see_a_projects_link(step, projects_link):
     assert world.browser.find_link_by_text(projects_link)
+
+@step('Then I should see "([^"]*)"')
+def then_i_should_see_text(step, expected_text):
+    assert world.browser.is_text_present(expected_text)
+
+@step('Then I click on the first project update')
+def then_i_click_on_the_first_project_update(step):
+    element = world.browser.find_by_css('#project_update_0').first
+    title = world.project_update_title = element.find_by_tag('h2').first.text
+    element.find_by_css('div div a').first.click()
+
+@step('Then I expect to see the page for the selected update')
+def then_i_expect_to_see_details_for_the_selected_update(step):
+    assert world.browser.is_text_present(world.project_update_title)
