1b70c1a299a7d8f1bcbb346a95a27e249127a39d
Gabriel von Heijne <gabriel@akvo.org>
2012-03-26 17:23:10 +0200

    [#4142254, #27018861]Add terrain.py and move setup and teardown there plus a world global for the site to run the tests against. Add tests for project listing page including test of the paginantion and a focus area listing.

--- features/smoke_tests/home_page_steps.py
+++ features/smoke_tests/home_page_steps.py
@@ -3,19 +3,10 @@
 from lettuce import after, before, step, world
 from splinter.browser import Browser
 
-SITE_UNDER_TEST = 'test.akvo.org'
-
-@before.all
-def setUp():
-    world.browser = Browser('firefox')
-
-@after.all
-def tearDown(test_results):
-    world.browser.quit()
 
 @step('Go to home page')
 def go_to_home_page(step):
-    world.browser.visit('http://%s' % SITE_UNDER_TEST)
+    world.browser.visit('http://%s' % world.SITE_UNDER_TEST)
 
 @step('Then I should see the title "([^"]*)"')
 def then_i_should_see_the_title(step, expected_title):
@@ -24,7 +15,7 @@ def then_i_should_see_the_title(step, expected_title):
 @step('And I also see a "([^"]*)" link')
 def and_i_also_see_link(step, expected_link_text):
     assert world.browser.find_link_by_text(expected_link_text)
-    
+
 @step('And I also see a "([^"]*)" link')
 def and_i_also_see_a_projects_link(step, projects_link):
     assert world.browser.find_link_by_text(projects_link)
@@ -33,6 +24,8 @@ def and_i_also_see_a_projects_link(step, projects_link):
 def then_i_should_see_text(step, expected_text):
     assert world.browser.is_text_present(expected_text)
 
+
+# Scenario: Home page displays recent project updates
 @step('Then I click on the first project update')
 def then_i_click_on_the_first_project_update(step):
     element = world.browser.find_by_css('#project_update_0').first
@@ -42,3 +35,35 @@ def then_i_click_on_the_first_project_update(step):
 @step('Then I expect to see the page for the selected update')
 def then_i_expect_to_see_details_for_the_selected_update(step):
     assert world.browser.is_text_present(world.project_update_title)
+
+@step('Then I expect to see the page for the selected update')
+def then_i_expect_to_see_details_for_the_selected_update(step):
+    assert world.browser.is_text_present(world.project_update_title)
+
+
+# Scenario: Home page displays recent blog posts
+@step(u'Then I click on the first blog post')
+def then_i_click_on_the_first_blog_post(step):
+    blog_block = world.browser.find_by_css('.span-12 .last').last
+    blog_link = blog_block.find_by_css('a').first
+    blog_title = world.blog_post_title = blog_block.find_by_css('h2').first.text
+    blog_link.click()
+
+@step(u'Then I expect to see details for the selected post')
+def then_i_expect_to_see_details_for_the_selected_post(step):
+    assert world.browser.is_text_present(world.blog_post_title)
+
+
+# Scenario: Home page displays accordion with focus areas
+@step(u'Then I click on the last focus area link')
+def then_i_click_on_the_last_focus_area_link(step):
+    accordion_block = world.browser.find_by_id('index-accordion').first
+    accordion_link = accordion_block.find_by_css('a').last
+    accordion_title_anchor = accordion_block.find_by_css('h3').last.find_by_css('a').first
+    accordion_title = world.accordion_title = accordion_title_anchor.text
+    accordion_title_anchor.click()
+    accordion_link.click()
+
+@step(u'Then I expect to see a list of projects for the focus area')
+def then_i_expect_to_see_a_list_of_projects_for_the_focus_area(step):
+    assert world.browser.is_text_present(world.accordion_title)
---
+++ features/terrain.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from lettuce import after, before, step, world
+from splinter.browser import Browser
+
+world.SITE_UNDER_TEST = 'test.akvo.org'
+
+@before.all
+def setUp():
+    world.browser = Browser('firefox')
+
+@after.all
+def tearDown(test_results):
+    world.browser.quit()
+
