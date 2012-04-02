02c9214fa2401a0fbead927dc49e728c54bd6d97
Oliver Galloway-Lunn <olivergl@akvo.org>
2012-04-02 15:10:23 +0100

    [#21406319] Added RSRDeploymentConfig.deployed_database_credentials_file setting.

--- scripts/deployment/fab/config/rsr/deployment.py
+++ scripts/deployment/fab/config/rsr/deployment.py
@@ -12,35 +12,37 @@ from fab.config.rsr.codebase import RSRCodebaseConfig
 
 class RSRDeploymentConfig(object):
 
-    DJANGO_LIB_PATH = "lib/python2.5/site-packages/django"
+    DJANGO_LIB_PATH = 'lib/python2.5/site-packages/django'
 
     def __init__(self, deployment_host_paths, deployment_user, codebase_config):
         self.deployment_user = deployment_user
 
         self.repo_checkout_home = deployment_host_paths.repo_checkout_home
-        self.repo_archives_dir  = os.path.join(self.repo_checkout_home, "archives")
+        self.repo_archives_dir  = os.path.join(self.repo_checkout_home, 'archives')
 
         self.rsr_archive_url            = codebase_config.rsr_archive_url
         self.unpacked_archive_dir_mask  = RSRCodebaseConfig.UNPACKED_RSR_ARCHIVE_DIR_MASK
 
-        self.rsr_deployment_dir_name    = "rsr_%s" % codebase_config.repo_branch_without_type
+        self.rsr_deployment_dir_name    = 'rsr_%s' % codebase_config.repo_branch_without_type
         self.rsr_deployment_home        = os.path.join(self.repo_checkout_home, self.rsr_deployment_dir_name)
         self.rsr_settings_home          = os.path.join(self.rsr_deployment_home, RSRCodebaseConfig.RSR_SETTINGS_HOME)
         self.rsr_media_root             = os.path.join(self.rsr_deployment_home, RSRCodebaseConfig.RSR_MEDIA_ROOT)
-        self.current_rsr_media_root     = os.path.join(self.repo_checkout_home, "current", RSRCodebaseConfig.RSR_MEDIA_ROOT)
+        self.current_rsr_media_root     = os.path.join(self.repo_checkout_home, 'current', RSRCodebaseConfig.RSR_MEDIA_ROOT)
 
-        self.current_virtualenv_path    = os.path.join(deployment_host_paths.virtualenvs_home, "current")
-        self.django_media_admin_path    = os.path.join(self.current_virtualenv_path, self.DJANGO_LIB_PATH, "contrib/admin/media")
-        self.rsr_static_media_home      = os.path.join(deployment_host_paths.static_media_home, "akvo")
-        self.static_media_db_path       = os.path.join(self.rsr_static_media_home, "db")
+        self.current_virtualenv_path    = os.path.join(deployment_host_paths.virtualenvs_home, 'current')
+        self.django_media_admin_path    = os.path.join(self.current_virtualenv_path, self.DJANGO_LIB_PATH, 'contrib/admin/media')
+        self.rsr_static_media_home      = os.path.join(deployment_host_paths.static_media_home, 'akvo')
+        self.static_media_db_path       = os.path.join(self.rsr_static_media_home, 'db')
 
-        self.host_config_home               = deployment_host_paths.config_home
-        self.local_rsr_settings_file_name   = RSRCodebaseConfig.LOCAL_SETTINGS_FILE
-        self.deployed_rsr_settings_file     = os.path.join(self.host_config_home, self.local_rsr_settings_file_name)
-        self.mod_python_file_name           = RSRCodebaseConfig.MOD_PYTHON_FILE
-        self.deployed_mod_python_file       = os.path.join(self.host_config_home, self.mod_python_file_name)
+        self.host_config_home                   = deployment_host_paths.config_home
+        self.deployed_database_credentials_file = os.path.join(self.host_config_home, 'credentials/database.json')
+        self.local_rsr_settings_file_name       = RSRCodebaseConfig.LOCAL_SETTINGS_FILE
+        self.deployed_rsr_settings_file         = os.path.join(self.host_config_home, self.local_rsr_settings_file_name)
+        self.mod_python_file_name               = RSRCodebaseConfig.MOD_PYTHON_FILE
+        self.deployed_mod_python_file           = os.path.join(self.host_config_home, self.mod_python_file_name)
 
     @staticmethod
     def create_with(deployment_host_config, deployment_user=None):
-        return RSRDeploymentConfig(deployment_host_config.host_paths, deployment_user, 
+        return RSRDeploymentConfig(deployment_host_config.host_paths,
+                                   deployment_user,
                                    RSRCodebaseConfig(deployment_host_config.repository_branch))
