7480a51eca1159bf83d0a95a2797087de0b81c0a
Oliver Galloway-Lunn <olivergl@akvo.org>
2012-04-04 15:36:21 +0100

    [#21406319] Updated the DatabaseCredentialsTest to use the TemplateLoader helper where necessary.

--- scripts/deployment/fab/tests/config/rsr/credentials/database_credentials_test.py
+++ scripts/deployment/fab/tests/config/rsr/credentials/database_credentials_test.py
@@ -5,29 +5,27 @@
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
 
-import mox, os
-import simplejson as json
+import mox
 
 from testing.helpers.execution import TestRunner, TestSuiteLoader
 
 from fab.config.rsr.credentials.database import DatabaseCredentials
 from fab.verifiers.config import RSRCredentialsVerifier
+from fab.tests.template.loader import TemplateLoader
 
 
 class FakeDeploymentConfig(object):
 
-    def __init__(self, database_credentials_file_path):
-        self.deployed_database_credentials_file = database_credentials_file_path
+    def __init__(self):
+        self.deployed_database_credentials_file = TemplateLoader.CREDENTIALS_TEMPLATE_PATH
+
 
 class DatabaseCredentialsTest(mox.MoxTestBase):
-
-    FAB_SCRIPTS_HOME = os.path.realpath(os.path.join(os.path.dirname(__file__), '../../../..'))
-    CREDENTIALS_TEMPLATE_PATH = os.path.join(FAB_SCRIPTS_HOME, 'config/rsr/credentials/database.json.template')
 
     def setUp(self):
         super(DatabaseCredentialsTest, self).setUp()
 
-        self.database_credentials = DatabaseCredentials(json.load(open(self.CREDENTIALS_TEMPLATE_PATH)))
+        self.database_credentials = TemplateLoader.load_database_credentials()
 
     def test_can_load_with_given_deployment_config(self):
         """fab.tests.config.rsr.credentials.database_credentials_test  Can load with given deployment config"""
@@ -37,7 +35,7 @@ class DatabaseCredentialsTest(mox.MoxTestBase):
         mock_credentials_verifier.exit_if_database_credentials_not_available()
         self.mox.ReplayAll()
 
-        DatabaseCredentials.load_with(FakeDeploymentConfig(self.CREDENTIALS_TEMPLATE_PATH), mock_credentials_verifier)
+        DatabaseCredentials.load_with(FakeDeploymentConfig(), mock_credentials_verifier)
 
     def test_has_admin_user(self):
         """fab.tests.config.rsr.credentials.database_credentials_test  Has database admin user"""
