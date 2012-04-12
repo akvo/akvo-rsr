c7a38ecb3c0ac3ca8436b0ebd7280e31e8441a69
Oliver Galloway-Lunn <olivergl@akvo.org>
2012-04-12 10:08:14 +0100

    [#23633289] Added custom SSH user config for daily deployments and updated the run_deployment_unit_tests.py script to check for the expected configuration.

---
+++ scripts/deployment/fab/config/rsr/credentials/custom_ci.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Akvo RSR is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+
+from fab.config.rsr.credentials.user import SSHIDPath, UserCredentials
+
+
+class CustomUserCredentials(object):
+
+    @staticmethod
+    def create():
+        return UserCredentials.for_deployer_with_ssh_id(SSHIDPath.DEPLOYER)
