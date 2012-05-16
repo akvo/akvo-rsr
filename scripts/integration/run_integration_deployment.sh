61080555604f8f11af20bba3bdb537255226f4b3
Oliver Galloway-Lunn <olivergl@akvo.org>
2012-05-16 17:33:11 +0100

    [#28939451] Updated the run_integration_deployment.sh wrapper script so that any deployment unit test failures and deployment failures will be propagated with non-zero exit codes for the wrapper script.

--- scripts/integration/run_integration_deployment.sh
+++ scripts/integration/run_integration_deployment.sh
@@ -8,11 +8,21 @@ CI_VIRTUALENV_PATH="/var/virtualenvs/ci_develop"
 CI_EXECUTION_MODE="ci"
 
 "$SCRIPTS_HOME/testing/run_deployment_unit_tests.py" "$CI_VIRTUALENV_PATH" $CI_EXECUTION_MODE
+FAILURE_COUNT=$? # last exit code
 
-INTEGRATION_HOST_ALIAS="uat"
-REPOSITORY_BRANCH="develop"
-RSR_DATABASE="rsrdb_develop"
+if [ $FAILURE_COUNT -eq 0 ]; then # proceed if no failures occurred
+    INTEGRATION_HOST_ALIAS="uat"
+    REPOSITORY_BRANCH="develop"
+    RSR_DATABASE="rsrdb_develop"
 
-source "$CI_VIRTUALENV_PATH/bin/activate"
-"$SCRIPTS_HOME/deployment/execution/scenarios/deploy_rsr_release_candidate.py" $INTEGRATION_HOST_ALIAS $REPOSITORY_BRANCH $RSR_DATABASE
-deactivate
+    source "$CI_VIRTUALENV_PATH/bin/activate"
+    "$SCRIPTS_HOME/deployment/execution/scenarios/deploy_rsr_release_candidate.py" $INTEGRATION_HOST_ALIAS $REPOSITORY_BRANCH $RSR_DATABASE
+    DEPLOYMENT_FAILURES=$? # last exit code
+    deactivate
+
+    if [ $DEPLOYMENT_FAILURES -ne 0 ]; then # deployment failed
+        exit $DEPLOYMENT_FAILURES
+    fi
+else
+    exit $FAILURE_COUNT
+fi
