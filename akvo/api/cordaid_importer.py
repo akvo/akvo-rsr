7086c21a2c429cb366a7546b3d50d9dcb2e29bbb
Gabriel von Heijne <gabriel@akvo.org>
2013-04-25 11:05:29 +0200

    [#121] Add multiple hosts to the importer scrip
    Add truncation to organisation name in the xslt

--- akvo/api/cordaid_importer.py
+++ akvo/api/cordaid_importer.py
@@ -8,25 +8,41 @@ import requests
 
 from lxml import etree
 
-domain = 'localhost:8000'
-api_ver = 'v1'
+localhost = dict(
+    domain='localhost:8000',
+    api_ver='v1',
+    api_key='c719f880b815a1d4fcc267b6529e6643d33b54b2',
+    username='gabriel',
+)
 
+test2 = dict(
+    domain='test2.akvo.org',
+    api_ver='v1',
+    api_key='f7e7f139280fb7149241957e84cc22bb552dd8f6',
+    username='gabriel',
+)
 
-def post_an_activity(activity_xml):
+def post_an_activity(activity_element, host):
     response = requests.post(
         "http://{domain}/api/{api_ver}/iati_activity/"
-        "?format=xml&api_key=c719f880b815a1d4fcc267b6529e6643d33b54b2&username=gabriel".format(
-            domain=domain, api_ver=api_ver
-        ),
+        "?format=xml&api_key={api_key}&username={username}".format(**host),
         headers={'content-type': 'application/xml', 'encoding':'utf-8'},
-        data=activity_xml,
+        data=etree.tostring(activity_element),
     )
+    if response.text:
+        print "**** Error creating iati-activity: {id}".format(id=activity_element.findall('iati-identifier')[0].text)
+    else:
+        print "Created project for iati-activity: {id}".format(id=activity_element.findall('iati-identifier')[0].text)
+    # import pdb
+    # pdb.set_trace()
+
+# root[i].findall('iati-identifier')[0].text
 
 def post_some_activities():
     with open('./xml/cordaid_iati_activities.xml', 'r') as f:
         root = etree.fromstring(f.read())
         for i in range(len(root)):
-            post_an_activity(etree.tostring(root[i]))
+            post_an_activity(root[i], localhost)
 
 if __name__ == '__main__':
     post_some_activities()
