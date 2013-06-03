5491aea33deaf0a698565c9c832f4c99a3e08e06
Paul Burt <paul.burt@gmail.com>
2013-06-03 15:58:41 +0100

    Minor update to akvo.api.resource.ProjectUpdateResource.

--- akvo/api/resources.py
+++ akvo/api/resources.py
@@ -998,7 +998,7 @@ class ProjectUpdateModelForm(ModelForm):
 
 
 class ProjectUpdateResource(ConditionalFullResource):
-    file_field = Base64FileField(blank=True)
+    photo = Base64FileField("photo", blank=True)
     project = ConditionalFullToOneField('akvo.api.resources.ProjectResource', 'project')
     user = ConditionalFullToOneField('akvo.api.resources.UserResource', 'user')
 
