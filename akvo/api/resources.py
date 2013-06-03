f044292972a0e0a7cf38a281e136b11138f832af
Paul Burt <paul.burt@gmail.com>
2013-06-03 15:22:50 +0100

    Updated akvo.api.resources.ProjectUpdateResource.photo to return base 64 instead of URL.

--- akvo/api/resources.py
+++ akvo/api/resources.py
@@ -998,7 +998,7 @@ class ProjectUpdateModelForm(ModelForm):
 
 
 class ProjectUpdateResource(ConditionalFullResource):
-    photo = Base64FileField(blank=True)
+    photo = Base64FileField(blank=True, return64=True)
     project = ConditionalFullToOneField('akvo.api.resources.ProjectResource', 'project')
     user = ConditionalFullToOneField('akvo.api.resources.UserResource', 'user')
 
