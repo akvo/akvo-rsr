27cbe1f385d90fdc33dc8aa5ee774ddb2de041e9
Carl Crowder <cark@akvo.org>
2013-06-11 11:25:56 +0200

    Adding /etc/hosts management to start and stop scripts

--- scripts/dev/run_dev_env.sh
+++ scripts/dev/run_dev_env.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+sudo bash -c 'echo "192.168.50.101 rsr.localdev.akvo.org   # localdev_rsr" >> /etc/hosts'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/vagrant.sh up
--- scripts/dev/stop_dev_env.sh
+++ scripts/dev/stop_dev_env.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/vagrant.sh halt
+
+TMP_FILE=`mktemp /tmp/tmphosts`
+sed '/# localdev_rsr/d' /etc/hosts > $TMP_FILE
+sudo mv $TMP_FILE /etc/hosts
