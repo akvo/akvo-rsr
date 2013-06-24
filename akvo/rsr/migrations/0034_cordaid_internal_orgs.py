2e3c84f2b85e9f25150d9db2d3874b3bdd46df81
Gabriel von Heijne <gabriel@akvo.org>
2013-06-24 22:12:45 +0200

    [#121] Fix migration for Cordaid business units
    Add Cordaid Security and Justice as business unit to migration 0034

--- akvo/rsr/migrations/0034_cordaid_internal_orgs.py
+++ akvo/rsr/migrations/0034_cordaid_internal_orgs.py
@@ -23,7 +23,7 @@ class Migration(DataMigration):
         DGIS_ID = 464
         business_units = [(949, 'K6010'),  (959, 'K6020'), (961, 'K6030'), (955, 'K6040'), (960, 'K6050'),
                           (950, 'K6070'), (946, 'K6080'), (962, 'K6090'), (953, 'K6100'),
-                          (1099, 'K6110'),
+                          (1099, 'K6110'), (1241, 'K6060'),
         ]
         cordaid = orm['rsr.Organisation'].objects.get(pk=CORDAID_ID)
         for pk, identifier in business_units:
