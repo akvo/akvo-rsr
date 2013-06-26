f137658c72ce51953cb01baf21b1dfe03b879353
Carl Crowder <cark@akvo.org>
2013-06-26 14:13:36 +0200

    Fixing manage helper script to use new paths

--- scripts/devhelpers/manage.sh
+++ scripts/devhelpers/manage.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-manage='/var/akvo/rsr/venv/bin/python /var/akvo/rsr/git/current/akvo/manage.py'
+PWD=$(pwd)
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/../../vagrant
 
 CONF=`mktemp vagrant-ssh-conf`
 vagrant ssh-config > $CONF
+manage='/var/akvo/rsr/venv/bin/python /var/akvo/rsr/git/current/akvo/manage.py'
 ssh -t -F $CONF default "sudo -u rsr bash -c '$manage $@'"
 rm $CONF
+
+cd $PWD
