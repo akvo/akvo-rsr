c59edb6125636fcd91cc56aaed7dc018be4daab6
Carl Crowder <carl@akvo.org>
2013-07-08 11:02:34 +0200

    Using sudo to run provisioning as RSR user

--- vagrant/provision_vagrant.sh
+++ vagrant/provision_vagrant.sh
@@ -6,18 +6,16 @@ then
     #exit 0
 fi
 
-su rsr
-
 if [ ! -L /var/akvo/rsr/git/current ]
 then
-    ln -s /vagrant/rsr/checkout /var/akvo/rsr/git/current
+    sudo -u rsr ln -s /vagrant/rsr/checkout /var/akvo/rsr/git/current
 fi
 
-ln -sf /var/akvo/rsr/local_settings.conf /var/akvo/rsr/git/current/akvo/settings/65_puppet.conf
+sudo -u rsr ln -sf /var/akvo/rsr/local_settings.conf /var/akvo/rsr/git/current/akvo/settings/65_puppet.conf
 
-/var/akvo/rsr/venv/bin/pip install -r /var/akvo/rsr/git/current/scripts/deployment/pip/requirements/2_rsr.txt
+sudo -u rsr /var/akvo/rsr/venv/bin/pip install -r /var/akvo/rsr/git/current/scripts/deployment/pip/requirements/2_rsr.txt
 
-manage='/var/akvo/rsr/venv/bin/python /var/akvo/rsr/git/current/akvo/manage.py'
+manage='sudo -u rsr /var/akvo/rsr/venv/bin/python /var/akvo/rsr/git/current/akvo/manage.py'
 
 $manage syncdb --noinput &&\
 $manage migrate &&\
