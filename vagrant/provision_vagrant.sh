1cfff1339463d722a3711b9e650ca5ce47d73369
Carl Crowder <carl@akvo.org>
2013-07-09 10:34:37 +0200

    Inserting barebones datadump on provision

--- vagrant/provision_vagrant.sh
+++ vagrant/provision_vagrant.sh
@@ -11,13 +11,16 @@ then
     sudo -u rsr ln -s /vagrant/rsr/checkout /var/akvo/rsr/git/current
 fi
 
-sudo -u rsr ln -sf /var/akvo/rsr/local_settings.conf /var/akvo/rsr/git/current/akvo/settings/65_puppet.conf
+ln -sf /var/akvo/rsr/local_settings.conf /var/akvo/rsr/git/current/akvo/settings/65_puppet.conf
 
 sudo -u rsr /var/akvo/rsr/venv/bin/pip install -r /var/akvo/rsr/git/current/scripts/deployment/pip/requirements/2_rsr.txt
 
 manage='sudo -u rsr /var/akvo/rsr/venv/bin/python /var/akvo/rsr/git/current/akvo/manage.py'
 
 $manage syncdb --noinput &&\
+mysql -u root rsr < /bin/zcat /vagrant/files/barebones.sql.gz &&\
 $manage migrate &&\
+cp -r /vagrant/files/db /var/akvo/rsr/mediaroot/ &&\
+chown -R rsr.rsr /var/akvo/rsr/mediaroot/db &&\
 supervisorctl restart rsr &&\
 echo `date` > /etc/localdev_rsr_provisioned
