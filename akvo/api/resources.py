9ba9716d52323ee9dcc865e77ed083a883bc54bb
Gabriel von Heijne <gabriel@akvo.org>
2013-07-10 14:47:29 +0200

    [#121] Remove django-cacheback
    Remove code that used django-cacheback and its pip dependencies.
    
    Remove unused imports in api/resources.py and reshuffle the importing
    statements some.

--- akvo/api/resources.py
+++ akvo/api/resources.py
@@ -9,7 +9,6 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.core.urlresolvers import reverse
 from django.forms.models import ModelForm
-from django.http import HttpResponse
 
 from tastypie import fields
 from tastypie import http
@@ -17,29 +16,21 @@ from tastypie import http
 from tastypie.authentication import ApiKeyAuthentication
 from tastypie.authorization import Authorization
 from tastypie.cache import SimpleCache
-from tastypie.serializers import Serializer
-from tastypie.bundle import Bundle
 from tastypie.constants import ALL, ALL_WITH_RELATIONS
-from tastypie.exceptions import ApiFieldError
-from tastypie.fields import ApiField, NOT_PROVIDED
 from tastypie.resources import ModelResource, Resource
-from tastypie.authentication import ApiKeyAuthentication
-from akvo.api.authentication import ConditionalApiKeyAuthentication
 
-from tastypie.utils.mime import build_content_type
-
-from cacheback.base import Job
 
 import logging
 logger = logging.getLogger('akvo.rsr')
 
+from akvo.api.authentication import ConditionalApiKeyAuthentication
+from akvo.rsr.iati_code_lists import IATI_LIST_ORGANISATION_TYPE
 from akvo.api.fields import (
     Base64FileField, ConditionalFullToManyField,
     ConditionalFullToOneField, bundle_related_data_info_factory
 )
 from akvo.api.serializers import IATISerializer
 from akvo.api.validation import ModelFormValidation
-from akvo.rsr.iati_code_lists import IATI_LIST_ORGANISATION_TYPE
 
 from akvo.rsr.models import (
     Benchmark, Benchmarkname, BudgetItem, BudgetItemLabel, Category, Country, FocusArea, Goal, Link,
@@ -83,71 +74,6 @@ def get_extra_thumbnails(image_field):
         return {key: thumbs[key].absolute_url for key in thumbs.keys()}
     except:
         return None
-
-
-class CachedResourceJob(Job):
-
-    def __init__(self, resource, request, kwargs):
-        self.resource = resource
-        self.request = request
-        self.kwargs = kwargs
-        super(CachedResourceJob, self).__init__()
-
-    def fetch(self, url):
-        """
-        emulates most of Resource.get_list() but stops before calling create_response()
-        """
-        resource = self.resource
-        request = self.request
-        kwargs = self.kwargs
-        # code of Resource.get_list()
-
-        base_bundle = resource.build_bundle(request=request)
-        objects = resource.obj_get_list(bundle=base_bundle, **resource.remove_api_resource_names(kwargs))
-        sorted_objects = resource.apply_sorting(objects, options=request.GET)
-
-        paginator = resource._meta.paginator_class(request.GET, sorted_objects, resource_uri=resource.get_resource_uri(),
-                                               limit=resource._meta.limit, max_limit=resource._meta.max_limit,
-                                               collection_name=resource._meta.collection_name)
-        to_be_serialized = paginator.page()
-
-        # Dehydrate the bundles in preparation for serialization.
-        # Dehydrate the bundles in preparation for serialization.
-        bundles = [resource.build_bundle(obj=obj, request=request) for obj in to_be_serialized['objects']]
-        # add metadata to bundle to keep track of "depth", "ancestor" and "full" info
-        for bundle in bundles:
-            bundle.related_info = bundle_related_data_info_factory(request=request)
-        # end add
-
-        for obj in to_be_serialized[resource._meta.collection_name]:
-            bundle = resource.build_bundle(obj=obj, request=request)
-            bundles.append(resource.full_dehydrate(bundle))
-
-        to_be_serialized[resource._meta.collection_name] = bundles
-        to_be_serialized = resource.alter_list_data_to_serialize(request, to_be_serialized)
-        # code of Resource.create_response() but only as far as serializing
-        # meaning the serialized result is what gets cached
-        desired_format = resource.determine_format(request)
-        return resource.serialize(request, to_be_serialized, desired_format)#.encode("utf8").encode("zlib")
-
-        # objects = resource.obj_get_list(request=request, **resource.remove_api_resource_names(kwargs))
-        # sorted_objects = resource.apply_sorting(objects, options=request.GET)
-        #
-        # paginator = resource._meta.paginator_class(request.GET, sorted_objects, resource_uri=resource.get_resource_uri(), limit=resource._meta.limit)
-        # to_be_serialized = paginator.page()
-        #
-        # # Dehydrate the bundles in preparation for serialization.
-        # bundles = [resource.build_bundle(obj=obj, request=request) for obj in to_be_serialized['objects']]
-        # # add metadata to bundle to keep track of "depth", "ancestor" and "full" info
-        # for bundle in bundles:
-        #     bundle.related_info = bundle_related_data_info_factory(request=request)
-        #     # end add
-        # to_be_serialized['objects'] = [resource.full_dehydrate(bundle) for bundle in bundles]
-        # to_be_serialized = resource.alter_list_data_to_serialize(request, to_be_serialized)
-        # # code of Resource.create_response() but only as far as serializing
-        # # meaning the serialized result is what gets cached
-        # desired_format = resource.determine_format(request)
-        # return resource.serialize(request, to_be_serialized, desired_format).encode("utf8").encode("zlib")
 
 
 class ConditionalFullResource(ModelResource):
