9de13745d9ec0d9aac81253fb7ec0c627944d04f
Gabriel von Heijne <gabriel@akvo.org>
2013-07-18 15:06:07 +0200

    [#224] Refactor resources.py into its own module
    Split resources.py into separate files under api/resources.
    Some resources are grouped in one file based on sharing of the
    underlying model or function of the resource.

---
+++ akvo/api/resources/project_update.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# Akvo RSR is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+
+from django.forms.models import ModelForm
+
+from tastypie.authorization import Authorization
+from tastypie.constants import ALL, ALL_WITH_RELATIONS
+
+from akvo.api.authentication import ConditionalApiKeyAuthentication
+from akvo.api.fields import Base64FileField, ConditionalFullToOneField
+from akvo.api.validation import ModelFormValidation
+
+from akvo.rsr.models import ProjectUpdate
+
+from .resources import ConditionalFullResource
+
+
+class ProjectUpdateModelForm(ModelForm):
+    class Meta:
+        model = ProjectUpdate
+
+
+class ProjectUpdateResource(ConditionalFullResource):
+    photo = Base64FileField("photo", blank=True, null=True)
+    project = ConditionalFullToOneField('akvo.api.resources.ProjectResource', 'project')
+    user = ConditionalFullToOneField('akvo.api.resources.UserResource', 'user')
+
+    class Meta:
+        allowed_methods         = ['get', 'post']
+        authorization           = Authorization()
+        authentication          = ConditionalApiKeyAuthentication(methods_requiring_key=['POST'])
+        validation              = ModelFormValidation(form_class=ProjectUpdateModelForm)
+        queryset                = ProjectUpdate.objects.all()
+        resource_name           = 'project_update'
+        include_absolute_url    = True
+        always_return_data      = True
+
+        filtering               = dict(
+            # other fields
+            time                = ALL,
+            time_last_updated   = ALL,
+            title               = ALL,
+            update_method       = ALL,
+            # foreign keys
+            project             = ALL_WITH_RELATIONS,
+            user                = ALL_WITH_RELATIONS,
+        )
