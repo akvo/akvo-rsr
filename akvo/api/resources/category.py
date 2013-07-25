2cf1353db1845bb61072dfcb72a525d11d9bbc3d
Gabriel von Heijne <gabriel@akvo.org>
2013-07-25 10:41:33 +0200

    [#224] Add IATICategoryResource
    Add IATICategoryResource to be able to create Project.categories
    correctly from the IATI XML.

--- akvo/api/resources/category.py
+++ akvo/api/resources/category.py
@@ -5,11 +5,33 @@
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
 
+from tastypie import fields
+from tastypie.authorization import Authorization
+
 from tastypie.constants import ALL, ALL_WITH_RELATIONS
+from tastypie.resources import ModelResource
+from akvo.api.authentication import ConditionalApiKeyAuthentication
 
 from akvo.rsr.models import Category
 
 from .resources import ConditionalFullResource
+
+
+class IATICategoryResource(ModelResource):
+    # project = fields.ToOneField('akvo.api.resources.IATIProjectResource', 'project')
+
+    class Meta:
+        allowed_methods = ['get', 'post']
+        authorization   = Authorization()
+        authentication  = ConditionalApiKeyAuthentication(methods_requiring_key=['POST'])
+        queryset        = Category.objects.all()
+        resource_name   = 'iati_category'
+        filtering       = dict(
+            # other fields
+            name        = ALL,
+            # foreign keys
+            focus_area  = ALL_WITH_RELATIONS,
+        )
 
 
 class CategoryResource(ConditionalFullResource):
