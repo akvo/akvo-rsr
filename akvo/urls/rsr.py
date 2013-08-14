a380db188d854bf1648cd9530ae1eaa11058727d
Oliver Galloway-Lunn <olivergl@akvo.org>
2013-08-14 13:21:18 +0100

    [#139] Removed obsolete /api URL pattern using django-piston.

--- akvo/urls/rsr.py
+++ akvo/urls/rsr.py
@@ -290,9 +290,6 @@ urlpatterns += patterns(
 urlpatterns += patterns(
     '',
 
-    #django-piston
-    # (r'^rsr/api/', include('akvo.api.piston_urls')),
-
     #tastypie
     # generate all resource urls for version one of the api,
     # e.g. /api/v1/project/
