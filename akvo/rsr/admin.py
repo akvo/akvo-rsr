4a153e021d58b6008c1d665415e08698019d4b0e
Gabriel von Heijne <gabriel@akvo.org>
2013-09-25 15:41:09 +0200

    [#151] Add notes field to admin for OrganisationAdmin
    Add migration for the notes fields

--- akvo/rsr/admin.py
+++ akvo/rsr/admin.py
@@ -121,7 +121,7 @@ class OrganisationAdmin(admin.ModelAdmin):
         (_(u'General information'), {'fields': ('name', 'long_name', 'partner_types', 'organisation_type',
                                                 'new_organisation_type', 'logo', 'url', 'iati_org_id', 'language',)}),
         (_(u'Contact information'), {'fields': ('phone', 'mobile', 'fax',  'contact_person',  'contact_email', ), }),
-        (_(u'About the organisation'), {'fields': ('description', )}),
+        (_(u'About the organisation'), {'fields': ('description', 'notes',)}),
     )
     form = OrganisationAdminForm
     inlines = (OrganisationLocationInline, InternalOrganisationIDInline)
