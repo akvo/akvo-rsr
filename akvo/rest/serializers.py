fdc547ada5b842f6e6bd402a78ec83e475bcec00
Gabriel von Heijne <gabriel@akvo.org>
2014-01-17 15:19:16 +0100

    [#415] Add InternalOrganisationIDSerializer and OrganisationLocationSerializer
    Those serializers are used by the OrganisationSerializer.
    
    Expand the list of fields exposed by OrganisationSerializer.

--- akvo/rest/serializers.py
+++ akvo/rest/serializers.py
@@ -7,10 +7,36 @@
 
 from rest_framework import serializers
 
-from akvo.rsr.models import Organisation
+from akvo.rsr.models import Organisation, OrganisationLocation, InternalOrganisationID
+
+
+#class CountrySerializer(serializers.HyperlinkedModelSerializer):
+#    class Meta:
+#        model = Country
+#        fields = ('id',)
+
+class InternalOrganisationIDSerializer(serializers.HyperlinkedModelSerializer):
+    recording_org = serializers.PrimaryKeyRelatedField()
+    referenced_org = serializers.PrimaryKeyRelatedField()
+
+    class Meta:
+        model = InternalOrganisationID
+        fields = ('recording_org', 'referenced_org', 'identifier',)
+
+
+class OrganisationLocationSerializer(serializers.HyperlinkedModelSerializer):
+    country = serializers.PrimaryKeyRelatedField()
+    class Meta:
+        model = OrganisationLocation
+        fields = ('id', 'latitude', 'longitude', 'country', 'primary',)
+
 
 class OrganisationSerializer(serializers.HyperlinkedModelSerializer):
+    locations = OrganisationLocationSerializer(many=True, required=False)
+    internal_ids = InternalOrganisationIDSerializer(many=True, required=False)
     class Meta:
         model = Organisation
-        fields = ('id', 'name', 'new_organisation_type',)
-
+        fields = (
+            'id', 'name', 'long_name', 'organisation_type', 'new_organisation_type', 'description',
+            'url', 'locations', 'internal_ids',
+        )
