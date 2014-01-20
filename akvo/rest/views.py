9c490782a69506f46037b6ca3bd7785e5ecd0d34
Gabriel von Heijne <gabriel@akvo.org>
2014-01-20 10:35:04 +0100

    [#415] Add OrganisationLocationViewSet and InternalOrganisationIDViewSet
    Both viewsets are used by OrganisationViewSet.
    
    InternalOrganisationIDViewSet.get_queryset() is a custom filter that
    returns an organisation when supplied with a recording_org and an
    identifier.

--- akvo/rest/views.py
+++ akvo/rest/views.py
@@ -17,3 +17,45 @@ class OrganisationViewSet(viewsets.ModelViewSet):
     """
     queryset = Organisation.objects.all()
     serializer_class = OrganisationSerializer
+class OrganisationLocationViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows organisations to be viewed or edited.
+    """
+    queryset = OrganisationLocation.objects.all()
+    serializer_class = OrganisationLocationSerializer
+    authentication_classes = (SessionAuthentication, BasicAuthentication, TastyTokenAuthentication)
+    permission_classes = (IsAuthenticated,)
+
+
+class InternalOrganisationIDViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows organisations to be viewed or edited.
+    """
+    serializer_class = InternalOrganisationIDSerializer
+    queryset = InternalOrganisationID.objects.all()
+    authentication_classes = (SessionAuthentication, BasicAuthentication, TastyTokenAuthentication)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """
+        filter on recording org and/or identifier
+        """
+        queryset = self.queryset
+        recording_org = self.request.QUERY_PARAMS.get('recording_org', None)
+        identifier = self.request.QUERY_PARAMS.get('identifier', None)
+        filter_params = {}
+        if recording_org is not None:
+            filter_params.update(recording_org__id=recording_org)
+        if identifier is not None:
+            filter_params.update(identifier=identifier)
+        if filter:
+            queryset = queryset.filter(**filter_params)
+        return queryset
+
+
+#class CountryViewSet(viewsets.ModelViewSet):
+#    """
+#    API endpoint that allows organisations to be viewed or edited.
+#    """
+#    queryset = Country.objects.all()
+#    serializer_class = CountrySerializer
