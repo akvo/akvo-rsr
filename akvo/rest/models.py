bb12b66a357596ce1cfc53d2671b766d730bce6e
Gabriel von Heijne <gabriel@akvo.org>
2014-01-20 10:46:42 +0100

    [#415] Add TastyTokenAuthentication
    TastyTokenAuthentication is a child of
    rest_framework.TokenAuthentication that uses the ApiKey model from
    tastypie. This way we can use the same APi keys for both APIs.

--- akvo/rest/models.py
+++ akvo/rest/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from rest_framework.authentication import TokenAuthentication
+from tastypie.models import ApiKey
+
+
+class TastyTokenAuthentication(TokenAuthentication):
+    """
+    Override the model attribute to use Tastypie's token model
+
+    Simple token based authentication.
+
+    Clients should authenticate by passing the token key in the "Authorization"
+    HTTP header, prepended with the string "Token ".  For example:
+
+        Authorization: Token 401f7ac837da42b97f613d789819ff93537bee6a
+    """
+
+    model = ApiKey
+    """
+    A custom token model may be used, but must have the following properties.
+
+    * key -- The string identifying the token
+    * user -- The user to which the token belongs
+    """
