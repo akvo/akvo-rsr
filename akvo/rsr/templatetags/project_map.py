3ae0915237c271d6dc6f66300a4e16b81fe453c1
Kasper Brandt <kasperbrandt87@gmail.com>
2014-04-04 17:01:31 +0200

    Test maps: Added project and org overview

--- akvo/rsr/templatetags/project_map.py
+++ akvo/rsr/templatetags/project_map.py
@@ -10,11 +10,12 @@ import os
 from django import template
 from django.conf import settings
 
-from akvo.rsr.models import ProjectLocation
+from akvo.rsr.models import ProjectLocation, OrganisationLocation
 
 register = template.Library()
 
 PROJECT_MARKER_ICON = getattr(settings, 'GOOGLE_MAPS_PROJECT_MARKER_ICON', '')
+ORGANISATION_MARKER_ICON = getattr(settings, 'GOOGLE_MAPS_ORGANISATION_MARKER_ICON', '')
 
 
 # TODO: this should be fixed so partner sites use their own domain
@@ -26,21 +27,31 @@ def project_map(project, width, height):
     Project google map
     
     params:
-        project: a Project object
+        project: a Project object or integer. 1 for the projects overview and 2 for the organisations overview.
         width, height: the dimensions of the map
     """
 
     map_id = 'akvo_map_%s' % os.urandom(8).encode('hex')
-
+    marker_icon = PROJECT_MARKER_ICON
     locations = []
-    for location in ProjectLocation.objects.filter(location_target=project.pk):
-        locations.append([location.latitude, location.longitude])
+
+    if isinstance(project, int):
+        if project > 0:
+            for location in ProjectLocation.objects.all():
+                locations.append([location.latitude, location.longitude])
+        else:
+            for location in OrganisationLocation.objects.all():
+                locations.append([location.latitude, location.longitude])
+            marker_icon = ORGANISATION_MARKER_ICON
+    else:
+        for location in ProjectLocation.objects.filter(location_target=project.pk):
+            locations.append([location.latitude, location.longitude])
 
     template_context = {
         'map_id': map_id,
         'width': width,
         'height': height,
-        'marker_icon': PROJECT_MARKER_ICON,
+        'marker_icon': marker_icon,
         'locations': locations
     }
 
