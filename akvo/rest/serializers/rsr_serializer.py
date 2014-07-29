d71a7656312d158696e0c90271d180a1e1ad587a
Gabriel von Heijne <gabriel@akvo.org>
2014-07-29 12:08:36 +0200

    [#632] Extend BaseRSRSerializer.__init__()
    BaseRSRSerializer now adds an "absolute_url" field if the associated
    model defines the get_absolute_url() method.

--- akvo/rest/serializers/rsr_serializer.py
+++ akvo/rest/serializers/rsr_serializer.py
@@ -3,6 +3,8 @@
 # Akvo RSR is covered by the GNU Affero General Public License.
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+
 from django.db import models
 
 from rest_framework import serializers
@@ -18,10 +20,13 @@ from akvo.rsr.fields import ValidXMLCharField, ValidXMLTextField
 
 class BaseRSRSerializer(serializers.ModelSerializer):
     def __init__(self, *args, **kwargs):
-        """ Add the ValidXMLXXXFields to the model-to-rest-field mapping and use the modified CharField,
-            NonNullCharField, that returns '' for None values
-        """
         super(BaseRSRSerializer, self).__init__(*args, **kwargs)
+        # Add 'absolute_url' field if the model defines the get_absolute_url method
+        if getattr(self.opts.model, 'get_absolute_url', None):
+            self.fields['absolute_url'] = serializers.Field(source='get_absolute_url')
+        # Add the ValidXMLXXXFields to the model-to-rest-field mapping and use the modified CharField,
+        # NonNullCharField, that returns '' for None values
+        # Do the same for URLField using NonNullURLField
         self.field_mapping.update(
             {
                 models.URLField: NonNullURLField,
