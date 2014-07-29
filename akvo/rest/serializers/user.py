adf0459b70f7e588752ae86b1644da0ef64c1520
Gabriel von Heijne <gabriel@akvo.org>
2014-07-29 11:51:20 +0200

    [#632] Add UserExtraSerializer
    UserExtraSerializer is used by ProjectUpdateExtraSerializer

--- akvo/rest/serializers/user.py
+++ akvo/rest/serializers/user.py
@@ -4,9 +4,9 @@
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
-
 from akvo.rsr.models import User
 
+from .organisation import OrganisationExtraSerializer
 from .rsr_serializer import BaseRSRSerializer
 
 
@@ -19,3 +19,24 @@ class UserSerializer(BaseRSRSerializer):
             'last_name',
             'email',
         )
+
+
+class UserExtraSerializer(BaseRSRSerializer):
+
+    organisation = OrganisationExtraSerializer(source='get_profile.organisation')
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'organisation',
+        )
+        exclude = ('absolute_url',)
+
+    def __init__(self, *args, **kwargs):
+        """ Delete the 'absolute_url' field added in BaseRSRSerializer.__init__().
+        It's neither correct nor do we want this data to be visible.
+        """
+        super(UserExtraSerializer, self).__init__(*args, **kwargs)
+        del self.fields['absolute_url']
