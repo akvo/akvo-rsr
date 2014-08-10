2aabdc07867dba0e486750683117b60df1a680a8
Gabriel von Heijne <gabriel@akvo.org>
2014-08-10 23:46:33 +0200

    [#710] Add RainActivity class
    RainActivity is used to map data in the IATI activity to RSR fields. It
    is used in rain_project_upload.py and post_import.py.

--- akvo/scripts/rain/__init__.py
+++ akvo/scripts/rain/__init__.py
@@ -21,9 +21,10 @@ API_VERSION = 'v1'
 me = sys.modules[__name__]
 
 rain_settings = dict(
-    AKVO_NS='{http://akvo.org/iati-activities}',
+    AKVO_NS='http://akvo.org/iati-activities',
+    RAIN_NS="http://data.rainfoundation.org",
 
-    RAIN_ROOT_DIR = '/Users/gabriel/git/akvo-rsr/akvo/cordaid',
+    RAIN_ROOT_DIR = '/var/tmp/rain',
     RAIN_PROJECT_IMAGES_SUBDIR = 'project_images',
     RAIN_LOGOS_SUBDIR = 'logos',
 
@@ -34,14 +35,11 @@ rain_settings = dict(
     RAIN_ACTIVITIES_CSV_FILENAME = 'rain_activities_import_{datetime}.csv',
     RAIN_UPLOAD_CSV_FILENAME = 'rain_activities_upload_{datetime}.csv',
 
-    RAIN_ORG_ID = 273,
-    RAIN_IATI_ID = 'NL-KVK-41160054',
-    DGIS_ORG_ID = 464,
-    DGIS_IATI_ID = 'NL-1',
-    OTHERS_ORG_ID = 1653,
+    RAIN_ORG_ID = 35,
+    RAIN_IATI_ID = 'NL-KVK-34200988',
 )
 
-# construct local variables for Cordaid supporting data
+# construct local variables for RAIN supporting data
 for key, val in rain_settings.items():
     # try to grab the identifier from settings, if not found use the default from rain_settings
     setattr(me, key, getattr(settings, key, val))
@@ -111,20 +109,20 @@ def print_log(log_file, column_names, to_console=False):
         if do_append:
             dataset.append(row)
         elif to_console:
-            print  bit['text'].format(**bit['data'])
+            print bit['text'].format(**bit['data'])
 
     log_to_file(dataset.csv, log_file)
 
 # Activities
 # ==========
-# Cordaid ID      Akvo ID     Title       Event     Error
-# ==========      =======     =====       =====     =====
-#
+# RAIN ID       Akvo ID IATI ID Event   Error/info
+# ===========   ======= ======= =====   ==========
+# internal_id   rsr_id  iati_id event   extra
 #
 # Organiations
 # ============
-# Cordaid org ID      Akvo ID     Name    Event     Error
-# ==============      =======     ====    =====     =====
+# RAIN org ID   Akvo ID     Name    Event     Error
+# ===========   =======     ====    =====     =====
 
 LOG_ACTIVITIES = 'activities'
 LOG_ORGANISATIONS = 'organisations'
@@ -154,3 +152,54 @@ ERROR_NO_ORGS = "participating orgs missing error"
 ERROR_CREATE_ORG = 'organisation create error'
 ERROR_UPDATE_ORG = 'organisation update error'
 ERROR_UPLOAD_ORG = "organisation upload error"
+
+
+class RainActivity():
+    """ This class holds an etree representation of an activity and is responsible for mapping the relevant bits in the
+        activity to the model fields in RSR (or closely related data)
+    """
+    def __init__(self, tree, rain_ns, akvo_ns):
+        # the iati-lactivity etree
+        self.tree = tree
+        # the Akvo XML namespace
+        self.ns_map = {'rain': rain_ns, 'akvo': akvo_ns}
+
+    def rsr_id(self):
+        """ Look for the RSR Id of the project, search for a tag similar to:
+            <other-identifier owner-name="akvorsr" owner-ref="29"/>
+        """
+        tags = self.tree.xpath("//other-identifier[@owner-name='akvorsr']")
+        if len(tags) == 1:
+            return tags[0].get('owner-ref')
+
+    def internal_id(self):
+        """ Look for the internal Id of the project, search for a tag similar to:
+            <other-identifier owner-name="rainpms" rain:type="id" owner-ref="520"/>
+        """
+        tags =  self.tree.xpath("//other-identifier[@owner-name='rainpms' and @rain:type='id']", namespaces=self.ns_map)
+        if len(tags) == 1:
+            return tags[0].get('owner-ref')
+
+    def iati_id(self):
+        return self.tree.find('iati-identifier').text
+
+    def current_image(self):
+        url = self.tree.xpath("document-link['akvo:photo-id']/@url")
+        if url:
+            return url[0]
+        return None
+
+    def current_image_caption(self):
+        img_caption = self.tree.xpath("document-link['akvo:photo-id']/title", namespaces=self.ns_map)
+        if img_caption:
+            return img_caption[0].text.strip()
+        return None
+
+    def current_image_credit(self):
+        credit = self.tree.xpath("document-link['akvo:photo-id']/@akvo:photo-credit", namespaces=self.ns_map)
+        if credit:
+            return credit[0]
+        return None
+
+    def publishing_status(self):
+        return self.tree.get(self.akvo_ns + 'publish') == 'true'
