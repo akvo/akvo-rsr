18d154ec50d5a3a099a8c2cf854e93972371e397
Gabriel von Heijne <gabriel@akvo.org>
2014-08-19 15:47:30 +0200

    [#192] Change import of User model in rest user view and serializer

--- akvo/rest/serializers/user.py
+++ akvo/rest/serializers/user.py
@@ -4,7 +4,8 @@
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
-from akvo.rsr.models import User
+
+from django.contrib.auth.models import User
 
 from .organisation import OrganisationExtraSerializer
 from .rsr_serializer import BaseRSRSerializer
