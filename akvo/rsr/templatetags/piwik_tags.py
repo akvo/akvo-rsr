edc11db7d3d04647772f58e3ce36729beb72b084
Gabriel von Heijne <gabriel@akvo.org>
2014-08-27 14:20:11 +0200

    [#630] "Internalise" the current piwik code
    Since the piwik app is so small, it seems easiest to integrate the code
    in the rsr templatetags and then do the modifications needed there.
    
    This commit just moves the code from django-piwik into rsr.

---
+++ akvo/rsr/templatetags/piwik_tags.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Akvo RSR is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+# code based on django-piwik, https://github.com/raphaa/django-piwik
+
+from django import template
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('piwik/tracking_code.html')
+def tracking_code():
+    try:
+        id = settings.PIWIK_SITE_ID
+    except AttributeError:
+        raise ImproperlyConfigured('PIWIK_SITE_ID does not exist.')
+    try:
+        url = settings.PIWIK_URL
+    except AttributeError:
+        raise ImproperlyConfigured('PIWIK_URL does not exist.')
+    return {'id': id, 'url': url}
