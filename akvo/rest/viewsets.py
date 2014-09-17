d21de85d4c20518fa9e9631c114ca71dea3ec04b
Gabriel von Heijne <gabriel@akvo.org>
2014-09-17 12:54:23 +0200

    [#632] Add simple write permissions
    Add rsr_rest_user_<model_name> permissions for all rsr models. This
    requires the running of manage.py syncdb --all to get into the DB.
    
    Add IsRestUserOrReadOnly and set BaseRSRViewSet.permission_classes to use
    it. This allows users with the rsr_rest_user_<model_name> permission to
    use the "write" methods of the API (POST, PUT, PATCH and DELETE).
    
    Suggested use is to create a Group with the rsr_rest_user_<model_name>
    permissions and assigin users that are allowed to write to this group.

--- akvo/rest/viewsets.py
+++ akvo/rest/viewsets.py
@@ -8,9 +8,27 @@
 from rest_framework import viewsets
 
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, BasePermission, SAFE_METHODS
 
 from akvo.rest.models import TastyTokenAuthentication
+from akvo.utils import RSR_REST_USER
+
+
+class IsRestUserOrReadOnly(BasePermission):
+    """ Simple and API security.
+        Assign all models to a group and add the RSR rest API user permission of all models to the group.
+    """
+    def has_permission(self, request, view):
+
+        model_name = view.serializer_class.Meta.model.__name__.lower()
+        has_permission = request.user.has_perms(["rsr.{}_{}".format(RSR_REST_USER, model_name)])
+
+        return (
+            request.method in SAFE_METHODS or
+                request.user and
+                request.user.is_authenticated() and
+                has_permission
+        )
 
 
 class BaseRSRViewSet(viewsets.ModelViewSet):
@@ -18,4 +36,4 @@ class BaseRSRViewSet(viewsets.ModelViewSet):
     Base class used for the view sets for RSR models. Provides unified auth and perms settings.
     """
     authentication_classes = (SessionAuthentication, TastyTokenAuthentication)
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsRestUserOrReadOnly,)
--- akvo/rsr/management.py
+++ akvo/rsr/management.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
+from django.db.models import get_app, get_models
 from django.db.models.signals import post_syncdb
+from akvo.utils import RSR_REST_USER
 
 if "akvo.rsr" in settings.INSTALLED_APPS:
     from akvo.rsr import models as rsr
@@ -27,3 +29,26 @@ if "akvo.rsr" in settings.INSTALLED_APPS:
                 print 'RSR limited change %s Permission already exists in the database' % model_name
 
     post_syncdb.connect(create_limited_change_permissions, sender=rsr)
+
+    def create_rest_user_permissions(sender, **kwargs):
+        """ This is a temporary permission for blanket DRF API access
+        """
+        # TODO: replace this with granular permissions for the DRF API
+        app = get_app('rsr')
+        for model in get_models(app):
+            opts = model._meta
+            model_name = opts.object_name.lower()
+            permission, created = Permission.objects.get_or_create(
+                codename="{}_{}".format(RSR_REST_USER, model_name),
+                defaults={
+                    'name': u'RSR rest API user',
+                    'content_type_id': ContentType.objects.get_for_model(model).id,
+                }
+            )
+            if created:
+                print 'Created RSR rest API user Permission for {}'.format(model_name)
+            else:
+                print 'RSR rest API user Permission for {} already exists in the database'.format(model_name)
+
+
+    post_syncdb.connect(create_rest_user_permissions, sender=rsr)
