f6bcfe4f26b4ce5d934f6480d57ff788e408dfba
Gabriel von Heijne <gabriel@akvo.org>
2014-11-07 10:35:28 +0100

    [#896] Add filtering to /rest/v1/project
    Add filtering on organisation and publishing status.
    
    The query params should look like this:
    
    partnerships__organisation=<organisation_id>
    
    publishingstatus__status=published|unpublished

--- akvo/rest/views/project.py
+++ akvo/rest/views/project.py
@@ -5,7 +5,7 @@
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
 
-from akvo.rsr.models import Project
+from akvo.rsr.models import Project, PublishingStatus
 
 from ..serializers import ProjectSerializer
 from ..viewsets import BaseRSRViewSet
@@ -16,3 +16,23 @@ class ProjectViewSet(BaseRSRViewSet):
     """
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    paginate_by_param = 'limit'
+    max_paginate_by = 1000
+
+    def get_queryset(self):
+        """ Allow simple filtering on selected fields
+        """
+        queryset = self.queryset
+
+        ### DEBUG ###
+        import pdb
+        pdb.set_trace()
+        ### DEBUG ###
+
+        organisation = self.request.QUERY_PARAMS.get('partnerships__organisation', None)
+        if organisation is not None:
+            queryset = queryset.filter(partnerships__organisation=organisation).distinct()
+            published = self.request.QUERY_PARAMS.get('publishingstatus__status', None)
+        if published in [PublishingStatus.STATUS_PUBLISHED, PublishingStatus.STATUS_UNPUBLISHED]:
+            queryset = queryset.filter(publishingstatus__status=published)
+        return queryset
