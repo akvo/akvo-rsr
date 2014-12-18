1233bb2fc001ac2198ff279e65495d165bfef599
Daniel Karlsson <daniel@kardans.com>
2014-12-18 09:40:57 +0100

    [#978] Added pre migration script
    Added a python script that uses the Django env to remove Django
    sessions instead of using the mysql client.

--- pg_migrate.sh
+++ pg_migrate.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "No password argument supplied"
-    exit 2
-fi
+# if [ -z "$1" ]; then
+#     echo "No password argument supplied"
+#     exit 2
+# fi
 
-# Since we switch user we need to store password on disk
-# we're using the rsr disk location and not /tmp
-PASSWORD_FILE="/var/akvo/rsr/code/pg_db_password.tmp"
-echo $1 > $PASSWORD_FILE
+# # Since we switch user we need to store password on disk
+# # we're using the rsr disk location and not /tmp
+# PASSWORD_FILE="/var/akvo/rsr/code/pg_db_password.tmp"
+# echo $1 > $PASSWORD_FILE
+
+# # Truncate Django session
+# mysql --user=rsr --password=$1 rsr -e "Truncate table django_session;"
 
 # Truncate Django session
-mysql --user=rsr --password=$1 rsr -e "Truncate table django_session;"
+# sudo -u rsr bash <<'EOF'
+# cd /var/akvo/rsr
+# . venv/bin/activate
+# cd code
+# python pg_pre.py > pg_pre.log
+# EOF
 
-# Create rsr user and database
-sudo -u postgres bash <<'EOF'
-PASSWORD=`cat /var/akvo/rsr/code/pg_db_password.tmp`
-psql -c "CREATE USER rsr WITH PASSWORD '$PASSWORD';"
-psql -c "CREATE DATABASE rsr OWNER rsr TEMPLATE template0 ENCODING 'UTF8';"
-EOF
+# # Create rsr user and database
+# sudo -u postgres bash <<'EOF'
+# PASSWORD=`cat /var/akvo/rsr/code/pg_db_password.tmp`
+# psql -c "CREATE USER rsr WITH PASSWORD '$PASSWORD';"
+# psql -c "CREATE DATABASE rsr OWNER rsr TEMPLATE template0 ENCODING 'UTF8';"
+# EOF
 
-# Remove password file
-rm -f $PASSWORD_FILE
+# # Remove password file
+# rm -f $PASSWORD_FILE
 
 # Do migration
 sudo -u rsr bash <<'EOF'
@@ -32,6 +40,7 @@ if ! [ -f  /var/akvo/rsr/code/pg_migrate_conf.yml ]; then
    echo "Could not find pg_migrate_conf.yml. Use pg_migrate_conf.yml.template."
    exit 2
 fi
+python pg_pre.py > pg_pre.log
 py-mysql2pgsql -v -f pg_migrate_conf.yml
 python pg_alts.py > pg_tests.log
 python pg_tests.py > pg_tests.log
