82c67f474edc3d89ad98cbba6a1b5d52f06f2e41
Kasper Brandt <kasperbrandt87@gmail.com>
2015-02-03 11:21:13 +0100

    [#955] Added IATI models to DRF
    Models added:
    - Indicator(period)
    - Legacy data
    - Planned disbursement
    - Policy marker
    - Project condition
    - Project contact
    - Recipient country
    - Recipient region
    - Related project
    - Result
    - Sector
    - Transaction

--- akvo/rest/serializers/__init__.py
+++ akvo/rest/serializers/__init__.py
@@ -13,20 +13,32 @@ from .country import CountrySerializer
 from .employment import EmploymentSerializer
 from .focus_area import FocusAreaSerializer
 from .goal import GoalSerializer
+from .indicator import IndicatorPeriodSerializer, IndicatorSerializer
 from .internal_organisation_id import InternalOrganisationIDSerializer
 from .invoice import InvoiceSerializer
+from .legacy_data import LegacyDataSerializer
 from .link import LinkSerializer
 from .organisation import OrganisationSerializer
 from .organisation_location import OrganisationLocationSerializer
 from .partner_site import PartnerSiteSerializer
 from .partner_type import PartnerTypeSerializer
 from .partnership import PartnershipSerializer
+from .planned_disbursement import PlannedDisbursementSerializer
+from .policy_marker import PolicyMarkerSerializer
 from .project import ProjectSerializer
 from .project_comment import ProjectCommentSerializer
+from .project_condition import ProjectConditionSerializer
+from .project_contact import ProjectContactSerializer
 from .project_location import ProjectLocationSerializer
 from .project_update import ProjectUpdateSerializer, ProjectUpdateExtraSerializer
 from .project_update_location import ProjectUpdateLocationSerializer
 from .publishing_status import PublishingStatusSerializer
+from .recipient_country import RecipientCountrySerializer
+from .region import RecipientRegionSerializer
+from .related_project import RelatedProjectSerializer
+from .result import ResultSerializer
+from .sector import SectorSerializer
+from .transaction import TransactionSerializer
 from .user import UserSerializer, UserDetailsSerializer, UserPasswordSerializer
 
 __all__ = [
@@ -39,21 +51,34 @@ __all__ = [
     'EmploymentSerializer',
     'FocusAreaSerializer',
     'GoalSerializer',
+    'IndicatorPeriodSerializer',
+    'IndicatorSerializer',
     'InternalOrganisationIDSerializer',
     'InvoiceSerializer',
+    'LegacyDataSerializer',
     'LinkSerializer',
     'OrganisationSerializer',
     'OrganisationLocationSerializer',
     'PartnerSiteSerializer',
     'PartnerTypeSerializer',
     'PartnershipSerializer',
+    'PlannedDisbursementSerializer',
+    'PolicyMarkerSerializer',
     'ProjectSerializer',
     'ProjectCommentSerializer',
+    'ProjectConditionSerializer',
+    'ProjectContactSerializer',
     'ProjectLocationSerializer',
     'ProjectUpdateSerializer',
     'ProjectUpdateExtraSerializer',
     'ProjectUpdateLocationSerializer',
     'PublishingStatusSerializer',
+    'RecipientCountrySerializer',
+    'RecipientRegionSerializer',
+    'RelatedProjectSerializer',
+    'ResultSerializer',
+    'SectorSerializer',
+    'TransactionSerializer',
     'UserSerializer',
     'UserDetailsSerializer',
     'UserPasswordSerializer',
--- akvo/rest/views/__init__.py
+++ akvo/rest/views/__init__.py
@@ -14,20 +14,32 @@ from .country import CountryViewSet
 from .employment import EmploymentViewSet, approve_employment, set_group
 from .focus_area import FocusAreaViewSet
 from .goal import GoalViewSet
+from .indicator import IndicatorViewSet, IndicatorPeriodViewSet
 from .internal_organisation_id import InternalOrganisationIDViewSet
 from .invoice import InvoiceViewSet
+from .legacy_data import LegacyDataViewSet
 from .link import LinkViewSet
 from .organisation import OrganisationViewSet
 from .organisation_location import OrganisationLocationViewSet
 from .partner_site import PartnerSiteViewSet
 from .partner_type import PartnerTypeViewSet
 from .partnership import PartnershipViewSet
+from .planned_disbursement import PlannedDisbursementViewSet
+from .policy_marker import PolicyMarkerViewSet
 from .project import ProjectViewSet
 from .project_comment import ProjectCommentViewSet
+from .project_condition import ProjectConditionViewSet
+from .project_contact import ProjectContactViewSet
 from .project_location import ProjectLocationViewSet
 from .project_update import ProjectUpdateViewSet, ProjectUpdateExtraViewSet
 from .project_update_location import ProjectUpdateLocationViewSet
 from .publishing_status import PublishingStatusViewSet
+from .recipient_country import RecipientCountryViewSet
+from .related_project import RelatedProjectViewSet
+from .region import RecipientRegionViewSet
+from .result import ResultViewSet
+from .sector import SectorViewSet
+from .transaction import TransactionViewSet
 from .user import UserViewSet, change_password, update_details, request_organisation
 
 __all__ = [
@@ -42,21 +54,34 @@ __all__ = [
     'set_group',
     'FocusAreaViewSet',
     'GoalViewSet',
+    'IndicatorViewSet',
+    'IndicatorPeriodViewSet',
     'InternalOrganisationIDViewSet',
     'InvoiceViewSet',
+    'LegacyDataViewSet',
     'LinkViewSet',
     'OrganisationViewSet',
     'OrganisationLocationViewSet',
     'PartnerSiteViewSet',
     'PartnerTypeViewSet',
     'PartnershipViewSet',
+    'PlannedDisbursementViewSet',
+    'PolicyMarkerViewSet',
     'ProjectViewSet',
     'ProjectCommentViewSet',
+    'ProjectConditionViewSet',
+    'ProjectContactViewSet',
     'ProjectLocationViewSet',
     'ProjectUpdateLocationViewSet',
     'ProjectUpdateViewSet',
     'ProjectUpdateExtraViewSet',
     'PublishingStatusViewSet',
+    'RecipientCountryViewSet',
+    'RecipientRegionViewSet',
+    'RelatedProjectViewSet',
+    'ResultViewSet',
+    'SectorViewSet',
+    'TransactionViewSet',
     'UserViewSet',
     'change_password',
     'update_details',
