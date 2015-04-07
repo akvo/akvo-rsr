24ff2796bf6efae119795464949a3a8af9dc2e73
Gabriel von Heijne <gabriel@akvo.org>
2015-04-07 16:14:05 +0200

    [#1384] Add project_up API endpoint to rest API
    The project_up endpoint is tailored to the needs of the Up app.
    
    It includes the full primary_location object and allows the requesting
    of an image thumbnail of arbitrary size using the
    Base64ImageField.to_native() method.

--- akvo/rest/views/project.py
+++ akvo/rest/views/project.py
@@ -3,7 +3,7 @@
 # Akvo RSR is covered by the GNU Affero General Public License.
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
-
+from akvo.rest.serializers.project import ProjectUpSerializer
 
 from akvo.rsr.models import Project, PublishingStatus
 
@@ -50,6 +50,29 @@ class ProjectExtraViewSet(ProjectViewSet):
     """ Viewset providing extra data and limited filtering for Up in one go
     """
     serializer_class = ProjectExtraSerializer
+    paginate_by_param = 'limit'
+    max_paginate_by = 1000
+
+    def get_queryset(self):
+        """ Allow simple filtering on selected fields
+            Note that the query string keys "mimic" the django ORM filtering syntax,
+            but in reality are totally arbitrary. Some day we might get full filtering
+            using the Django ORM and then it'd nice if those custom filters continue to work.
+        """
+        queryset = self.queryset
+        organisation = self.request.QUERY_PARAMS.get('partnerships__organisation', None)
+        if organisation is not None:
+            queryset = queryset.filter(partnerships__organisation=organisation).distinct()
+        published = self.request.QUERY_PARAMS.get('publishingstatus__status', None)
+        if published in [PublishingStatus.STATUS_PUBLISHED, PublishingStatus.STATUS_UNPUBLISHED]:
+            queryset = queryset.filter(publishingstatus__status=published)
+        return queryset
+
+
+class ProjectUpViewSet(ProjectViewSet):
+    """ Viewset providing extra data and limited filtering for Up in one go
+    """
+    serializer_class = ProjectUpSerializer
     paginate_by_param = 'limit'
     max_paginate_by = 1000
 
