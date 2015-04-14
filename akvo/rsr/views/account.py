58d3cbb945f712acf7133d1cc9c82e83615cbed1
Daniel Karlsson <daniel@kardans.com>
2015-04-14 14:49:54 +0200

    [#1453] Polish

--- akvo/rsr/views/account.py
+++ akvo/rsr/views/account.py
@@ -88,6 +88,12 @@ def activate(request, activation_key, extra_context=None):
 
 
 def sign_in(request):
+    """Sign in.
+
+    POST have two variants with username & email:
+     - username > normal sign in
+     - email > password reset workflow
+    """
     context = RequestContext(request)
     form = AuthenticationForm()
     reset_form = PasswordResetForm()
@@ -103,7 +109,8 @@ def sign_in(request):
         if reset_form.is_valid():
             reset_form.save(domain_override=settings.RSR_DOMAIN)
         return HttpResponse()
-    return render_to_response('sign_in.html', {'form': form, 'reset_form': reset_form}, context_instance=context)
+    return render_to_response('sign_in.html', {'form': form, 'reset_form': reset_form},
+                              context_instance=context)
 
 
 def sign_out(request):
