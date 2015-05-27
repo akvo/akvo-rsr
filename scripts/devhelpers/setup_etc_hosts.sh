38874b5b93b2c5d01deb241af11bf653b31a91c1
Paul Burt <paul.burt@gmail.com>
2015-05-27 16:30:42 +0000

    Reverted comment in setup_etc_hosts.sh script
    cleanup_etc_hosts.sh specifically refers to string "localdev_rsr".
    Reinstated this in setup_etc_hosts.sh to re-enable this script.

--- scripts/devhelpers/setup_etc_hosts.sh
+++ scripts/devhelpers/setup_etc_hosts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Hello. This script will edit /etc/hosts and therefore may require your administrator password."
-sudo bash -c 'printf "\n192.168.50.101 rsr.localdev.akvo.org   # Akvo RSR local development\n" >> /etc/hosts'
+sudo bash -c 'printf "\n192.168.50.101 rsr.localdev.akvo.org   # localdev_rsr\n" >> /etc/hosts'
