cefc9be5be612e5d6a73a749c540a452fad68ad8
gabemart <g4bemart@gmail.com>
2015-06-08 21:37:37 +0100

    [#1525] section 9 data population

--- akvo/rsr/views/my_rsr.py
+++ akvo/rsr/views/my_rsr.py
@@ -30,7 +30,7 @@ from akvo.codelists.store.codelists_v201 import (AID_TYPE, FLOW_TYPE, TIED_STATU
                                                 GEOGRAPHIC_LOCATION_REACH, GEOGRAPHIC_LOCATION_CLASS, LOCATION_TYPE, 
                                                 GEOGRAPHIC_VOCABULARY, SECTOR_VOCABULARY, POLICY_MARKER,
                                                 POLICY_SIGNIFICANCE, POLICY_MARKER_VOCABULARY, BUDGET_IDENTIFIER_VOCABULARY,
-                                                BUDGET_TYPE, BUDGET_IDENTIFIER)
+                                                BUDGET_TYPE, BUDGET_IDENTIFIER, DISBURSEMENT_CHANNEL)
 
 
 @login_required
@@ -165,6 +165,7 @@ def project_admin(request, project_id):
     country_budget_vocabulary = BUDGET_IDENTIFIER_VOCABULARY
     budget_type = BUDGET_TYPE
     budget_identifier = BUDGET_IDENTIFIER
+    disbursement_channel = DISBURSEMENT_CHANNEL
     
     context = {
         'id': project_id,
@@ -196,6 +197,7 @@ def project_admin(request, project_id):
         'budget_item_labels': budget_item_labels,
         'budget_type': budget_type,
         'budget_identifier': budget_identifier,
+        'disbursement_channel': disbursement_channel,
 
     }
 
