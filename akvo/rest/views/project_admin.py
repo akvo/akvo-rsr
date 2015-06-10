b5c203504c257726642313c2afebd6ed80f7f3a5
Kasper Brandt <kasperbrandt87@gmail.com>
2015-06-10 14:58:05 +0200

    [#1525] Admin form step 1 (without related projects)

--- akvo/rest/views/project_admin.py
+++ akvo/rest/views/project_admin.py
@@ -6,6 +6,7 @@ For additional details on the GNU license please see < http://www.gnu.org/licens
 """
 
 from akvo.rsr.models import Project
+import datetime
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.exceptions import PermissionDenied
@@ -26,8 +27,75 @@ def project_admin_step1(request, pk=None):
     data = request.POST
 
     # Title
-    title = data.get('projectTitle', '')
-    project.title = title
+    # title = data.get('projectTitle', '')
+    project.title = data['projectTitle']
     project.save(update_fields=["title"])
+
+    # Subtitle
+    project.subtitle = data['projectSubTitle']
+    project.save(update_fields=["subtitle"])
+
+    # IATI Identifier
+    project.iati_activity_id = data['iatiId']
+    project.save(update_fields=["iati_activity_id"])
+
+    # Status
+    project.status = data['projectStatus']
+    project.save(update_fields=["status"])
+
+    # Start date (planned)
+    date_start_planned = data['eventFromPlanned']
+    project.date_start_planned = date_start_planned if date_start_planned else None
+    project.save(update_fields=["date_start_planned"])
+
+    # Start date (actual)
+    date_start_actual = data['eventFromActual']
+    project.date_start_actual = date_start_actual if date_start_actual else None
+    project.save(update_fields=["date_start_actual"])
+
+    # End date (planned)
+    date_end_planned = data['eventEndPlanned']
+    project.date_end_planned = date_end_planned if date_end_planned else None
+    project.save(update_fields=["date_end_planned"])
+
+    # End date (actual)
+    date_end_actual = data['eventEndActual']
+    project.date_end_actual = date_end_actual if date_end_actual else None
+    project.save(update_fields=["date_end_actual"])
+
+    # Language
+    project.language = data['projectLanguage']
+    project.save(update_fields=["language"])
+
+    # Currency
+    project.currency = data['projectCurrency']
+    project.save(update_fields=["currency"])
+
+    # Hierarchy
+    hierarchy = data.get('projectHierarchy', '')
+    project.hierarchy = int(hierarchy) if hierarchy else None
+    project.save(update_fields=["hierarchy"])
+
+    # Default aid type
+    project.default_aid_type = data['defaultAidType']
+    project.save(update_fields=["default_aid_type"])
+
+    # Default flow type
+    project.default_flow_type = data['defaultFlowType']
+    project.save(update_fields=["default_flow_type"])
+
+    # Default tied status
+    project.default_tied_status = data['defaultTiedStatus']
+    project.save(update_fields=["default_tied_status"])
+
+    # Default collaboration type
+    project.collaboration_type = data['collaborationType']
+    project.save(update_fields=["collaboration_type"])
+
+    # Default finance type
+    project.default_finance_type = data['defaultFinanceType']
+    project.save(update_fields=["default_finance_type"])
+
+    # TODO: Related projects
 
     return Response({'status': 'title updated'})
