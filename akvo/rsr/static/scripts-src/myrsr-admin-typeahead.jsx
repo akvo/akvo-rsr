ca0228a3e6cd512ada07000c5939aed032b58df9
gabemart <g4bemart@gmail.com>
2015-06-19 16:44:26 +0100

    [#1525] typeahead for related projects in section 1

---
+++ akvo/rsr/static/scripts-src/myrsr-admin-typeahead.jsx
@@ -0,0 +1,57 @@
+function buildReactComponents(placeholder, typeaheadOptions, typeaheadCallback, displayOption, selector, valueId, label, help) {
+    var Typeahead, TypeaheadLabel, TypeaheadContainer;
+
+    Typeahead = ReactTypeahead.Typeahead;   
+
+    var inputClass = selector + " form-control";
+
+    TypeaheadContainer = React.createClass({
+        render: function() {
+            return (
+                    <div>
+                        <Typeahead
+                            placeholder={placeholder}
+                            options={typeaheadOptions}
+                            onOptionSelected={typeaheadCallback}
+                            maxVisible={10}
+                            displayOption={displayOption}
+                            filterOption='title'
+                            childID={selector}
+                            customClasses={{
+                              typeahead: "",
+                              input: inputClass,
+                              results: "",
+                              listItem: "",
+                              token: "",
+                              customAdd: "",           
+                            }}
+                            inputProps={{
+                                name: selector, 
+                                id: selector,
+                            }} />
+                    </div>
+            );
+        }
+    });
+
+    React.render(
+        <TypeaheadContainer />,
+        document.querySelector('.' + selector)
+    );
+
+    var savedResult;
+
+    if (valueId !== null) {
+        for (var i = 0; i < typeaheadOptions.length; i++) {
+            if (parseInt(typeaheadOptions[i].id, 10) == parseInt(valueId, 10)) {
+                savedResult = typeaheadOptions[i];
+                $('.' + selector + ' .typeahead' + ' input').attr('value', savedResult.id);
+                $('.' + selector + ' .typeahead' + ' input').prop('value', savedResult.title);   
+            }
+        }
+    }
+  
+    $('.' + selector + ' .typeahead').append(label);
+    $('.' + selector + ' .typeahead').append(help);   
+    $('.' + selector).addClass('has-typeahead');
+}          
