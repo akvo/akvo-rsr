2bedde6552c6a7e64f524d3863e06ae1ce037397
gabemart <g4bemart@gmail.com>
2015-06-29 15:42:11 +0100

    [#1525] Add remaining typeaheads

--- akvo/rsr/static/scripts-src/myrsr-admin-typeahead.js
+++ akvo/rsr/static/scripts-src/myrsr-admin-typeahead.js
@@ -1,5 +1,5 @@
 /** @jsx React.DOM */
-function buildReactComponents(placeholder, typeaheadOptions, typeaheadCallback, displayOption, selector, valueId, label, help) {
+function buildReactComponents(placeholder, typeaheadOptions, typeaheadCallback, displayOption, selector, valueId, label, help, filterOption) {
     var Typeahead, TypeaheadLabel, TypeaheadContainer;
 
     Typeahead = ReactTypeahead.Typeahead;   
@@ -16,7 +16,7 @@ function buildReactComponents(placeholder, typeaheadOptions, typeaheadCallback, 
                             onOptionSelected:typeaheadCallback,
                             maxVisible:10,
                             displayOption:displayOption,
-                            filterOption:"title",
+                            filterOption:filterOption,
                             childID:selector,
                             customClasses:{
                               typeahead: "",
@@ -45,7 +45,7 @@ function buildReactComponents(placeholder, typeaheadOptions, typeaheadCallback, 
             if (parseInt(typeaheadOptions[i].id, 10) == parseInt(valueId, 10)) {
                 var savedResult = typeaheadOptions[i];
                 $('.' + selector + ' .typeahead' + ' input').attr('value', savedResult.id);
-                $('.' + selector + ' .typeahead' + ' input').prop('value', savedResult.title);   
+                $('.' + selector + ' .typeahead' + ' input').prop('value', savedResult[filterOption]);   
             }
         }
     }
--- akvo/rsr/static/scripts-src/myrsr-admin-typeahead.jsx
+++ akvo/rsr/static/scripts-src/myrsr-admin-typeahead.jsx
@@ -1,4 +1,4 @@
-function buildReactComponents(placeholder, typeaheadOptions, typeaheadCallback, displayOption, selector, valueId, label, help) {
+function buildReactComponents(placeholder, typeaheadOptions, typeaheadCallback, displayOption, selector, valueId, label, help, filterOption) {
     var Typeahead, TypeaheadLabel, TypeaheadContainer;
 
     Typeahead = ReactTypeahead.Typeahead;   
@@ -15,7 +15,7 @@ function buildReactComponents(placeholder, typeaheadOptions, typeaheadCallback, 
                             onOptionSelected={typeaheadCallback}
                             maxVisible={10}
                             displayOption={displayOption}
-                            filterOption='title'
+                            filterOption={filterOption}
                             childID={selector}
                             customClasses={{
                               typeahead: "",
@@ -44,7 +44,7 @@ function buildReactComponents(placeholder, typeaheadOptions, typeaheadCallback, 
             if (parseInt(typeaheadOptions[i].id, 10) == parseInt(valueId, 10)) {
                 var savedResult = typeaheadOptions[i];
                 $('.' + selector + ' .typeahead' + ' input').attr('value', savedResult.id);
-                $('.' + selector + ' .typeahead' + ' input').prop('value', savedResult.title);   
+                $('.' + selector + ' .typeahead' + ' input').prop('value', savedResult[filterOption]);   
             }
         }
     }
