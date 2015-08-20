0f289025dfed5ca35ebf43d62b67b7416914e0fe
Gabriel von Heijne <gabriel@akvo.org>
2015-08-20 10:10:28 +0200

    [#1633] Add keywords for sponsor partners
    To prepare for the removal of sponsor partners, create a keyword for
    each organisation that is a sposor partner.
    
    The keywords are of the format <org ID>:<org name> to distinguish them
    from "ordinary" keywords.

---
+++ akvo/rsr/migrations/0026_auto_20150820_0251.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+def keywords_from_sponsor_partners(apps, schema_editor):
+    Keyword = apps.get_model('rsr', 'Keyword')
+    Project = apps.get_model('rsr', 'Project')
+    print
+
+    projects = Project.objects.filter(partnerships__partner_type=u'sponsor')
+    for project in projects:
+        for sponsor in project.partnerships.filter(partner_type='sponsor'):
+            keyword, created = Keyword.objects.get_or_create(
+                label="{}:{}".format(
+                    sponsor.organisation.id,
+                    sponsor.organisation.name,
+                )
+            )
+            project.keywords.add(keyword)
+
+    print "Keyword distribution after migration:"
+    for keyword in Keyword.objects.all():
+        projects = Project.objects.filter(keywords__exact=keyword)
+        print keyword.label, projects.count()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rsr', '0025_auto_20150820_0144'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            keywords_from_sponsor_partners
+        ),
+    ]
