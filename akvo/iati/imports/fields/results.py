999aef6de9129618853153cb3c7f4a6f192071f8
Kasper Brandt <kasperbrandt87@gmail.com>
2015-09-08 19:58:00 +0300

    [#1770] Added indicator period import

--- akvo/iati/imports/fields/results.py
+++ akvo/iati/imports/fields/results.py
@@ -8,13 +8,15 @@ from ..utils import get_text
 
 from django.db.models import get_model
 
+from datetime import datetime
+
 
 def results(activity, project, activities_globals):
     """
     Retrieve and store the result information.
     The results will be extracted from the 'result' elements.
 
-    :param activity: ElementTree; contains all data for the activity
+    :param activity: ElementTree; contains all data of the activity
     :param project: Project instance
     :param activities_globals: Dictionary; contains all global activities information
     :return: List; contains fields that have changed
@@ -56,12 +58,15 @@ def results(activity, project, activities_globals):
             aggregation_status=result_aggr_status,
             description=result_desc_text
         )
-        imported_results.append(res)
-        if created:
-            changes.append(u'added result (id: %s): %s' % (str(res.pk), res))
+        # Disregard double results
+        if not res in imported_results:
+            imported_results.append(res)
+            if created:
+                changes.append(u'added result (id: %s): %s' % (str(res.pk), res))
 
-        for ind_change in indicators(result, res, activities_globals):
-            changes.append(ind_change)
+            # Process indicators
+            for ind_change in indicators(result, res, activities_globals):
+                changes.append(ind_change)
 
     for result in project.results.all():
         if not result in imported_results:
@@ -78,10 +83,9 @@ def indicators(result_element, result, activities_globals):
     Retrieve and store the indicator information of a result.
     The indicators will be extracted from the 'indicator' elements.
 
-    :param result_element: ElementTree; contains all data for the result
+    :param result_element: ElementTree; contains all data of the result
     :param result: Result instance
     :param activities_globals: Dictionary; contains all global activities information
-    :param created: Boolean; indicates whether the result has just been created or not
     :return: List; contains fields that have changed
     """
     imported_indicators = []
@@ -139,11 +143,15 @@ def indicators(result_element, result, activities_globals):
             baseline_value=value,
             baseline_comment=base_comment_text
         )
-        imported_indicators.append(ind)
-        if created:
-            changes.append(u'added indicator (id: %s): %s' % (str(ind.pk), ind))
+        # Disregard double indicators
+        if not ind in imported_indicators:
+            imported_indicators.append(ind)
+            if created:
+                changes.append(u'added indicator (id: %s): %s' % (str(ind.pk), ind))
 
-        # TODO: indicator periods
+            # Process indicator periods
+            for period_change in indicator_periods(indicator, ind, activities_globals):
+                changes.append(period_change)
 
     for indicator in result.indicators.all():
         if not indicator in imported_indicators:
@@ -151,5 +159,86 @@ def indicators(result_element, result, activities_globals):
                            (str(indicator.pk),
                             indicator.__unicode__()))
             indicator.delete()
+
+    return changes
+
+
+def indicator_periods(indicator_element, indicator, activities_globals):
+    """
+    Retrieve and store the indicator period information of an indicator.
+    The indicator periods will be extracted from the 'period' elements.
+
+    :param indicator_element: ElementTree; contains all data of the indicator
+    :param indicator: Indicator instance
+    :param activities_globals: Dictionary; contains all global activities information
+    :return: List; contains fields that have changed
+    """
+    imported_periods = []
+    changes = []
+
+    for period in indicator_element.findall('period'):
+        period_start_element = period.find('period-start')
+        if not period_start_element is None and 'iso-date' in period_start_element.attrib.keys():
+            period_start = datetime.strptime(period_start_element.attrib['iso-date'], '%Y-%m-%d')
+        else:
+            period_start = None
+
+        period_end_element = period.find('period-end')
+        if not period_end_element is None and 'iso-date' in period_end_element.attrib.keys():
+            period_end = datetime.strptime(period_end_element.attrib['iso-date'], '%Y-%m-%d')
+        else:
+            period_end = None
+
+        target_element = period.find('target')
+        if not target_element is None:
+            if 'value' in target_element.attrib.keys():
+                target_value = target_element.attrib['value']
+            else:
+                target_value = ''
+
+            target_comment_element = target_element.find('comment')
+            if not target_comment_element is None:
+                target_comment = get_text(target_comment_element, activities_globals['version'])
+            else:
+                target_comment = ''
+        else:
+            target_value = ''
+            target_comment = ''
+
+        actual_element = period.find('actual')
+        if not actual_element is None:
+            if 'value' in actual_element.attrib.keys():
+                actual_value = actual_element.attrib['value']
+            else:
+                actual_value = ''
+
+            actual_comment_element = actual_element.find('comment')
+            if not actual_comment_element is None:
+                actual_comment = get_text(actual_comment_element, activities_globals['version'])
+            else:
+                actual_comment = ''
+        else:
+            actual_value = ''
+            actual_comment = ''
+
+        per, created = get_model('rsr', 'indicatorperiod').objects.get_or_create(
+            indicator=indicator,
+            period_start=period_start,
+            period_end=period_end,
+            target_value=target_value,
+            target_comment=target_comment,
+            actual_value=actual_value,
+            actual_comment=actual_comment
+        )
+        imported_periods.append(per)
+        if created:
+            changes.append(u'added indicator period (id: %s): %s' % (str(per.pk), per))
+
+    for indicator_period in indicator.periods.all():
+        if not indicator_period in imported_periods:
+            changes.append(u'deleted indicator period (id: %s): %s' %
+                           (str(indicator_period.pk),
+                            indicator_period.__unicode__()))
+            indicator_period.delete()
 
     return changes
