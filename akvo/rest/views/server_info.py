8ca972bd7d00e2dc43967d55df37be3c8e20e405
Gabriel von Heijne <gabriel@akvo.org>
2015-09-17 15:39:26 +0200

    [#1798] Add info about the server hostname and deployment specifics

--- akvo/rest/views/server_info.py
+++ akvo/rest/views/server_info.py
@@ -4,6 +4,7 @@
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
+import socket
 
 from django.conf import settings
 from rest_framework.decorators import api_view
@@ -12,15 +13,17 @@ from rest_framework.response import Response
 
 @api_view(['GET',])
 def server_info(request):
-    """ Info about the server. Currently setting for update edit timeout.
-        Extend the server_setting tuple to expose other settings.
-        Used by Up.
+    """ Info about the server.
+        Used by RSR Up.
     """
     # tuple with settings we want exposed in the endpoint
     server_settings = (
         "PROJECT_UPDATE_TIMEOUT",
+        "DEPLOY_TAG",
+        "DEPLOY_BRANCH",
+        "DEPLOY_COMMIT_ID",
     )
-    settings_dict = {}
+    settings_dict = {'CANONICAL_HOST_NAME': socket.gethostname()}
     for setting in server_settings:
         settings_dict[setting] = getattr(settings, setting, "")
     return Response(settings_dict)
