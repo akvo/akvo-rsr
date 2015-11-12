66afd876f55355cd32ad88f7535d6c9092d39870
Kasper Brandt <kasperbrandt87@gmail.com>
2015-11-12 11:44:06 +0300

    [#1902] Use dict().get() in Partnership model

--- akvo/rsr/models/partnership.py
+++ akvo/rsr/models/partnership.py
@@ -135,16 +135,10 @@ class Partnership(models.Model):
     )
 
     def iati_organisation_role_label(self):
-        if self.iati_organisation_role:
-            return dict(self.IATI_ROLES)[self.iati_organisation_role]
-        else:
-            return None
+        return dict(self.IATI_ROLES).get(self.iati_organisation_role)
 
     def iati_role_to_partner_type(self):
-        if self.iati_organisation_role:
-            return self.ROLES_TO_PARTNER_TYPES_MAP[int(self.iati_organisation_role)]
-        else:
-            return None
+        return dict(self.ROLES_TO_PARTNER_TYPES_MAP).get(int(self.iati_organisation_role))
 
     class Meta:
         app_label = 'rsr'
