fa84ac8c8dc4a81693dcf60578ffe63ae72aee94
Gabriel von Heijne <gabriel@akvo.org>
2015-12-27 22:28:17 +0100

    [#1828] django-crontab setup

---
+++ akvo/rsr/management/commands/iati_import.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Akvo Reporting is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+from django.core.management.base import BaseCommand
+from ...models.iati_import import IatiImport
+
+
+class Command(BaseCommand):
+    help = u"Check all enabled iati_imports to see if they should execute"
+
+    def handle(self, *args, **options):
+
+        iati_imports = (
+            IatiImport.objects.filter(run_immediately=True) |
+            IatiImport.objects.filter(enabled=True)
+        )
+
+        for iati_import in iati_imports:
+            iati_import.check_execution()
+
