df66cb5344f576e79d811f7fcffdf25e4b1e3faa
Gabriel von Heijne <gabriel@akvo.org>
2015-12-27 22:16:12 +0100

    [#1828] Refactor IATI import models
    The models used when importing IATI activity XML are revised.
    See the docstrings of the models for more info.

--- akvo/rsr/models/iati_import.py
+++ akvo/rsr/models/iati_import.py
@@ -1,65 +1,211 @@
 # -*- coding: utf-8 -*-
 
-# Akvo RSR is covered by the GNU Affero General Public License.
+# Akvo Reporting is covered by the GNU Affero General Public License.
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+from datetime import timedelta, datetime
+from importlib import import_module
 
 from django.db import models
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
-
-from .iati_project_import import IatiProjectImport
-
-
-def file_path(self, filename):
-    return 'db/iati_import/%s' % filename
+from akvo.rsr.models.iati_import_log import LOG_ENTRY_TYPE
 
 
 class IatiImport(models.Model):
-    PENDING_STATUS = 1
-    RETRIEVING_STATUS = 2
-    IN_PROGRESS_STATUS = 3
-    COMPLETED_STATUS = 4
-    CANCELLED_STATUS = 5
+    """
+    This model sets up the running of an import of an IATI XML file into RSR on a regular basis.
+    Here we manage the URL from where the XML is fetched, as well as the User running the imports,
+    the frequency the imports are run at and any special code needed to run the import.
 
-    STATUS_CODES = (
-        (PENDING_STATUS, _(u'pending')),
-        (RETRIEVING_STATUS, _(u'retrieving file')),
-        (IN_PROGRESS_STATUS, _(u'import in progress')),
-        (COMPLETED_STATUS, _(u'completed')),
-        (CANCELLED_STATUS, _(u'cancelled'))
+    For each running of the import an IatiImportJob object is created and associated with the parent
+    IatiImport object. Running the job in turn creates a number of IatiActivityImport objects as
+    well as IatiImportLog objects, both types of objects recording the progress of the import.
+
+    Notable methods
+    ===============
+
+    model_mappers()
+    Here we assemble the mapper classes used to map the XML data to the various RSR
+    models. model_mappers() checks IatiImport.mapper_prefix to determine if there are custom sub-
+    classes of the base mapper classes to use in lieu of the parents, this way managing custom needs
+    for the XML in question.
+
+    set_next_execution()
+    Sets the next time the import is to be run. This is done just as an import job is started, so
+    the actual frequency is as close to the actual value of IatiImport.frequency as possible.
+
+    check_execution()
+    Checks if we should run an import job. This can be triggered by it either being a manually
+    started job, typically run from the Django admin, or if it time for a scheduled import.
+
+    execute_import()
+    An actual import is managed by an IatiImportJob object which is either crated in execute_import
+    or supplied to the method when calling it. execute_import() is also responsible for setting the
+    next time a scheduled import is to occur
+    """
+
+    HOURLY = 1
+    EVERY_SIX_HOURS = 2
+    DAILY = 3
+    EVERY_THREE_DAYS = 4
+    WEEKLY = 5
+    BI_WEEKLY = 6
+    EVERY_FOUR_WEEKS = 7
+    # EVERY_TWO_MINUTES = 8 # used for testing
+    FREQUENCIES = (
+        (HOURLY, _('hourly'),),
+        (EVERY_SIX_HOURS, _('every six hours'),),
+        (DAILY, _('daily'),),
+        (EVERY_THREE_DAYS, _('every three days'),),
+        (WEEKLY, _('weekly'),),
+        (BI_WEEKLY, _('bi-weekly'),),
+        (EVERY_FOUR_WEEKS, _('every four weeks'),),
+        # (EVERY_TWO_MINUTES, _('every two minutes'),), # used for testing
     )
 
+    label = models.CharField(max_length=50, verbose_name=_(u'label'), unique=True)
+    next_execution = models.DateTimeField(
+            verbose_name=_(u'next time the import is run'), null=True, blank=True)
+    frequency = models.PositiveIntegerField(choices=FREQUENCIES)
+    # TODO: set to request.user in admin
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, verbose_name=_(u'user'), related_name='iati_imports'
-    )
+            settings.AUTH_USER_MODEL, verbose_name=_(u'user'), related_name='iati_imports',)
     url = models.URLField(_(u'url'), blank=True)
-    local_file = models.FileField(_(u'local file'), blank=True, upload_to=file_path)
-    status = models.PositiveSmallIntegerField(_(u'status'), choices=STATUS_CODES,
-                                              default=PENDING_STATUS)
-    start_date = models.DateTimeField(_(u'start date'), null=True, blank=True)
-    end_date = models.DateTimeField(_(u'end date'), null=True, blank=True)
-    projects = models.ManyToManyField(
-        'Project', verbose_name=_(u'projects'), through=IatiProjectImport, blank=True
-    )
+    # TODO: make the prefixes a list of available modules in iati/imports/mappers
+    mapper_prefix = models.CharField(
+            max_length=30, verbose_name=_(u'mapper prefix'), blank=True,
+            help_text='Prefix used to identify child classes overriding default import behaviour')
+    enabled = models.BooleanField(verbose_name=_(u'scheduled importing enabled'), default=False,
+                                  help_text='Set to enable scheduled running of this import.')
+    run_immediately = models.BooleanField(verbose_name=_(u'run immediately'), default=False,
+                                  help_text='Run the job immediately. Overrides the enabled state.')
+    running = models.BooleanField(verbose_name=_(u'import currently running'), default=False,
+            help_text='Running is set while the import executes. This is to guarantee that the same '
+                      'import never runs twice (or more) in parallel.')
+
 
     def __unicode__(self):
-        if self.url:
-            return u'%s: %s (%s)' % (_(u'IATI import'),
-                                     self.url,
-                                     self.show_status())
-        elif self.local_file:
-            return u'%s: %s (%s)' % (_(u'IATI import'),
-                                     str(self.local_file).rsplit('/',1)[1],
-                                     self.show_status())
-        else:
-            return u'%s: %s' % (_(u'IATI import'),
-                                _(u'no file'))
+        return unicode(_(u'IATI import (ID {}): {}'.format(self.pk, self.label)))
 
     class Meta:
         app_label = 'rsr'
         verbose_name = _(u'IATI import')
         verbose_name_plural = _(u'IATI imports')
 
-    def show_status(self):
-        return dict(map(lambda x: x, self.STATUS_CODES))[self.status]
+    def  model_mappers(self):
+        """
+        Import the ImportMapper subclasses for this IatiImport instance.
+        See ImportMapper docstring for more info.
+        :return: list of classes to use in IatiActivityImport.do_import()
+        """
+
+        from ...iati.imports import mappers
+        # assign all base class names to mapper_class_names
+        from ...iati.imports.mappers import __all__ as mapper_class_names
+        klasses = []
+
+        for class_name in mapper_class_names:
+            klass = getattr(mappers, class_name)
+            # if a prefix is set for this import, look for a submodule with that name
+            if self.mapper_prefix:
+                module = import_module(
+                        'akvo.iati.imports.mappers.{}'.format(self.mapper_prefix))
+                if getattr(module, class_name, None):
+                    klass = getattr(module, class_name)
+            klasses.append(klass)
+
+        return klasses
+
+    def set_next_execution(self):
+        """
+        Set self.next_execution to the next time the import is going to be run. If run_on_save is
+        set, time is set to now()
+        """
+        time_adds = {
+            # self.EVERY_TWO_MINUTES: timedelta(seconds=120), # used for testing
+            self.HOURLY: timedelta(hours=1),
+            self.EVERY_SIX_HOURS: timedelta(hours=6),
+            self.DAILY: timedelta(days=1),
+            self.EVERY_THREE_DAYS: timedelta(days=3),
+            self.WEEKLY: timedelta(weeks=1),
+            self.BI_WEEKLY: timedelta(weeks=2),
+            self.EVERY_FOUR_WEEKS: timedelta(weeks=4),
+        }
+        if self.run_immediately:
+            self.next_execution = datetime.now()
+        else:
+            if not self.next_execution:
+                self.next_execution = datetime.now() + time_adds[self.frequency]
+            else:
+                self.next_execution += time_adds[self.frequency]
+
+    def save(self, *args, **kwargs):
+        self.set_next_execution()
+        super(IatiImport, self).save(*args, **kwargs)
+
+    def it_is_time_to_execute(self):
+        return self.enabled and self.next_execution and self.next_execution < datetime.now()
+
+    def check_execution(self):
+        """
+        Checks if we should run a job on this import. This is true if either:
+            1) we find a job object with pending status, in that case this is a manual run initiated
+               in the admin
+            2) if no job is present we check if it's time for a scheduled execution of the import
+        :param job: an IatiImportJob object
+        :return:
+        """
+        from .iati_import_job import IatiImportJob
+        from .iati_import_log import IatiImportLog
+
+        try:
+            job = IatiImportJob.objects.get(iati_import=self, status=LOG_ENTRY_TYPE.STATUS_PENDING)
+        except IatiImportJob.DoesNotExist:
+            job = None
+        except IatiImportJob.MultipleObjectsReturned:
+            for job in IatiImportJob.objects.filter(
+                    iati_import=self, status=LOG_ENTRY_TYPE.STATUS_PENDING):
+                IatiImportLog.objects.create(
+                    iati_import_job=job,
+                    text="Fatal error executing import. Multiple pending jobs found.",
+                    created_at=datetime.now()
+                )
+            return
+        if self.run_immediately or self.it_is_time_to_execute():
+            self.run_immediately = False
+            self.execute_import(job)
+
+    def execute_import(self, job=None):
+        """
+        Execute the import by instantiating and running an IatiImportJob. The job may already exist,
+        or it can be created in this method.
+        :param job: IatiImportJob object; if present this is a manually initiated execution
+        :return:
+        """
+        from .iati_import_job import IatiImportJob
+        from .iati_import_log import IatiImportLog
+
+        # safe-guard against running the same import in parallel
+        # TODO: this only records that a job is running, we should check for this before starting a
+        # new job too!
+        self.running = True
+        self.save()
+
+        try:
+            self.set_next_execution()
+            if not job:
+                job = IatiImportJob(iati_import=self)
+                job.save()
+            job.run()
+            job.save()
+        except Exception as e:
+            IatiImportLog.objects.create(
+                iati_import_job=job,
+                text="Fatal error executing import. Error message:\n{}".format(e.message),
+                created_at=datetime.now()
+            )
+        finally:
+            self.running = False
+            self.save()
---
+++ akvo/rsr/models/iati_import_job.py
@@ -0,0 +1,317 @@
+# -*- coding: utf-8 -*-
+
+# Akvo Reporting is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+# -*- coding: utf-8 -*-
+
+# Akvo RSR is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+import hashlib
+import inspect
+import urllib2
+
+from datetime import datetime
+from lxml import etree
+
+from django.conf import settings
+from django.core import urlresolvers
+from django.core.files import File
+from django.core.files.temp import NamedTemporaryFile
+from django.db import models, transaction, IntegrityError
+from django.utils.translation import ugettext_lazy as _
+
+from akvo.rsr.models.iati_import_log import LOG_ENTRY_TYPE, IatiImportLogCollector
+from akvo.utils import rsr_send_mail
+
+
+def file_path(self, filename):
+    return 'db/iati_import/{}'.format(filename)
+
+class IatiImportJob(models.Model):
+    """
+    The IatiImportJob model records the running of an import once.
+    Fields:
+    iati_import: the IAtiImport object the job is for
+    iati_xml_file: The IATI XML file the import is run for
+    status: is used to show various stages of the import and if the job was completed or cancelled
+    projects: all projects that were created or updated by the job
+    sha1_hexdigest: used to determine if the file has changed compared to the previous job
+    """
+    SUPPORTED_IATI_VERSIONS = ['1.01', '1.02', '1.03', '1.04', '1.05', '2.01',]
+    STATUS_CODES = (
+        (LOG_ENTRY_TYPE.STATUS_PENDING, _(u'pending')),
+        (LOG_ENTRY_TYPE.STATUS_IN_PROGRESS, _(u'in progress')),
+        (LOG_ENTRY_TYPE.STATUS_RETRIEVING, _(u'retrieving')),
+        (LOG_ENTRY_TYPE.STATUS_COMPLETED, _(u'completed')),
+        (LOG_ENTRY_TYPE.STATUS_CANCELLED, _(u'cancelled')),
+    )
+
+    iati_import = models.ForeignKey('IatiImport', related_name='jobs')
+    iati_xml_file = models.FileField(_(u'local file'), blank=True, upload_to=file_path)
+    status = models.PositiveSmallIntegerField(
+            _(u'status'), choices=STATUS_CODES, default=LOG_ENTRY_TYPE.STATUS_PENDING)
+    projects = models.ManyToManyField(
+        'Project', verbose_name=_(u'projects'), through='IatiActivityImport', blank=True
+    )
+    sha1_hexdigest = models.CharField(
+            max_length=40, verbose_name=_(u'sha1 hexadecimal digest of the XML file'), blank=True,)
+
+    def __init__(self, *args, **kwargs):
+        self._log_objects = []
+        super(IatiImportJob, self).__init__(*args, **kwargs)
+
+    def __unicode__(self):
+        return unicode(u'IATI import job (ID: {}) for {}'.format(self.pk, self.iati_import))
+        # return unicode(u'IATI import job (ID: {})'.format(self.pk))
+
+    class Meta:
+        app_label = 'rsr'
+        verbose_name = _(u'IATI import job')
+        verbose_name_plural = _(u'IATI import jobs')
+        get_latest_by = 'iati_import_logs__created_at'
+
+    def add_log(self, text, message_type=LOG_ENTRY_TYPE.VALUE_NOT_SAVED):
+        """
+        Add a log entry in the IatiImportLog model.
+
+        :param text: String; the log message text
+        :param severity: Integer, must be one of LOG_ENTRY_TYPE
+        """
+        if message_type in [code for code, label in self.STATUS_CODES]:
+            self.status = message_type
+        # get the name of the function where add_log was called
+        from .iati_import_log import IatiImportLog
+        caller = inspect.stack()[1][3]
+        # prefix log text with model and method
+        text = "IatiImportJob.{}(): {}".format(caller, text)
+
+        IatiImportLog.objects.create(
+            iati_import_job=self,
+            message_type=message_type,
+            text=text,
+            created_at=datetime.now(),
+        )
+
+    def save_import_logs(self, iati_activity_import):
+        # TODO: Tried using bulk_create here, but got  a pk constraint integrity error, on the
+        # IatiImportLog. Really weird. May have to do with the transactions we're in. But
+        # bulk_create is much more efficient if one can get it to work.
+
+        for log_obj in iati_activity_import._log_objects:
+            # adding the iati_activity_import object here because when doing it in the mappers the
+            # iati_activity_import object isn't instantiated yet resulting in the FK being None :-(
+            log_obj.iati_activity_import = iati_activity_import
+            # if the project associated with the log object was deleted, we get an integrity error
+            # here, since we're referring to a project that doesn't exist
+            try:
+                log_obj.save()
+            # in that case, check that project_id is mentioned in the error message, if so remove
+            # the FK to the now non-existing project and save again.
+            except IntegrityError as e:
+                if e.message.find('project_id'):
+                    log_obj.project = None
+                    log_obj.save()
+
+    def admin_url(self):
+        return u'<a href="{}">{}</a>'.format(
+                urlresolvers.reverse('admin:rsr_iatiimportjob_change', args=(self.pk,)),
+                self.__unicode__())
+
+    admin_url.short_description = "IATI import job"
+    admin_url.allow_tags = True
+
+
+    def show_status(self):
+        return dict(map(lambda x: x, self.STATUS_CODES))[self.status]
+
+    def send_mail(self):
+        """
+        Send an email to the RSR admins (gathered from settings) and the user linked to the
+        IATI import instance.
+        """
+
+        email_addresses = [email for _name, email in settings.ADMINS]
+        email_addresses.append(self.iati_import.user.email)
+
+        rsr_send_mail(
+            email_addresses,
+            subject='iati_import/import_done_subject.txt',
+            message='iati_import/import_done_message.txt',
+            subject_context={
+                'iati_import': self.iati_import
+            },
+            msg_context={
+                'iati_import': self.iati_import,
+                'project_count': self.projects.count(),
+                'projects_created': self.iati_import_logs.filter(
+                        message_type=LOG_ENTRY_TYPE.ACTION_CREATE).count(),
+                'projects_updated': self.iati_import_logs.filter(
+                    message_type=LOG_ENTRY_TYPE.ACTION_UPDATE).count(),
+                'projects_published': self.projects.all().published().count(),
+                'critical_errors_log': self.iati_import_logs.filter(
+                    message_type=LOG_ENTRY_TYPE.CRITICAL_ERROR
+                ),
+                'warnings_log': self.iati_import_logs.filter(message_type__in=[
+                    LOG_ENTRY_TYPE.VALUE_PARTLY_SAVED, LOG_ENTRY_TYPE.VALUE_NOT_SAVED
+                ]),
+                'projects_log': self.iati_import_logs.all()
+            },
+            html_message='iati_import/import_done_message.html'
+        )
+
+    def check_version(self):
+        """
+        Check if the version of the IATI XML file is specified and supported.
+        """
+        version = self.activities.attrib.get('version')
+        if version:
+            if version in self.SUPPORTED_IATI_VERSIONS:
+                return True
+            else:
+                self.add_log(
+                        'IATI Version %s not supported' % version, LOG_ENTRY_TYPE.CRITICAL_ERROR)
+                return False
+        else:
+            self.add_log('No version specified', LOG_ENTRY_TYPE.CRITICAL_ERROR)
+            return False
+
+    def get_activities(self):
+        """
+        Get the activities from iati_import.local_file.
+
+        :return: ElementTree; the root node of the XML or False when a critical error precludes the
+                 continuation of the import
+        """
+        if self.iati_xml_file:
+            try:
+                parsed_xml = etree.parse(self.iati_xml_file)
+            except Exception as e:
+                self.add_log('Error parsing XML file. Error message:\n{}'.format(e.message),
+                             LOG_ENTRY_TYPE.CRITICAL_ERROR)
+                return False
+
+            activities = parsed_xml.getroot()
+            if activities.tag == 'iati-activities':
+                self.add_log(
+                        'Retrieved {} activities'.format(len(activities.findall('iati-activity'))),
+                        LOG_ENTRY_TYPE.INFORMATIONAL)
+                return activities
+            else:
+                self.add_log('Not a valid IATI XML file, '
+                             'iati-activities not root of document.', LOG_ENTRY_TYPE.CRITICAL_ERROR)
+                return False
+        else:
+            self.add_log('No file found', LOG_ENTRY_TYPE.CRITICAL_ERROR)
+            return False
+
+    def fetch_file(self):
+        """
+        Download the file from iati_import.url and store it in iati_import.local_file.
+        """
+        tmp_file = NamedTemporaryFile(delete=True)
+        tmp_file.write(urllib2.urlopen(self.iati_import.url, timeout=100).read())
+        tmp_file.flush()
+        filename = u'iati_import_%s.xml' % str(self.pk)
+        self.iati_xml_file.save(filename, File(tmp_file))
+        self.add_log('Downloaded file from %s' % str(self.iati_import.url),
+                     LOG_ENTRY_TYPE.INFORMATIONAL)
+
+    def check_file(self):
+        """
+        Check if the IATI import has a local_file or url specified. Retrieve the file if
+        only the URL is specified.
+        """
+        if self.iati_xml_file:
+            # File already present, means manual import.
+            self.add_log('Using uploaded file: {}'.format(self.iati_xml_file), LOG_ENTRY_TYPE.INFORMATIONAL)
+            return True
+
+        elif self.iati_import.url:
+            # No file, but URL specified. Download file from URL.
+            self.add_log('Fetching file from URL: {}'.format(self.iati_import.url),
+                         LOG_ENTRY_TYPE.INFORMATIONAL)
+            try:
+                self.fetch_file()
+                return True
+            except Exception as e:
+                self.add_log('Error while fetching file from URL. '
+                             'Error message:\n%s'.format(e.message),
+                             LOG_ENTRY_TYPE.CRITICAL_ERROR)
+        else:
+        # No file or URL specified.
+            self.add_log('No file or URL specified', LOG_ENTRY_TYPE.CRITICAL_ERROR)
+
+        self.add_log(u"Import cancelled. File missing.", LOG_ENTRY_TYPE.STATUS_CANCELLED)
+        return False
+
+    def is_new_file(self):
+        """ Returns True if this job's xml file is the same as the previous job's, otherwise False.
+         Sameness is based on the sha1 hash of the file.
+        """
+        if self.iati_xml_file:
+            f = self.iati_xml_file.file
+            f.open('rb')
+            hash = hashlib.sha1()
+            if f.multiple_chunks():
+                for chunk in f.chunks():
+                    hash.update(chunk)
+            else:
+                hash.update(f.read())
+            # re-open the file for further processing
+            f.open('r')
+            self.sha1_hexdigest = hash.hexdigest()
+            self.save()
+
+        try:
+            previous_job = IatiImportJob.objects.exclude(pk=self.pk).latest()
+        except IatiImportJob.DoesNotExist:
+            previous_job = None
+
+        if previous_job and previous_job.sha1_hexdigest == self.sha1_hexdigest:
+            self.add_log(u"Import cancelled since the file hasn't changed since last import.",
+                         LOG_ENTRY_TYPE.STATUS_CANCELLED)
+            return False
+
+        return True
+
+    def run(self):
+        """
+        """
+
+        from akvo.rsr.models.iati_activity_import import IatiActivityImport
+
+        # Start initialize
+        self.add_log(u'Starting import job.', LOG_ENTRY_TYPE.INFORMATIONAL)
+        self.add_log(u'Fetching and parsing XML file.', LOG_ENTRY_TYPE.STATUS_RETRIEVING)
+        if self.check_file() and self.is_new_file():
+
+            # Start import process
+            self.activities = self.get_activities()
+            if self.activities and self.check_version():
+                self.add_log(u'Importing activities.', LOG_ENTRY_TYPE.STATUS_IN_PROGRESS)
+                for activity in self.activities.findall('iati-activity'):
+
+                    iati_activity_import = None
+                    try:
+                        with transaction.atomic():
+                            iati_activity_import = IatiActivityImport(
+                                    iati_import_job=self, activity_xml=etree.tostring(activity))
+                            iati_activity_import.do_import(self.activities.attrib)
+                            iati_activity_import.save()
+
+                    except Exception as e:
+                        self.add_log(u"Error when running import of activity. "
+                                     u"Error message:\n{}".format(e.message),
+                                     LOG_ENTRY_TYPE.CRITICAL_ERROR)
+
+                    if iati_activity_import:
+                        self.save_import_logs(iati_activity_import)
+
+                # Import process complete
+                self.add_log(u'Import finished.', LOG_ENTRY_TYPE.STATUS_COMPLETED)
+        self.send_mail()
+        self.add_log(u'Job finished.', LOG_ENTRY_TYPE.INFORMATIONAL)
