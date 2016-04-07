dd444d5c725b18006e71e2e1af110e99f7b936bf
Gabriel von Heijne <gabriel@akvo.org>
2016-04-07 21:37:22 +0200

    [#2095] Rewrite of RSRGenericFilterBackend.filter_queryset()
    Total rewrite using ast.literal_eval().
    
    Using literal_eval() allows the use of literal python dictionaries as the
    query string param values. So one dict is enough for all filter clauses,
    and one can be specified for use in queryset.exclude() in the same
    manner.
    
    Also added the query string params select_related and prefetch related,
    that take a python list literal of model names to be included in calls to
    queryset.select_related() and .prefetch_related(). See the docstring for
    an example.

--- akvo/rest/filters.py
+++ akvo/rest/filters.py
@@ -4,6 +4,7 @@
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
+import ast
 from rest_framework import filters
 
 
@@ -12,32 +13,47 @@ class RSRGenericFilterBackend(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         """
         Return a queryset possibly filtered by query param values.
-        The filter looks for the query param key filter and then filter2, filter3... up to filter24
-        For each of these query param keys it uses the value to construct a filter for the
-        queryset using the full Django queryset field lookups.
-        This is done by splitting the value on = and assigning the bits to the key and value of a
-        dict used as kwarg to filter()
+        The filter looks for the query param keys filter and exclude
+        For each of these query param the value is evaluated using ast.literal_eval() and used as
+        kwargs in queryset.filter and queryset.exclude respectively.
 
-        Example URL:
-            http://rsr.akvo.org/rest/v1/project/?filter=title__icontains=water&filter2=currency=EUR
+        Example URLs:
+            http://rsr.akvo.org/rest/v1/project/?filter={'title__icontains':'water','currency':'EUR'}
+            http://rsr.akvo.org/rest/v1/project/?filter={'title__icontains':'water'}&exclude={'currency':'EUR'}
 
-        TODO: this is a tech demo, there are a bunch of things that need addressing if we're to use
-        this, like supporting __in=[1 2 3], adding params so you can set select_related and
-        prefetch_related on models used in the filters and probably more.
+        It's also possible to specify models to be included in select_related() and
+        prefetch_related() calls on the queryset, but specifying these in lists of strings as the
+        values for the query sting params select_relates and prefetch_related.
+
+        Example:
+            http://rsr.akvo.org/rest/v1/project/?filter={'partners__in':[42,43]}&prefetch_related=['partners']
         """
-        def param_to_kwarg(param):
-            "split a string on = and return the bits as key and value of a dict"
-            bits = param.split('=')
-            return {bits[0]: bits[1]}
+        filter = request.QUERY_PARAMS.get('filter', None)
+        try:
+            filter_kwargs = ast.literal_eval(filter)
+            queryset = queryset.filter(**filter_kwargs)
+        except ValueError:
+            pass
 
-        params = request.QUERY_PARAMS
+        exclude = request.QUERY_PARAMS.get('exclude', None)
+        try:
+            exclude_kwargs = ast.literal_eval(exclude)
+            queryset = queryset.exclude(**exclude_kwargs)
+        except ValueError:
+            pass
 
-        if params.get('filter', False):
-            for i in [''] + range(2, 25):
-                key = 'filter{}'.format(i)
-                if params.get(key, False):
-                    queryset = queryset.filter(**param_to_kwarg(params[key]))
-                else:
-                    break
+        select_related = request.QUERY_PARAMS.get('select_related', None)
+        try:
+            select_related_args = ast.literal_eval(select_related)
+            queryset = queryset.select_related(*select_related_args)
+        except ValueError:
+            pass
 
-            return queryset
+        prefetch_related = request.QUERY_PARAMS.get('prefetch_related', None)
+        try:
+            prefetch_related_args = ast.literal_eval(prefetch_related)
+            queryset = queryset.prefetch_related(*prefetch_related_args)
+        except ValueError:
+            pass
+
+        return queryset
