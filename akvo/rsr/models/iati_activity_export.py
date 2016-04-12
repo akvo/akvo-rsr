62a59e2bd684cd6c52d6ffb57730b70dffba0cb9
Kasper Brandt <kasperbrandt87@gmail.com>
2016-04-12 19:12:09 +0200

    [#2102] Move IATI export to a cron job implementation

---
+++ akvo/rsr/models/iati_activity_export.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# Akvo RSR is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+from akvo.rsr.mixins import TimestampsMixin
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+STATUS_CODE = {
+    1: _(u'in progress'),
+    2: _(u'finished')
+}
+
+
+class IatiActivityExport(TimestampsMixin):
+    """
+    The IatiActivityExport tracks the export of one IATI activity. Each project exported by an
+    IATI export will have a IatiActivityExport record.
+    """
+    iati_export = models.ForeignKey('IatiExport', related_name='iati_activity_exports')
+    project = models.ForeignKey(
+        'Project', verbose_name=_(u'project'), related_name='iati_project_exports', null=True
+    )
+    status = models.PositiveSmallIntegerField(_(u'status'), default=1)
+
+    class Meta:
+        app_label = 'rsr'
+        verbose_name = _(u'IATI activity export')
+        verbose_name_plural = _(u'IATI activity exports')
+
+    def __unicode__(self):
+        if self.project and self.project.title:
+            return unicode(_('IATI export for {0}'.format(self.project.title)))
+        else:
+            return unicode(_('IATI export for project with no title'))
+
+    def show_status(self):
+        if self.status not in STATUS_CODE.keys():
+            return _(u'unknown status')
+        else:
+            return STATUS_CODE[int(self.status)].title()
