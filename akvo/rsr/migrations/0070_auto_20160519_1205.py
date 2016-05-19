9510897bf54784c8e259ac941ce6885e3d9a9608
Kasper Brandt <kasperbrandt87@gmail.com>
2016-05-19 12:06:21 +0200

    [#1999] Update the helptexts for keywords

--- akvo/rsr/migrations/0070_auto_20160519_1205.py
+++ akvo/rsr/migrations/0070_auto_20160519_1205.py
@@ -13,6 +13,12 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AlterField(
+            model_name='keyword',
+            name='label',
+            field=akvo.rsr.fields.ValidXMLCharField(help_text='Select keywords in case you are using an Akvo Page. Keywords linked to a project will determine if a project appears on the Akvo Page or not.', unique=True, max_length=30, verbose_name='label', db_index=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
             model_name='sector',
             name='sector_code',
             field=akvo.rsr.fields.ValidXMLCharField(help_text='It is possible to specify a variety of sector codes, based on the selected vocabulary. The sector codes for the DAC-5 and DAC-3 vocabularies can be found here: <a href="http://iatistandard.org/202/codelists/Sector/" target="_blank">DAC-5 sector codes</a> and <a href="http://iatistandard.org/202/codelists/SectorCategory/" target="_blank">DAC-3 sector codes</a>.', max_length=25, verbose_name='sector code', blank=True),
