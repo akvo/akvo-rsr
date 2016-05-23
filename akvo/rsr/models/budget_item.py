1f7402081bfa29c87a8d29a4d82d2f53b6f2f3c9
Damien Allen <contact@dallen.co>
2016-05-23 10:14:50 +0200

    [Fixes #1801] Further currency changes to project editor

--- akvo/rsr/models/budget_item.py
+++ akvo/rsr/models/budget_item.py
@@ -88,7 +88,7 @@ class BudgetItem(models.Model):
             budget_unicode = u'%s' % _(u'No budget item specified')
 
         if self.amount:
-            budget_unicode += u' - %s %s' % (self.project.currency,
+            budget_unicode += u' - %s %s' % (self.currency,
                                              unicode('{:,}'.format(int(self.amount))))
         else:
             budget_unicode += u' - %s' % _(u'No amount specified')
