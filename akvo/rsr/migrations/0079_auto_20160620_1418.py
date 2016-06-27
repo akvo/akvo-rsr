84a50ce7a79b713c4380e25015ba4f1666bc5569
Kasper Brandt <kasperbrandt87@gmail.com>
2016-06-27 15:53:35 +0200

    [#2223] Removed comment of old migration

--- akvo/rsr/migrations/0079_auto_20160620_1418.py
+++ akvo/rsr/migrations/0079_auto_20160620_1418.py
@@ -28,7 +28,6 @@ def undo_sector_validation(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        # ('rsr', '0077_auto_20160608_1227'),
         ('rsr', '0078_auto_20160613_1428'),
     ]
 
