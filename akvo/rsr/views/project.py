f0a8bb0357e134358a1e08ae6084076f9e51b92b
Damien Allen <contact@dallen.co>
2016-06-30 11:38:08 +0200

    [Fixes #2270] Only show public projects in hierarchy

--- akvo/rsr/views/project.py
+++ akvo/rsr/views/project.py
@@ -219,9 +219,9 @@ def _get_hierarchy_row(max_rows, projects):
 
 
 def _get_hierarchy_grid(project):
-    parents = project.parents().published()
-    siblings = project.siblings().published()
-    children = project.children().published()
+    parents = project.parents().published().public()
+    siblings = project.siblings().published().public()
+    children = project.children().published().public()
 
     # Create the lay-out of the grid
     max_rows = max(parents.count(), siblings.count() + 1, children.count())
