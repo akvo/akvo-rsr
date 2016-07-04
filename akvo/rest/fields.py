6c45a2e71bc8d4c8fe4d0afe36f95d0b99b2283e
Damien Allen <contact@dallen.co>
2016-07-04 10:18:35 +0200

    [Fixes #2273] Catch IOError for thumbnails in API calls

--- akvo/rest/fields.py
+++ akvo/rest/fields.py
@@ -126,12 +126,18 @@ class Base64ImageField(ImageField):
                         return get_thumbnail(value, '{}'.format(max_size), quality=99)
                 except ThumbnailParseError:
                     return None
+                except IOError:
+                    return None
             # no size specification matching the name found; give up
             return None
 
         if value:
             default_width = '191' # width of update images on akvo.org/seeithappen
-            default_thumb = get_thumbnail(value, default_width, quality=99)
+            try:
+                default_thumb = get_thumbnail(value, default_width, quality=99)
+            except IOError:
+                return None
+
             try:
                 request = self.context['request']
             except KeyError:
