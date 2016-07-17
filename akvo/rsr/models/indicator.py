1dbffe070ffcb318b96275318610881aadf71164
Kasper Brandt <kasperbrandt87@gmail.com>
2016-07-17 15:38:00 +0200

    [#2181] Small code refactor

--- akvo/rsr/models/indicator.py
+++ akvo/rsr/models/indicator.py
@@ -803,15 +803,18 @@ class IndicatorPeriodData(TimestampsMixin, models.Model):
             raise ValidationError(validation_errors)
 
         # Don't allow a data update to an aggregated parent period with 'percentage' as measurement
+        if self.period.indicator.children_aggregate_percentage():
+            validation_errors['period'] = unicode(
+                _(u'Indicator period has an average aggregate of the child projects. Disable '
+                  u'aggregations to add data to it'))
+            raise ValidationError(validation_errors)
+        
         parent_period = self.period.parent_period()
         if parent_period and parent_period.indicator.measure == '2':
             aggregate_from_children = parent_period.indicator.result.project.aggregate_children
             aggregate_to_parent = self.period.indicator.result.project.aggregate_to_parent
             if aggregate_from_children and aggregate_to_parent:
-                validation_errors['period'] = unicode(
-                    _(u'Indicator period has an average aggregate of the child projects. Disable '
-                      u'aggregations to add data to it'))
-                raise ValidationError(validation_errors)
+
 
         if self.pk:
             orig = IndicatorPeriodData.objects.get(pk=self.pk)
