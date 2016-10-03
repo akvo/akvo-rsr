31cb1be60eb287d9b380e8c33b169e7115683002
Puneeth Chaganti <punchagan@muse-amuse.in>
2016-10-03 16:28:06 +0530

    [#2366] Make arrows smaller and move them around
    To avoid overlaps, the arrows are placed at slightly different positions
    based on the index of the connector for which we are using an arrow, and
    the curviness of the connectors is also changed.

--- akvo/rsr/static/scripts-src/project-hierarchy.js
+++ akvo/rsr/static/scripts-src/project-hierarchy.js
@@ -2,22 +2,28 @@ jsPlumb.ready(function() {
     var instance = jsPlumb.getInstance({
         Connector : [ "Bezier", { curviness:20 } ],
         DragOptions : { cursor: "pointer", zIndex:200 },
-        PaintStyle : { strokeStyle:"grey", lineWidth:2 },
-        EndpointStyle : { radius:3, fillStyle:"grey" },
+        PaintStyle : { strokeStyle:"grey", lineWidth:1 },
+        EndpointStyle : { radius:2, fillStyle:"grey" },
         HoverPaintStyle : {strokeStyle:"orange" },
         EndpointHoverStyle : {fillStyle:"orange" },
         Container:"project-hierarchy"
     });
 
     instance.doWhileSuspended(function() {
-        var arrowCommon = { foldback:0.7, fillStyle:"grey", width:7 },
+        var arrowCommon = { foldback:0.7, fillStyle:"grey", width:7, length:7 },
             arrow = [
                 [ "Arrow", { location:0.5 }, arrowCommon ]
-            ],
-            two_arrows = [
-                [ "Arrow", { location:0.7 }, arrowCommon ],
-                [ "Arrow", { location:0.3, direction:-1 }, arrowCommon ]
             ];
+
+        // Function to off-set arrows based on the index of connector on the
+        // element for which we are using the arrows
+        var get_two_arrows = function (index) {
+            var two_arrows = [
+                [ "Arrow", { location:Math.max(0.9 - index*0.1, 0.55) }, arrowCommon ],
+                [ "Arrow", { location:Math.min(0.1 + index*0.1, 0.45), direction:-1 }, arrowCommon ]
+            ];
+            return two_arrows;
+        }
 
         var project_window = $("#project-hierarchy-project");
         var parent_windows = $(".project-hierarchy .project-hierarchy-parent");
@@ -42,22 +48,38 @@ jsPlumb.ready(function() {
         for (i = 0; i < parent_windows.length; i++) {
             var parent_uuid = parent_windows[i].getAttribute("id") + "-right";
             instance.addEndpoint(parent_windows[i], {uuid:parent_uuid, anchor:"Right"});
-            instance.connect({uuids:[parent_uuid, "project-left"], overlays:arrow});
+            instance.connect({
+                uuids:[parent_uuid, "project-left"],
+                overlays:arrow,
+                connector:["Bezier", {curviness:(i+1) * 20}]
+            });
         }
         for (i = 0; i < top_sibling_windows.length; i++) {
             var top_sibling_uuid = top_sibling_windows[i].getAttribute("id") + "-bottom";
             instance.addEndpoint(top_sibling_windows[i], {uuid:top_sibling_uuid, anchor:"Bottom"});
-            instance.connect({uuids:[top_sibling_uuid, "project-top"], overlays:two_arrows});
+            instance.connect({
+                uuids:[top_sibling_uuid, "project-top"],
+                overlays:get_two_arrows(i),
+                connector:["Bezier", {curviness:(i+1) * 20}]
+            });
         }
         for (i = 0; i < bottom_sibling_windows.length; i++) {
             var bottom_sibling_uuid = bottom_sibling_windows[i].getAttribute("id") + "-top";
             instance.addEndpoint(bottom_sibling_windows[i], {uuid:bottom_sibling_uuid, anchor:"Top"});
-            instance.connect({uuids:[bottom_sibling_uuid, "project-bottom"], overlays:two_arrows});
+            instance.connect({
+                uuids:[bottom_sibling_uuid, "project-bottom"],
+                overlays:get_two_arrows(i),
+                connector:["Bezier", {curviness:(i+1) * 20}]
+            });
         }
         for (i = 0; i < child_windows.length; i++) {
             var child_uuid = child_windows[i].getAttribute("id") + "-left";
             instance.addEndpoint(child_windows[i], {uuid:child_uuid, anchor:"Left"});
-            instance.connect({uuids:["project-right", child_uuid], overlays:arrow});
+            instance.connect({
+                uuids:["project-right", child_uuid],
+                overlays:arrow,
+                connector:["Bezier", {curviness:(i+1) * 20}]
+            });
         }
     });
 });
