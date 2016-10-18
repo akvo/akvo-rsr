4c5778c1c96ee0a25260fe747f63836112abb3a6
Gabriel von Heijne <gabriel@akvo.org>
2016-10-18 17:38:29 +0200

    [#2298] Add fields and create method to IndicatorPeriodDataFrameworkSerializer

--- akvo/rest/serializers/indicator_period_data.py
+++ akvo/rest/serializers/indicator_period_data.py
@@ -3,12 +3,12 @@
 # Akvo RSR is covered by the GNU Affero General Public License.
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
-
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from akvo.rest.serializers.rsr_serializer import BaseRSRSerializer
 from akvo.rest.serializers.user import UserDetailsSerializer
-from akvo.rsr.models import IndicatorPeriodData, IndicatorPeriodDataComment
+from akvo.rsr.models import IndicatorPeriod, IndicatorPeriodData, IndicatorPeriodDataComment
 
 
 class IndicatorPeriodDataCommentSerializer(BaseRSRSerializer):
@@ -32,7 +32,9 @@ class IndicatorPeriodDataSerializer(BaseRSRSerializer):
 
 class IndicatorPeriodDataFrameworkSerializer(BaseRSRSerializer):
 
-    comments = IndicatorPeriodDataCommentSerializer(many=True, required=False)
+    period = serializers.PrimaryKeyRelatedField(queryset=IndicatorPeriod.objects.all())
+    user = serializers.PrimaryKeyRelatedField(queryset=get_user_model().objects.all())
+    comments = IndicatorPeriodDataCommentSerializer(read_only=True, many=True, required=False)
     user_details = UserDetailsSerializer(required=False, source='user')
     status_display = serializers.ReadOnlyField()
     photo_url = serializers.ReadOnlyField()
@@ -40,3 +42,6 @@ class IndicatorPeriodDataFrameworkSerializer(BaseRSRSerializer):
 
     class Meta:
         model = IndicatorPeriodData
+
+    def create(self, validated_data):
+        return IndicatorPeriodData.objects.create(**validated_data)
