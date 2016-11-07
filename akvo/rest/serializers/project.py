akvo/akvo-rsr

commit 85d2c1244cc084421b6a9830c56ddfb3daf7faa4

[Fixes #2298] Completed unicode fixes for IATI fields

akvo/rest/serializers/project.py
@@ -48,8 +48,8 @@ class ProjectSerializer(BaseRSRSerializer):
 class ProjectIatiExportSerializer(BaseRSRSerializer):
 
     publishing_status = serializers.ReadOnlyField(source='publishingstatus.status')
-    checks_errors = serializers.ReadOnlyField(source='iati_errors')
-    checks_warnings = serializers.ReadOnlyField(source='iati_warnings')
+    checks_errors = serializers.ReadOnlyField(source='iati_errors_unicode')
+    checks_warnings = serializers.ReadOnlyField(source='iati_warnings_unicode')
 
     class Meta:
         model = Project

akvo/rsr/models/project.py
@@ -1167,20 +1167,38 @@ class Project(TimestampsMixin, models.Model):
     def iati_project_scope(self):
         return codelist_value(ActivityScope, self, 'project_scope')
 
+    def iati_project_scope_unicode(self):
+        return str(self.iati_project_scope())
+
     def iati_collaboration_type(self):
         return codelist_value(CollaborationType, self, 'collaboration_type')
+
+    def iati_collaboration_type_unicode(self):
+        return str(self.iati_collaboration_type())
 
     def iati_default_flow_type(self):
         return codelist_value(FlowType, self, 'default_flow_type')
 
+    def iati_default_flow_type_unicode(self):
+        return str(self.iati_default_flow_type())
+
     def iati_default_finance_type(self):
         return codelist_value(FinanceType, self, 'default_finance_type')
+
+    def iati_default_finance_type_unicode(self):
+        return str(self.iati_default_finance_type())
 
     def iati_default_aid_type(self):
         return codelist_value(AidType, self, 'default_aid_type')
 
+    def iati_default_aid_type_unicode(self):
+        return str(self.iati_default_aid_type())
+
     def iati_default_tied_status(self):
         return codelist_value(TiedStatus, self, 'default_tied_status')
+
+    def iati_default_tied_status_unicode(self):
+        return str(self.iati_default_tied_status())
 
     def sector_categories_codes(self):
         from .sector import Sector
@@ -1300,11 +1318,20 @@ class Project(TimestampsMixin, models.Model):
     def iati_successes(self):
         return [check.description for check in self.iati_checks_status(1)]
 
+    def iati_successes_unicode(self):
+        return str(self.iati_successes())
+
     def iati_warnings(self):
         return [check.description for check in self.iati_checks_status(2)]
 
+    def iati_warnings_unicode(self):
+        return str(self.iati_warnings())
+
     def iati_errors(self):
         return [check.description for check in self.iati_checks_status(3)]
+
+    def iati_errors_unicode(self):
+        return str(self.iati_errors())
 
     def keyword_logos(self):
         """Return the keywords of the project which have a logo."""

akvo/rest/serializers/budget_item.py
@@ -29,12 +29,12 @@ class BudgetItemSerializer(BudgetItemRawSerializer):
     label_label = serializers.ReadOnlyField(source='get_label')
     type_label = serializers.ReadOnlyField(source='iati_type_unicode')
     currency_label = serializers.ReadOnlyField(source='iati_currency_unicode')
-    status_label = serializers.ReadOnlyField(source='iati_status')
+    status_label = serializers.ReadOnlyField(source='iati_status_unicode')
 
 
 class CountryBudgetItemSerializer(BaseRSRSerializer):
 
-    code_label = serializers.ReadOnlyField(source='iati_code')
+    code_label = serializers.ReadOnlyField(source='iati_code_unicode')
 
     class Meta:
         model = CountryBudgetItem

akvo/rest/serializers/crs_add.py
@@ -14,10 +14,10 @@ from rest_framework import serializers
 
 class CrsAddSerializer(BaseRSRSerializer):
 
-    repayment_type_label = serializers.ReadOnlyField(source='iati_repayment_type')
-    repayment_plan_label = serializers.ReadOnlyField(source='iati_repayment_plan')
+    repayment_type_label = serializers.ReadOnlyField(source='iati_repayment_type_unicode')
+    repayment_plan_label = serializers.ReadOnlyField(source='iati_repayment_plan_unicode')
     currency_label = serializers.ReadOnlyField(source='iati_currency_unicode')
-    channel_code_label = serializers.ReadOnlyField(source='iati_channel_code')
+    channel_code_label = serializers.ReadOnlyField(source='iati_channel_code_unicode')
 
     class Meta:
         model = CrsAdd
@@ -25,7 +25,7 @@ class CrsAddSerializer(BaseRSRSerializer):
 
 class CrsAddOtherFlagSerializer(BaseRSRSerializer):
 
-    code_label = serializers.ReadOnlyField(source='iati_code')
+    code_label = serializers.ReadOnlyField(source='iati_code_unicode')
 
     class Meta:
         model = CrsAddOtherFlag

akvo/rest/serializers/humanitarian_scope.py
@@ -15,7 +15,7 @@ from rest_framework import serializers
 class HumanitarianScopeSerializer(BaseRSRSerializer):
 
     type_label = serializers.ReadOnlyField(source='iati_type_unicode')
-    vocabulary_label = serializers.ReadOnlyField(source='iati_vocabulary')
+    vocabulary_label = serializers.ReadOnlyField(source='iati_vocabulary_unicode')
 
     class Meta:
         model = HumanitarianScope

akvo/rest/serializers/partnership.py
@@ -25,8 +25,8 @@ class PartnershipRawDeepSerializer(PartnershipRawSerializer):
 class PartnershipSerializer(PartnershipRawSerializer):
 
     organisation_show_link = serializers.ReadOnlyField()
-    partner_type = serializers.ReadOnlyField(source='iati_role_to_partner_type')
-    organisation_role_label = serializers.ReadOnlyField(source='iati_organisation_role_label')
+    partner_type = serializers.ReadOnlyField(source='iati_role_to_partner_type_unicode')
+    organisation_role_label = serializers.ReadOnlyField(source='iati_organisation_role_label_unicode')
     funding_amount_label = serializers.ReadOnlyField(source='funding_amount_with_currency')
 
 

akvo/rest/serializers/policy_marker.py
@@ -20,6 +20,6 @@ class PolicyMarkerRawSerializer(BaseRSRSerializer):
 
 class PolicyMarkerSerializer(PolicyMarkerRawSerializer):
 
-    policy_marker_label = serializers.ReadOnlyField(source='iati_policy_marker')
-    vocabulary_label = serializers.ReadOnlyField(source='iati_vocabulary')
-    significance_label = serializers.ReadOnlyField(source='iati_significance')
+    policy_marker_label = serializers.ReadOnlyField(source='iati_policy_marker_unicode')
+    vocabulary_label = serializers.ReadOnlyField(source='iati_vocabulary_unicode')
+    significance_label = serializers.ReadOnlyField(source='iati_significance_unicode')

akvo/rest/serializers/project_document.py
@@ -21,15 +21,15 @@ class ProjectDocumentRawSerializer(BaseRSRSerializer):
 class ProjectDocumentSerializer(ProjectDocumentRawSerializer):
 
     document_show_link = serializers.ReadOnlyField()
-    language_label = serializers.ReadOnlyField(source='iati_language')
-    title_language_label = serializers.ReadOnlyField(source='iati_title_language')
-    format_label = serializers.ReadOnlyField(source='iati_format')
+    language_label = serializers.ReadOnlyField(source='iati_language_unicode')
+    title_language_label = serializers.ReadOnlyField(source='iati_title_language_unicode')
+    format_label = serializers.ReadOnlyField(source='iati_format_unicode')
 
 
 class ProjectDocumentCategorySerializer(BaseRSRSerializer):
 
     document_unicode = serializers.ReadOnlyField(source='document.__unicode__')
-    category_label = serializers.ReadOnlyField(source='iati_category')
+    category_label = serializers.ReadOnlyField(source='iati_category_unicode')
 
     class Meta:
         model = ProjectDocumentCategory

commit 6f6c82bf77e796ab5cc2364cae5a84d3688ae996

[#2298] Remove _unicode ProjectIatiExportSerializer sources
This is a bug introduced in 85d2c1244cc084421b6a9830c56ddfb3daf7faa4

akvo/rest/serializers/project.py
@@ -48,8 +48,8 @@ class ProjectSerializer(BaseRSRSerializer):
 class ProjectIatiExportSerializer(BaseRSRSerializer):
 
     publishing_status = serializers.ReadOnlyField(source='publishingstatus.status')
-    checks_errors = serializers.ReadOnlyField(source='iati_errors_unicode')
-    checks_warnings = serializers.ReadOnlyField(source='iati_warnings_unicode')
+    checks_errors = serializers.ReadOnlyField(source='iati_errors')
+    checks_warnings = serializers.ReadOnlyField(source='iati_warnings')
 
     class Meta:
         model = Project