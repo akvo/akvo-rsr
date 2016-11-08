akvo/akvo-rsr

commit 1aa60508350d4477c1f0964db7f3c124d9d1bc01

[#1577] Check project update photo file extension
When the POST has a multipart-encoded file, the file extension checks
don't happen, and unknown file types don't give a proper error.  This
commit adds a check on the file type even for these cases.

akvo/rest/fields.py
@@ -60,14 +60,14 @@ class Base64ImageField(ImageField):
             file_name = str(uuid.uuid4())[:12] # 12 characters are more than enough.
             # Get the file name extension:
             file_extension = self.get_file_extension(file_name, decoded_file)
-            if file_extension not in self.ALLOWED_IMAGE_TYPES:
-                raise serializers.ValidationError(
-                    _(u"The type of the image couldn't been determined.")
-                )
+            self.check_file_extension(file_extension)
             complete_file_name = file_name + "." + file_extension
             data = ContentFile(decoded_file, name=complete_file_name)
         else:
             data = base64_data
+            file_extension = self.get_file_extension(data.name, data.read())
+            self.check_file_extension(file_extension)
+            data.seek(0)
 
         return super(Base64ImageField, self).from_native(data)
 
@@ -154,3 +154,10 @@ class Base64ImageField(ImageField):
         extension = imghdr.what(filename, decoded_file)
         extension = "jpg" if extension == "jpeg" else extension
         return extension
+
+    def check_file_extension(self, file_extension):
+        if file_extension not in self.ALLOWED_IMAGE_TYPES:
+            formats = {'format': ', '.join(self.ALLOWED_IMAGE_TYPES)}
+            raise serializers.ValidationError(
+                _(u"Unknown image type. Only the following types are accepted: %(format)s") % formats
+            )

akvo/rsr/tests/rest/test_project_update.py
@@ -7,12 +7,26 @@ See more details in the license.txt file located at the root folder of the Akvo 
 For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 """
 
+import base64
 import json
+import tempfile
 
 from akvo.rsr.models import Project, User, ProjectUpdate
 
 from django.conf import settings
 from django.test import TestCase, Client
+
+# Data for a WMF image file
+WMF_DATA = (
+    '\xd7\xcd\xc6\x9a\x00\x00\x00\x00\x00\x00\xd4\x00\xd4\x00\xb0\x04\x00\x00\x00\x00\xa1S\x01\x00\t\x00\x00\x03'
+    'p\x00\x00\x00\x03\x00\x14\x00\x00\x00\x00\x00\x05\x00\x00\x00\x0c\x02\xd4\x00\xd4\x00\x05\x00\x00\x00\x0b\x02'
+    '\x00\x00\x00\x00\x04\x00\x00\x00\x03\x01\x08\x00\x05\x00\x00\x00\x02\x01\x01\x00\x00\x00\x05\x00\x00\x00\x06\x01'
+    '\x02\x00\x00\x00\x05\x00\x00\x00.\x01\x18\x00\x00\x00\x05\x00\x00\x00\t\x02\x00\x00\x00\x00\x05\x00\x00\x00'
+    '\x04\x01\r\x00\x00\x00\x07\x00\x00\x00&\x06\x17\x00\x04\x00\x05\x00\x00\x00\x08\x00\x00\x00\xfa\x02\x00\x00'
+    '\x01\x00\x00\x00\x00\x00\x00\x00\x08\x00\x00\x00\xfa\x02\x05\x00\x01\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00'
+    '-\x01\x01\x00\x07\x00\x00\x00\xfc\x02\x01\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00-\x01\x02\x00\x04\x00'
+    '\x00\x00\xf0\x01\x02\x00\x04\x00\x00\x00\xf0\x01\x01\x00\x04\x00\x00\x00\xf0\x01\x00\x00\x03\x00\x00\x00\x00\x00'
+)
 
 
 class RestProjectUpdateTestCase(TestCase):
@@ -104,3 +118,38 @@ class RestProjectUpdateTestCase(TestCase):
                                    'title': 'Allowed'
                                })
         self.assertEqual(response.status_code, 201)
+
+    def test_rest_post_project_update_wmf_string(self):
+        """
+        Checks posting a project update with a WMF photo base64 encoded.
+        """
+
+        self.c.login(username=self.user.username, password='password')
+        response = self.c.post('/rest/v1/project_update/?format=json',
+                               {
+                                   'project': self.project.pk,
+                                   'user': self.user.pk,
+                                   'title': 'Allowed',
+                                   'photo': base64.standard_b64encode(WMF_DATA)
+                               })
+        self.assertEqual(response.status_code, 400)
+
+    def test_rest_post_project_update_wmf_file(self):
+        """
+        Checks posting a project update with a WMF photo base64 encoded.
+        """
+
+        with tempfile.NamedTemporaryFile(suffix='.wmf') as f:
+            f.write(WMF_DATA)
+            f.flush()
+
+            self.c.login(username=self.user.username, password='password')
+            response = self.c.post('/rest/v1/project_update/?format=json',
+                                   {
+                                       'project': self.project.pk,
+                                       'user': self.user.pk,
+                                       'title': 'Allowed',
+                                       'photo': open(f.name)
+                                   })
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('photo', json.loads(response.content))

commit 36fd830dfcd5d185299204c09440f90cb94264c4

[#2417] project_update API POST fails when photo is None
This bug was introduced in 1aa60508350d4477c1f0964db7f3c124d9d1bc01

akvo/rest/fields.py
@@ -48,8 +48,10 @@ class Base64ImageField(ImageField):
         'png',
     )
     def from_native(self, base64_data):
+        if base64_data is None:
+            data = base64_data
         # Check if this is a base64 string
-        if isinstance(base64_data, basestring):
+        elif isinstance(base64_data, basestring):
             # Try to decode the file. Return validation error if it fails.
             try:
                 decoded_file = base64.b64decode(base64_data)

akvo/rsr/tests/rest/test_project_update.py
@@ -119,6 +119,22 @@ class RestProjectUpdateTestCase(TestCase):
                                })
         self.assertEqual(response.status_code, 201)
 
+    def test_rest_post_project_update_photo_none(self):
+        """
+        Checks posting a project update with photo being None
+        """
+
+        self.c.login(username=self.user.username, password='password')
+        response = self.c.post('/rest/v1/project_update/?format=json',
+                               json.dumps({
+                                   'project': self.project.pk,
+                                   'user': self.user.pk,
+                                   'title': 'Allowed',
+                                   'photo': None,
+                               }),
+                               content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
     def test_rest_post_project_update_wmf_string(self):
         """
         Checks posting a project update with a WMF photo base64 encoded.