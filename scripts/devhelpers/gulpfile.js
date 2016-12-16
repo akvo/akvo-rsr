5810bda68d85e20f86c6a715669b84b1de862be7
Gabriel von Heijne <gabriel@akvo.org>
2016-12-16 09:28:09 +0100

    [#2447] Add gulp tasks vendors and result
    Separate the npm packages from local files and manage them using the gulp task "vendors". This means that `const React = require('react');` now works in my-new-results.jsx!
    The vendors task only needs to be run when npm packages are added or updated. All npm packages are bundled into the vendors.js file.
    
    Update my_new_results.html and 40-pipeline.conf to support this change.
    
    Refactor the gulp task transpiling my-new-results.jsx. It now only bundles the local code and "links" to the npm packages using browserify.external().
    
    Add watchify so the task is re-run on edit of my-new-results.jsx.
    
    Rename the gulp task to "results".

--- scripts/devhelpers/gulpfile.js
+++ scripts/devhelpers/gulpfile.js
@@ -14,28 +14,72 @@ var sass = require('gulp-sass');
 
 var source = require('vinyl-source-stream'); // Used to stream bundle for further handling
 var browserify = require('browserify');
-// var watchify = require('watchify');
+var watchify = require('watchify');
 var babelify = require('babelify');
+var notify = require('gulp-notify');
 
 var preset_react = require('babel-preset-react');
 var preset_es2015 = require('babel-preset-es2015');
 
 // TODO: Split my-new-results.jsx into multiple files each with one React component plus a main
-// TODO: Set up watchify
-gulp.task('build-results', function() {
-    return browserify({
-        debug: true,
-        entries: '../../akvo/rsr/static/scripts-src/my-new-results.jsx',
-        transform: [
-            [babelify, {presets: [preset_react, preset_es2015]}]
-        ]
-    })
-    .bundle()
-    .pipe(source('my-new-results.js'))
-    .pipe(gulp.dest('../../akvo/rsr/static/scripts-src/'));
+
+var development = true;
+
+/* vendor bundling */
+
+var vendors = {
+    packages: [
+        'react',
+        'react-dom',
+        'rc-collapse'
+    ],
+    dest: '../../akvo/rsr/static/scripts-src/'
+};
+
+gulp.task('vendors', function () {
+    var stream = browserify({
+        debug: development,
+        require: vendors.packages
+    });
+    stream.bundle()
+          .pipe(source('vendors.js'))
+          .pipe(gulp.dest(vendors.dest));
+    return stream;
 });
 
-gulp.task('build', ['build-results']);
+/* my-new-result bundling */
+
+var options = {
+    debug: development,
+    entries: '../../akvo/rsr/static/scripts-src/my-new-results.jsx',
+    transform: [
+        [babelify, {presets: [preset_react, preset_es2015]}]
+    ]
+};
+
+var opts = Object.assign(watchify.args, options);
+var b = watchify(browserify(opts));
+
+function bundle() {
+    var start = new Date();
+    console.log('Building result');
+    vendors.packages.forEach(function(vendor) {
+        b.external(vendor);
+    });
+
+    return b
+        .bundle()
+        .pipe(source('my-new-results.js'))
+        .pipe(gulp.dest('../../akvo/rsr/static/scripts-src/'))
+        .pipe(notify(function () {
+            console.log('Result bundle built in ' + (Date.now() - start) + 'ms');
+        }));
+}
+
+gulp.task('results', bundle);
+b.on('update', bundle);
+
+/* legacy bundling */
 
 gulp.task('lint', function() {
   return gulp.src('../../akvo/rsr/static/scripts-src/**/*.js')
