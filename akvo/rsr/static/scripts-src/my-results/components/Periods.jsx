88cba28277921da5ddf3f42a6d97d3b52fea7eb4
Gabriel von Heijne <gabriel@akvo.org>
2017-04-19 11:56:20 +0200

    [#2447] Fix PeriodLockToggle.lockToggle()
    The callback in lockToggle() was removed when it should have been altered.

--- akvo/rsr/static/scripts-src/my-results/components/Periods.jsx
+++ akvo/rsr/static/scripts-src/my-results/components/Periods.jsx
@@ -59,15 +59,13 @@ class PeriodLockToggle extends React.Component {
 
     lockToggle(e) {
         const period = this.props.period;
-        // const toggleCallback = () => {
-        //     this.lockingToggle(false);
-        //     selectablePeriods();
-        // };
+        const toggleCallback = () => {
+            this.lockingToggle(false);
+        };
         if (!this.state.locking) {
             this.lockingToggle(true);
             this.updatePeriodLock(
-                // period.id, {locked: !period.locked}, toggleCallback
-                period.id, {locked: !period.locked}
+                period.id, {locked: !period.locked}, toggleCallback
             );
         }
         e.stopPropagation();
