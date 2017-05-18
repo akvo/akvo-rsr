ecc6b7a5e4f09f118f2ca39668099506449b5413
Gabriel von Heijne <gabriel@akvo.org>
2017-05-18 09:59:08 +0200

    [#2671] Add admin action to set project status to complete

---
+++ akvo/rsr/admin_actions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Akvo Reporting is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+
+from .models import Project
+
+def set_project_status_complete(modeladmin, request, queryset):
+    queryset.update(status=Project.STATUS_COMPLETE)
+
+set_project_status_complete.short_description = "Set status of selected projects to complete"
