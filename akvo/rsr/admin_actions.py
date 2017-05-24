7ebfceaebc2dab9da13772df9cfadc3fb4eb89d1
Gabriel von Heijne <gabriel@akvo.org>
2017-05-24 08:54:33 +0200

    [#2671] Flake 8 fixes

--- akvo/rsr/admin_actions.py
+++ akvo/rsr/admin_actions.py
@@ -5,15 +5,12 @@
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
 
-from .models import Project
-
-
 def set_project_status_complete(modeladmin, request, queryset):
-    """ Admin action setting selected projects iati_status to u'3', i.e. 'Completion' in 
+    """ Admin action setting selected projects iati_status to u'3', i.e. 'Completion' in
     codelists_v202.ACTIVITY_STATUS and status to Project.STATUS_COMPLETE
     """
     for project in queryset:
-        project.iati_status=u'3'
+        project.iati_status = u'3'
         # Project.status is set in Project.save() when iati_status is changed
         project.save()
 
