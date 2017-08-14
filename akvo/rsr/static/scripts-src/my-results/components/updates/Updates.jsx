82ca8f19c66748b4a644f65a51ba44111763ddcb
Loïc Sans <loic.sans@gmail.com>
2017-08-14 11:09:51 +0300

    [#2823] Added spacing in TimestampInfo between date and description

--- akvo/rsr/static/scripts-src/my-results/components/updates/Updates.jsx
+++ akvo/rsr/static/scripts-src/my-results/components/updates/Updates.jsx
@@ -116,13 +116,13 @@ UpdateStatus.propTypes = {
 const QuantitativeUpdateBody = ({update}) => {
     const {user_details, approver_details} = update;
     const approvedBy = approver_details ?
-        <TimestampInfo update={update} user={approver_details} label="Approved on" />
+        <TimestampInfo update={update} user={approver_details} label="Approved on " />
     :
         undefined;
     return (
         <div className="UpdateBody">
             <UpdateValue update={update} />
-            <TimestampInfo update={update} user={user_details} label="Created on" />
+            <TimestampInfo update={update} user={user_details} label="Created on " />
             {approvedBy}
             <UpdateStatus update={update} />
         </div>
