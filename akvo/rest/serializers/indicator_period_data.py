31de118bb0fe824e25817511bd59068d7da453c9
Puneeth Chaganti <punchagan@muse-amuse.in>
2017-08-28 16:35:17 +0530

    [#2674] Allow nested posting with disaggregations
    In the end-point for posting indicator_period_data_framework, allow posting
    updates along with corresponding disaggregations. The nested posting is not
    supported by default, by the version of DRF being used currently, and this
    needed a slight hack in the data validation step to get working, correctly. Not
    sure, if there's a way to do this, without the hack.q

--- akvo/rest/serializers/indicator_period_data.py
+++ akvo/rest/serializers/indicator_period_data.py
@@ -6,9 +6,12 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from akvo.rest.serializers.disaggregation import DisaggregationSerializer
 from akvo.rest.serializers.rsr_serializer import BaseRSRSerializer
 from akvo.rest.serializers.user import UserDetailsSerializer
-from akvo.rsr.models import IndicatorPeriod, IndicatorPeriodData, IndicatorPeriodDataComment
+from akvo.rsr.models import (
+    IndicatorPeriod, IndicatorPeriodData, IndicatorPeriodDataComment
+)
 
 
 class IndicatorPeriodDataCommentSerializer(BaseRSRSerializer):
@@ -36,6 +39,7 @@ class IndicatorPeriodDataFrameworkSerializer(BaseRSRSerializer):
     period = serializers.PrimaryKeyRelatedField(queryset=IndicatorPeriod.objects.all())
     user = serializers.PrimaryKeyRelatedField(queryset=get_user_model().objects.all())
     comments = IndicatorPeriodDataCommentSerializer(read_only=True, many=True, required=False)
+    disaggregations = DisaggregationSerializer(many=True, required=False)
     user_details = UserDetailsSerializer(required=False, source='user')
     approver_details = UserDetailsSerializer(required=False, source='approved_by')
     status_display = serializers.ReadOnlyField()
@@ -44,3 +48,34 @@ class IndicatorPeriodDataFrameworkSerializer(BaseRSRSerializer):
 
     class Meta:
         model = IndicatorPeriodData
+
+    def create(self, validated_data):
+        """Over-ridden to handle nested writes."""
+        update = super(IndicatorPeriodDataFrameworkSerializer, self).create(validated_data)
+        for disaggregation in self._disaggregations_data:
+            disaggregation['update'] = update.id
+            serializer = DisaggregationSerializer(data=disaggregation)
+            serializer.is_valid(raise_exception=True)
+            serializer.create(serializer.validated_data)
+
+        return update
+
+    def update(self, instance, validated_data):
+        """Over-ridden to handle nested updates."""
+        super(IndicatorPeriodDataFrameworkSerializer, self).update(instance, validated_data)
+        for disaggregation in self._disaggregations_data:
+            disaggregation['update'] = instance.id
+            serializer = DisaggregationSerializer(data=disaggregation)
+            serializer.is_valid(raise_exception=True)
+            disaggregation_instance, _ = instance.disaggregations.get_or_create(
+                update=instance,
+                dimension=serializer.validated_data['dimension'],
+            )
+            serializer.update(disaggregation_instance, serializer.validated_data)
+
+        return instance
+
+    def is_valid(self, raise_exception=False):
+        # HACK to allow nested posting...
+        self._disaggregations_data = self.initial_data.pop('disaggregations', [])
+        super(IndicatorPeriodDataFrameworkSerializer, self).is_valid(raise_exception=raise_exception)
