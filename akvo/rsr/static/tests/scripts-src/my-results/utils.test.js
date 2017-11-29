3bf94bae0fefb27255febfa2775da97cdd0db78b
Emelie Jakobsson <emelie.jakobsson87@gmail.com>
2017-11-29 16:05:18 +0100

    [#2987] added two tests and fixed indents on const/let declaration

--- akvo/rsr/static/tests/scripts-src/my-results/utils.test.js
+++ akvo/rsr/static/tests/scripts-src/my-results/utils.test.js
@@ -17,7 +17,7 @@ let expect = chai.expect;
 describe("function distinct", () => {
     it('expect to return an array with unique values', () => {
         const arrayWithDuplicates = [1,2,3,4,4],
-              uniqueArray = [1,2,3,4];
+            uniqueArray = [1,2,3,4];
         expect(utils.distinct(arrayWithDuplicates)).to.deep.equal(uniqueArray);
     });
 });
@@ -25,7 +25,7 @@ describe("function distinct", () => {
 describe("function identicalArrays", () => {
     it('returns true if two arrays are identical', () => {
         const arr1 = [1,2,3],
-              arr2 = [1,2,3];
+            arr2 = [1,2,3];
         expect(utils.identicalArrays(arr1, arr2)).to.equal(true);
     });
 });
@@ -33,8 +33,8 @@ describe("function identicalArrays", () => {
 describe("function isEmpty", () => {
     it('expect to return true if empty', () => {
         const object = {},
-              string = '',
-              array = [];
+            string = '',
+            array = [];
         expect(utils.isEmpty(object)).to.be.true;
         expect(utils.isEmpty(string)).to.be.true;
         expect(utils.isEmpty(array)).to.be.true;
@@ -54,6 +54,23 @@ describe("function isNumeric", () => {
         const string = 'noNumeric';
         expect(utils.isNumeric(string)).to.be.false;
     });
+});
+
+describe("function getCookie", () => {
+    it('returns the cookies name', () => {
+        const document = global.document;
+            global.document = {
+                cookie: 'name=value; otherName=otherValue'
+            };
+         let name = 'name',
+            otherName = 'otherName';
+         const value = 'value',
+            otherValue = 'otherValue';
+         expect(utils.getCookie(name)).to.equal(value);
+         expect(utils.getCookie(otherName)).to.equal(otherValue);
+         expect(utils.getCookie(name)).to.not.equal(otherValue);
+         global.document = document;
+     });
 });
 
 describe("function idsToActiveKey", () => {
@@ -85,16 +102,15 @@ describe("arrowFunction isNewUpdate", () => {
 describe("function collapseId", () => {
     it('expect to create an collapsedId from the model name and parent object id', () => {
         const model = 'model2',
-              id = 200;
+            id = 200;
         expect(utils.collapseId(model, id)).to.equal('model2-200');
     });
 });
 
-// requires that the function tested is changed to an exported function
 describe("function childModelName", () => {
     it('expect to return child model name', () => {
         const model = c.OBJECTS_RESULTS,
-              child = c.OBJECTS_INDICATORS;
+            child = c.OBJECTS_INDICATORS;
         expect(utils.childModelName(model)).to.equal(child);
     });
 });
@@ -102,42 +118,53 @@ describe("function childModelName", () => {
 describe("function parentModelName", () => {
     it('expect to return parent model name', () => {
         const model = c.OBJECTS_INDICATORS,
-              parent = c.OBJECTS_RESULTS;
+            parent = c.OBJECTS_RESULTS;
         expect(utils.parentModelName(model)).to.equal(parent);
     });
 });
 
 describe("function levelAbove", () => {
     it('expect to compare index of array to decide that level is above', () => {
-        const models = [1, 2],
-              model = models[1],
-              compare = models[0];
+        const model = c.OBJECTS_PERIODS,
+            compare = c.OBJECTS_UPDATES;
         expect(utils.levelAbove(model, compare)).to.equal(model < compare);
     });
 });
 
 describe("function levelBelow", () => {
     it('expect to compare index of array to decide that level is below', () => {
-        const models = [1, 2],
-              model = models[0],
-              compare = models[1];
+        const model = c.OBJECTS_UPDATES,
+            compare = c.OBJECTS_PERIODS;
         expect(utils.levelBelow(model, compare)).to.equal(model > compare);
     });
 });
 
-// requires that the function tested is changed to an exported function
+describe("function fullUpdateVisibility", () => {
+    it('expect to return false if an update have filters and should not be full visible', () => {
+        const filter = c.FILTER_NEED_REPORTING,
+            visible = [c.UPDATE_STATUS_NEW, c.UPDATE_STATUS_DRAFT, c.UPDATE_STATUS_REVISION],
+            update = visible[1];
+        expect(utils.fullUpdateVisibility(update, filter)).to.be.false;
+    });
+    it('expect to be true if an update have no filters and should be fully visible', () => {
+        let noFilter,
+            updateFullyVisible;
+        expect(utils.fullUpdateVisibility(updateFullyVisible, noFilter)).to.be.true;
+    });
+});
+
 describe("function flatten", () => {
-    it('expect to return an flatten array of arrays', () => {
+    it('expect to return an flatten array of a nested original', () => {
         const arrayOfArrays = [[1, 2], [3, 4], [5, 6]],
-              arrayFlatten = [1, 2, 3, 4, 5, 6];
-        expect(utils.flatten(arrayOfArrays)).to.deep.equal(arrayFlatten);
+            flatArray= [1, 2, 3, 4, 5, 6];
+        expect(utils.flatten(arrayOfArrays)).to.deep.equal(flatArray);
     });
 });
 
 describe("function fieldValueOrSpinner", () => {
     it('expect to return an object with a key "value" from object, if objects defined', () => {
         const object = {a: 1, b: 2},
-              field = 'b';
+            field = 'b';
         expect(utils.fieldValueOrSpinner(object, field)).to.have.key('value');
         expect(utils.fieldValueOrSpinner(object, field)).to.deep.equal({value: 2});
     });
@@ -150,14 +177,12 @@ describe("function fieldValueOrSpinner", () => {
 
 describe("function setHash", () => {
     it('expect to set hash if hash is defined', () => {
-        const window = global.widow;
+        const window = global.window;
             global.window = {
                 location: {}
             };
-        console.log("global.window: " + JSON.stringify(global.window));
         const hash = 'setHash';
         utils.setHash(hash);
-        console.log("global.window: " + JSON.stringify(global.window));
         expect(JSON.stringify(global.window)).to.equal('{"location":{"hash":"#setHash"}}');
         global.window = window;
     });
@@ -166,10 +191,8 @@ describe("function setHash", () => {
             global.window = {
                 location: {}
             };
-        console.log("global.window: " + JSON.stringify(global.window));
         let hash;
         utils.setHash(hash);
-        console.log("global.window: " + JSON.stringify(global.window));
         expect(JSON.stringify(global.window)).to.equal('{"location":{"hash":""}}');
         global.window = window;
     });
@@ -178,22 +201,22 @@ describe("function setHash", () => {
 describe("function computePercentage", () => {
     it('computes the percentage of integer numbers', () => {
         const numerator = 20,
-              denominator = 20;
+            denominator = 20;
         expect(utils.computePercentage(numerator, denominator)).to.equal(100);
     });
     it('computes the percentage of decimal numbers', () => {
         const numerator = 88.7,
-              denominator = 8.87;
+            denominator = 8.87;
         expect(utils.computePercentage(numerator, denominator)).to.equal(1000);
     });
     it('computes the percentage of decimal and integer numbers', () => {
         const numerator = 47.79,
-              denominator = 531;
+            denominator = 531;
         expect(utils.computePercentage(numerator, denominator)).to.equal(9);
     });
     it('expect to return 0 if the denominator is 0', () => {
         const numerator = 88,
-              denominator = 0;
+            denominator = 0;
         expect(utils.computePercentage(numerator, denominator)).to.equal(0);
     });
 });
