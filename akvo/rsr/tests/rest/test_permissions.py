c0ff4ac23a6411808aa3dfe26d14bd35ac3edb19
Puneeth Chaganti <punchagan@muse-amuse.in>
2017-12-18 19:08:12 +0530

    [#3012] Fix permissions tests for the OrganisationIndicatorLabel view

--- akvo/rsr/tests/rest/test_permissions.py
+++ akvo/rsr/tests/rest/test_permissions.py
@@ -219,7 +219,10 @@ class PermissionFilteringTestCase(TestCase):
             if isinstance(obj, type) and issubclass(obj, PublicProjectViewSet)
         ]
         view_set_models = {view_set.queryset.model for view_set in view_sets}
-        missing_models = view_set_models - set(model_map.keys())
+        # HACK: Some models are not associated with Projects, we exclude
+        # checking for permissions for them...
+        exception_models = {M.OrganisationIndicatorLabel}
+        missing_models = view_set_models - set(model_map.keys()) - exception_models
         assert not missing_models, '{} are missing from model_map'.format(missing_models)
 
     @staticmethod
