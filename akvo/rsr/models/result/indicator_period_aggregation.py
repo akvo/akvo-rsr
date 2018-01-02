0cc20b9a9107e17e5308eb4173ce75ab0662d81c
Gabriel von Heijne <gabriel@akvo.org>
2018-01-02 15:57:16 +0100

    [#3026] Update views and add tests
    Bugfixes to the views.
    
    Add basic tests for the aggregations.

--- akvo/rsr/models/result/indicator_period_aggregation.py
+++ akvo/rsr/models/result/indicator_period_aggregation.py
@@ -12,29 +12,29 @@ from django_pgviews import view as pg
 ACTUAL_VALUE_SQL = """
     SELECT
         -- row_number() OVER... creates an artificial "pk" column, without which Django will protest
-        row_number()
-        OVER (ORDER BY period.id) AS id,
+        row_number() OVER (ORDER BY period.id) AS id,
         period.id AS period_id,
         indicator.measure as measure,
-        sum((update.value) :: BIGINT) AS value,
-        sum((update.numerator) :: BIGINT) AS numerator,
-        sum((update.denominator) :: BIGINT) AS denominator
+        sum((update.value) :: DECIMAL(20,2)) AS value,
+        sum((update.numerator) :: DECIMAL(20,2)) AS numerator,
+        sum((update.denominator) :: DECIMAL(20,2)) AS denominator
     FROM
-        rsr_indicatorperiod period, 
-        rsr_indicator indicator, 
-        rsr_indicatorperioddata "update"
-    WHERE 
+        rsr_indicatorperiod period,
+        rsr_indicator indicator,
+        rsr_indicatorperioddata update
+    WHERE
         (
-            (((indicator.id = period.indicator_id) AND 
+            (((indicator.id = period.indicator_id) AND
             (period.id = update.period_id)) AND
-            ((update.status) :: TEXT = 'A' :: TEXT)) AND 
-            ((update.value) :: TEXT ~ '^\d+$' :: TEXT)
+            ((update.status) :: TEXT = 'A' :: TEXT)) AND
+            ((update.value) :: TEXT ~ '^\d+\.?\d{0,2}$' :: TEXT OR update.value IS NULL)
         )
     GROUP BY period.id, indicator.measure;
 """
 
 
 class PeriodActualValue(pg.View):
+    # on_delete=models.DO_NOTHING is needed to prevent problems with PG trying to delete views' data
     period = models.ForeignKey('IndicatorPeriod', on_delete=models.DO_NOTHING)
     measure = models.CharField(max_length=1)
     value = models.IntegerField()
@@ -53,33 +53,39 @@ DISAGG_SQL = """
     WITH aggregated_disaggs AS (
         SELECT
             dimension_id,
-            sum(("value") :: BIGINT) AS value,
-            sum((numerator) :: BIGINT) AS numerator,
-            sum((denominator) :: BIGINT) AS denominator
+            sum(("value") :: DECIMAL(20,2)) AS value,
+            sum((numerator) :: DECIMAL(20,2)) AS numerator,
+            sum((denominator) :: DECIMAL(20,2)) AS denominator
         FROM
             rsr_disaggregation
         GROUP BY
             dimension_id
+    ),
+    period_disaggs AS (
+        SELECT DISTINCT
+            indicator.id AS indicator_id,
+            period.id AS period_id,
+            dimension.name AS dimension_name,
+            dimension.value AS dimension_value,
+            agg.value,
+            agg.numerator,
+            agg.denominator
+        FROM
+            rsr_indicator indicator,
+            rsr_indicatorperiod period,
+            rsr_indicatorperioddata update,
+            aggregated_disaggs agg,
+            rsr_indicatordimension dimension
+        WHERE
+            indicator.id = period.indicator_id AND
+            period.id = update.period_id AND
+            indicator.id = dimension.indicator_id AND
+            dimension.id = agg.dimension_id
     )
-    SELECT DISTINCT
-        indicator.id AS indicator_id,
-        period.id AS period_id,
-        dimension.name AS dimension_name,
-        dimension.value AS dimension_value,
-        agg.value,
-        agg.numerator,
-        agg.denominator
-    FROM
-        rsr_indicator indicator,
-        rsr_indicatorperiod period,
-        rsr_indicatorperioddata update,
-        aggregated_disaggs agg,
-        rsr_indicatordimension dimension
-    WHERE
-        indicator.id = period.indicator_id AND
-        period.id = update.period_id AND
-        indicator.id = dimension.indicator_id AND
-        dimension.id = agg.dimension_id
+    SELECT
+        row_number() OVER (ORDER BY indicator_id) AS id,
+        *
+    FROM period_disaggs
 """
 
 
@@ -98,5 +104,3 @@ class PeriodDisaggregation(pg.View):
         app_label = 'rsr'
         db_table = 'rsr_indicator_period_disaggregation'
         managed = False
-
-
---
+++ akvo/rsr/tests/models/test_results_aggregation.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+
+# Akvo Reporting is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+
+from unittest import TestCase
+
+from decimal import Decimal
+from django.contrib.auth import get_user_model
+from django_pgviews.models import ViewSyncer
+
+from akvo.rsr.models import (Indicator, IndicatorPeriod, IndicatorPeriodData, Project, Result,
+                             PeriodActualValue, IndicatorDimension, Disaggregation)
+from akvo.rsr.models.result.indicator_period_aggregation import PeriodDisaggregation
+
+User = get_user_model()
+
+
+class PeriodActualValueTestCase(TestCase):
+    """Tests for the indicator period model"""
+
+    def setUp(self):
+        # Clear all projects, users since some tests may not tear down!
+        self.tearDown()
+
+        # Setup a project with results framework and a user
+        self.project = Project.objects.create(title="Test project 1")
+        self.result = Result.objects.create(project=self.project, title='Test Result')
+        self.indicator = Indicator.objects.create(result=self.result, title='Test Indicator')
+        self.period = IndicatorPeriod.objects.create(indicator=self.indicator,
+                                                     actual_comment='initial actual comment')
+        self.user = User.objects.create(username='user1@com.com', email='user1@com.com')
+
+        #  set up PG views
+        vs = ViewSyncer()
+        vs.run(True, True)
+
+    def tearDown(self):
+        Project.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_actual_value_with_two_approved_updates(self):
+        # Given
+        period = self.period
+        user = self.user
+        approved = IndicatorPeriodData.STATUS_APPROVED_CODE
+
+        # When
+        IndicatorPeriodData.objects.create(period=period, user=user, status=approved, value="17.1")
+        IndicatorPeriodData.objects.create(period=period, user=user, status=approved,
+                                           value="4711.2")
+
+        # Then
+        actual_value = PeriodActualValue.objects.get(period=period)
+        self.assertEquals(actual_value.value, Decimal("4728.3"))
+
+    def test_actual_value_with_one_approved_update(self):
+        # Given
+        period = self.period
+        user = self.user
+        approved = IndicatorPeriodData.STATUS_APPROVED_CODE
+        draft = IndicatorPeriodData.STATUS_DRAFT_CODE
+
+        # When
+        IndicatorPeriodData.objects.create(period=period, user=user, status=approved, value="17")
+        IndicatorPeriodData.objects.create(period=period, user=user, status=draft, value="4711")
+
+        # Then
+        actual_value = PeriodActualValue.objects.get(period=period)
+        self.assertEquals(actual_value.value, 17)
+
+    def test_actual_value_with_two_updates_but_only_one_numeric(self):
+        # Given
+        period = self.period
+        user = self.user
+        approved = IndicatorPeriodData.STATUS_APPROVED_CODE
+
+        # When
+        IndicatorPeriodData.objects.create(period=period, user=user, status=approved, value="17")
+        IndicatorPeriodData.objects.create(period=period, user=user, status=approved,
+                                           value="All the things!")
+
+        # Then
+        actual_value = PeriodActualValue.objects.get(period=period)
+        self.assertEquals(actual_value.value, 17)
+
+    def test_actual_value_percentages_with_two_updates(self):
+        # Given
+        period = self.period
+        user = self.user
+        approved = IndicatorPeriodData.STATUS_APPROVED_CODE
+
+        # When
+        IndicatorPeriodData.objects.create(period=period, user=user, status=approved, value=None,
+                                           numerator="3.14", denominator="17")
+        IndicatorPeriodData.objects.create(period=period, user=user, status=approved, value=None,
+                                           numerator="47.11", denominator="4711")
+
+        # Then
+        actual_value = PeriodActualValue.objects.get(period=period)
+        self.assertEquals(actual_value.numerator, 50.25)
+        self.assertEquals(actual_value.denominator, 4728)
+
+    def test_actual_value_percentages_with_one_approved_updates(self):
+        # Given
+        period = self.period
+        user = self.user
+        approved = IndicatorPeriodData.STATUS_APPROVED_CODE
+        draft = IndicatorPeriodData.STATUS_DRAFT_CODE
+
+        # When
+        IndicatorPeriodData.objects.create(period=period, user=user, status=approved,
+                                           numerator=3.14, denominator=17)
+        IndicatorPeriodData.objects.create(period=period, user=user, status=draft,
+                                           numerator=47.11, denominator=4711)
+
+        # Then
+        actual_value = PeriodActualValue.objects.get(period=period)
+        self.assertEquals(actual_value.numerator, Decimal("3.14"))
+        self.assertEquals(actual_value.denominator, 17)
+
+
+class PeriodDisaggregationTestCase(TestCase):
+    """Tests for the indicator period model"""
+
+    def setUp(self):
+        # Clear all projects, users since some tests may not tear down!
+        self.tearDown()
+
+        # Setup a project with results framework and a user
+        self.user = User.objects.create(username='user1@com.com', email='user1@com.com')
+        self.project = Project.objects.create(title="Test project 1")
+        self.result = Result.objects.create(project=self.project, title='Test Result')
+        self.indicator = Indicator.objects.create(result=self.result, title='Test Indicator')
+        self.period = IndicatorPeriod.objects.create(indicator=self.indicator,
+                                                     actual_comment='initial actual comment')
+        self.dimension1 = IndicatorDimension.objects.create(
+            indicator=self.indicator, name="Flavor", value="Vanilla"
+        )
+        self.dimension2 = IndicatorDimension.objects.create(
+            indicator=self.indicator, name="Flavor", value="Chocolate"
+        )
+        self.dimension3 = IndicatorDimension.objects.create(
+            indicator=self.indicator, name="Color", value="Red"
+        )
+        self.update1 = IndicatorPeriodData.objects.create(
+            period=self.period, user=self.user, value="17.11"
+        )
+        self.update2 = IndicatorPeriodData.objects.create(
+            period=self.period, user=self.user, value="47.89"
+        )
+        #  set up PG views
+        vs = ViewSyncer()
+        vs.run(True, True)
+
+    def tearDown(self):
+        Project.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_disaggregated_values_aggregated_over_period(self):
+        # Given
+        indicator = self.indicator
+        period = self.period
+        update1 = self.update1
+        update2 = self.update2
+        dimension1 = self.dimension1
+        dimension2 = self.dimension2
+        dimension3 = self.dimension3
+
+        # When
+        Disaggregation.objects.create(
+            dimension=dimension1, update=update1, value=11.1
+        )
+        Disaggregation.objects.create(
+            dimension=dimension2, update=update1, value=6.01
+        )
+        Disaggregation.objects.create(
+            dimension=dimension1, update=update2, value=31.57
+        )
+        Disaggregation.objects.create(
+            dimension=dimension2, update=update2, value=16.32
+        )
+        Disaggregation.objects.create(
+            dimension=dimension3, update=update1, value=23.45
+        )
+        period_disaggregations = PeriodDisaggregation.objects.filter(
+            indicator=indicator, period=period
+        )
+        # Then
+        vanilla = period_disaggregations.get(dimension_name="Flavor", dimension_value="Vanilla")
+        chocolate = period_disaggregations.get(dimension_name="Flavor", dimension_value="Chocolate")
+        red = period_disaggregations.get(dimension_name="Color", dimension_value="Red")
+        self.assertEquals(vanilla.value, Decimal("42.67"))
+        self.assertEquals(chocolate.value, Decimal("22.33"))
+        self.assertEquals(red.value, Decimal("23.45"))
+
+    def test_disaggregated_percentages_aggregated_over_period(self):
+        # Given
+        indicator = self.indicator
+        period = self.period
+        update1 = self.update1
+        update2 = self.update2
+        dimension1 = self.dimension1
+        dimension2 = self.dimension2
+
+        # When
+        Disaggregation.objects.create(
+            dimension=dimension1, update=update1, numerator=3.14, denominator=17
+        )
+        Disaggregation.objects.create(
+            dimension=dimension2, update=update1, numerator=3.14, denominator=17
+        )
+        Disaggregation.objects.create(
+            dimension=dimension1, update=update2, numerator=2.72, denominator=34
+        )
+        Disaggregation.objects.create(
+            dimension=dimension2, update=update2, numerator=1.62, denominator=51
+        )
+        period_disaggregations = PeriodDisaggregation.objects.filter(
+            indicator=indicator, period=period
+        )
+        # Then
+        vanilla = period_disaggregations.get(dimension_name="Flavor", dimension_value="Vanilla")
+        chocolate = period_disaggregations.get(dimension_name="Flavor", dimension_value="Chocolate")
+        self.assertEquals(vanilla.numerator, Decimal("5.86"))
+        self.assertEquals(vanilla.denominator, Decimal("51"))
+        self.assertEquals(chocolate.numerator, Decimal("4.76"))
+        self.assertEquals(chocolate.denominator, Decimal("68"))
