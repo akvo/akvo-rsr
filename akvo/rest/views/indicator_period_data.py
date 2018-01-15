3c897354bc3ccace63c2d02dc6a791a6d9fc011d
Gabriel von Heijne <gabriel@akvo.org>
2018-01-15 14:50:20 +0100

    [#2739] Add 'disaggregations' to prefetch
    Also remove period__indicator and hierarchy "above" as they don't seem to matter.

--- akvo/rest/views/indicator_period_data.py
+++ akvo/rest/views/indicator_period_data.py
@@ -37,14 +37,12 @@ class IndicatorPeriodDataFrameworkViewSet(PublicProjectViewSet):
     """
     queryset = IndicatorPeriodData.objects.select_related(
         'period',
-        'period__indicator',
-        'period__indicator__result',
-        'period__indicator__result__project',
         'user',
         'approved_by',
     ).prefetch_related(
         'comments',
         'comments__user',
+        'disaggregations',
     ).all()
     serializer_class = IndicatorPeriodDataFrameworkSerializer
     project_relation = 'period__indicator__result__project__'
