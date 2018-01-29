300d0dd4f4fa5d9ccb61634b70fcad959d5cbdf4
Gabriel von Heijne <gabriel@akvo.org>
2018-01-29 17:24:15 +0100

    [#3110] Revise the script
    After checking with Ethel, revise the script to add periods to indicators for output results.

--- akvo/rsr/management/commands/add_periods.py
+++ akvo/rsr/management/commands/add_periods.py
@@ -8,15 +8,13 @@ from django.core.management.base import BaseCommand
 
 from ...models import Indicator, IndicatorPeriod
 
-QUANTITATIVE = 1
-RESULT_TYPE_OUTCOME = u"2"
+RESULT_TYPE_OUTPUT = u"1"
 
 
 class Command(BaseCommand):
     help = """
-    Script that replaces indicator periods for a project if it doesn't have exactly 9 periods.
+    Script that adds indicator periods for a project to indicators of output results.
     The new periods have the following start and end dates:
-        2017-07-01", "2017-12-31"
         2018-01-01", "2018-06-30"
         2018-07-01", "2018-12-31"
         2019-01-01", "2019-06-30"
@@ -26,15 +24,12 @@ class Command(BaseCommand):
         2021-01-01", "2021-06-30"
         2021-07-01", "2021-12-31"
 
-    Only results of type "outcome" will be changed in this way.
-
     NOTE: this is a custom command for OneDrop.
     """
 
     def handle(self, *args, **options):
 
         DATES = [
-            ["2017-07-01", "2017-12-31"],
             ["2018-01-01", "2018-06-30"],
             ["2018-07-01", "2018-12-31"],
             ["2019-01-01", "2019-06-30"],
@@ -52,14 +47,13 @@ class Command(BaseCommand):
             indicators = Indicator.objects.filter(
                 result__project=project_id
             ).filter(
-                result__type=RESULT_TYPE_OUTCOME
-            ).select_related('result')
+                result__type=RESULT_TYPE_OUTPUT
+            )
 
             for indicator in indicators:
                 periods = IndicatorPeriod.objects.filter(indicator=indicator)
-                if periods.count() != 9:
+                if periods.count() == 1:
                     print u"{}\t{}".format(indicator.pk, indicator.title).encode('utf-8')
-                    periods.delete()
                     for (period_start, period_end) in DATES:
                         IndicatorPeriod.objects.create(indicator=indicator,
                                                        period_start=period_start,
