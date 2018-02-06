82d1fd8c825182444db863cf2cb0fe2d8caacea8
Gabriel von Heijne <gabriel@akvo.org>
2018-02-06 12:28:06 +0100

    [#3105] Add tests for IATI export checks of results

--- akvo/rsr/tests/iati_checks/test_iati_checks_fields_results.py
+++ akvo/rsr/tests/iati_checks/test_iati_checks_fields_results.py
@@ -8,209 +8,300 @@ For additional details on the GNU license please see < http://www.gnu.org/licens
 """
 
 import datetime
-import os
+
+from django.test import TestCase
 
 from akvo.iati.checks.fields import results as results_checks
-from akvo.iati.checks.iati_checks import IatiChecks
-from akvo.rsr.models import (Partnership, Project, RecipientCountry, Sector, Transaction,
-                             TransactionSector, Result, Indicator, IndicatorPeriod, Organisation,
-                             User, ProjectEditorValidationSet)
-from django.test import TestCase
+from akvo.rsr.models import (
+    Project, Result, Indicator, IndicatorPeriod, ProjectEditorValidationSet, IndicatorReference
+)
+from akvo.rsr.models.result.utils import QUANTITATIVE
 
 
 class IatiChecksFieldsReultsTestCase(TestCase):
     """Tests the IATI checks."""
 
     def setUp(self):
-        """
-        In order to correctly test an IATI export, we need the following objects in the database:
+        self.project = Project.objects.create()
 
-        - An organisation with a name, IATI organisation identifier and 'can_create_projects' set
-        to True.
-        - A user.
-        """
-
-        # Create organisation
-        self.reporting_org = Organisation.objects.create(
-            name="Test Organisation Export",
-            long_name="Test Organisation for IATI export",
-            iati_org_id="NL-KVK-1234567890",
-            can_create_projects=True,
-            new_organisation_type=22
-        )
-
-        # Create (super)user
-        self.user = User.objects.create_superuser(
-            username="Super user export",
-            email="superuser.export@test.akvo.org",
-            password="password"
-        )
-
-        # Set test image path
-        base_image_path = '/var/akvo/rsr/code/'
-        if 'TRAVIS' in os.environ:
-            base_image_path = '/home/travis/build/akvo/akvo-rsr/'
-        self.image_path = base_image_path + 'akvo/rsr/tests/iati_export/test_image.jpg'
-
-    def test_iati_checks_fields_results_with_dgis_validation(self):
-        """
-        Test the checks of results for a project that uses the DGIS validation set.
-        """
-        # Create empty project
-        project = Project.objects.create(
-            date_start_actual=datetime.date.today() + datetime.timedelta(days=1),
-            date_end_actual=datetime.date.today() + datetime.timedelta(days=2),
-            currency=''
-        )
-
-        # Add reporting partner
-        Partnership.objects.create(
-            project=project,
-            organisation=self.reporting_org,
-            iati_organisation_role=Partnership.IATI_REPORTING_ORGANISATION,
-        )
-
-        validation = ProjectEditorValidationSet.objects.create(
-            name="DGIS IATI", description="DGIS IATI"
-        )
-        project.validations.add(validation)
-
-        result = Result.objects.create(
-            project=project,
+        self.result = Result.objects.create(
+            project=self.project,
             type="1",
             aggregation_status=True,
             title="Title",
             description="Description",
         )
-        indicator = Indicator.objects.create(
-            result=result,
+
+    def test_iati_checks_fields_results_result_and_indicator_pass(self):
+        # Given
+        Indicator.objects.create(
+            result=self.result,
             measure="1",
             ascending=True,
             title="Title",
             description="Description",
+            baseline_value="4711",
             baseline_year=2017,
         )
+
+        # When
+        all_checks_passed, checks = results_checks(self.project)
+
+        # Then
+        self.assertTrue(all_checks_passed)
+        self.assertEquals(len(checks), 1)
+        self.assertIn(u'has valid result(s)', checks[0][1])
+
+    def test_iati_checks_fields_results_result_errors(self):
+        # When
+        self.result.title = ''
+        self.result.type = ''
+        self.result.save()
+        all_checks_passed, checks = results_checks(self.project)
+
+        # Then
+        self.assertFalse(all_checks_passed)
+        self.assertEquals(len(checks), 3)
+        self.assertIn(u'has no type specified', checks[0][1])
+        self.assertIn(u'has no title specified', checks[1][1])
+        self.assertIn(u'has no indicator', checks[2][1])
+
+    def test_iati_checks_fields_results_indicator_error(self):
+        # Given an indicator missing title, measure and baseline year and value
+        Indicator.objects.create(
+            result=self.result,
+            type=QUANTITATIVE,
+            ascending=True,
+            description="Description",
+            baseline_comment="Comment",
+        )
+
+        # When
+        all_checks_passed, checks = results_checks(self.project)
+
+        # Then
+        self.assertFalse(all_checks_passed)
+        self.assertEquals(len(checks), 4)
+        self.assertIn(u'has no measure specified', checks[0][1])
+        self.assertIn(u'has no title specified', checks[1][1])
+        self.assertEquals(u'error', checks[2][0])
+        self.assertIn(u'baseline has no value specified', checks[2][1])
+        self.assertEquals(u'error', checks[3][0])
+        self.assertIn(u'baseline has no year specified', checks[3][1])
+
+    def test_iati_checks_fields_results_indicator_dgis_error(self):
+        # Given the use of the DGIS validation set
+        validation = ProjectEditorValidationSet.objects.create(
+            name="DGIS IATI", description="DGIS IATI"
+        )
+        self.project.validations.add(validation)
+
+        Indicator.objects.create(
+            result=self.result,
+            measure="1",
+            ascending=True,
+            title="Title",
+            description="Description",
+        )
+
+        # When
+        all_checks_passed, checks = results_checks(self.project)
+
+        # Then
+        self.assertFalse(all_checks_passed)
+        self.assertEquals(len(checks), 2)
+        self.assertEquals(u'warning', checks[0][0])
+        self.assertIn(u'no value specified, however the value of "N/A"', checks[0][1])
+        self.assertEquals(u'warning', checks[1][0])
+        self.assertIn(u'no year specified, however the value of "N/A"', checks[1][1])
+
+    def test_iati_checks_fields_results_period_pass(self):
+        # Given
+        indicator = Indicator.objects.create(
+            result=self.result,
+            measure="1",
+            ascending=True,
+            title="Title",
+            description="Description",
+            baseline_value="4711",
+            baseline_year=2017,
+        )
+
         IndicatorPeriod.objects.create(
             indicator=indicator,
             period_start=datetime.date.today(),
             period_end=datetime.date.today() + datetime.timedelta(days=1),
         )
 
-        all_checks_passed, checks = results_checks(project)
+        # When
+        all_checks_passed, checks = results_checks(self.project)
 
-        # Test the IATI checks
-        self.assertFalse(all_checks_passed)
-        self.assertEquals(len(checks), 3)
-        self.assertIn(u'baseline has no value', checks[0][1])
-        self.assertIn(u'has no target value', checks[1][1])
-        self.assertIn(u'has no actual value', checks[2][1])
+        # Then
+        self.assertTrue(all_checks_passed)
+        self.assertEquals(len(checks), 1)
+        self.assertIn(u'has valid result(s)', checks[0][1])
 
-    def test_iati_checks_multiple_level_sectors(self):
-        """
-        Test the checks for sectors on project and transaction level.
-        """
-        # Create empty project
-        project = Project.objects.create()
-
-        # Add sector
-        sector = Sector.objects.create(
-            project=project,
+    def test_iati_checks_fields_results_reference_pass(self):
+        # Given
+        indicator = Indicator.objects.create(
+            result=self.result,
+            measure="1",
+            ascending=True,
+            title="Title",
+            description="Description",
+            baseline_value="4711",
+            baseline_year=2017,
         )
 
-        # Add recipient country
-        RecipientCountry.objects.create(
-            project=project,
-        )
-
-        # Add transaction and sector
-        transaction = Transaction.objects.create(
-            project=project,
-        )
-        trans_sector = TransactionSector.objects.create(
-            transaction=transaction,
-        )
-        Transaction.objects.create(
-            project=project,
-        )
-
-        # Create IATI checks
-        iati_checks = IatiChecks(project)
-        all_checks_passed, checks_results = iati_checks.perform_checks()
-
-        # Test the IATI checks
-        self.assertFalse(all_checks_passed)
-
-        # Delete the transaction sector and sector
-        trans_sector.delete()
-        sector.delete()
-
-        # Create IATI checks again
-        iati_checks = IatiChecks(project)
-        all_checks_passed, checks_results = iati_checks.perform_checks()
-
-        # Test the IATI checks again
-        self.assertFalse(all_checks_passed)
-
-    def test_iati_checks_transaction_level_sectors(self):
-        """
-        Test the checks for sectors on transaction level.
-        """
-        # Create empty project
-        project = Project.objects.create()
-
-        # Add transaction sector
-        transaction = Transaction.objects.create(
-            project=project,
-            recipient_country="NL",
-            recipient_region="101",
-        )
-        TransactionSector.objects.create(
-            transaction=transaction,
+        IndicatorReference.objects.create(
+            indicator=indicator,
+            reference="1",
             vocabulary="99",
+            vocabulary_uri="https://akvo.org/vocabulary"
         )
-        TransactionSector.objects.create(
-            transaction=transaction,
+
+        # When
+        all_checks_passed, checks = results_checks(self.project)
+
+        # Then
+        self.assertTrue(all_checks_passed)
+        self.assertEquals(len(checks), 1)
+        self.assertIn(u'has valid result(s)', checks[0][1])
+
+    def test_iati_checks_fields_results_reference_error(self):
+        # Given
+        indicator = Indicator.objects.create(
+            result=self.result,
+            measure="1",
+            ascending=True,
+            title="Title",
+            description="Description",
+            baseline_value="4711",
+            baseline_year=2017,
+        )
+
+        IndicatorReference.objects.create(
+            indicator=indicator,
             vocabulary="99",
         )
 
-        # Create IATI checks
-        iati_checks = IatiChecks(project)
-        all_checks_passed, checks_results = iati_checks.perform_checks()
+        # When
+        all_checks_passed, checks = results_checks(self.project)
 
-        # Test the IATI checks
+        # Then
         self.assertFalse(all_checks_passed)
+        self.assertEquals(len(checks), 2)
+        self.assertIn(u'has no code', checks[0][1])
+        self.assertIn(u'has vocabulary 99', checks[1][1])
 
-        # Remove recipient region from transaction
-        transaction.recipient_region = ''
-        transaction.save()
-
-        # Create IATI checks
-        iati_checks = IatiChecks(project)
-        all_checks_passed, checks_results = iati_checks.perform_checks()
-
-        # Test the IATI checks
-        self.assertFalse(all_checks_passed)
-
-        # Add another transaction
-        Transaction.objects.create(
-            project=project,
+    def test_iati_checks_fields_results_period_no_dates(self):
+        # Given
+        indicator = Indicator.objects.create(
+            result=self.result,
+            measure="1",
+            ascending=True,
+            title="Title",
+            description="Description",
+            baseline_value="4711",
+            baseline_year=2017,
         )
 
-        # Create IATI checks
-        iati_checks = IatiChecks(project)
-        all_checks_passed, checks_results = iati_checks.perform_checks()
-
-        # Test the IATI checks
-        self.assertFalse(all_checks_passed)
-
-        # Add a recipient country
-        RecipientCountry.objects.create(
-            project=project,
+        IndicatorPeriod.objects.create(
+            indicator=indicator,
         )
 
-        # Create IATI checks
-        iati_checks = IatiChecks(project)
-        all_checks_passed, checks_results = iati_checks.perform_checks()
+        # When
+        all_checks_passed, checks = results_checks(self.project)
 
-        # Test the IATI checks
+        # Then
         self.assertFalse(all_checks_passed)
+        self.assertEquals(len(checks), 2)
+        self.assertIn(u'has no start date', checks[0][1])
+        self.assertIn(u'has no end date', checks[1][1])
+
+    def test_iati_checks_fields_results_period_bad_dates(self):
+        # Given
+        indicator = Indicator.objects.create(
+            result=self.result,
+            measure="1",
+            ascending=True,
+            title="Title",
+            description="Description",
+            baseline_value="4711",
+            baseline_year=2017,
+        )
+
+        IndicatorPeriod.objects.create(
+            indicator=indicator,
+            period_start=datetime.date.today() + datetime.timedelta(days=1),
+            period_end=datetime.date.today(),
+        )
+
+        # When
+        all_checks_passed, checks = results_checks(self.project)
+
+        # Then
+        self.assertFalse(all_checks_passed)
+        self.assertEquals(len(checks), 1)
+        self.assertIn(u'has a start date later than the end date', checks[0][1])
+
+    def test_iati_checks_fields_results_period_values_error(self):
+        # Given
+        indicator = Indicator.objects.create(
+            result=self.result,
+            measure="1",
+            ascending=True,
+            title="Title",
+            description="Description",
+            baseline_value="4711",
+            baseline_year=2017,
+        )
+
+        IndicatorPeriod.objects.create(
+            indicator=indicator,
+            period_start=datetime.date.today(),
+            period_end=datetime.date.today() + datetime.timedelta(days=1),
+            target_comment="Target",
+            actual_comment="Actual",
+        )
+
+        # When
+        all_checks_passed, checks = results_checks(self.project)
+
+        # Then
+        self.assertFalse(all_checks_passed)
+        self.assertEquals(len(checks), 2)
+        self.assertIn(u'has no target value', checks[0][1])
+        self.assertIn(u'has no actual value', checks[1][1])
+
+    def test_iati_checks_fields_results_period_values_dgis_warning(self):
+        # Given
+        validation = ProjectEditorValidationSet.objects.create(
+            name="DGIS IATI", description="DGIS IATI"
+        )
+        self.project.validations.add(validation)
+
+        indicator = Indicator.objects.create(
+            result=self.result,
+            measure="1",
+            ascending=True,
+            title="Title",
+            description="Description",
+            baseline_value="4711",
+            baseline_year=2017,
+        )
+
+        IndicatorPeriod.objects.create(
+            indicator=indicator,
+            period_start=datetime.date.today(),
+            period_end=datetime.date.today() + datetime.timedelta(days=1),
+        )
+
+        # When
+        all_checks_passed, checks = results_checks(self.project)
+
+        # Then
+        self.assertFalse(all_checks_passed)
+        self.assertEquals(len(checks), 2)
+        self.assertIn(u'no target value specified. The value "N/A"', checks[0][1])
+        self.assertIn(u'no actual value specified. The value "N/A"', checks[1][1])
