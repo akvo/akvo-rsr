0a7b78ee8831c22728604610a64323c413c4ad7f
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-04-06 17:00:41 +0530

    [#3200] Patch the PaginatedCSVRenderer to work for non-paginated data
    The paginated renderer looks for the results_field on the data it is rendering,
    and just uses an empty list if the results_field doesn't exist on the data.
    Instead, we try to use the vanilla CSVRenderer.

--- akvo/rest/renderers.py
+++ akvo/rest/renderers.py
@@ -14,6 +14,24 @@ from django.utils.encoding import smart_text
 
 from rest_framework.renderers import BaseRenderer, BrowsableAPIRenderer, JSONRenderer
 from rest_framework_xml.renderers import XMLRenderer
+from rest_framework_csv.renderers import CSVRenderer, PaginatedCSVRenderer
+
+
+class AkvoCSVRenderer(PaginatedCSVRenderer):
+    """Fixed paginated renderer
+
+    Use the PaginatedCSVRenderer or the simple CSVRenderer based on what kind
+    of data we have. If the data is a list or has the "results_field" field on
+    it, use the paginated renderer, otherwise the vanilla one.
+
+    """
+
+    def render(self, data, *args, **kwargs):
+        if not isinstance(data, list) and self.results_field in data:
+            data = data[self.results_field]
+        if isinstance(data, list):
+            return super(PaginatedCSVRenderer, self).render(data, *args, **kwargs)
+        return CSVRenderer.render(self, data, *args, **kwargs)
 
 
 def _rename_fields(results):
