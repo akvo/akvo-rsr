86c2260ea956adf1e7c2e9d809e6930226853654
Gabriel von Heijne <gabriel@akvo.org>
2018-06-05 16:18:29 +0200

    [#3217] Add is_restricted field to UserProjects model
    The UserProjects.is_restricted field indicates if the UserProjects.projects field list of projects is to be used to limit which projects the user can access

--- akvo/rsr/models/user_projects.py
+++ akvo/rsr/models/user_projects.py
@@ -5,40 +5,22 @@
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
 from django.db import models
-from django.db.models.query import QuerySet as DjangoQuerySet
 from django.utils.translation import ugettext_lazy as _
-
-from .models_utils import QuerySetManager
 
 
 class UserProjects(models.Model):
 
     user = models.OneToOneField('User', related_name='user_projects')
-    projects = models.ManyToManyField('Project', related_name='permitted_users')
+    is_restricted = models.BooleanField(default=False)
+    projects = models.ManyToManyField('Project', related_name='permitted_users', null=True, blank=True)
 
     def __unicode__(self):
         return '{} - {} projects'.format(
             self.user.email,
             self.projects.count())
 
-    # objects = QuerySetManager()
-    #
-    # class QuerySet(DjangoQuerySet):
-    #     def projects(self):
-    #         """
-    #         """
-    #         from ..models import Project
-    #
-    #         ### DEBUG ###
-    #         import pdb
-    #         pdb.set_trace()
-    #         ### DEBUG ###
-    #
-    #         return Project.objects.filter(employees__in=self).distinct()
-
     class Meta:
         app_label = 'rsr'
         verbose_name = _(u'user projects')
         verbose_name_plural = _(u'users projects')
         ordering = ('user_id',)
-
