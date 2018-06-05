cda02674c98c52d586274f1f7a38ad64e921ada6
Gabriel von Heijne <gabriel@akvo.org>
2018-06-05 16:21:41 +0200

    [#3217] Fix UserProjectAccessSerializer.update()
    UserProjectAccessSerializer.update() didn't return correct data. Also incorporate updating the is_restricted field.

--- akvo/rest/serializers/user_projects.py
+++ akvo/rest/serializers/user_projects.py
@@ -4,6 +4,7 @@
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
+from django.contrib.auth import get_user_model
 from django.db import transaction
 
 from rest_framework import serializers
@@ -18,6 +19,7 @@ class UserProjectsSerializer(BaseRSRSerializer):
     class Meta:
         model = UserProjects
         fields = (
+            'is_restricted',
             'projects',
         )
 
@@ -43,28 +45,37 @@ class UserProjectAccessSerializer(BaseRSRSerializer):
         del self.fields['absolute_url']
 
     def get_all_projects(self, obj):
-
         return obj.user.my_projects().values('id', 'title')
 
-    def update(self, instance, validated_data):
+    def update(self, user, validated_data):
         """ Custom update method that adds and/or removes projects from the UserProjects.projects
             list
         """
         # Wrap in a transaction to make sure we don't "forget" the original list if there's a
         # PermissionDenied error
         with transaction.atomic():
-            try:
-                user_projects = UserProjects.objects.get(user=instance)
-            except UserProjects.DoesNotExist:
-                # If this is the first time we set access for this user we need to create the
-                # UserProjects object
-                user_projects = UserProjects.objects.create(user=instance)
-            user_projects.projects.clear()
-            allowable_projects = instance.my_projects()
-            for project in validated_data['user_projects']['projects']:
-                # Check that each project is accessible by the user
-                if not project in allowable_projects:
-                    raise PermissionDenied(u"Project {} is not accessible by user {}".format(
-                        project.pk, instance.pk))
-                user_projects.projects.add(project)
-        return instance
+            user_projects_data = validated_data.get('user_projects', None)
+
+            if user_projects_data is not None:
+                user_projects, _created = UserProjects.objects.get_or_create(user=user)
+
+                is_restricted = user_projects_data.get('is_restricted', None)
+                if is_restricted is not None:
+                    user_projects.is_restricted = is_restricted
+                    user_projects.save()
+
+                allowable_projects = user.my_projects()
+                projects = user_projects_data.get('projects', None)
+                if projects is not None:
+                    if not _created:
+                        user_projects.projects.clear()
+                    for project in projects:
+                        # Check that each project is accessible by the user
+                        if not project in allowable_projects:
+                            raise PermissionDenied(u"Project {} is not accessible by user {}".format(
+                                project.pk, user.pk))
+                        user_projects.projects.add(project)
+        # For reasons I don't understand, on the original instance, user, the fields on
+        # UserProjects for the instance are not updated resulting in returning stale data if there
+        # have been changes. Fix by fetching a fresh instance of User.
+        return get_user_model().objects.get(pk=user.pk)
