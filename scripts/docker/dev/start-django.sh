cd9f3acac74baaf8db2ea71abae807fb3c285d8e
Dan Lebrero <dlebrero@gmail.com>
2018-08-08 17:02:33 +0200

    [#3337] migrations to not prompt

--- scripts/docker/dev/start-django.sh
+++ scripts/docker/dev/start-django.sh
@@ -4,6 +4,6 @@ set -eu
 
 ./scripts/docker/dev/wait-for-dependencies.sh
 
-python manage.py migrate
+python manage.py migrate --noinput
 #python manage.py collectstatic
 python manage.py runserver 0.0.0.0:8000
