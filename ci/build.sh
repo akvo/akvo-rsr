09744542bc93b4d09ba50da95f799a08174a228a
Dan Lebrero <dlebrero@gmail.com>
2018-08-13 17:34:06 +0200

    [#3337] debugging if CI gets to running the build script

--- ci/build.sh
+++ ci/build.sh
@@ -14,8 +14,8 @@ fi
 log Starting docker-compose
 docker-compose -p rsrci -f docker-compose.yaml -f docker-compose.ci.yaml up -d --build
 
-log Running tests
-docker-compose -p rsrci -f docker-compose.yaml -f docker-compose.ci.yaml run web scripts/docker/dev/run-as-user.sh scripts/docker/ci/build.sh
+#log Running tests
+#docker-compose -p rsrci -f docker-compose.yaml -f docker-compose.ci.yaml run web scripts/docker/dev/run-as-user.sh scripts/docker/ci/build.sh
 #log Stopping docker-compose
 #docker-compose -p rsrci -f docker-compose.yaml -f docker-compose.ci.yaml down
 log Done
