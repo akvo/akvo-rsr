671baf04a0cec866a2f62984c6f24507eb1fcdcc
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-08-14 15:28:56 +0530

    [#3355] Fix tests to really test content owned organisations
    Org A & B were not set to be paying organisations, and C was not given the
    IATI_IMPLEMENTING_PARTNER role, making C not really a content owned
    organisation.

--- akvo/rsr/tests/test_project_access.py
+++ akvo/rsr/tests/test_project_access.py
@@ -31,8 +31,8 @@ class RestrictedUserProjectsByOrgTestCase(TestCase):
 
         check_auth_groups(settings.REQUIRED_AUTH_GROUPS)
 
-        self.org_a = Organisation.objects.create(name='A', long_name='A')
-        self.org_b = Organisation.objects.create(name='B', long_name='B')
+        self.org_a = Organisation.objects.create(name='A', long_name='A', can_create_projects=True)
+        self.org_b = Organisation.objects.create(name='B', long_name='B', can_create_projects=True)
 
         self.projects = {}
         for title in "XYZ":
@@ -112,7 +112,11 @@ class RestrictedUserProjectsByOrgTestCase(TestCase):
         org_content_owned = Organisation.objects.create(
             name='C', long_name='C', can_create_projects=False
         )
-        Partnership.objects.create(organisation=org_content_owned, project=self.projects['Y'])
+        Partnership.objects.create(
+            organisation=org_content_owned,
+            project=self.projects['Y'],
+            iati_organisation_role=Partnership.IATI_IMPLEMENTING_PARTNER
+        )
         user = PermissionsTestCase.create_user('P@org.org')
         Employment.objects.create(
             user=user, organisation=org_content_owned, group=self.users, is_approved=True
@@ -131,15 +135,6 @@ class RestrictedUserProjectsByOrgTestCase(TestCase):
 
         self.assertTrue(self.user_o.has_perm('rsr.view_project', self.projects['Z']))
 
-    def test_admin_cannot_restrict_other_admins(self):
-        with self.assertRaises(InvalidPermissionChange):
-            RestrictedUserProjectsByOrg.restrict_projects(
-                self.user_m, self.user_n, [self.projects['Y']]
-            )
-
-        self.assertTrue(self.user_n.has_perm('rsr.view_project', self.projects['Y']))
-        self.assertTrue(self.user_n.has_perm('rsr.view_project', self.projects['Z']))
-
     def test_admin_cannot_restrict_inaccessible_projects_for_content_owned_user(self):
         """
         User M      User N      User O         User P
@@ -154,10 +149,18 @@ class RestrictedUserProjectsByOrgTestCase(TestCase):
                         +-------------------------+
         """
         org_content_owned = Organisation.objects.create(
-            name='C', long_name='C', can_create_projects=False
+            name='C', long_name='C', can_create_projects=False,
         )
-        Partnership.objects.create(organisation=org_content_owned, project=self.projects['Y'])
-        Partnership.objects.create(organisation=org_content_owned, project=self.projects['Z'])
+        Partnership.objects.create(
+            organisation=org_content_owned,
+            project=self.projects['Y'],
+            iati_organisation_role=Partnership.IATI_IMPLEMENTING_PARTNER
+        )
+        Partnership.objects.create(
+            organisation=org_content_owned,
+            project=self.projects['Z'],
+            iati_organisation_role=Partnership.IATI_IMPLEMENTING_PARTNER
+        )
         user = PermissionsTestCase.create_user('P@org.org')
         Employment.objects.create(
             user=user, organisation=org_content_owned, group=self.users, is_approved=True
@@ -165,6 +168,23 @@ class RestrictedUserProjectsByOrgTestCase(TestCase):
 
         with self.assertRaises(InvalidPermissionChange):
             RestrictedUserProjectsByOrg.restrict_projects(self.user_m, user, [self.projects['Z']])
+
+    def test_admin_cannot_restrict_other_admins(self):
+        with self.assertRaises(InvalidPermissionChange):
+            RestrictedUserProjectsByOrg.restrict_projects(
+                self.user_m, self.user_n, [self.projects['Y']]
+            )
+
+        self.assertTrue(self.user_n.has_perm('rsr.view_project', self.projects['Y']))
+        self.assertTrue(self.user_n.has_perm('rsr.view_project', self.projects['Z']))
+
+    def test_admin_cannot_restrict_unmanageable_user(self):
+        Y = self.projects['Y']
+
+        with self.assertRaises(InvalidPermissionChange):
+            RestrictedUserProjectsByOrg.restrict_projects(self.user_m, self.user_o, [Y])
+
+        self.assertTrue(self.user_o.has_perm('rsr.view_project', Y))
 
     # Remove Restrictions
 
@@ -243,8 +263,16 @@ class RestrictedUserProjectsByOrgTestCase(TestCase):
         org_content_owned = Organisation.objects.create(
             name='C', long_name='C', can_create_projects=False
         )
-        Partnership.objects.create(organisation=org_content_owned, project=self.projects['Y'])
-        Partnership.objects.create(organisation=org_content_owned, project=self.projects['Z'])
+        Partnership.objects.create(
+            organisation=org_content_owned,
+            project=self.projects['Y'],
+            iati_organisation_role=Partnership.IATI_IMPLEMENTING_PARTNER
+        )
+        Partnership.objects.create(
+            organisation=org_content_owned,
+            project=self.projects['Z'],
+            iati_organisation_role=Partnership.IATI_IMPLEMENTING_PARTNER
+        )
         user_p = PermissionsTestCase.create_user('P@org.org')
         Employment.objects.create(
             user=user_p, organisation=org_content_owned, group=self.users, is_approved=True
@@ -253,7 +281,11 @@ class RestrictedUserProjectsByOrgTestCase(TestCase):
 
         project = Project.objects.create(title='W')
         Partnership.objects.create(organisation=self.org_b, project=project)
-        Partnership.objects.create(organisation=org_content_owned, project=project)
+        Partnership.objects.create(
+            organisation=org_content_owned,
+            project=project,
+            iati_organisation_role=Partnership.IATI_IMPLEMENTING_PARTNER
+        )
 
         self.assertTrue(user_p.has_perm('rsr.view_project', self.projects['Z']))
         self.assertTrue(user_p.has_perm('rsr.view_project', project))
@@ -277,7 +309,11 @@ class RestrictedUserProjectsByOrgTestCase(TestCase):
         org_content_owned = Organisation.objects.create(
             name='C', long_name='C', can_create_projects=False
         )
-        Partnership.objects.create(organisation=org_content_owned, project=self.projects['Y'])
+        Partnership.objects.create(
+            organisation=org_content_owned,
+            project=self.projects['Y'],
+            iati_organisation_role=Partnership.IATI_IMPLEMENTING_PARTNER
+        )
         user_p = PermissionsTestCase.create_user('P@org.org')
         Employment.objects.create(
             user=user_p, organisation=org_content_owned, group=self.users, is_approved=True
