84881df84fd9edb09569496f52b2dc8eef364ccf
Dan Lebrero <dlebrero@gmail.com>
2018-08-22 18:30:57 +0200

    [#3337] create-db: prompt for passwords

--- scripts/data/create-db.sh
+++ scripts/data/create-db.sh
@@ -2,15 +2,21 @@
 
 set -eu
 
-NEW_USER=$1
-NEW_DB_NAME=$2
-NEW_USER_PASSWORD=$3
-DB_HOST=$4
-SUPER_USER=$5
+DB_HOST=$1
+SUPER_USER=$2
+NEW_DB_NAME=$3
+NEW_USER=$4
+
+read -s -p "Password for the new user ${NEW_USER} for the new DB ${NEW_DB_NAME}@${DB_HOST}: " NEW_USER_PASSWORD
+
+echo ""
+read -s -p "Password for the **super** user ${SUPER_USER} for host ${DB_HOST}: " SUPER_USER_PASSWORD
+
+echo ""
 
 psql_settings="--username=${SUPER_USER} --host=${DB_HOST}"
 
-psql ${psql_settings} -c "CREATE USER $NEW_USER WITH ENCRYPTED PASSWORD '$NEW_USER_PASSWORD'";
-psql ${psql_settings}  -c "CREATE DATABASE $NEW_DB_NAME OWNER $NEW_USER;"
-psql ${psql_settings} --dbname=${NEW_DB_NAME} -c "ALTER SCHEMA public OWNER TO $NEW_USER;"
+PGPASSWORD=${SUPER_USER_PASSWORD} psql ${psql_settings} -c "CREATE USER $NEW_USER WITH ENCRYPTED PASSWORD '$NEW_USER_PASSWORD'";
+PGPASSWORD=${SUPER_USER_PASSWORD} psql ${psql_settings}  -c "CREATE DATABASE $NEW_DB_NAME OWNER $NEW_USER;"
+PGPASSWORD=${SUPER_USER_PASSWORD} psql ${psql_settings} --dbname=${NEW_DB_NAME} -c "ALTER SCHEMA public OWNER TO $NEW_USER;"
 
