888561ad954648f9c6029a27fe43de838baaf4ad
Dan Lebrero <dlebrero@gmail.com>
2018-08-22 18:42:14 +0200

    [#3337] restore db: prompt for passwords

--- scripts/data/restore-from-dump.sh
+++ scripts/data/restore-from-dump.sh
@@ -2,15 +2,21 @@
 
 set -eu
 
-RSR_USER=$1
+DB_HOST=$1
 RSR_DB_NAME=$2
-DB_HOST=$3
+RSR_USER=$3
 DUMP_FILE=$4
+
+read -s -p "Password for user ${RSR_USER} for the new DB ${RSR_DB_NAME}@${DB_HOST}: " PASSWORD
+
+export PGPASSWORD=${PASSWORD}
+
+echo ""
 
 psql_settings="--username=${RSR_USER} --host=${DB_HOST} --dbname=${RSR_DB_NAME}"
 
 psql ${psql_settings} -c 'DROP SCHEMA public CASCADE'
 psql ${psql_settings} -c 'CREATE SCHEMA public'
-psql ${psql_settings} --set ON_ERROR_STOP=on < "${DUMP_FILE}"
+gunzip -c "${DUMP_FILE}" | psql ${psql_settings} --set ON_ERROR_STOP=on
 
 
