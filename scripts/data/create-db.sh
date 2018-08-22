a5d2470a65749dba635b913098afbfc143e9b75f
Dan Lebrero <dlebrero@gmail.com>
2018-08-22 18:42:58 +0200

    [#3337] create-db: clean up

--- scripts/data/create-db.sh
+++ scripts/data/create-db.sh
@@ -16,7 +16,9 @@ echo ""
 
 psql_settings="--username=${SUPER_USER} --host=${DB_HOST}"
 
-PGPASSWORD=${SUPER_USER_PASSWORD} psql ${psql_settings} -c "CREATE USER $NEW_USER WITH ENCRYPTED PASSWORD '$NEW_USER_PASSWORD'";
-PGPASSWORD=${SUPER_USER_PASSWORD} psql ${psql_settings}  -c "CREATE DATABASE $NEW_DB_NAME OWNER $NEW_USER;"
-PGPASSWORD=${SUPER_USER_PASSWORD} psql ${psql_settings} --dbname=${NEW_DB_NAME} -c "ALTER SCHEMA public OWNER TO $NEW_USER;"
+export PGPASSWORD=${SUPER_USER_PASSWORD}
+
+psql ${psql_settings} -c "CREATE USER $NEW_USER WITH ENCRYPTED PASSWORD '$NEW_USER_PASSWORD'";
+psql ${psql_settings}  -c "CREATE DATABASE $NEW_DB_NAME OWNER $NEW_USER;"
+psql ${psql_settings} --dbname=${NEW_DB_NAME} -c "ALTER SCHEMA public OWNER TO $NEW_USER;"
 
