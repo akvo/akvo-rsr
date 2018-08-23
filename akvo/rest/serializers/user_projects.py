7ba235e3b27586433966fa0993e4283f044820df
Gabriel von Heijne <gabriel@akvo.org>
2018-08-23 18:11:10 +0200

    [#3355] Refactor UserProjectAccessSerializer
    The refactoring mainly does two things:
      - add the read-only field organisation_groups that provides the client with a custom data set for rendering the project list in the project access management page
      - uses the restrict_projects() and unrestrict_projects() functions defined in models.user_projects in the update() method of the serializer
    
    See comments in the module for more details

--- akvo/rest/serializers/user_projects.py
+++ akvo/rest/serializers/user_projects.py
@@ -11,7 +11,8 @@ from rest_framework import serializers
 from rest_framework.exceptions import PermissionDenied
 
 from akvo.rest.serializers.rsr_serializer import BaseRSRSerializer
-from akvo.rsr.models import User, UserProjects
+from akvo.rsr.models import User, UserProjects, Organisation, Project
+from akvo.rsr.models.user_projects import restrict_projects, unrestrict_projects
 
 
 class UserProjectsSerializer(BaseRSRSerializer):
@@ -25,17 +26,23 @@ class UserProjectsSerializer(BaseRSRSerializer):
 
 
 class UserProjectAccessSerializer(BaseRSRSerializer):
+    """ This serializer includes the "synthetic" field organisation_groups which groups the projects
+        according to which organisations that the admin or the user has in common with the projects.
 
-    all_projects = serializers.SerializerMethodField()
+       organisation_groups is read only and is used to populate the React client in a straight
+       mapping of the data.
+    """
+
+    organisation_groups = serializers.SerializerMethodField()
     user_projects = UserProjectsSerializer()
 
     class Meta:
         model = User
         fields = (
-            'id',
             'user_projects',
-            'all_projects',
+            'organisation_groups',
         )
+        read_only_fields = ('organisation_groups',)
 
     def __init__(self, *args, **kwargs):
         """ Delete the 'absolute_url' field added in BaseRSRSerializer.__init__().
@@ -44,12 +51,71 @@ class UserProjectAccessSerializer(BaseRSRSerializer):
         super(UserProjectAccessSerializer, self).__init__(*args, **kwargs)
         del self.fields['absolute_url']
 
-    @staticmethod
-    def get_all_projects(obj):
-        return obj.user.my_projects().values('id', 'title')
+        user = self.instance
+        # for some reason the serializer is called three times when accessing the endpoint,
+        # therefore we check for the existence of a UserProjects object to not have to call
+        # restrict_projects more than once
+        try:
+            UserProjects.objects.get(user=user)
+        except UserProjects.DoesNotExist:
+            admin = self.context['request'].user
+            # Make sure we have a UserProjects object, but don't change an existing one
+            restrict_projects(admin, user, Project.objects.none())
 
-    @staticmethod
-    def update(user, validated_data):
+    def get_organisation_groups(self, obj):
+
+        admin = self.context['request'].user.user
+        admin_orgs = admin.get_admin_employment_orgs()
+        # All projects that I may admin
+        admin_associated_projects = admin.admin_projects()
+
+        user = obj.user
+        # All orgs the user is employed by as a non-admin
+        user_orgs = user.get_non_admin_employment_orgs()
+        # All projects the user is associated with
+        user_associated_project = user_orgs.all_projects()
+        try:
+            user_projects = UserProjects.objects.get(user=user)
+        except UserProjects.DoesNotExist:
+            user_projects = None
+
+        # All projects the admin and the user have in common.
+        # It is those projects that the admin may restrict
+        common_projects = admin_associated_projects & user_associated_project
+
+        # The project list is split on partners shared by the project and the admin or the user
+        project_sets = {}
+        for project in common_projects:
+            # Determine which partners the the project has in common with the admin and/or the user
+            # See the tests at rest.test_project_access for more detail on the resulting data
+            # structure
+            project_partners = project.all_partners()
+            admin_orgs_for_project = admin_orgs & project_partners
+            user_orgs_for_project = user_orgs & project_partners
+            admin_org_pks = admin_orgs_for_project.values_list('pk', flat=True)
+            user_org_pks = user_orgs_for_project.values_list('pk', flat=True)
+            # Crete a tuple of the org's pks so it can be used as a dict key
+            common_orgs_pks = tuple(set().union(admin_org_pks, user_org_pks))
+            project_sets.setdefault(common_orgs_pks, []).append(project)
+
+        projects_grouped_by_orgs = []
+        user_projects_projects = user_projects.projects.all() if user_projects else False
+        for orgs_pks, projects in project_sets.items():
+            orgs = Organisation.objects.filter(pk__in=orgs_pks)
+            orgs_string = ', '.join([org.name for org in orgs])
+            project_group = []
+            for project in projects:
+                project_group.append(dict(
+                    id=project.pk,
+                    title=project.title,
+                    subtitle=project.subtitle,
+                    access=project in user_projects_projects
+                ))
+            projects_grouped_by_orgs.append(dict(organisations=orgs_string, projects=project_group))
+
+        return projects_grouped_by_orgs
+
+    def update(self, user, validated_data):
         """ Custom update method that adds and/or removes projects from the UserProjects.projects
             list
         """
@@ -59,24 +125,38 @@ class UserProjectAccessSerializer(BaseRSRSerializer):
             user_projects_data = validated_data.get('user_projects', None)
 
             if user_projects_data is not None:
-                user_projects, _created = UserProjects.objects.get_or_create(user=user)
 
+                admin = self.context['request'].user.user
+                # All projects the admin can restrict
+                admin_associated_projects = admin.admin_projects()
+
+                # Check if we have a list of projects. These are all projects that the admin can
+                # grant access to
+                accessible_projects = user_projects_data.get('projects', None)
+
+                # To set restrictions, we restrict all admin_associated_projects and then add
+                # back the accessible_projects.
+
+                if accessible_projects is not None:
+                    user_projects = restrict_projects(admin, user, admin_associated_projects)
+
+                # If accessible_projects is None no changes to restrictions should be made, so we
+                # call restrict_projects with an empty Project QS to get the current one back
+                # without modifying it
+                else:
+                    accessible_projects = Project.objects.none()
+                    user_projects = restrict_projects(admin, user, accessible_projects)
+
+                # Now we unrestrict the accessible_projects
+                if accessible_projects:
+                    unrestrict_projects(admin, user, accessible_projects)
+
+                # Finally set is_restricted
                 is_restricted = user_projects_data.get('is_restricted', None)
                 if is_restricted is not None:
                     user_projects.is_restricted = is_restricted
                     user_projects.save()
 
-                allowable_projects = user.my_projects()
-                projects = user_projects_data.get('projects', None)
-                if projects is not None:
-                    if not _created:
-                        user_projects.projects.clear()
-                    for project in projects:
-                        # Check that each project is accessible by the user
-                        if project not in allowable_projects:
-                            raise PermissionDenied(u"Project {} is not accessible by user {}".format(
-                                project.pk, user.pk))
-                        user_projects.projects.add(project)
         # For reasons I don't understand, on the original instance, user, the fields on
         # UserProjects for the instance are not updated resulting in returning stale data if there
         # have been changes. Fix by fetching a fresh instance of User.
