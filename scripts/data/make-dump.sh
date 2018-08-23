40a4128646aaeba8691435954e74d0a4796e99c4
Dan Lebrero <dlebrero@gmail.com>
2018-08-23 11:20:07 +0200

    [#3337] some feedback that the dump has started

--- scripts/data/make-dump.sh
+++ scripts/data/make-dump.sh
@@ -9,4 +9,7 @@ DUMP_FILE=$4
 
 read -s -p "Password for ${RSR_USER} for DB ${RSR_DB_NAME}@${DB_HOST}: " PASSWORD
 
+echo ""
+echo "Starting dump ..."
+
 PGPASSWORD=${PASSWORD} pg_dump -n public --host=${DB_HOST} --username=${RSR_USER} ${RSR_DB_NAME} --no-owner --no-acl | gzip > "${DUMP_FILE}"
