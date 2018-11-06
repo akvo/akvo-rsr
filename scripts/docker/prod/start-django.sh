f3f31ed3f3b659735fb9074e29bd7b672ca2ea91
Dan Lebrero <dlebrero@gmail.com>
2018-11-06 10:19:21 +0100

    [#3337] Increasing number of gunicorn workers
    Health check is failing in k8s

--- scripts/docker/prod/start-django.sh
+++ scripts/docker/prod/start-django.sh
@@ -3,4 +3,4 @@
 set -eu
 python manage.py migrate --noinput
 # python manage.py runserver 0.0.0.0:8000
-gunicorn akvo.wsgi --max-requests 200 --workers 5 --timeout 300 --bind 0.0.0.0:8000
+gunicorn akvo.wsgi --max-requests 200 --workers 15 --timeout 300 --bind 0.0.0.0:8000
