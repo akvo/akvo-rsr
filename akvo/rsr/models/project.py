5141f358a96d03428567c07fc3843c281c54cedb
Dan Lebrero <dlebrero@gmail.com>
2018-11-23 18:05:38 +0100

    [#3434] Load partnerships and orgs in one go, in another place

--- akvo/rsr/models/project.py
+++ akvo/rsr/models/project.py
@@ -1141,7 +1141,7 @@ class Project(TimestampsMixin, models.Model):
 
     def funding_partnerships(self):
         "Return the Partnership objects associated with the project that have funding information"
-        return self.partnerships.filter(iati_organisation_role=Partnership.IATI_FUNDING_PARTNER).order_by('organisation__name')
+        return self.partnerships.filter(iati_organisation_role=Partnership.IATI_FUNDING_PARTNER).order_by('organisation__name').prefetch_related('organisation').all()
 
     def show_status_large(self):
         "Show the current project status with background"
