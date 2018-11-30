a5850817a2ba94afc47a2d0231e13c380b32a2c9
Gabriel von Heijne <gabriel@akvo.org>
2018-11-30 15:14:01 +0100

    [#3410] Fix migration

--- akvo/rsr/migrations/0138_organisation_original.py
+++ akvo/rsr/migrations/0138_organisation_original.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='organisation',
             name='original',
-            field=models.OneToOneField(related_name='shadow', null=True, on_delete=django.db.models.deletion.SET_NULL, blank=True, to='rsr.Organisation'),
+            field=models.OneToOneField(related_name='shadow', null=True, on_delete=django.db.models.deletion.SET_NULL, blank=True, to='rsr.Organisation', help_text='Pointer to original organisation if this is a shadow. Used by EUTF'),
             preserve_default=True,
         ),
     ]
