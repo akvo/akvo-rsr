bf2ecfef247375a9fc4ac983c72875e836efadd5
Dan Lebrero <dlebrero@gmail.com>
2019-01-16 02:14:10 +0100

    [#3455] Start cron

--- scripts/docker/dev/start-django.sh
+++ scripts/docker/dev/start-django.sh
@@ -6,4 +6,10 @@ set -eu
 
 python manage.py migrate --noinput
 #python manage.py collectstatic
+
+## Not running cron jobs in dev
+#python manage.py crontab add
+#env >> /etc/environment
+#/usr/sbin/cron
+
 python manage.py runserver 0.0.0.0:8000
