1eda4ff8ce6b1727f29044270d2c8aaf39f4ac1e
Dan Lebrero <dlebrero@gmail.com>
2019-01-18 12:49:38 +0100

    [#3462] Backend to handle SIGTERM properly
    Kubernetes sends a SIGTERM that by default is ignored by bash.
    
    Trapping the signal and propagate it to gunicorn/python
    
    Googling, another possible solution is running gunicorn/python with "exec",
    but local testing shows that the "exec" approach doesnt seem to work

--- scripts/docker/prod/start-django.sh
+++ scripts/docker/prod/start-django.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
 set -eu
+
+_term() {
+  echo "Caught SIGTERM signal!"
+  kill -TERM "$child" 2>/dev/null
+}
+
+trap _term SIGTERM
+
 python manage.py migrate --noinput
 python manage.py crontab add
 ## Making all environment vars available to cron jobs
 env >> /etc/environment
 /usr/sbin/cron
-gunicorn akvo.wsgi --max-requests 200 --workers 5 --timeout 300 --bind 0.0.0.0:8000
+gunicorn akvo.wsgi --max-requests 200 --workers 5 --timeout 300 --bind 0.0.0.0:8000 &
+
+child=$!
+wait "$child"
