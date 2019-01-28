16f33986799f49f58bb83d4bd718023e20468972
Gabriel von Heijne <gabriel@akvo.org>
2019-01-28 15:30:14 +0100

    [#3340] Docker configuration for webpack pipeline
    Add docker config to run the webpack pipeline for both development and production

--- scripts/docker/dev/start-django.sh
+++ scripts/docker/dev/start-django.sh
@@ -4,6 +4,16 @@ set -eu
 
 ./scripts/docker/dev/wait-for-dependencies.sh
 
+pushd akvo/rsr/static
+if [[ ! -f "node_modules" ]]; then
+  npm install
+fi
+if [[ ! -f "dist/vendors.js" ]]; then
+  npm run dev
+fi
+npm run devw&
+popd
+
 python manage.py migrate --noinput
 #python manage.py collectstatic
 python manage.py runserver 0.0.0.0:8000
