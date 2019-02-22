1675f7e36995e71b323be23bb4478cc46c18f54e
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-02-22 21:41:18 +0530

    [#2384] Fix broken symbols in password test

--- akvo/rsr/tests/views/test_account.py
+++ akvo/rsr/tests/views/test_account.py
@@ -215,10 +215,10 @@ class AccountRegistrationTestCase(TestCase):
 
         # Then
         self.assertEqual(response.status_code, 200)
-
         self.assertTrue(response.content.decode('utf-8').find(
             u'The password must contain at least one symbol: '
-            u'()[]{}|\\`~!@#$%^&amp;*_-+=;:&#39;&quot;,&lt;&gt;./?') > 0)
+            u'()[]{}|\`~!@#$%%^&amp;*_-+=;:&#39;&quot;,&lt;&gt;./?') > 0
+        )
 
     def test_registration_password_has_no_uppercase(self):
         # Given
