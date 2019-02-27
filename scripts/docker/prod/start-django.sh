8729b69f072897cf6e4b50b1bf31e9b4ce4006cc
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-02-27 16:14:12 +0530

    Add debug flags when running gunicorn in test

--- scripts/docker/prod/start-django.sh
+++ scripts/docker/prod/start-django.sh
@@ -28,7 +28,7 @@ env >> /etc/environment
 log Starting cron
 /usr/sbin/cron
 log Starting gunicorn in background
-gunicorn akvo.wsgi --max-requests 200 --workers 5 --timeout 300 --bind 0.0.0.0:8000 &
+gunicorn "${GUNICORN_DEBUG_ARGS:-}"  akvo.wsgi --max-requests 200 --workers 5 --timeout 300 --bind 0.0.0.0:8000 &
 
 child=$!
 wait "$child"
