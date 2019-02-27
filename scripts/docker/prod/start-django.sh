59e074ff9f759745dbba0758768aa369ff2400e6
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-02-27 16:41:17 +0530

    Fix order of module name and debug args for gunicorn.

--- scripts/docker/prod/start-django.sh
+++ scripts/docker/prod/start-django.sh
@@ -28,7 +28,7 @@ env >> /etc/environment
 log Starting cron
 /usr/sbin/cron
 log Starting gunicorn in background
-gunicorn "${GUNICORN_DEBUG_ARGS:-}"  akvo.wsgi --max-requests 200 --workers 5 --timeout 300 --bind 0.0.0.0:8000 &
+gunicorn akvo.wsgi "${GUNICORN_DEBUG_ARGS:-}" --max-requests 200 --workers 5 --timeout 300 --bind 0.0.0.0:8000 &
 
 child=$!
 wait "$child"
