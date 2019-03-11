8bb2e30dc2cde23d3a231994f03568d30a17d111
zuhdil <zuhdil@gmail.com>
2019-03-11 16:16:46 +0700

    [#3488] Tests to make sure static assets are reachable

--- scripts/docker/ci/end-to-end.sh
+++ scripts/docker/ci/end-to-end.sh
@@ -6,23 +6,46 @@ function log {
    echo "$(date +"%T") - BUILD INFO - $*"
 }
 
-URL=http://rsr.localdev.akvo.org/
-MAX_ATTEMPTS=60
+function get_status {
+  echo $(curl --output /dev/null --silent --head --write-out %{http_code} $1)
+}
+
+function test_http_status {
+  local status=$(get_status "$2")
+  if [ "$status" -ne "$1" ]; then
+    log Failed to load "$2" with expected status "$1" got "$status".
+    exit 1
+  fi
+  printf "."
+}
+
+BASE_URL=http://rsr.localdev.akvo.org
+HTTP_OK=200
+HTTP_ERRORS_START=400
+
+MAX_ATTEMPTS=120
 ATTEMPTS=0
-RES=400
+RESPONSE_STATUS="${HTTP_ERRORS_START}"
 
 log Waiting for application to be ready ...
-while [[ "${RES}" -ge 400 && "${ATTEMPTS}" -lt "${MAX_ATTEMPTS}" ]]; do
-    RES=$(curl --output /dev/null --silent --head --write-out %{http_code} "${URL}")
+while [[ "${RESPONSE_STATUS}" -ge ${HTTP_ERRORS_START} && "${ATTEMPTS}" -lt "${MAX_ATTEMPTS}" ]]; do
+    RESPONSE_STATUS=$(get_status "${BASE_URL}")
     let ATTEMPTS+=1
     sleep 1
 done
 
-if [[ $RES -ge 400 ]]; then
+if [[ "${RESPONSE_STATUS}" -ge ${HTTP_ERRORS_START} ]]; then
   log Application takes too long to respond!
   exit 1
 fi
 
-wget "${URL}"
+log Testing static assets
+PAGE_CONTENT=$(curl --location --silent "${BASE_URL}")
+ASSETS=$(echo "${PAGE_CONTENT}" | grep -Eo "\/static/rsr/dist/.+(css|js)" | head -n 3)
 
-log Done
+while read -r path; do
+  test_http_status "${HTTP_OK}" "$BASE_URL$path"
+done <<< "$ASSETS"
+echo ""
+
+log End-to-end testing done!
