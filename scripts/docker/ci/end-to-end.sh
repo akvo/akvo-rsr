a8e74207a5b984c3ae8699da6bebe9715bdd3b71
zuhdil <zuhdil@gmail.com>
2019-03-11 08:18:31 +0700

    [#3488] Poll to wait for application to be ready in end-to-end testing

--- scripts/docker/ci/end-to-end.sh
+++ scripts/docker/ci/end-to-end.sh
@@ -6,7 +6,23 @@ function log {
    echo "$(date +"%T") - BUILD INFO - $*"
 }
 
-log Running end to end test. For Puneeth to fill!
+URL=http://rsr.localdev.akvo.org/
+MAX_ATTEMPTS=60
+ATTEMPTS=0
+RES=400
 
-wget http://rsr.localdev.akvo.org/
+log Waiting for application to be ready ...
+while [[ "${RES}" -ge 400 && "${ATTEMPTS}" -lt "${MAX_ATTEMPTS}" ]]; do
+    RES=$(curl --output /dev/null --silent --head --write-out %{http_code} "${URL}")
+    let ATTEMPTS+=1
+    sleep 1
+done
+
+if [[ $RES -ge 400 ]]; then
+  log Application takes too long to respond!
+  exit 1
+fi
+
+wget "${URL}"
+
 log Done
