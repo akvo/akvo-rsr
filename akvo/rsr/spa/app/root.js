c29ff444c1ca85254c2f3a040a4b565d2d0fd09a
martin christov <martin.christov@sensika.com>
2019-04-12 12:31:35 +0300

    [#3520] testing 2nd level paths

--- akvo/rsr/spa/app/root.js
+++ akvo/rsr/spa/app/root.js
@@ -1,16 +1,33 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 const App = () => (
   <div>Hello world</div>
 )
 
+const Test = () => (
+  <div>Test</div>
+)
+
+const Test2 = () => (
+  <div>Something else</div>
+)
+
 const Root = () => {
   return (
     <Router basename="/en/my-rsr">
-      <Switch>
-        <Route path="/" component={App} exact />
-      </Switch>
+      <div>
+        <Link to="/">Home</Link>
+        <span> | </span>
+        <Link to="/something">Something</Link>
+        <span> | </span>
+        <Link to="/something/else">Something else</Link>
+        <Switch>
+          <Route path="/" component={App} exact />
+          <Route path="/something" exact component={Test} />
+          <Route path="/something/else" component={Test2} />
+        </Switch>
+      </div>
     </Router>
   )
 }
