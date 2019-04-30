4041d3cda01829c744e1402aa9a65d58288ac4a0
martin christov <martin.christov@sensika.com>
2019-04-30 15:16:04 +0300

    [#3545] geocoding with opencagedata

--- akvo/rsr/spa/app/modules/editor/locations/comp/search-item.jsx
+++ akvo/rsr/spa/app/modules/editor/locations/comp/search-item.jsx
@@ -4,7 +4,7 @@ import querystring from 'querystring'
 import { Form, Select, Spin, Icon } from 'antd'
 
 import InputLabel from '../../../../utils/input-label'
-import { googleGeocodeApiKey } from '../../../../utils/constants'
+import { opencagedataKey } from '../../../../utils/constants'
 
 const { Item } = Form
 const { Option } = Select
@@ -21,16 +21,17 @@ function $fetch(value, callback) {
 
   function req() {
     const str = querystring.encode({
-      key: googleGeocodeApiKey,
-      address: value,
+      key: opencagedataKey,
+      q: value,
+      pretty: 1
     })
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?${str}`)
+    fetch(`https://api.opencagedata.com/geocode/v1/json?${str}`)
       .then(response => response.json())
       .then((d) => {
         if (currentValue === value) {
           const data = d.results.map(r => ({
-            value: { address: r.formatted_address, coordinates: r.geometry.location },
-            text: r.formatted_address,
+            coordinates: r.geometry,
+            text: r.formatted,
           }))
           callback(data)
         }
@@ -51,15 +52,15 @@ class SearchItem extends React.Component{
     })
     $fetch(value, data => this.setState({ data, fetching: false }))
   }
-  handleChange = (value) => {
-    this.props.onChange(value)
+  handleChange = (index) => {
+    this.props.onChange(this.state.data[index])
   }
   render(){
-    const options = this.state.data.map((d, index) => <Option key={index} value={d.value}>{d.text}</Option>)
+    const options = this.state.data.map((d, index) => <Option value={index}>{d.text}</Option>)
     return (
       <Item label={<InputLabel tooltip="..." more={<Icon type="delete" onClick={this.props.onRemove} />}>City</InputLabel>}>
         <Select
-          value={this.props.location.address}
+          value={this.props.location.text}
           showSearch
           defaultActiveFirstOption={false}
           showArrow={false}
