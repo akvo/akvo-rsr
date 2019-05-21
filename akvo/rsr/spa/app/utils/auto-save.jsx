a5bf842abc968ae496d77b92305ab3c8d5189bf3
martin christov <martin.christov@sensika.com>
2019-05-21 13:27:21 +0300

    [#3545] auto-save making proper diff

--- akvo/rsr/spa/app/utils/auto-save.jsx
+++ akvo/rsr/spa/app/utils/auto-save.jsx
@@ -3,32 +3,46 @@ import { FormSpy } from 'react-final-form'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { cloneDeep, isEmpty } from 'lodash'
-import diff from 'object-diff'
+import {diff} from 'deep-object-diff'
 import * as actions from '../modules/editor/actions'
 
 const debounce = 1000
 
+const customDiff = (oldObj, newObj) => {
+  const difference = diff(oldObj, newObj)
+  // override deep diff to take the entire updated array
+  Object.keys(difference).forEach((key) => {
+    if(typeof difference[key] === 'object'){
+      difference[key] = newObj[key]
+    }
+  })
+  return difference
+}
+
 class AutoSave extends React.Component {
+  componentWillMount(){
+    this.lastSavedValues = cloneDeep(this.props.values)
+  }
   componentWillReceiveProps() {
     if (this.timeout) {
       clearTimeout(this.timeout)
     }
-    const values = cloneDeep(this.props.values)
-    this.timeout = setTimeout(() => this.save(values), debounce)
+    this.timeout = setTimeout(this.save, debounce)
   }
 
-  save = (prevValues) => {
+  save = () => {
     const { values, setName, itemIndex, sectionIndex } = this.props
 
     if(setName !== undefined && itemIndex !== undefined){
       // if this item has changed
-      if(prevValues[setName].length !== values[setName].length){
+      if(this.lastSavedValues[setName].length !== values[setName].length){
         return
       }
 
-      const difference = diff(prevValues[setName][itemIndex], values[setName][itemIndex])
+      const difference = customDiff(this.lastSavedValues[setName][itemIndex], values[setName][itemIndex])
       if(!isEmpty(difference)){
         this.props.editSetItem(sectionIndex, setName, itemIndex, difference)
+        this.lastSavedValues = {...this.lastSavedValues, ...difference}
       }
     }
   }
