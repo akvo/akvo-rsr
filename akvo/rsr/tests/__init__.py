7aa858102872bce8727fc0a3a715fea412798dd2
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-05-31 10:20:47 +0530

    [#3615] Mock the logger.info method to prevent log messages in tests
    This should make the tests run slightly faster!

--- akvo/rsr/tests/__init__.py
+++ akvo/rsr/tests/__init__.py
@@ -5,3 +5,8 @@
 See more details in the license.txt file located at the root folder of the Akvo RSR module.
 For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 """
+
+import logging
+
+logger = logging.getLogger('akvo.rsr')
+logger.info = lambda *args: None
