565652d512ac1342245d4e09ce3b4f0b16a7989d
Daniel Lebrero Berna <dlebrero@gmail.com>
2019-06-06 18:25:00 +0200

    [#3617] Deploy production version by default

--- ci/rsrchart/new-env.sh
+++ ci/rsrchart/new-env.sh
@@ -6,16 +6,33 @@ function log {
    echo "$(date +"%T") - INFO - $*"
 }
 
+function switch_cluster () {
+    CLUSTER=$1
+    log "Reading ${CLUSTER} version"
+    log "running: gcloud container clusters get-credentials ${CLUSTER} --zone europe-west1-d --project akvo-lumen"
+    if ! gcloud container clusters get-credentials "${CLUSTER}" --zone europe-west1-d --project akvo-lumen; then
+        log "Could not change context to ${CLUSTER}. Nothing done."
+        exit 3
+    fi
+}
+
+function read_version () {
+    VERSION=$(kubectl get deployments rsr -o jsonpath="{@.spec.template.metadata.labels['rsr-version']}")
+}
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 release_name=$1
 restore_from=$2
+rsr_version=${3:-prod}
 HELM_EXTRA_OPTS=""
 
 if [[ ! $release_name =~ ^rsr[1-4]$ ]]; then
     echo "Release must be one of rsr1,rsr2,rsr3 or rsr4"
     exit 1
 fi
+
+switch_cluster "test"
 
 if [[ $restore_from == "prod" ]]; then
 
@@ -34,10 +51,17 @@ elif [[ $restore_from != "empty" ]]; then
     exit 1
 fi
 
+if [[ ${rsr_version} == "prod" ]]; then
+    switch_cluster "production"
+    read_version
+    rsr_version=${VERSION}
+    switch_cluster "test"
+fi
+
 kubectl get pods > /dev/null
 
 log Running helm ...
-helm install . --dep-up --namespace rsr-demo --name ${release_name} --set restoreFrom="${restore_from}" ${HELM_EXTRA_OPTS}
+helm install . --dep-up --namespace rsr-demo --name ${release_name} --set restoreFrom="${restore_from}" --set rsrVersion="${rsr_version}" ${HELM_EXTRA_OPTS}
 
 log Waiting for new environment to be ready
 ${DIR}/helpers/wait-for-k8s-deployment-to-be-ready.sh ${release_name}
