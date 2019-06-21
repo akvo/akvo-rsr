1752bc7088e2e6c23eb0b1e5e88a4b508352e34c
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-06-21 17:43:31 +0530

    [#3652] Allow passing a categories in project_document endpoint
    Closes #3652

--- akvo/rest/serializers/project_document.py
+++ akvo/rest/serializers/project_document.py
@@ -7,9 +7,9 @@
 
 from akvo.rsr.models import ProjectDocument, ProjectDocumentCategory
 
-from .rsr_serializer import BaseRSRSerializer
-
 from rest_framework import serializers
+
+from .rsr_serializer import BaseRSRSerializer
 
 
 class ProjectDocumentRawSerializer(BaseRSRSerializer):
@@ -19,12 +19,41 @@ class ProjectDocumentRawSerializer(BaseRSRSerializer):
         model = ProjectDocument
 
 
+class CategoryListingField(serializers.RelatedField):
+
+    def to_representation(self, category):
+        return category.category
+
+    def to_internal_value(self, data):
+        return data
+
+
 class ProjectDocumentSerializer(ProjectDocumentRawSerializer):
 
     document_show_link = serializers.ReadOnlyField()
     language_label = serializers.ReadOnlyField(source='iati_language_unicode')
     title_language_label = serializers.ReadOnlyField(source='iati_title_language_unicode')
     format_label = serializers.ReadOnlyField(source='iati_format_unicode')
+    categories = CategoryListingField(
+        many=True, required=False, queryset=ProjectDocumentCategory.objects.all())
+
+    def create(self, validated_data):
+        categories = validated_data.pop('categories', [])
+        document = super(ProjectDocumentSerializer, self).create(validated_data)
+        for category in categories:
+            ProjectDocumentCategory.objects.create(category=category, document=document)
+        return document
+
+    def update(self, document, validated_data):
+        categories = validated_data.pop('categories', [])
+        instance = super(ProjectDocumentSerializer, self).update(document, validated_data)
+        instance.categories.all().delete()
+        categories = [
+            ProjectDocumentCategory(category=category, document=document)
+            for category in categories
+        ]
+        ProjectDocumentCategory.objects.bulk_create(categories)
+        return document
 
 
 class ProjectDocumentCategorySerializer(BaseRSRSerializer):
