90ea7f93a4a9bc5f5f34f5f5bf0b51250431c3ec
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-07-04 11:01:12 +0530

    [#3674] Add nested flags to CrsAddSerializer

--- akvo/rest/serializers/crs_add.py
+++ akvo/rest/serializers/crs_add.py
@@ -12,22 +12,23 @@ from .rsr_serializer import BaseRSRSerializer
 from rest_framework import serializers
 
 
-class CrsAddSerializer(BaseRSRSerializer):
-
-    repayment_type_label = serializers.ReadOnlyField(source='iati_repayment_type_unicode')
-    repayment_plan_label = serializers.ReadOnlyField(source='iati_repayment_plan_unicode')
-    currency_label = serializers.ReadOnlyField(source='iati_currency_unicode')
-    channel_code_label = serializers.ReadOnlyField(source='iati_channel_code_unicode')
-
-    class Meta:
-        model = CrsAdd
-        fields = '__all__'
-
-
 class CrsAddOtherFlagSerializer(BaseRSRSerializer):
 
     code_label = serializers.ReadOnlyField(source='iati_code_unicode')
 
     class Meta:
         model = CrsAddOtherFlag
+        fields = '__all__'
+
+
+class CrsAddSerializer(BaseRSRSerializer):
+
+    repayment_type_label = serializers.ReadOnlyField(source='iati_repayment_type_unicode')
+    repayment_plan_label = serializers.ReadOnlyField(source='iati_repayment_plan_unicode')
+    currency_label = serializers.ReadOnlyField(source='iati_currency_unicode')
+    channel_code_label = serializers.ReadOnlyField(source='iati_channel_code_unicode')
+    other_flags = CrsAddOtherFlagSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = CrsAdd
         fields = '__all__'
