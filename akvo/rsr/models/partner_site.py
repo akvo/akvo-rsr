bd2fb1db40d7685fad588fb66000f2270015ef0d
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-07-19 16:21:51 +0530

    [#3704] Show private projects on password protected partner sites
    Closes #3704

--- akvo/rsr/models/partner_site.py
+++ akvo/rsr/models/partner_site.py
@@ -205,9 +205,15 @@ class PartnerSite(TimestampsMixin, models.Model):
         # Add (or remove) projects via keywords
         return self.apply_keywords(fk_projects)
 
+    @property
+    def show_private_projects(self):
+        """Show private projects if a password has been set."""
+        return bool(self.password)
+
     def projects(self):
         """Publicly published projects of the page."""
-        return self.all_projects().published().public()
+        all_published = self.all_projects().published()
+        return all_published if self.show_private_projects else all_published.public()
 
     def apply_keywords(self, projects):
         """Apply keywords to the Page's projects."""
