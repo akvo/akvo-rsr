a0e8e0c4ccc4b0c2b9219561ead101c412c49ed5
Martin Christov <martin.christov@gmail.com>
2019-07-29 20:12:27 +0300

    [#3701] section11

--- akvo/rsr/spa/app/modules/editor/section11/comp/forecasts-stack.jsx
+++ akvo/rsr/spa/app/modules/editor/section11/comp/forecasts-stack.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button, Row, Col, Form } from 'antd'
 import currencies from 'currency-codes/data'
 import { Field } from 'react-final-form'
+import { useTranslation } from 'react-i18next'
 
 import getSymbolFromCurrency from '../../../../utils/get-symbol-from-currency'
 import FinalField from '../../../../utils/final-field'
@@ -11,11 +12,12 @@ import InputLabel from '../../../../utils/input-label'
 const { Item } = Form
 
 const ForecastsStack = ({ formPush, fssParent }) => {
+  const { t } = useTranslation()
   return (
     <ItemArray
       setName="forecasts"
       sectionIndex={11}
-      header="CRS++ other forecast $index"
+      header={`${t('CRS++ other forecast')} $index`}
       newItem={{ fss: fssParent ? fssParent.id : null }}
       headerMore={(index, value) => {
         return (
@@ -34,7 +36,7 @@ const ForecastsStack = ({ formPush, fssParent }) => {
         <div>
           <Row gutter={16}>
             <Col span={12}>
-              <Item label={<InputLabel tooltip="...">Currency</InputLabel>}>
+              <Item label={<InputLabel>{t('currency')}</InputLabel>}>
                 <FinalField
                   name={`${name}.currency`}
                   showSearch
@@ -45,7 +47,7 @@ const ForecastsStack = ({ formPush, fssParent }) => {
               </Item>
             </Col>
             <Col span={12}>
-              <Item label={<InputLabel tooltip="..." optional>Value</InputLabel>}>
+              <Item label={<InputLabel tooltip={t('The forecast value for each year.')} optional>{t('value')}</InputLabel>}>
                 <FinalField
                   name={`${name}.value`}
                   control="input-number"
@@ -55,7 +57,7 @@ const ForecastsStack = ({ formPush, fssParent }) => {
           </Row>
           <Row gutter={16}>
             <Col span={12}>
-              <Item label={<InputLabel optional>Year</InputLabel>}>
+              <Item label={<InputLabel optional tooltip={t('The calendar year that the forward spend covers.')}>{t('year')}</InputLabel>}>
                 <FinalField
                   name={`${name}.year`}
                   control="input-number"
@@ -63,7 +65,7 @@ const ForecastsStack = ({ formPush, fssParent }) => {
               </Item>
             </Col>
             <Col span={12}>
-              <Item label={<InputLabel optional>Value Date</InputLabel>}>
+              <Item label={<InputLabel optional tooltip={t('Enter the specific date (DD/MM/YYYY) for the forecast value.')}>{t('value date')}</InputLabel>}>
                 <FinalField
                   name={`${name}.date`}
                   control="datepicker"
@@ -73,7 +75,7 @@ const ForecastsStack = ({ formPush, fssParent }) => {
           </Row>
         </div>
       )}
-      addButton={({onClick}) => <Button onClick={onClick} className="bottom-btn" block icon="plus" type="dashed" disabled={!fssParent}>Add CRS++ other flag</Button>}
+      addButton={({onClick}) => <Button onClick={onClick} className="bottom-btn" block icon="plus" type="dashed" disabled={!fssParent}>{t('Add CRS++ other flag')}</Button>}
     />
   )
 }
--- akvo/rsr/spa/app/modules/editor/section11/section11.jsx
+++ akvo/rsr/spa/app/modules/editor/section11/section11.jsx
@@ -5,6 +5,7 @@ import { Field, Form as FinalForm } from 'react-final-form'
 import currencies from 'currency-codes/data'
 import arrayMutators from 'final-form-arrays'
 import { isEqual } from 'lodash'
+import { useTranslation } from 'react-i18next'
 
 import FinalField from '../../../utils/final-field'
 import AutoSave from '../../../utils/auto-save'
@@ -19,111 +20,115 @@ import './styles.scss'
 const { Item } = Form
 const { Option } = Select
 
-const LoanTerms = () => (
-  <section>
-    <div className="h-holder">
-      <h4>Loan terms</h4>
-    </div>
-    <Row gutter={16}>
-      <Col span={12}>
-        <FinalField
-          name="crs[0].repaymentType"
-          render={({ input }) => (
-            <Item label={<InputLabel optional tooltip="...">Repayment type</InputLabel>}>
-              <Select {...input}>
-                <Option value="">None</Option>
-                <Option value="1">1 - Equal Principal Payments (EPP)</Option>
-                <Option value="2">2 - Annuity</Option>
-                <Option value="3">3 - Lump sum</Option>
-                <Option value="5">5 - Other</Option>
-              </Select>
-            </Item>
-          )}
-        />
-      </Col>
-      <Col span={12}>
-        <FinalField
-          name="crs[0].repaymentPlan"
-          render={({ input }) => (
-            <Item label={<InputLabel optional tooltip="...">Repayment plan</InputLabel>}>
-              <Select {...input}>
-                <Option value="">None</Option>
-                <Option value="1">1 - Annual</Option>
-                <Option value="2">2 - Semi-annual</Option>
-                <Option value="4">4 - Quarterly</Option>
-                <Option value="12">12 - Monthly</Option>
-              </Select>
-            </Item>
-          )}
-        />
-      </Col>
-    </Row>
-    <Row gutter={16}>
-      <Col span={12}>
-        <Item label={<InputLabel optional tooltip="...">Commitment date</InputLabel>}>
-        <FinalField
-          control="datepicker"
-          name="crs[0].commitmentDate"
-        />
-        </Item>
-      </Col>
-    </Row>
-    <Row gutter={16}>
-      <Col span={12}>
-        <Item label={<InputLabel optional tooltip="...">First repayment date</InputLabel>}>
-        <FinalField
-          name="crs[0].repaymentFirstDate"
-          control="datepicker"
-        />
-        </Item>
-      </Col>
-      <Col span={12}>
-        <Item label={<InputLabel optional tooltip="...">Last repayment date</InputLabel>}>
-        <FinalField
-          name="crs[0].repaymentFinalDate"
-          control="datepicker"
-        />
-        </Item>
-      </Col>
-    </Row>
-    <Row gutter={16}>
-      <Col span={12}>
-        <Item label={<InputLabel optional tooltip="...">Rate 1</InputLabel>}>
-        <FinalField
-          name="crs[0].loanTermsRate1"
-          suffix={<span>%</span>}
-        />
-        </Item>
-      </Col>
-      <Col span={12}>
-        <Item label={<InputLabel optional tooltip="...">Rate 2</InputLabel>}>
-        <FinalField
-          name="crs[0].loanTermsRate2"
-          suffix={<span>%</span>}
-        />
-        </Item>
-      </Col>
-    </Row>
-  </section>
-)
+const LoanTerms = () => {
+  const { t } = useTranslation()
+  return (
+    <section>
+      <div className="h-holder">
+        <h4>{t('Loan terms')}</h4>
+      </div>
+      <Row gutter={16}>
+        <Col span={12}>
+          <FinalField
+            name="crs[0].repaymentType"
+            render={({ input }) => (
+              <Item label={<InputLabel optional tooltip={t('An IATI codelist tabulating CRS-specified values for the type of Repayment. See the <a href="http://iatistandard.org/202/codelists/LoanRepaymentType/" target="_blank">IATI codelist</a>.')}>{t('Repayment type')}</InputLabel>}>
+                <Select {...input}>
+                  <Option value="">None</Option>
+                  <Option value="1">1 - Equal Principal Payments (EPP)</Option>
+                  <Option value="2">2 - Annuity</Option>
+                  <Option value="3">3 - Lump sum</Option>
+                  <Option value="5">5 - Other</Option>
+                </Select>
+              </Item>
+            )}
+          />
+        </Col>
+        <Col span={12}>
+          <FinalField
+            name="crs[0].repaymentPlan"
+            render={({ input }) => (
+              <Item label={<InputLabel optional tooltip={t('An IATI codelist tabulating CRS-specified values for the number of repayments per annum. See the <a href="http://iatistandard.org/202/codelists/LoanRepaymentPeriod/" target="_blank">IATI codelist</a>.')}>{t('Repayment plan')}</InputLabel>}>
+                <Select {...input}>
+                  <Option value="">None</Option>
+                  <Option value="1">1 - Annual</Option>
+                  <Option value="2">2 - Semi-annual</Option>
+                  <Option value="4">4 - Quarterly</Option>
+                  <Option value="12">12 - Monthly</Option>
+                </Select>
+              </Item>
+            )}
+          />
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col span={12}>
+          <Item label={<InputLabel optional tooltip={t('The CRS++ reported commitment date.')}>{t('Commitment date')}</InputLabel>}>
+            <FinalField
+              control="datepicker"
+              name="crs[0].commitmentDate"
+            />
+          </Item>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col span={12}>
+          <Item label={<InputLabel optional tooltip={t('The CRS++ reported first repayment date.')}>{t('First repayment date')}</InputLabel>}>
+            <FinalField
+              name="crs[0].repaymentFirstDate"
+              control="datepicker"
+            />
+          </Item>
+        </Col>
+        <Col span={12}>
+          <Item label={<InputLabel optional tooltip={t('The CRS++ reported final repayment date.')}>{t('Final repayment date')}</InputLabel>}>
+            <FinalField
+              name="crs[0].repaymentFinalDate"
+              control="datepicker"
+            />
+          </Item>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col span={12}>
+          <Item label={<InputLabel optional tooltip={t('Interest Rate. If an ODA loan with variable interest rate, report the variable rate here and the reference fixed rate as rate 2.')}>{t('Rate')} 1</InputLabel>}>
+            <FinalField
+              name="crs[0].loanTermsRate1"
+              suffix={<span>%</span>}
+            />
+          </Item>
+        </Col>
+        <Col span={12}>
+          <Item label={<InputLabel optional tooltip={t('Second Interest Rate. If an ODA loan with variable interest rate, report the variable rate as rate 1 and the reference fixed rate here.')}>{t('Rate')} 2</InputLabel>}>
+            <FinalField
+              name="crs[0].loanTermsRate2"
+              suffix={<span>%</span>}
+            />
+          </Item>
+        </Col>
+      </Row>
+    </section>
+  )
+}
 
 const LoanStatus = ({ currency }) => {
+  const { t } = useTranslation()
   const currencySymbol = getSymbolFromCurrency(currency)
   return (
     <section>
       <div className="h-holder">
-        <h4>Loan Status</h4>
+        <h4>{t('Loan Status')}</h4>
       </div>
       <Row gutter={16}>
         <Col span={12}>
-          <Item label={<InputLabel optional tooltip="...">Year</InputLabel>}>
+          <Item label={<InputLabel optional tooltip={t('CRS reporting year (CRS++ Column 1).')}>{t('Year')}</InputLabel>}>
           <FinalField
             name="crs[0].loanStatusYear"
           />
           </Item>
         </Col>
         <Col span={12}>
-          <Item label={<InputLabel optional tooltip="...">Currency</InputLabel>}>
+          <Item label={<InputLabel optional>{t('Currency')}</InputLabel>}>
           <FinalField
             name="crs[0].loanStatusCurrency"
             control="select"
@@ -136,7 +141,7 @@ const LoanStatus = ({ currency }) => {
       </Row>
       <Row>
         <Col span={12}>
-          <Item label={<InputLabel optional tooltip="...">Value Date</InputLabel>}>
+          <Item label={<InputLabel optional tooltip={t('Enter the specific date (DD/MM/YYYY) for the loan status values.')}>{t('value date')}</InputLabel>}>
           <FinalField
             name="crs[0].loanStatusValueDate"
             control="datepicker"
@@ -146,7 +151,7 @@ const LoanStatus = ({ currency }) => {
       </Row>
       <Row gutter={16}>
         <Col span={12}>
-          <Item label={<InputLabel optional tooltip="...">Interest received</InputLabel>}>
+          <Item label={<InputLabel optional tooltip={t('Interest received during the reporting year.')}>{t('Interest received')}</InputLabel>}>
           <FinalField
             name="crs[0].interestReceived"
             control="input-number"
@@ -155,7 +160,7 @@ const LoanStatus = ({ currency }) => {
           </Item>
         </Col>
         <Col span={12}>
-          <Item label={<InputLabel optional tooltip="...">Principal outstanding</InputLabel>}>
+          <Item label={<InputLabel optional tooltip={t('The amount of principal owed on the loan at the end of the reporting year.')}>{t('Principal outstanding')}</InputLabel>}>
           <FinalField
             name="crs[0].principalOutstanding"
             control="input-number"
@@ -166,7 +171,7 @@ const LoanStatus = ({ currency }) => {
       </Row>
       <Row gutter={16}>
         <Col span={12}>
-          <Item label={<InputLabel optional tooltip="...">Principal arrears</InputLabel>}>
+          <Item label={<InputLabel optional tooltip={t('Arrears of principal at the end of the year. Included in principal outstanding.')}>{t('Principal arrears')}</InputLabel>}>
           <FinalField
             name="crs[0].principalArrears"
             control="input-number"
@@ -175,7 +180,7 @@ const LoanStatus = ({ currency }) => {
           </Item>
         </Col>
         <Col span={12}>
-          <Item label={<InputLabel optional tooltip="...">Interest arrears</InputLabel>}>
+          <Item label={<InputLabel optional tooltip={t('Arrears of interest at the end of the year.')}>{t('Interest arrears')}</InputLabel>}>
           <FinalField
             name="crs[0].interestArrears"
             control="input-number"
@@ -190,13 +195,8 @@ const LoanStatus = ({ currency }) => {
 
 
 const Reporting = ({ fields, projectId }) => {
+  const { t } = useTranslation()
   const initialValues = { ...fields }
-  // if (initialValues.crs.length === 0) {
-  //   initialValues.crs.push({ project: projectId })
-  // }
-  // if (initialValues.fss.length === 0) {
-  //   initialValues.fss.push({ project: projectId })
-  // }
   return (
     <div className="reporting view">
       <h3>CRS++</h3>
@@ -226,7 +226,7 @@ const Reporting = ({ fields, projectId }) => {
                   name="currency"
                   render={({ input }) => <LoanStatus currency={input.value} />}
                 />
-                <Item className="channel-code-item" label={<InputLabel optional tooltip="...">Channel code</InputLabel>}>
+                <Item className="channel-code-item" label={<InputLabel optional tooltip={t('The CRS channel code for this activity. The codelist contains both organisation types and names of organisations. For non-CRS purposes these should be reported using participating organisations. See the <a href="http://iatistandard.org/202/codelists/CRSChannelCode/" target="_blank">IATI codelist</a>.')}>{t('channel code')}</InputLabel>}>
                   <FinalField
                     name="crs[0].channelCode"
                     showSearch
@@ -238,31 +238,31 @@ const Reporting = ({ fields, projectId }) => {
                 <FlagsStack formPush={push} crsParent={fields.crs[0]} />
                 <Divider />
                 <h3>FSS</h3>
-                <Item label={<InputLabel tooltip="...">Extraction date</InputLabel>}>
+                <Item label={<InputLabel tooltip={t('The exact date when the information was collected or extracted from donors\' aid management systems.')}>{t('extraction date')}</InputLabel>}>
                   <FinalField
                     name="fss[0].extractionDate"
                     style={{ display: 'block' }}
                     control="datepicker"
                   />
                 </Item>
-                <Item label={<InputLabel optional tooltip="...">Phaseout year</InputLabel>}>
+                <Item label={<InputLabel optional tooltip={t('If there are plans to phase out operations from the partner country, this shows the projected year of last disbursements.')}>{t('phaseout year')}</InputLabel>}>
                   <FinalField
                     name="fss[0].phaseoutYear"
                     control="input-number"
                     className="phaseout-year-input"
                   />
                 </Item>
-                <Item label={<InputLabel optional tooltip="...">Priority</InputLabel>}>
+                <Item label={<InputLabel optional tooltip={t('True if the partner country is a priority partner country.')}>{t('priority')}</InputLabel>}>
                   <FinalField
                     name="fss[0].priority"
                     control="select"
-                    options={[{ value: true, label: 'Yes' }, { value: false, label: 'No' }]}
+                    options={[{ value: true, label: t('Yes') }, { value: false, label: t('No') }]}
                     withEmptyOption
                   />
                 </Item>
-                <h3>FSS forecasts</h3>
+                <h3>{t('FSS forecasts')}</h3>
                 <ForecastsStack formPush={push} fssParent={fields.fss[0]} />
-                <h3>Legacy data</h3>
+                <h3>{t('Legacy data')}</h3>
                 <LegaciesStack formPush={push} />
               </div>
             )
