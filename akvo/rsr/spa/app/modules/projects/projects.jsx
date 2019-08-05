928c87cb88d01db241a81b24e49d80f6fd73678d
Martin Christov <martin.christov@gmail.com>
2019-08-05 12:13:55 +0300

    [#3636] projects listing search -> coverted to classic (non-hooks) component to resolve erroneous state update

--- akvo/rsr/spa/app/modules/projects/projects.jsx
+++ akvo/rsr/spa/app/modules/projects/projects.jsx
@@ -1,7 +1,7 @@
 /* global window */
-import React, { useReducer, useEffect } from 'react'
+import React from 'react'
 import { Button, Divider, Table, Input, Icon } from 'antd'
-import { useTranslation } from 'react-i18next'
+import { withTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
 import api from '../../utils/api'
@@ -10,106 +10,109 @@ import './styles.scss'
 let tmid
 const pageSize = 15
 
-const Projects = () => {
-  const { t } = useTranslation()
-  const columns = [
-    {
-      title: 'Projects',
-      dataIndex: 'title',
-      key: 'title',
-      render: (text, record) => (
-        <Link to={`/projects/${record.id}`}>
-          {text !== '' ? text : 'Untitled project'}
-          {record.subtitle !== '' && <small><br />{record.subtitle}</small>}
-        </Link>
-      )
-    },
-    {
-      title: 'Location',
-      dataIndex: 'primaryLocation.countryLabel',
-      key: 'location',
-      width: 170,
-      render: (text, record) => {
-        const listOfUniqueCountries = record.locations.map(it => it.country).reduce((acc, val) => { if (acc.indexOf(val) === -1) return [...acc, val]; return acc }, []).join(', ')
-        return (<span>{listOfUniqueCountries}</span>)
-      }
-    },
-    {
-      title: 'Date End (Planned)',
-      dataIndex: 'dateEndPlanned',
-      key: 'dateEndPlanned',
-      width: 160,
-      render: (text) => {
-        return (<span>{text ? moment(text, 'YYYY-MM-DD').format('DD MMM YYYY') : '-'}</span>)
-      }
-    }
-  ]
-  const [state, setState] = useReducer(
-    (state, newState) => { console.log({ ...state, ...newState }); return ({ ...state, ...newState })}, // eslint-disable-line
-    { data: {}, loading: false, pagination: { pageSize }, searchStr: '' }
-  )
-  const fetch = (params = {}) => {
-    setState({ loading: true })
-    if(state.searchStr !== ''){
+class Projects extends React.Component{
+  state = {
+    results: [], loading: false, pagination: { pageSize }, searchStr: ''
+  }
+  componentDidMount(){
+    this.fetch()
+  }
+  fetch = (params = {}) => {
+    this.setState({ loading: true })
+    if (this.state.searchStr !== '') {
       params = {
         ...params,
-        q_filter1: `{ 'title__icontains': '${state.searchStr}' }`,
-        q_filter2: `{ 'subtitle__icontains': '${state.searchStr}' }`
+        q_filter1: { title__icontains: this.state.searchStr },
+        q_filter2: { subtitle__icontains: this.state.searchStr }
       }
     }
-    api.get('/editable_project/', {...params, limit: pageSize})
-      .then(({data}) => {
-        const pagination = { ...state.pagination }
+    api.get('/editable_project/', { ...params, limit: pageSize })
+      .then(({ data }) => {
+        const pagination = { ...this.state.pagination }
         pagination.total = data.count
-        setState({
+        this.setState({
           results: data.results,
           pagination,
           loading: false
         })
       })
   }
-  useEffect(fetch, [])
-  const handleTableChange = (pagination) => {
-    const pager = { ...state.pagination }
+  handleTableChange = (pagination) => {
+    const pager = { ...this.state.pagination }
     pager.current = pagination.current
-    setState({
+    this.setState({
       pagination: pager
     })
-    fetch({ page: pagination.current })
+    this.fetch({ page: pagination.current })
     window.scroll({ top: 150, left: 0, behavior: 'smooth' })
   }
-  const handleSearch = ({ target: {value}}) => {
+  handleSearch = ({ target: { value } }) => {
     clearTimeout(tmid)
-    setState({ searchStr: value })
+    this.setState({ searchStr: value })
     tmid = setTimeout(() => {
-      setState({
+      this.setState({
         loading: true,
-        pagination: {...state.pagination, current: 1}
+        pagination: { ...this.state.pagination, current: 1 }
       })
-      fetch({ page: 1 })
+      this.fetch({ page: 1 })
     }, 200)
   }
-  const clearSearch = () => {
-    setState({ searchStr: '', pagination: {...state.pagination, current: 1 }})
-    fetch({ page: 1 })
+  clearSearch = () => {
+    this.setState({ searchStr: '', pagination: { ...this.state.pagination, current: 1 } })
+    setTimeout(() => this.fetch({ page: 1 }), 100)
   }
-  return (
-    <div>
-      <div style={{ display: 'flex'}}>
-        <h1>{t('My projects')}</h1>
-        <Input value={state.searchStr} suffix={state.searchStr === '' ? <Icon type="search" /> : <Icon onClick={clearSearch} type="close" />} placeholder={t('Find a project...')} onChange={handleSearch} />
-        <Link className="add-project-btn" to="/projects/new"><Button type="primary" icon="plus">{t('Create new project')}</Button></Link>
+  render(){
+    const { t } = this.props
+    const columns = [
+      {
+        title: t('Projects'),
+        dataIndex: 'title',
+        key: 'title',
+        render: (text, record) => (
+          <Link to={`/projects/${record.id}`}>
+            {text !== '' ? text : t('Untitled project')}
+            {record.subtitle !== '' && <small><br />{record.subtitle}</small>}
+          </Link>
+        )
+      },
+      {
+        title: t('Location'),
+        dataIndex: 'primaryLocation.countryLabel',
+        key: 'location',
+        width: 170,
+        render: (text, record) => {
+          const listOfUniqueCountries = record.locations.map(it => it.country).reduce((acc, val) => { if (acc.indexOf(val) === -1) return [...acc, val]; return acc }, []).join(', ')
+          return (<span>{listOfUniqueCountries}</span>)
+        }
+      },
+      {
+        title: t('Date End (Planned)'),
+        dataIndex: 'dateEndPlanned',
+        key: 'dateEndPlanned',
+        width: 160,
+        render: (text) => {
+          return (<span>{text ? moment(text, 'YYYY-MM-DD').format('DD MMM YYYY') : '-'}</span>)
+        }
+      }
+    ]
+    return (
+      <div>
+        <div style={{ display: 'flex' }}>
+          <h1>{t('My projects')}</h1>
+          <Input value={this.state.searchStr} suffix={this.state.searchStr === '' ? <Icon type="search" /> : <Icon onClick={this.clearSearch} type="close" />} placeholder={t('Find a project...')} onChange={this.handleSearch} />
+          <Link className="add-project-btn" to="/projects/new"><Button type="primary" icon="plus">{t('Create new project')}</Button></Link>
+        </div>
+        <Divider />
+        <Table
+          dataSource={this.state.results}
+          columns={columns}
+          loading={this.state.loading}
+          pagination={this.state.pagination}
+          onChange={this.handleTableChange}
+        />
       </div>
-      <Divider />
-      <Table
-        dataSource={state.results}
-        columns={columns}
-        loading={state.loading}
-        pagination={state.pagination}
-        onChange={handleTableChange}
-      />
-    </div>
-  )
+    )
+  }
 }
 
-export default Projects
+export default withTranslation()(Projects)
