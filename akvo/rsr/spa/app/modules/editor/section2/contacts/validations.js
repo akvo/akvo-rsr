ad1210ba068b1a97e0373226a496589134fde3aa
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-10-08 18:20:32 +0530

    [#3785] Fix section 2 validations

--- akvo/rsr/spa/app/modules/editor/section2/contacts/validations.js
+++ akvo/rsr/spa/app/modules/editor/section2/contacts/validations.js
@@ -2,7 +2,7 @@ import * as yup from 'yup'
 import { transformUndefined } from '../../../../utils/validation-utils'
 
 const RSR = yup.object().shape({
-  name: yup.string().default(''),
+  personName: yup.string().default(''),
   type: yup.mixed(),
   organisation: yup.string().default(''),
   department: yup.string().default(''),
@@ -18,10 +18,15 @@ const DGIS = RSR.clone().shape({
   mailingAddress: RSR.fields.mailingAddress.required()
 })
 
+const DFID = RSR.clone().shape({
+  personName: yup.string().required(),
+  email: RSR.fields.email.required()
+})
+
 const defs = {
   1: yup.array().of(RSR),
-  2: yup.array().of(RSR),
-  3: yup.array().of(DGIS).min(1)
+  3: yup.array().of(DGIS).min(1),
+  6: yup.array().of(DFID)
 }
 
 export default defs
