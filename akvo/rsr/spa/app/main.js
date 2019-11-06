c2923e47946492650b12f326bc39a7b5fbfc9437
Martin Christov <martin.christov@gmail.com>
2019-11-06 15:17:28 +0200

    bugfix: IE polyfill for scrollTo w options

--- akvo/rsr/spa/app/main.js
+++ akvo/rsr/spa/app/main.js
@@ -6,10 +6,12 @@ import { AppContainer } from 'react-hot-loader'
 import JavascriptTimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en'
 import 'babel-polyfill'
+import smoothscroll from 'smoothscroll-polyfill'
 import './i18n'
 import Root from './root'
 import configureStore from './store/config'
 
+smoothscroll.polyfill()
 JavascriptTimeAgo.locale(en)
 
 const store = configureStore()
