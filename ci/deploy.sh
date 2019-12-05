8d53a7b5f68067264ae15f0772ddbe63fb276af5
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-12-05 15:53:22 +0530

    Revert "Merge pull request #3890 from akvo/issue/3876-py-report-container-k8s"
    This reverts commit be742e5041d5bb556285eb05934fb83fccdaf936, reversing
    changes made to 91e47c942d8bb75090715818e2fbb4be2639769c.

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -8,9 +8,9 @@ function log {
 
 export PROJECT_NAME=akvo-lumen
 
-# if [[ "${TRAVIS_BRANCH}" != "develop" ]] && [[ "${TRAVIS_BRANCH}" != "master" ]]; then
-#     exit 0
-# fi
+if [[ "${TRAVIS_BRANCH}" != "develop" ]] && [[ "${TRAVIS_BRANCH}" != "master" ]]; then
+    exit 0
+fi
 
 if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
     exit 0
@@ -27,7 +27,7 @@ which gcloud kubectl
 
 log Authentication with gcloud and kubectl
 openssl aes-256-cbc -K $encrypted_12c8071d2874_key -iv $encrypted_12c8071d2874_iv \
-  -in ci/gcloud-service-account.json.enc -out ci/gcloud-service-account.json -d
+	-in ci/gcloud-service-account.json.enc -out ci/gcloud-service-account.json -d
 gcloud auth activate-service-account --key-file ci/gcloud-service-account.json
 gcloud config set project akvo-lumen
 gcloud config set container/cluster europe-west1-d
@@ -48,12 +48,12 @@ docker push eu.gcr.io/${PROJECT_NAME}/rsr-backend
 docker push eu.gcr.io/${PROJECT_NAME}/rsr-nginx
 docker push eu.gcr.io/${PROJECT_NAME}/rsr-statsd-to-prometheus
 
-# sed -e "s/\${TRAVIS_COMMIT}/$TRAVIS_COMMIT/" ci/k8s/deployment.yml > deployment.yml.tmp
+sed -e "s/\${TRAVIS_COMMIT}/$TRAVIS_COMMIT/" ci/k8s/deployment.yml > deployment.yml.tmp
 
-# kubectl apply -f ci/k8s/media-disk.yml
-# kubectl apply -f ci/k8s/service.yml
-# kubectl apply -f deployment.yml.tmp
-# kubectl apply -f ci/k8s/grafana/main.yml
+kubectl apply -f ci/k8s/media-disk.yml
+kubectl apply -f ci/k8s/service.yml
+kubectl apply -f deployment.yml.tmp
+kubectl apply -f ci/k8s/grafana/main.yml
 
-# log Waiting for k8s to finish
-# ./ci/k8s/helpers/wait-for-k8s-deployment-to-be-ready.sh
+log Waiting for k8s to finish
+./ci/k8s/helpers/wait-for-k8s-deployment-to-be-ready.sh
