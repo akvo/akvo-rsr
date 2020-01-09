b8d9d04941768605fdb1d221798d3404c92cd4a8
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-01-09 15:18:20 +0530

    [#2208] Modify load-test scripts to work on Linux

--- scripts/load-test/compare-all-pages.sh
+++ scripts/load-test/compare-all-pages.sh
@@ -1,8 +1,8 @@
 ## Compare the output of all urls found in the cloudVps log file
 ## Saves the ones that are different in a file
 for i in `cat prod.logs.txt | grep " 200 " | grep GET | grep -v "Monitoring" | cut -f 7 -d\  `; do
-	sh compare-one-page.sh $i
-	if [ ! $? -eq 0 ]; then
-		echo $i >> different.txt
-	fi
+  sh compare-one-page.sh $i
+  if [ ! $? -eq 0 ]; then
+    echo $i >> different.txt
+  fi
 done
