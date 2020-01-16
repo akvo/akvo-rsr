a9d6241a3f4fdab50b471e4b0d5d768d58902f45
Martin Christov <martin.christov@gmail.com>
2020-01-16 15:45:39 +0200

    [#3915] init new react app

---
+++ akvo/rsr/dir/app/main.js
@@ -0,0 +1,12 @@
+/* global document */
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Root = () => (
+  <div>test</div>
+)
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root'),
+)
