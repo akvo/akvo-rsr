2f4811fc92117f20c9fd827192767d7a2eb674ab
zuhdil <zuhdil@gmail.com>
2020-01-23 12:55:19 +0700

    [#3876] Change mapbox style and center+zoom behavior.

--- akvo/rsr/staticmap.py
+++ akvo/rsr/staticmap.py
@@ -17,34 +17,40 @@ Size = collections.namedtuple('Size', ['width', 'height'])
 
 class MapboxAdapter(object):
 
-    URL = "https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/static/{}/{}/{}?access_token={}&logo=false"
+    URL = "https://api.mapbox.com/styles/v1/mapbox/light-v10/static/{}/{}/{}?access_token={}&logo=false"
 
     def __init__(self, key):
         self.key = key
 
     def get_url(self, locations, size=Size(600, 300), zoom=None):
-        if len(locations) > 1:
-            return self._handle_multiple_locations(locations, size)
-
-        return self._handle_single_location(locations[0], size, zoom)
-
-    def _handle_multiple_locations(self, locations, size):
         location_groups = _group_locations(locations)
-        location_strings = [
+        overlay_string = self._make_multiple_markers(location_groups)\
+            if len(locations) > 1\
+            else self._make_single_marker(locations[0])
+        center_zoom = self._make_center_and_zoom(location_groups, zoom or 8)
+        size_string = "{}x{}".format(size.width, size.height)
+
+        return self.URL.format(overlay_string, center_zoom, size_string, self.key)
+
+    def _make_multiple_markers(self, location_groups):
+        markers = [
             'pin-s-{}+1890ff({},{})'.format(v, c.longitude, c.latitude)
             for c, v
             in sorted(location_groups.items(), key=lambda kv:(kv[1], kv[0]))
         ]
-        size_string = "{}x{}".format(size.width, size.height)
 
-        return self.URL.format(','.join(location_strings), 'auto', size_string, self.key)
+        return ','.join(markers)
 
-    def _handle_single_location(self, location, size, zoom):
-        location_string = 'pin-s+1890ff({},{})'.format(location.longitude, location.latitude)
-        zoom_center = '{},{},{}'.format(location.longitude, location.latitude, zoom or 8)
-        size_string = "{}x{}".format(size.width, size.height)
+    def _make_single_marker(self, location):
+        return 'pin-s+1890ff({},{})'.format(location.longitude, location.latitude)
 
-        return self.URL.format(location_string, zoom_center, size_string, self.key)
+    def _make_center_and_zoom(self, location_groups, zoom):
+        if len(location_groups) > 1:
+            return 'auto'
+
+        location = list(location_groups.keys())[0]
+
+        return '{},{},{}'.format(location.longitude, location.latitude, zoom)
 
 
 class MapquestAdapter(object):
