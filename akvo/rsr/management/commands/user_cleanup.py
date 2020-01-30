87f6aeade20916e7d6eae8bbe90006830e4bdf6d
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-01-30 17:34:45 +0530

    [#3940] Re-arrange imports in the user_cleanup management command

--- akvo/rsr/management/commands/user_cleanup.py
+++ akvo/rsr/management/commands/user_cleanup.py
@@ -4,10 +4,13 @@
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
-from django.core.management.base import BaseCommand
-from django.utils import timezone
 from datetime import timedelta
 from itertools import chain
+
+from django.core.management.base import BaseCommand
+from django.db.models import F
+from django.utils import timezone
+
 from ...models import User
 
 
