468af927df69992b8315a290c2ecce85c1dc04a0
Martin Christov <martin.christov@gmail.com>
2020-02-12 13:52:27 +0200

    [#3810] sending email invitation

--- akvo/rsr/spa/app/modules/editor/section3/access.jsx
+++ akvo/rsr/spa/app/modules/editor/section3/access.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { Radio, Icon, Button, Select, Modal, Input, Collapse, Dropdown, Menu } from 'antd'
+import React, { useState, useEffect, useRef, useReducer } from 'react'
+import { Radio, Icon, Button, Modal, Input, Collapse, Dropdown, Menu, Form, Alert } from 'antd'
+import { Form as FinalForm } from 'react-final-form'
 import { useFetch } from '../../../utils/hooks'
 import './access.scss'
 import api from '../../../utils/api'
 
 const { Panel } = Collapse
+const { Item } = Form
 const roleTypes = ['Admins', 'M&E Managers', 'Project Editors', 'Users', 'Enumerators']
 const roleDesc = {
   Admins: 'Can view and edit projects, add and remove members and change all settings',
@@ -22,7 +24,7 @@ const roleLabelDict = {
 }
 
 const Access = ({ projectId, partners }) => {
-  const [roleData, loading] = useFetch(`project/${projectId}/project_roles/`)
+  const [roleData, loading] = useFetch(`project/${projectId}/project-roles/`)
   const [useProjectRoles, setUseProjectRoles] = useState(false)
   const [modalVisible, setModalVisible] = useState(false)
   const [roles, setRoles] = useState([])
@@ -35,14 +37,14 @@ const Access = ({ projectId, partners }) => {
   const handleAddRole = (userRole) => {
     const _roles = [...roles, userRole]
     setRoles(_roles)
-    api.patch(`project/${projectId}/project_roles/`, {
+    api.patch(`project/${projectId}/project-roles/`, {
       roles: _roles.map(({ email, role}) => ({ email, role })),
     })
   }
   const handleProjectRolesChange = ({ target: {value}}) => {
     setUseProjectRoles(value)
     setRoles([])
-    api.patch(`project/${projectId}/project_roles/`, {
+    api.patch(`project/${projectId}/project-roles/`, {
       useProjectRoles: value
     })
   }
@@ -50,14 +52,14 @@ const Access = ({ projectId, partners }) => {
     const ind = roles.findIndex(it => it.email === user.email)
     const updatedRoles = [...roles.slice(0, ind), {...user, role}, ...roles.slice(ind + 1)]
     setRoles(updatedRoles)
-    api.patch(`project/${projectId}/project_roles/`, {
+    api.patch(`project/${projectId}/project-roles/`, {
       roles: updatedRoles.map(({ email, role }) => ({ email, role })) // eslint-disable-line
     })
   }
   const removeUser = (user) => {
     const updatedRoles = roles.filter(it => it.email !== user.email)
     setRoles(updatedRoles)
-    api.patch(`project/${projectId}/project_roles/`, {
+    api.patch(`project/${projectId}/project-roles/`, {
       roles: updatedRoles.map(({ email, role }) => ({ email, role })),
     })
   }
@@ -97,12 +99,31 @@ const Access = ({ projectId, partners }) => {
         )}
       </ul>,
       <Button className="bottom-btn" icon="plus" type="dashed" block onClick={() => setModalVisible(true)}>Add user</Button>]}
-      {!loading && <InviteUserModal {...{roles}} onAddRole={handleAddRole} visible={modalVisible} onCancel={() => setModalVisible(false)} orgs={partners} />}
+      {!loading && <InviteUserModal {...{roles, projectId}} onAddRole={handleAddRole} visible={modalVisible} onCancel={() => setModalVisible(false)} orgs={partners} />}
     </div>
   )
 }
 
-const InviteUserModal = ({ visible, onCancel, orgs, onAddRole, roles }) => {
+let tmid
+
+const Search = ({ isEmail, onChange, getReset }) => {
+  const [value, setValue] = useState('')
+  const handleChange = e => {
+    setValue(e.target.value);
+    clearTimeout(tmid);
+    ((val) => {
+      tmid = setTimeout(() => onChange(val), 200)
+    })(e.target.value)
+  }
+  if(getReset){
+    getReset = () => () => setValue('')
+  }
+  return (
+    <Input value={value} onChange={handleChange} allowClear suffix={!isEmail && <Icon type="search" />} placeholder="Type name or email" />
+  )
+}
+
+const InviteUserModal = ({ visible, onCancel, orgs, onAddRole, roles, projectId }) => {
   const [data, setData] = useState([])
   const [loading, setLoading] = useState(false)
   const prevData = useRef([])
@@ -117,18 +138,65 @@ const InviteUserModal = ({ visible, onCancel, orgs, onAddRole, roles }) => {
       })
     }
   }, [orgs])
-  const [src, setSrc] = useState('')
+  // const [src, setSrc] = useState('')
+  const resetRef = useRef()
+  const [state, setState] = useReducer(
+    (state, newState) => ({ ...state, ...newState }), // eslint-disable-line
+    { src: '', name: '', role: 'M&E Managers', sending: false, sendingStatus: '' }
+  )
   const rolesDict = {}; roles.forEach(role => { rolesDict[role.email] = role })
-  const filterName = it => { if(src === '') return true; return it.name.toLowerCase().indexOf(src) !== -1 }
+  const filterName = it => { if(state.src === '') return true; return it.name.toLowerCase().indexOf(state.src) !== -1 }
+  const isEmail = state.src.indexOf('@') !== -1
+  const sendInvite = () => {
+    setState({ sendingStatus: 'sending'})
+    const {src: email, name, role } = state
+    api.post(`/project/${projectId}/invite-user/`, {
+      email, name, role
+    }).then((e) => {
+      onAddRole({ email, name, role })
+      setState({ sendingStatus: 'sent' })
+      setTimeout(() => {
+        // setState({ name: '', src: '', sendingStatus: '' })
+      }, 1000)
+    }).error(() => {
+      setState({ sendingStatus: 'error' })
+    })
+  }
+  const reset = () => {
+    setState({
+      sendingStatus: '', src: '', name: ''
+    })
+    if(resetRef.current) resetRef.current()
+  }
   return (
     <Modal
       title="Invite user"
       {...{ visible, onCancel }}
       className="invite-user-modal"
-      footer={<Button type="primary" onClick={onCancel}>Done</Button>}
+      footer={isEmail ? [
+        state.sendingStatus !== 'sent' &&
+        <div>
+          <span>Add as: </span>
+          <Dropdown
+            align={{ points: ['tr', 'br'] }}
+            trigger={['click']}
+            overlay={(
+              <Menu className="roles-dropdown">
+                {roleTypes.map(role => <Menu.Item onClick={() => setState({ role })} key={role}>{roleLabelDict[role]}<br /><small>{roleDesc[role]}</small></Menu.Item>)}
+              </Menu>
+            )}
+          >
+            <a className="ant-dropdown-link">{roleLabelDict[state.role]} <Icon type="down" /></a>{/* eslint-disable-line */}
+          </Dropdown>
+        </div>,
+        state.sendingStatus !== 'sent' ? <Button type="primary" onClick={sendInvite} loading={state.sendingStatus === 'sending'}>Send Invite</Button> : [
+          <Button onClick={reset}>Invite Another</Button>,
+          <Button onClick={onCancel}>Close</Button>
+        ]
+      ] : <Button type="primary" onClick={onCancel}>Done</Button>}
     >
-      <Input value={src} onChange={e => setSrc(e.target.value)} allowClear suffix={<Icon type="search" />} placeholder="Type name or email" />
-      {!loading &&
+      <Search {...{isEmail}} onChange={(src) => setState({ src })} getRef={(ref) => { resetRef.current = ref }} />
+      {(!loading && !isEmail) &&
       <Collapse defaultActiveKey={data.map((a, i) => i)}>
         {data.map((org, index) => {
           const orgName = orgs.find(it => it.organisation === org.id).organisationName
@@ -154,6 +222,14 @@ const InviteUserModal = ({ visible, onCancel, orgs, onAddRole, roles }) => {
         )}
       </Collapse>
       }
+      {isEmail && (
+        <div className="invite-section">
+          <p>{`We'll send a message to ${state.src} and create a new user`}</p>
+          <Input name="name" placeholder="Full name" value={state.name} onChange={e => setState({ name: e.target.value })} />
+          {state.sendingStatus === 'sent' && <Alert message="Invitation sent!" type="success" />}
+          {state.sendingStatus === 'error' && <Alert message="Something went wrong" type="error" />}
+        </div>
+      )}
     </Modal>
   )
 }
