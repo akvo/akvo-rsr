2831445c5e620f40666b213c9fc4a6d9974daabf
Martin Christov <martin.christov@gmail.com>
2020-02-13 15:10:57 +0200

    [#3810] label update

--- akvo/rsr/spa/app/modules/editor/section3/access.jsx
+++ akvo/rsr/spa/app/modules/editor/section3/access.jsx
@@ -65,7 +65,7 @@ const Access = ({ projectId, partners }) => {
   }
   return (
     <div className="access-section">
-      <h3>Access</h3>
+      <h3>User Access</h3>
       <Radio.Group value={useProjectRoles} onChange={handleProjectRolesChange} disabled={loading}>
         <Radio.Button value={false}>
           <Icon type="unlock" /> <b>Organization</b>
