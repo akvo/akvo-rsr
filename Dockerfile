a3da444f2191ca5658c103256e828abbd6e9a2cb
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-02-26 19:21:10 +0100

    [#4003] Remove duplication and per branch cache, falling back to develop branch

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM akvo/rsr-backend:prod-no-code
+FROM akvo/rsr-backend-prod-no-code:local
 
 COPY akvo/ /var/akvo/rsr/code/akvo
 COPY ._66_deploy_info.conf /var/akvo/rsr/code/akvo/settings/66_deploy_info.conf
--- ci/build-semaphoreci.sh
+++ ci/build-semaphoreci.sh
@@ -8,20 +8,17 @@ function log {
 function docker_build {
   echo "$CI_BRANCH" > .branch_name
   branch_md5=$(checksum .branch_name)
-  echo "Branch name ${CI_BRANCH}"
-  echo "Branch md5 ${branch_md5}"
-  echo "Branch md5 $(echo "${CI_BRANCH}" | checksum)"
-  echo "Branch md5 `echo \"${CI_BRANCH}\" | checksum`"
-  echo "Branch md5 $(checksum Dockerfile)"
   image_branch="${1}:${branch_md5}"
   image_develop="${1}:develop"
+  image_local="${1}:local"
   shift
   other_params=$*
   log Pulling "$image_branch"
   docker pull --quiet "$image_branch" || docker pull --quiet "$image_develop" || true
 
   log Building "$image_branch"
-  docker build --cache-from="$image_branch" --cache-from "$image_develop" --rm=false -t "$image_branch" $other_params
+  log docker build --cache-from="$image_branch" --cache-from "$image_develop" --rm=false -t "$image_branch" -t "$image_local" $other_params
+  docker build --cache-from="$image_branch" --cache-from "$image_develop" --rm=false -t "$image_branch" -t "$image_local" $other_params
 
   log Pushing "$image_branch" container
   docker push "$image_branch"
@@ -52,55 +49,17 @@ echo "DEPLOY_COMMIT_ID = '`git rev-parse --short HEAD`'" >> ._66_deploy_info.con
 echo "DEPLOY_BRANCH = '$CI_BRANCH'" >> ._66_deploy_info.conf
 echo "DEPLOY_TAG = '$CI_TAG'" >> ._66_deploy_info.conf
 
-log Pulling akvo/rsr-backend:prod-no-code
-docker pull --quiet akvo/rsr-backend:prod-no-code || true
+docker_build akvo/rsr-backend-prod-no-code -f Dockerfile-prod-no-code .
 
-log Creating Production Backend image_develop without code
-docker build --rm=false --cache-from akvo/rsr-backend:prod-no-code -t akvo/rsr-backend:prod-no-code -f Dockerfile-prod-no-code . 
-
-log Push akvo/rsr-backend:prod-no-code
-docker push akvo/rsr-backend:prod-no-code
-
-log Creating Production Backend image_develop with code
+log Creating Production Backend image with code
 docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/rsr-backend:${CI_COMMIT} -t rsr-backend:prod . 
 
-log Pulling akvo/rsr-backend:prod-no-code-with-nodejs
-docker pull --quiet akvo/rsr-backend:prod-no-code-with-nodejs || true
+docker_build º -f Dockerfile-prod-no-code-with-nodejs .
+docker_build akvo/rsr-backend-front-end -f Dockerfile-front-end .
+docker_build akvo/rsr-backend-dir -f Dockerfile-dir .
+docker_build akvo/rsr-backend-spa -f Dockerfile-spa .
 
-log Creating npm base build image_develop
-docker build --rm=false --cache-from akvo/rsr-backend:prod-no-code-with-nodejs -t akvo/rsr-backend:prod-no-code-with-nodejs -f Dockerfile-prod-no-code-with-nodejs . 
-
-log Push akvo/rsr-backend:prod-no-code-with-nodejs
-docker push akvo/rsr-backend:prod-no-code-with-nodejs
-
-log Pulling akvo/rsr-backend:front-end
-docker pull --quiet akvo/rsr-backend:front-end || true
-
-log Creating front-end image_develop
-docker build --rm=false --cache-from akvo/rsr-backend:front-end -t akvo/rsr-backend:front-end -f Dockerfile-front-end . 
-
-log Push akvo/rsr-backend:front-end
-docker push akvo/rsr-backend:front-end
-
-log Pulling akvo/rsr-backend:dir
-docker pull --quiet akvo/rsr-backend:dir || true
-
-log Creating dir image_develop
-docker build --rm=false --cache-from akvo/rsr-backend:dir -t akvo/rsr-backend:dir -f Dockerfile-dir . 
-
-log Push akvo/rsr-backend:dir
-docker push akvo/rsr-backend:dir
-
-log Pulling akvo/rsr-backend:spa
-docker pull --quiet akvo/rsr-backend:spa || true
-
-log Creating spa image_develop
-docker build --rm=false --cache-from akvo/rsr-backend:spa -t akvo/rsr-backend:spa -f Dockerfile-spa . 
-
-log Push akvo/rsr-backend:spa
-docker push akvo/rsr-backend:spa
-
-log Creating Production Nginx image_develop
+log Creating Production Nginx image
 docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/rsr-nginx:${CI_COMMIT} -f Dockerfile-nginx .
 
 log Starting docker-compose for end to end tests
