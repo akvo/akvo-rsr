880925df7456c93d7c2a04e73269410729468cc3
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-02-26 18:38:59 +0100

    [#4003] Removing some duplication

--- ci/build-semaphoreci.sh
+++ ci/build-semaphoreci.sh
@@ -5,6 +5,20 @@ function log {
    echo "$(date +"%T") - BUILD INFO - $*"
 }
 
+function build {
+  image_develop="$1:develop"
+  image_branch="$1:${CI_BRANCH}"
+  shift
+  log Pulling "$image_branch"
+  docker pull --quiet "$image_branch" || docker pull --quiet "$image_develop" || true
+
+  log Building "$image_branch"
+  docker build --cache-from="$image_branch" --cache-from "$image_develop" --rm=false -t "$image_branch" $*
+
+  log Pushing "$image_branch" container
+  docker push "$image_branch"
+}
+
 export PROJECT_NAME=akvo-lumen
 
 if [ -z "$CI_COMMIT" ]; then
@@ -14,14 +28,7 @@ fi
 log Login to DockerHub
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 
-log Pulling akvo/rsr-backend:dev
-docker pull --quiet akvo/rsr-backend:dev || true
-
-log Building dev image
-docker build --cache-from akvo/rsr-backend:dev --rm=false -t akvo/rsr-backend:dev -t rsr-backend:dev -f Dockerfile-dev .  
-
-log Pushing rsr-backend:dev container
-docker push akvo/rsr-backend:dev
+build akvo/rsr-backend-dev -t rsr-backend:dev -f Dockerfile-dev .
 
 log Starting docker-compose
 docker-compose -p rsrci -f docker-compose.yaml -f docker-compose.ci.yaml up -d --build 
@@ -40,19 +47,19 @@ echo "DEPLOY_TAG = '$CI_TAG'" >> ._66_deploy_info.conf
 log Pulling akvo/rsr-backend:prod-no-code
 docker pull --quiet akvo/rsr-backend:prod-no-code || true
 
-log Creating Production Backend image without code
+log Creating Production Backend image_develop without code
 docker build --rm=false --cache-from akvo/rsr-backend:prod-no-code -t akvo/rsr-backend:prod-no-code -f Dockerfile-prod-no-code . 
 
 log Push akvo/rsr-backend:prod-no-code
 docker push akvo/rsr-backend:prod-no-code
 
-log Creating Production Backend image with code
+log Creating Production Backend image_develop with code
 docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/rsr-backend:${CI_COMMIT} -t rsr-backend:prod . 
 
 log Pulling akvo/rsr-backend:prod-no-code-with-nodejs
 docker pull --quiet akvo/rsr-backend:prod-no-code-with-nodejs || true
 
-log Creating npm base build image
+log Creating npm base build image_develop
 docker build --rm=false --cache-from akvo/rsr-backend:prod-no-code-with-nodejs -t akvo/rsr-backend:prod-no-code-with-nodejs -f Dockerfile-prod-no-code-with-nodejs . 
 
 log Push akvo/rsr-backend:prod-no-code-with-nodejs
@@ -61,7 +68,7 @@ docker push akvo/rsr-backend:prod-no-code-with-nodejs
 log Pulling akvo/rsr-backend:front-end
 docker pull --quiet akvo/rsr-backend:front-end || true
 
-log Creating front-end image
+log Creating front-end image_develop
 docker build --rm=false --cache-from akvo/rsr-backend:front-end -t akvo/rsr-backend:front-end -f Dockerfile-front-end . 
 
 log Push akvo/rsr-backend:front-end
@@ -70,7 +77,7 @@ docker push akvo/rsr-backend:front-end
 log Pulling akvo/rsr-backend:dir
 docker pull --quiet akvo/rsr-backend:dir || true
 
-log Creating dir image
+log Creating dir image_develop
 docker build --rm=false --cache-from akvo/rsr-backend:dir -t akvo/rsr-backend:dir -f Dockerfile-dir . 
 
 log Push akvo/rsr-backend:dir
@@ -79,13 +86,13 @@ docker push akvo/rsr-backend:dir
 log Pulling akvo/rsr-backend:spa
 docker pull --quiet akvo/rsr-backend:spa || true
 
-log Creating spa image
+log Creating spa image_develop
 docker build --rm=false --cache-from akvo/rsr-backend:spa -t akvo/rsr-backend:spa -f Dockerfile-spa . 
 
 log Push akvo/rsr-backend:spa
 docker push akvo/rsr-backend:spa
 
-log Creating Production Nginx image
+log Creating Production Nginx image_develop
 docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/rsr-nginx:${CI_COMMIT} -f Dockerfile-nginx .
 
 log Starting docker-compose for end to end tests
