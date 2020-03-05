akvo/akvo-rsr

commit 187f705edfb2fdb509b94444e64d8cbf932ff7ae

[#3810] Include projects with project level access in /my_projects

akvo/rsr/views/my_rsr.py
@@ -30,7 +30,9 @@ from akvo.codelists.store.default_codelists import (
 from akvo.rsr.models import IndicatorPeriodData, User, UserProjects
 from akvo.rsr.models.user_projects import InvalidPermissionChange, check_collaborative_user
 from akvo.rsr.permissions import (
-    GROUP_NAME_USERS, GROUP_NAME_USER_MANAGERS, GROUP_NAME_ENUMERATORS, user_accessible_projects
+    GROUP_NAME_USERS, GROUP_NAME_USER_MANAGERS, GROUP_NAME_ENUMERATORS,
+    GROUP_NAME_ADMINS, GROUP_NAME_ME_MANAGERS, GROUP_NAME_PROJECT_EDITORS,
+    user_accessible_projects
 )
 from ..forms import (ProfileForm, UserOrganisationForm, UserAvatarForm, SelectOrgForm,
                      RSRPasswordChangeForm)
@@ -174,6 +176,7 @@ def my_updates(request):
 def user_editable_projects(user):
     # User groups
     not_allowed_to_edit = [GROUP_NAME_USERS, GROUP_NAME_USER_MANAGERS, GROUP_NAME_ENUMERATORS]
+    allowed_to_edit = [GROUP_NAME_ADMINS, GROUP_NAME_ME_MANAGERS, GROUP_NAME_PROJECT_EDITORS]
     employments = user.approved_employments()
 
     # Get project list
@@ -187,13 +190,13 @@ def user_editable_projects(user):
         projects = Project.objects.none()
         # Not allowed to edit roles
         non_editor_roles = employments.filter(group__name__in=not_allowed_to_edit)
-        uneditable_projects = non_editor_roles.organisations().all_projects().published()
+        uneditable_projects = user.my_projects(group_names=not_allowed_to_edit)
         projects = (
             projects | user_accessible_projects(user, non_editor_roles, uneditable_projects)
         )
         # Allowed to edit roles
         editor_roles = employments.exclude(group__name__in=not_allowed_to_edit)
-        editable_projects = editor_roles.organisations().all_projects()
+        editable_projects = user.my_projects(group_names=allowed_to_edit)
         projects = (
             projects | user_accessible_projects(user, editor_roles, editable_projects)
         )

commit 86f3f5b961ae045dfe88de4e9ec8bae04f38f706

[#4046] Fix bug returning unpublished projects non editor users
Fixes a bug introduced in 187f705edfb2fdb509b94444e64d8cbf932ff7ae, where
the list of uneditable projects included unpublished projects. Unpublished
projects should be shown in the my projects list only to users who can edit
them, and not to other users like Enumerators or Users.

akvo/rsr/views/my_rsr.py
@@ -199,7 +199,7 @@ def user_viewable_projects(user):
         projects = Project.objects.none()
         # Not allowed to edit roles
         non_editor_roles = employments.filter(group__name__in=not_allowed_to_edit)
-        uneditable_projects = user.my_projects(group_names=not_allowed_to_edit)
+        uneditable_projects = user.my_projects(group_names=not_allowed_to_edit).published()
         projects = (
             projects | user_accessible_projects(user, non_editor_roles, uneditable_projects)
         )

akvo/rsr/tests/rest/test_my_projects.py
@@ -16,15 +16,15 @@ class MyProjectsViewSetTestCase(BaseTestCase):
     def setUp(self):
         super(MyProjectsViewSetTestCase, self).setUp()
         self.project = self.create_project('Foo Test')
-        email = password = 'example@foo.com'
-        self.create_user(email, password, is_superuser=True)
-        self.c.login(username=email, password=password)
+        self.email = self.password = 'example@foo.com'
 
     def test_my_projects_with_unicode_sector(self):
         # Given
         url = '/rest/v1/my_projects/?format=json'
         sector_code = "Women’s equality"
         Sector.objects.create(project=self.project, sector_code=sector_code)
+        self.create_user(self.email, self.password, is_superuser=True)
+        self.c.login(username=self.email, password=self.password)
 
         # When
         response = self.c.get(url, follow=True)
@@ -36,3 +36,21 @@ class MyProjectsViewSetTestCase(BaseTestCase):
         project = projects[0]
         self.assertEqual(len(project['sectors']), 1)
         self.assertEqual(project['sectors'][0]['code_label'], sector_code)
+
+    def test_my_projects_with_unpublished_projects(self):
+        # Given
+        project = self.create_project('Unpublished', published=False)
+        org = self.create_organisation('Organisation')
+        self.make_partner(project, org)
+        user = self.create_user(self.email, self.password)
+        self.make_employment(user, org, 'Users')
+        self.c.login(username=self.email, password=self.password)
+        url = '/rest/v1/my_projects/?format=json'
+
+        # When
+        response = self.c.get(url, follow=True)
+
+        # Then
+        self.assertEqual(response.status_code, 200)
+        projects = response.data['results']
+        self.assertEqual(len(projects), 0)