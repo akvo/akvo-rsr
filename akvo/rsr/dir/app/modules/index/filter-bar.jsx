201607e98f9bee41581fa4dbc3181547551fa5f2
Martin Christov <martin.christov@gmail.com>
2020-03-09 15:39:09 +0200

    [#4009] breadcrumbs

--- akvo/rsr/dir/app/modules/index/filter-bar.jsx
+++ akvo/rsr/dir/app/modules/index/filter-bar.jsx
@@ -6,13 +6,12 @@ import { useSpring, animated } from 'react-spring'
 import { Button, Icon } from 'antd'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import filterSvg from '../../images/filter.svg'
+import tr from '../../images/tr.svg'
 
 
 const FilterBar = ({ onSetFilter, filters, geoFilteredProjects }) => {
   const [open, setOpen] = useState(false)
-  // const [sub, setSub] = useState(null)
   const [subIndex, setSubIndex] = useState([])
-  // const [subSubIndex, setSubSubIndex] = useState(-1)
   const [step, setStep] = useState(0)
   const subRef = useRef([])
   const [height, setHeight] = useState(filters.length * 42)
@@ -22,7 +21,7 @@ const FilterBar = ({ onSetFilter, filters, geoFilteredProjects }) => {
     setStep(step + 1)
     setTimeout(() => {
       if (subRef.current.length > 0){
-        let _height = subRef.current[subRef.current.length - 1].clientHeight + 40
+        let _height = subRef.current[subRef.current.length - 1].clientHeight + 45
         if(_height > window.innerHeight - 100) { _height = window.innerHeight - 100 }
         setHeight(_height)
       }
@@ -39,9 +38,24 @@ const FilterBar = ({ onSetFilter, filters, geoFilteredProjects }) => {
   const back = () => {
     setStep(step - 1)
     subRef.current.pop()
-    if (subRef.current.length > 0) setHeight(subRef.current[subRef.current.length - 1].clientHeight + 40)
+    if (subRef.current.length > 0) setHeight(subRef.current[subRef.current.length - 1].clientHeight + 45)
     else setHeight(filters.length * 42)
     setTimeout(() => setSubIndex(subIndex.slice(0, subIndex.length - 1)), 300)
+  }
+  const goto = (index) => {
+    const stepsDiff = step - index
+    for(let i = 0; i < stepsDiff; i += 1){
+      subRef.current.pop()
+    }
+    if (step - stepsDiff > 0) {
+      setHeight(subRef.current[index - 1].clientHeight + 45)
+      setTimeout(() => setSubIndex(subIndex.slice(0, index)), 300)
+    }
+    else {
+      setHeight(filters.length * 42)
+      setTimeout(() => setSubIndex([]), 300)
+    }
+    setStep(index)
   }
   const close = () => {
     setOpen(false)
@@ -56,8 +70,6 @@ const FilterBar = ({ onSetFilter, filters, geoFilteredProjects }) => {
     if(!open) setOpen(true)
     else close()
   }
-  // console.log(filters[subIndex])
-  // console.log(subIndex)
   const handleSubRef = (index) => (ref) => { if(ref && index < step) subRef.current[index] = ref }
   return [
     <div className={classNames('filters-btn', { open })} onClick={toggle} role="button" tabIndex="-1">
@@ -85,17 +97,33 @@ const FilterBar = ({ onSetFilter, filters, geoFilteredProjects }) => {
                     sub = options[subIndex[i]]
                     options = options[subIndex[i]].options
                   }
-                  // console.log(geoFilteredProjects)
                   return (
                     <div className="sub">
+                      {inIndex < 1 &&
                       <div className="top">
-                        <Button type="link" icon="left" onClick={back}>Back</Button>
+                        <Button type="link" icon="left" onClick={back}>Filters</Button>
                         {sub.selected && sub.selected.length > 0 &&
                         <div className="selected">
                           {sub.selected.length} selected
                         </div>
                         }
                       </div>
+                      }
+                      {inIndex >= 1 &&
+                      <div className="top breadcrumbs">
+                        <div className="step" onClick={() => goto(0)}><div className="text">Filters</div></div>
+                        {subIndex.map((_index, _inIndex) => {
+                          let _options = filters[subIndex[0]].options
+                          let _sub = filters[subIndex[0]]
+                          for (let i = 1; i <= _inIndex; i += 1) {
+                            _sub = _options[subIndex[i]]
+                            _options = _options[subIndex[i]].options
+                          }
+                          console.log(_sub, _index)
+                          return <div className="step" onClick={_inIndex < subIndex.length - 1 ? () => goto(_inIndex + 1) : null}><SVGInline svg={tr} /><div className="text">{_sub.name}</div></div>
+                        })}
+                      </div>
+                      }
                       <ul ref={handleSubRef(inIndex)}>
                         {options && options.map((opt, optIndex) => {
                           let items = -1
