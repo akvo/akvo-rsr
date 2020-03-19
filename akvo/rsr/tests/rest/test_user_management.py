akvo/akvo-rsr

commit 958f5b5b28ae1589089240e4d699b800dd14e32c

[#3810] Extract function to send user an invitation email

akvo/rest/views/user_management.py
@@ -9,7 +9,6 @@ import json
 
 from django.contrib.auth.models import Group
 from django.core.exceptions import ValidationError
-from django.core.signing import TimestampSigner
 from django.core.validators import validate_email
 from django.utils.translation import ugettext_lazy as _
 
@@ -19,8 +18,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .utils import create_invited_user
+from akvo.utils import send_user_invitation
 from ...rsr.models import Employment, Organisation
-from akvo.utils import rsr_send_mail
 
 
 def valid_email(email_address):
@@ -123,21 +122,5 @@ def invite_user(request):
             group=group,
         )
 
-        # Send an activation email
-        __, token_date, token = TimestampSigner().sign(email).split(':')
-
-        rsr_send_mail(
-            [email],
-            subject='registration/invited_user_subject.txt',
-            message='registration/invited_user_message.txt',
-            html_message='registration/invited_user_message.html',
-            msg_context={
-                'user': user,
-                'invited_user': invited_user,
-                'employment': employment,
-                'token': token,
-                'token_date': token_date,
-            }
-        )
-
+    send_user_invitation(email, user, invited_user, employment)
     return Response('User invited', status=status.HTTP_201_CREATED)

akvo/rsr/tests/rest/test_user_management.py
@@ -30,7 +30,7 @@ class UserManagementTestCase(BaseTestCase):
         self.user = self.create_user(self.username, self.password, is_admin=True)
         self.c.login(username=self.username, password=self.password)
 
-    @patch.object(user_management, 'rsr_send_mail')
+    @patch.object(user_management, 'send_user_invitation')
     def test_should_invite_new_user(self, mock_send):
         # Given
         group = Group.objects.get(name='Users')
@@ -56,7 +56,7 @@ class UserManagementTestCase(BaseTestCase):
         self.assertEqual(employment.group_id, group.id)
         self.assertFalse(employment.is_approved)
 
-    @patch.object(user_management, 'rsr_send_mail')
+    @patch.object(user_management, 'send_user_invitation')
     def test_should_create_new_employment_for_inactive_user(self, mock_send):
         # Given
         group = Group.objects.get(name='Users')

akvo/utils.py
@@ -21,6 +21,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.cache import cache
 from django.core.mail import EmailMultiAlternatives, get_connection
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.signing import TimestampSigner
 from django.apps import apps
 from django.http import HttpResponse
 from django.template import loader
@@ -494,3 +495,20 @@ def get_project_for_object(Project, obj):
         logger.info('%s does not define a relation to a project', obj_model)
         project = None
     return project
+
+
+def send_user_invitation(email, user, invited_user, employment):
+    _, token_date, token = TimestampSigner().sign(email).split(':')
+    rsr_send_mail(
+        [email],
+        subject='registration/invited_user_subject.txt',
+        message='registration/invited_user_message.txt',
+        html_message='registration/invited_user_message.html',
+        msg_context={
+            'user': user,
+            'invited_user': invited_user,
+            'employment': employment,
+            'token': token,
+            'token_date': token_date,
+        }
+    )

commit d006d6553f042a60ebdb31471dcc2f61fa7e7971

[#4066] Fix bug sending unnecessary invitation emails
This bug was introduced in 958f5b5b28ae1589089240e4d699b800dd14e32c when the
extracted function send_user_invitation was indented wrongly, and led to
sending activation emails to already active users

Closes #4066

akvo/rest/views/user_management.py
@@ -126,5 +126,5 @@ def employ_user(invited_user, organisation, group, inviting_user):
             organisation=organisation,
             group=group,
         )
+        send_user_invitation(invited_user.email, inviting_user, invited_user, employment)
 
-    send_user_invitation(invited_user.email, inviting_user, invited_user, employment)

akvo/rsr/tests/rest/test_user_management.py
@@ -55,6 +55,7 @@ class UserManagementTestCase(BaseTestCase):
         employment = Employment.objects.get(user__email=email, organisation_id=self.org.id)
         self.assertEqual(employment.group_id, group.id)
         self.assertFalse(employment.is_approved)
+        mock_send.assert_called_once()
 
     @patch.object(user_management, 'send_user_invitation')
     def test_should_create_new_employment_for_inactive_user(self, mock_send):
@@ -83,10 +84,12 @@ class UserManagementTestCase(BaseTestCase):
         self.assertEqual(mock_send.call_count, 1)
         employments = Employment.objects.filter(user__email=email, organisation_id=self.org.id)
         self.assertEqual(employments.count(), 2)
+        mock_send.assert_called_once()
 
     @patch.object(signals, 'rsr_send_mail_to_users')
     @patch.object(signals, 'rsr_send_mail')
-    def test_should_create_new_employment_for_active_user(self, send_approved, send_approval_request):
+    @patch.object(user_management, 'send_user_invitation')
+    def test_should_create_new_employment_for_active_user(self, invite, approved, approval_request):
         # Given
         group = Group.objects.get(name='Users')
         email = 'rsr-tests-email@akvo.org'
@@ -107,13 +110,15 @@ class UserManagementTestCase(BaseTestCase):
 
         # Then
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(send_approved.call_count, 1)
-        self.assertFalse(send_approval_request.called)
+        approved.assert_called_once()
+        approval_request.assert_not_called()
+        invite.assert_not_called()
         employment = Employment.objects.get(user__email=email, organisation_id=self.org.id)
         self.assertTrue(employment.is_approved)
 
     @patch.object(signals, 'rsr_send_mail')
-    def test_should_approve_existing_employment_for_active_user(self, mock_send):
+    @patch.object(user_management, 'send_user_invitation')
+    def test_should_approve_existing_employment_for_active_user(self, invite, mock_send):
         # Given
         group = Group.objects.get(name='Users')
         email = 'rsr-tests-email@akvo.org'
@@ -136,7 +141,8 @@ class UserManagementTestCase(BaseTestCase):
 
         # Then
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(mock_send.call_count, 1)
+        mock_send.assert_called_once()
+        invite.assert_not_called()
         employment = Employment.objects.get(user__email=email, organisation_id=self.org.id)
         self.assertTrue(employment.is_approved)
 