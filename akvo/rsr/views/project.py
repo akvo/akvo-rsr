fbf8b61aceda53029f4fc083ac64fcd25ab2a9e6
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-03-19 18:12:44 +0530

    [#4058] Simplify the ProjectUpdateForm save

--- akvo/rsr/views/project.py
+++ akvo/rsr/views/project.py
@@ -265,7 +265,7 @@ def set_update(request, project_id, edit_mode=False, form_class=ProjectUpdateFor
     allow_update = True if request.user.has_perm('rsr.post_updates', project) else False
 
     updates = project.updates_desc()[:5]
-    update = None
+    update = ProjectUpdate(project=project, user=request.user, update_method='W')
     update_user = None
 
     if update_id is not None:
@@ -284,10 +284,11 @@ def set_update(request, project_id, edit_mode=False, form_class=ProjectUpdateFor
     if request.method == 'POST':
         if not allow_update:
             raise PermissionDenied
+
         updateform = form_class(request.POST, request.FILES, instance=update)
         if updateform.is_valid():
             if update:
-                update = updateform.save(project=project, user=update.user)
+                update = updateform.save()
             else:
                 # Don't upload the photo, until the update is created in the
                 # DB. This removes the need to move the update image from a
@@ -295,7 +296,7 @@ def set_update(request, project_id, edit_mode=False, form_class=ProjectUpdateFor
                 # This moving code breaks with the Google storage backend.
                 photo = updateform.instance.photo
                 updateform.instance.photo = None
-                update = updateform.save(project=project, user=request.user)
+                update = updateform.save()
                 if photo:
                     update.photo = photo
                     update.save(update_fields=['photo'])
