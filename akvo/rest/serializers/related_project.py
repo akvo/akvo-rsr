f5ac8b16e664fdd48633682c108d12cdfeb44244
zuhdil <zuhdil@gmail.com>
2020-03-27 17:03:11 +0700

    [#4083] Add *_is_program attribute in /related_project/

--- akvo/rest/serializers/related_project.py
+++ akvo/rest/serializers/related_project.py
@@ -5,7 +5,7 @@
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
 
-from akvo.rsr.models import RelatedProject, Project
+from akvo.rsr.models import RelatedProject, Project, ProjectHierarchy
 
 from .rsr_serializer import BaseRSRSerializer
 
@@ -26,4 +26,22 @@ class RelatedProjectSerializer(RelatedProjectRawSerializer):
 
     related_project_name = serializers.ReadOnlyField(source='related_project.title')
     related_project_show_link = serializers.ReadOnlyField()
+    related_project_is_program = serializers.SerializerMethodField()
+    project_is_program = serializers.SerializerMethodField()
     relation_label = serializers.ReadOnlyField(source='iati_relation_unicode')
+
+    def get_related_project_is_program(self, obj):
+        related_project = obj.related_project
+        try:
+            return related_project.projecthierarchy is not None
+        except ProjectHierarchy.DoesNotExist:
+            pass
+        return False
+
+    def get_project_is_program(self, obj):
+        project = obj.project
+        try:
+            return project.projecthierarchy is not None
+        except ProjectHierarchy.DoesNotExist:
+            pass
+        return False
