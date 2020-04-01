82cd27755f850668ddfcf8ee5c77f32abd3bb0de
Martin Christov <martin.christov@gmail.com>
2020-04-01 17:11:08 +0300

    [#4075] untitled project label for empty titles

--- akvo/rsr/spa/app/modules/editor/section1/comp/project-picker.jsx
+++ akvo/rsr/spa/app/modules/editor/section1/comp/project-picker.jsx
@@ -73,8 +73,8 @@ const ProjectPicker = ({ loading, projects, savedData, formPush, formPop, projec
                 render={({ input }) => {
                   const $options =
                     projects && projects.length > 0
-                      ? ((input.value && state.searchStr.length === 0) ? [projects.find(it => it.id === input.value)].filter(it => it != null).map(({ id, title }) => ({ value: id, label: title })) : state.options)
-                      : [{ value: input.value, label: nameProps.input.value }]
+                      ? ((input.value && state.searchStr.length === 0) ? [projects.find(it => it.id === input.value)].filter(it => it != null).map(({ id, title }) => ({ value: id, label: title ? title : t('Untitled project') })) : state.options)
+                      : [{ value: input.value, label: nameProps.input.value ? nameProps.input.value : t('Untitled project') }]
                   return (
                     <Select
                       {...input}
