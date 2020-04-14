958a864c0783b58bda9cdca5444a061bebda3ba5
Martin Christov <martin.christov@gmail.com>
2020-04-14 19:24:10 +0300

    [#3998] display periods and updates

--- akvo/rsr/spa/app/modules/results/results.jsx
+++ akvo/rsr/spa/app/modules/results/results.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Input, Icon, Spin, Collapse } from 'antd'
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
+import moment from 'moment'
+import SVGInline from 'react-svg-inline'
 import { resultTypes, indicatorTypes } from '../../utils/constants'
 import './styles.scss'
 import ProjectInitHandler from '../editor/project-init-handler'
+import api from '../../utils/api'
+import approvedSvg from '../../images/status-approved.svg'
 
 const { Panel } = Collapse
 
@@ -35,12 +39,14 @@ const Results = ({ results = [], isFetched, match: {params: {id}}}) => {
                     const findex = src === '' ? -1 : indicator.title.toLowerCase().indexOf(src.toLowerCase())
                     return (
                       <li>
+                        <Link to={`/projects/${id}/results/indicator/${indicator.id}`}>
                         <div>
                           <h5>Indicator <b>{iindex + 1}</b>: {findex === -1 ? indicator.title : [indicator.title.substr(0, findex), <b>{indicator.title.substr(findex, src.length)}</b>, indicator.title.substr(findex + src.length)]}</h5>
                           <div className="label">{indicatorTypes.find(it => it.value === indicator.type).label}</div>
                           <div className="count-label">{indicator.periods.length + 1} periods</div>
                         </div>
                         <Icon type="right" />
+                        </Link>
                       </li>
                     )
                   })}
@@ -50,26 +56,56 @@ const Results = ({ results = [], isFetched, match: {params: {id}}}) => {
           ))}
         </ul>
       </div>
-      <Route path="/projects/:projectId/results/indicator/:id" exact render={(props) => <Indicator {...props} />} />
+      <div className="main-content">
+        {/* <Collapse accordion>
+          {results.map(result => (
+            <Panel header={result.title}>
+              <div>
+                <Collapse>
+                {result.indicators.map(indicator => (
+                  <Panel header={indicator.title}>asd</Panel>
+                ))}
+                </Collapse>
+              </div>
+            </Panel>
+          ))}
+        </Collapse> */}
+      <Route path="/projects/:projectId/results/indicator/:id" exact render={(props) => <Indicator {...{...props, projectId: id}} />} />
+      </div>
     </div>
   )
 }
 
-const Indicator = ({ match: {params: {id}} }) => {
+const Indicator = ({ projectId, match: {params: {id}} }) => {
+  const [periods, setPeriods] = useState(null)
+  const [loading, setLoading] = useState(true)
+  // const [data, loading] = useFetch(`/project/${projectId}/indicator/${id}/`)
+  useEffect(() => {
+    if(id){
+      api.get(`/project/${projectId}/indicator/${id}/`)
+      .then(({data}) => {
+        setPeriods(data.periods)
+        setLoading(false)
+        console.log(data.periods)
+      })
+    }
+  }, [id])
   return (
     <div className="indicator-content">
+      {loading && <Spin indicator={<Icon type="loading" style={{ fontSize: 25 }} spin />} />}
       <Collapse accordion className="periods">
-        <Panel header="01 Sep 2019 - 01 Feb 2020">
+        {periods && periods.map(period =>
+        <Panel header={<div>{period.periodStart} - {period.periodEnd}</div>}>
           <div className="graph">
             <div className="timeline">
               <div className="target">
                 <div className="cap">target value</div>
-                <div><b>70</b></div>
+                <div><b>{period.targetValue}</b></div>
               </div>
               <div className="actual">
                 <div className="text">
                   <div className="cap">actual value</div>
-                  <div><small>23%</small><b>23</b></div>
+                  <div><small>23%</small><b>{period.actualValue}</b></div>
                 </div>
               </div>
               <svg width="370px" height="260px" version="1.1" xmlns="http://www.w3.org/2000/svg">
@@ -91,8 +127,59 @@ const Indicator = ({ match: {params: {id}} }) => {
                 <div style={{ left: 120 }}>3</div>
               </div>
             </div>
+            {/* <div className="disaggregations">
+              <div className="cap">disaggregations</div>
+              <div className="bars">
+                <div className="bar" />
+              </div>
+            </div> */}
+          </div>
+          <div className="updates">
+            {period.updates.map(update =>
+            <div className="update">
+              <header>
+                <div className="label">{moment(update.createdAt).format('DD MMM YYYY')}</div>
+                <div className="label">{update.user.name}</div>
+                {/* <div className="status">Pending approval</div> */}
+                {update.status.code === 'A' && (
+                  <div className="status approved">
+                    <SVGInline svg={approvedSvg} />
+                    <div className="text">
+                    Approved<br />
+                    by {update.approvedBy.name}
+                    </div>
+                  </div>
+                )}
+              </header>
+              <div className="values">
+                <div className="value-container">
+                  <div className="value">{update.value}</div>
+                </div>
+                <div className="disaggregations">
+                  <span>Disaggregations: Gender breakdown</span>
+                  <ul>
+                    <li><span>Men</span><span>4 (26%)</span></li>
+                    <li><span>Women</span><span>4 (26%)</span></li>
+                  </ul>
+                </div>
+              </div>
+              <div className="comments">
+                <div className="label">Value comment</div>
+                {update.comments.map(comment => (
+                  <div className="comment">
+                    <div className="top">
+                      <b>{comment.user.name}</b>
+                      <b>{moment(comment.createdAt).format('DD MMM YYYY')}</b>
+                    </div>
+                    <p>{comment.comment}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+            )}
           </div>
         </Panel>
+        )}
       </Collapse>
     </div>
   )
