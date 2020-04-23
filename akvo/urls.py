bd4c35bc6398fa40fd4a8a8f3ea225bca6d66833
zuhdil <zuhdil@gmail.com>
2020-04-23 21:53:10 +0700

    [#4027] Results and indicators export excel report

--- akvo/urls.py
+++ akvo/urls.py
@@ -200,6 +200,10 @@ urlpatterns += (
         py_reports.render_eutf_org_results_table_excel_report,
         name='py-reports-organisation-eutf-results-indicators-table'),
 
+    url(r'^py-reports/organisation/(?P<org_id>\d+)/results-indicators-table/$',
+        py_reports.render_results_indicators_excel_report,
+        name='py-reports-organisation-results-indicators-table'),
+
     # IATI file
     url(r'^project/(?P<project_id>\d+)/iati/$',
         project.iati, name='project-iati'),
