bd09e9d9643a95376a7e87d31ccd73b4b895b711
zuhdil <zuhdil@gmail.com>
2020-04-23 21:53:10 +0700

    [#4027] Results and indicators export excel report

--- akvo/rsr/views/py_reports/__init__.py
+++ akvo/rsr/views/py_reports/__init__.py
@@ -24,6 +24,8 @@ from .organisation_data_quality_overview_report import \
     render_report as render_organisation_data_quality_overview
 from .eutf_org_results_table_excel_report import \
     render_report as render_eutf_org_results_table_excel_report
+from .results_indicators_excel_report import \
+    render_report as render_results_indicators_excel_report
 
 
 def check(request):
@@ -46,4 +48,5 @@ __all__ = [
     'render_project_results_indicators_excel_report',
     'render_organisation_data_quality_overview',
     'render_eutf_org_results_table_excel_report',
+    'render_results_indicators_excel_report',
 ]
