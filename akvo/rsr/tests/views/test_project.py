b400028f481b82b7d86d2c390ea10e1d536f6fe3
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-04-27 12:37:53 +0530

    [#4140] Remove unused Django perf rec requirement

--- akvo/rsr/tests/views/test_project.py
+++ akvo/rsr/tests/views/test_project.py
@@ -7,26 +7,8 @@ See more details in the license.txt file located at the root folder of the Akvo 
 For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 """
 
-
-from unittest import skip
-
-import django_perf_rec
-from django.test import TestCase
-
 from akvo.rsr.models import Organisation, Partnership, Project, ProjectUpdate
 from akvo.rsr.tests.base import BaseTestCase
-
-
-@skip('Needs Django >= 1.8')
-class ProjectPerfomanceTestCase(TestCase):
-    """Test performance of project views."""
-
-    def setUp(self):
-        return
-
-    def test_project_directory_listing(self):
-        with django_perf_rec.record():
-            self.client.get('/en/projects/', follow=True)
 
 
 class SetProjectUpdate(BaseTestCase):
