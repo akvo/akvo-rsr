2aa80a1835415075562e67a140d9edf1634f93eb
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-04-30 16:10:15 +0200

    [#4146] gcloud login if there is login
    [skip ci]

--- scripts/data/update-dev-fixture.sh
+++ scripts/data/update-dev-fixture.sh
@@ -5,6 +5,10 @@
 ## Then just run this script
 set -eu
 
+if [[ -z "$(gcloud config list --format='value(core.account)')" ]]; then
+  gcloud auth login
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker run --rm -it -e FIXTURE_PATH=/data/dev-db.json -v "$DIR":/data -v ~/.config:/home/gkh/.config -v "$DIR"/helper/update-dev-fixture.sh:/data/commands.sh:ro kiwigrid/gcloud-kubectl-helm:2.12.3-234.0.0-88
