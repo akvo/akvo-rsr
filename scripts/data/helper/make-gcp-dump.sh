022e2b631007ea96b948ff390ba387e74f6788ad
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-04-30 16:15:30 +0200

    [#4146] gcloud set project
    [skip ci]

--- scripts/data/helper/make-gcp-dump.sh
+++ scripts/data/helper/make-gcp-dump.sh
@@ -7,6 +7,8 @@ if [[ -z "$(gcloud config list --format='value(core.account)')" ]]; then
   gcloud auth login
 fi
 
+gcloud config set project akvo-lumen
+
 gcloud sql export sql rsr-prod-database "gs://akvo-rsr-db-dump/${dump_file}" --database=rsr_db
 gsutil cp "gs://akvo-rsr-db-dump/$dump_file" /data/db_dump.gz
 gsutil rm "gs://akvo-rsr-db-dump/$dump_file"
