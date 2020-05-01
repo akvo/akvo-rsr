83cdddb32a00c3281a928ffdb54e7471c5b1880a
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-05-01 11:28:20 +0200

    [#4058] Nginx to forward any media request to Google Storage
    Once we start using Google Storage, Nginx will proxy any media requests from the old url to the new Google Storage ones.

--- nginx/start.sh
+++ nginx/start.sh
@@ -13,19 +13,27 @@ rawurlencode () {
     }'
 }
 
-if [ -z "$REPORT_SERVER_URL" ] || [ -z "$REPORT_SERVER_API_KEY" ]; then
+if [ -z "$REPORT_SERVER_URL" ] || [ -z "$REPORT_SERVER_API_KEY" ] || [ -z "$DJANGO_DEFAULT_FILE_STORAGE" ] || [ -z "$GOOGLE_STORAGE_BUCKET_NAME" ]; then
  echo "Nginx not properly configured"
  exit 1
 fi
 
 escaped_url=$(printf '%s\n' "$REPORT_SERVER_URL" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
+escaped_bucket_name=$(printf '%s\n' "$GOOGLE_STORAGE_BUCKET_NAME" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
 
 urlencoded_key=$(rawurlencode "$REPORT_SERVER_API_KEY")
 escaped_key=$(printf '%s\n' "$urlencoded_key" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
 
 sed -i /etc/nginx/conf.d/default.conf \
     -e "s/%REPORT_SERVER_URL%/$escaped_url/" \
+    -e "s/%GOOGLE_STORAGE_BUCKET_NAME%/$escaped_bucket_name/" \
     -e "s/%REPORT_SERVER_API_KEY%/$escaped_key/"
+
+if [ "${DJANGO_DEFAULT_FILE_STORAGE}" = "storages.backends.gcloud.GoogleCloudStorage" ]; then
+  sed -i /etc/nginx/conf.d/default.conf -e '/GOOGLE_STORAGE_DISABLED/,/GOOGLE_STORAGE_DISABLED/d'
+else
+  sed -i /etc/nginx/conf.d/default.conf -e '/GOOGLE_STORAGE_ENABLED,/GOOGLE_STORAGE_ENABLED/d'
+fi
 
 ## Use the correct robots depending on the environment
 cp -f /usr/share/nginx/html/robots-${ENVIRONMENT}.txt /usr/share/nginx/html/robots.txt
