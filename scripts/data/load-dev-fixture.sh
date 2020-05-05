d42670bc3b1e09424cdbabcbdb5a3e64b3ece1aa
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-05-05 18:41:11 +0530

    SKIP_REQUIRED_AUTH_GROUPS when loading the fixture

--- scripts/data/load-dev-fixture.sh
+++ scripts/data/load-dev-fixture.sh
@@ -16,4 +16,4 @@ echo "Clearing current local DB..."
 docker-compose exec web bash -c 'python manage.py sqlflush | python manage.py dbshell'
 
 echo "Loading fixture from ${FIXTURE_PATH}..."
-docker-compose exec web python manage.py loaddata --ignorenonexistent "${FIXTURE_NAME}"
+docker-compose exec -e SKIP_REQUIRED_AUTH_GROUPS=1 web python manage.py loaddata --ignorenonexistent "${FIXTURE_NAME}"
