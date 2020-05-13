c0cc7b8b808015c40ebf6277445d54aa72960afb
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-05-13 10:22:10 +0200

    [#4176] Fix promote script Y/n logic

--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker run --rm -e ZULIP_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
-  -it akvo/akvo-devops:20200512.132847.78aa475 promote-test-to-prod.sh rsr rsr-version akvo-rsr zulip
+  -it akvo/akvo-devops:20200513.082032.37c61a6 promote-test-to-prod.sh rsr rsr-version akvo-rsr zulip
