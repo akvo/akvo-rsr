cb62ae8c6891d2a383e1ed2d6e38d35acecd8b4e
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-05-18 16:40:35 +0530

    [#4187] Start using the database cache for project directory data

--- akvo/rest/cache.py
+++ akvo/rest/cache.py
@@ -4,10 +4,12 @@ from akvo.cache import cache_with_key, delete_cache_data
 from akvo.rest.serializers import ProjectDirectorySerializer
 from akvo.rsr.models.project import Project, project_directory_cache_key
 
-# FIXME: Should we use a DB cache?
-PROJECT_DIRECTORY_CACHE = 'default'
+PROJECT_DIRECTORY_CACHE = 'database'
 
 
+# NOTE: The data doesn't timeout, since we expect the data to be invalidated
+# when the data is updated from the project editor. Also, the script to fill the
+# cache should be able to clear the cache and create new values.
 @cache_with_key(project_directory_cache_key, timeout=None, cache_name=PROJECT_DIRECTORY_CACHE)
 def serialized_project(project_id):
     project = Project.objects.only(
