b96fc0750e5829e6bdc4cf03860cb63fdc99c3c4
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-06-02 14:23:04 +0530

    [#4196] Show Add contrib project for EUTF org admins

--- akvo/rsr/spa/app/modules/hierarchy/hierarchy.jsx
+++ akvo/rsr/spa/app/modules/hierarchy/hierarchy.jsx
@@ -104,7 +104,8 @@ const Hierarchy = ({ match: { params }, program, userRdr }) => {
     }
   }
   const hasSecondLevel = selected.length > 0 && selected[0].children.filter(it => it.children.length > 0).length > 0
-  const showNewFeature = userRdr && userRdr.organisations && userRdr.organisations.findIndex(it => it.id === 42) !== -1
+  const prmOrgs = new Set([42, 3394])
+  const showNewFeature = userRdr.organisations && userRdr.organisations.findIndex(it => prmOrgs.has(it.id) || prmOrgs.has(it.contentOwner)) !== -1
   return (
     <div className={classNames('hierarchy', {noHeader: program})}>
       {!program &&
@@ -127,7 +128,7 @@ const Hierarchy = ({ match: { params }, program, userRdr }) => {
               {col.children.filter(filterCountry).map(item =>
                 <Card project={item} onClick={() => toggleSelect(item, index)} selected={selected[index + 1] === item} {...{ filterCountry, program, countryFilter, isAdmin }} />
               )}
-              {program && isAdmin && showNewFeature && canEditProjects && <div className="card create"><Link to={`/projects/new/settings?parent=${selected[index].id}&program=${selected[0].id}`}><Button icon="plus">{t('New Contributing Project')}</Button></Link></div>}
+              {program && showNewFeature && canEditProjects && <div className="card create"><Link to={`/projects/new/settings?parent=${selected[index].id}&program=${selected[0].id}`}><Button icon="plus">{t('New Contributing Project')}</Button></Link></div>}
             </Column>
           )
         })}
