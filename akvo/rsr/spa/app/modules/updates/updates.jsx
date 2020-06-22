8082a8657bfd57236f944b6b80c0476c2e0cf7d5
Martin Christov <martin.christov@gmail.com>
2020-06-22 18:21:13 +0300

    [#4232] edit update

--- akvo/rsr/spa/app/modules/updates/updates.jsx
+++ akvo/rsr/spa/app/modules/updates/updates.jsx
@@ -5,6 +5,7 @@ import { useTranslation } from 'react-i18next'
 import moment from 'moment'
 import axios from 'axios'
 import humps from 'humps'
+import { diff } from 'deep-object-diff'
 import { Form as FinalForm, Field } from 'react-final-form'
 import api, { config } from '../../utils/api'
 import { dateTransform } from '../../utils/misc'
@@ -16,11 +17,21 @@ const isLocal = window.location.href.indexOf('localhost') !== -1 || window.locat
 const urlPrefix = isLocal ? 'http://rsr.akvo.org' : ''
 const {Item} = Form
 const {Option} = Select
+const axiosConfig = {
+  headers: { ...config.headers, 'Content-Type': 'multipart/form-data' },
+  transformResponse: [
+    ...axios.defaults.transformResponse,
+    data => dateTransform.response(data),
+    data => humps.camelizeKeys(data)
+  ]
+}
 
 const Updates = ({projectId}) => {
   const [fileList, setFileList] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
+  const [editing, setEditing] = useState(-1)
+  const [render, setRender] = useState(true)
   const formRef = useRef()
   const { t } = useTranslation()
   const [updates, setUpdates] = useState([])
@@ -30,9 +41,42 @@ const Updates = ({projectId}) => {
       setUpdates(data.results)
     })
   }, [])
+  const handleUpdate = (values) => {
+    const _values = {...values}
+    if(_values.eventDate){
+      _values.eventDate = _values.eventDate.format('DD/MM/YYYY')
+    }
+    const updatedValues = diff(updates[editing], _values)
+    if(Object.keys(updatedValues).length > 0){
+      const payload = humps.decamelizeKeys(updatedValues)
+      if (updatedValues.eventDate != null) payload.event_date = values.eventDate.format('YYYY-MM-DD')
+      const formData = new FormData() // eslint-disable-line
+      Object.keys(payload).forEach(key => {
+        formData.append(key, payload[key])
+      })
+      if (fileList.length > 0) formData.append('photo', fileList[0])
+      axios.patch(`${config.baseURL}/project_update/${updates[editing].id}/`, formData, axiosConfig)
+        .then(({ data }) => {
+          setLoading(false)
+          setUpdates((state) => {
+            return [...state.slice(0, editing), data, ...state.slice(editing + 1)]
+          })
+          setEditing(-1)
+          setFileList([])
+          formRef.current.form.reset()
+        })
+        .catch((e) => {
+          setLoading(false)
+          console.log(e.response)
+          setError(true)
+        })
+    }
+  }
   const handleSubmit = (values) => {
-    // setLoading(true)
-    console.log(values)
+    if (editing !== -1) {
+      handleUpdate(values)
+      return
+    }
     setLoading(true)
     const payload = humps.decamelizeKeys({ ...values, project: projectId })
     if (values.eventDate != null) payload.event_date = values.eventDate.format('YYYY-MM-DD')
@@ -41,17 +85,7 @@ const Updates = ({projectId}) => {
       formData.append(key, payload[key])
     })
     if (fileList.length > 0) formData.append('photo', fileList[0])
-    axios.post(
-      `${config.baseURL}/project_update/`,
-      formData,
-      {
-        headers: { ...config.headers, 'Content-Type': 'multipart/form-data'},
-        transformResponse: [
-          ...axios.defaults.transformResponse,
-          data => dateTransform.response(data),
-          data => humps.camelizeKeys(data)
-        ]
-      })
+    axios.post(`${config.baseURL}/project_update/`, formData, axiosConfig)
     .then(({ data }) => {
       setLoading(false)
       setUpdates((state) => {
@@ -70,6 +104,25 @@ const Updates = ({projectId}) => {
     api.delete(`/project_update/${id}/`)
     setUpdates([...updates.slice(0, index), ...updates.slice(index + 1)])
   }
+  let initialValues = { title: '', text: '', language: 'en' }
+  if(editing !== -1){
+    initialValues = { ...updates[editing] }
+    if(updates[editing].eventDate){
+      console.log(updates[editing].eventDate)
+      initialValues.eventDate = moment(updates[editing].eventDate, 'DD/MM/YYYY')
+    }
+  }
+  const handleEdit = (index) => () => {
+    setRender(false)
+    setTimeout(() => {
+      setEditing(index)
+      setRender(true)
+    }, 50)
+  }
+  const handleCancel = () => {
+    setEditing(-1)
+    formRef.form.reset()
+  }
   return (
     <div className="updates-view">
       <ul className="updates">
@@ -81,19 +134,21 @@ const Updates = ({projectId}) => {
             <Exerpt text={update.text} max={400} />
             {/* <Divider /> */}
             <div className="btns">
-              <a href={update.absoluteUrl}><Button type="link">View</Button></a> | <Button type="link">Edit</Button> | <Button type="link" onClick={handleDelete(update.id, index)}>Delete</Button>
+              <a href={update.absoluteUrl}><Button type="link">View</Button></a> | <Button type="link" disabled={editing === index} onClick={handleEdit(index)}>Edit</Button> | <Button type="link" onClick={handleDelete(update.id, index)}>Delete</Button>
             </div>
           </li>
         )}
       </ul>
       <div className="new-update-container">
+      {render &&
       <div className="new-update">
-        <h2>Add an update</h2>
+        {editing === -1 && <h2>Add an update</h2>}
+        {editing !== -1 && <h2>Edit update</h2>}
         <FinalForm
           ref={(ref) => { formRef.current = ref }}
           onSubmit={handleSubmit}
           subscription={{}}
-          initialValues={{ title: '', text: '', language: 'en' }}
+          initialValues={initialValues}
           render={() => (
           <Form layout="vertical">
             <Item>
@@ -110,7 +165,7 @@ const Updates = ({projectId}) => {
               } />
             </Item>
             <Item label="Date">
-              <Field name="eventDate" component={({ input }) => <DatePicker {...input} />} />
+              <Field name="eventDate" component={({ input }) => <DatePicker {...input} format="DD/MM/YYYY" />} />
             </Item>
             <Item label="Photo">
               {/* <Input type="file" /> */}
@@ -150,11 +205,15 @@ const Updates = ({projectId}) => {
               <Field name="videoCaption" component={({ input }) => <Input placeholder="Video caption" {...input} />} />
               <Field name="videoCredit" component={({ input }) => <Input placeholder="Video Credit" {...input} />} />
             </Item>
-            <Button loading={loading} type="primary" size="large" onClick={() => formRef.current.form.submit()}>Add an update</Button>
+            <Button loading={loading} type="primary" size="large" onClick={() => formRef.current.form.submit()}>
+              {editing === -1 ? 'Add an update' : 'Update'}
+            </Button>
+            {editing !== -1 && <Button type="link" onClick={handleCancel}>Cancel</Button>}
           </Form>
           )}
         />
       </div>
+      }
       </div>
     </div>
   )
