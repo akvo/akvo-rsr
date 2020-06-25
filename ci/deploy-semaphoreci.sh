541b1447e94a232d63fc19527a227819042666fe
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-06-25 13:28:48 +0530

    TEMP: Push docker images from this branch

--- ci/deploy-semaphoreci.sh
+++ ci/deploy-semaphoreci.sh
@@ -9,13 +9,13 @@ function log {
 log Running deployment script
 export PROJECT_NAME=akvo-lumen
 
-if [[ "${CI_BRANCH}" != "master" ]] && [[ ! "${CI_TAG:-}" =~ promote-.* ]]; then
-    exit 0
-fi
+# if [[ "${CI_BRANCH}" != "master" ]] && [[ ! "${CI_TAG:-}" =~ promote-.* ]]; then
+#     exit 0
+# fi
 
-if [[ "${CI_PULL_REQUEST}" != "false" ]]; then
-    exit 0
-fi
+# if [[ "${CI_PULL_REQUEST}" != "false" ]]; then
+#     exit 0
+# fi
 
 log Authentication with gcloud and kubectl
 gcloud auth activate-service-account --key-file=/home/semaphore/.secrets/gcp.json
@@ -44,14 +44,14 @@ else
 
 fi
 
-sed -e "s/\${TRAVIS_COMMIT}/$CI_COMMIT/" ci/k8s/deployment.yml > deployment.yml.tmp
+# sed -e "s/\${TRAVIS_COMMIT}/$CI_COMMIT/" ci/k8s/deployment.yml > deployment.yml.tmp
 
-kubectl apply -f ${K8S_CONFIG_FILE}
-kubectl apply -f ci/k8s/memcached.yml
-kubectl apply -f ci/k8s/media-disk.yml
-kubectl apply -f ci/k8s/service.yml
-kubectl apply -f deployment.yml.tmp
-kubectl apply -f ci/k8s/grafana/main.yml
+# kubectl apply -f ${K8S_CONFIG_FILE}
+# kubectl apply -f ci/k8s/memcached.yml
+# kubectl apply -f ci/k8s/media-disk.yml
+# kubectl apply -f ci/k8s/service.yml
+# kubectl apply -f deployment.yml.tmp
+# kubectl apply -f ci/k8s/grafana/main.yml
 
-log Waiting for k8s to finish
-./ci/k8s/helpers/wait-for-k8s-deployment-to-be-ready.sh
+# log Waiting for k8s to finish
+# ./ci/k8s/helpers/wait-for-k8s-deployment-to-be-ready.sh
