10d30f28cbfb5336874ea56d2eb1f0ec8aed39d8
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-07-01 15:13:07 +0530

    Updgrade to the latest devops docker image

--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker run --rm -e ZULIP_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
-  -it akvo/akvo-devops:20200617.141037.610c18f promote-test-to-prod.sh rsr rsr-version akvo-rsr zulip
+  -it akvo/akvo-devops:20200701.075253.7c93843 promote-test-to-prod.sh rsr rsr-version akvo-rsr zulip
