28c5be9d13f30c1d2728dbd52a5ec36379f9941e
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-07-01 15:13:32 +0530

    Fetch latest code from upstream before promoting

--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+git fetch
+
 docker run --rm -e ZULIP_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
   -it akvo/akvo-devops:20200701.075253.7c93843 promote-test-to-prod.sh rsr rsr-version akvo-rsr zulip
