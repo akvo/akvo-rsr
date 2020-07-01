661797c4e3d197b948af1298fe0507c3ff020f48
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-07-01 10:43:50 +0530

    [#4244] Remove link to beta results from my projects page

--- akvo/rsr/spa/app/modules/projects/conditional-link.jsx
+++ akvo/rsr/spa/app/modules/projects/conditional-link.jsx
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const ConditionalLink = connect(({ userRdr: { lang, organisations } }) => ({ lang, organisations }))(({ record, children, lang, organisations }) => {
-  const resultsFlag = organisations && organisations.findIndex(it => it.id === 42) !== -1
   if(record.restricted === true){
     return <a href={`/en/project/${record.id}/`} target="_blank" rel="noopener noreferrer">{children}</a>
   }
@@ -14,12 +13,9 @@ const ConditionalLink = connect(({ userRdr: { lang, organisations } }) => ({ lan
       </Link>
     )
   }
-  if (!resultsFlag){
   return (
     <a href={`/${lang}/myrsr/my_project/${record.id}/`}>{children}</a>
   )
-  }
-  return <Link to={`/projects/${record.id}/results`}>{children}</Link>
 })
 
 export default ConditionalLink
