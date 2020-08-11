1bbce5e69071b44e060e9385527c430acb843c53
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-08-11 16:50:09 +0530

    Fix devops docker image ID

--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -3,4 +3,4 @@
 git fetch
 
 docker run --rm -e ZULIP_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
-  -it akvo/akvo-devops:20200810.101049.f106b48 promote-test-to-prod.sh rsr rsr-version akvo-rsr zulip
+  -it akvo/akvo-devops:20200810.101253.a1dbc6a promote-test-to-prod.sh rsr rsr-version akvo-rsr zulip
