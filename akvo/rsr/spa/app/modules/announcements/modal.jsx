3c371f6888f2d55b2003557a806ec9be4e499309
Martin Christov <martin.christov@gmail.com>
2020-08-19 12:16:18 +0300

    [#4300] tiny fix

--- akvo/rsr/spa/app/modules/announcements/modal.jsx
+++ akvo/rsr/spa/app/modules/announcements/modal.jsx
@@ -3,6 +3,7 @@ import { Modal } from 'antd'
 import api from '../../utils/api'
 
 export default ({ visible, onCancel, Body, openMenu }) => {
+  if(Body === null) return null
   return (
     <Modal {...{visible, onCancel}} footer={null}>
       <Suspense fallback={<div>Loading...</div>}><Body close={onCancel} {...{openMenu}} /></Suspense>
