7a2e6215b2cc3b4cc73c7ba36003cb4de49aaaa7
zuhdil <zuhdil@gmail.com>
2020-08-19 15:08:44 +0700

    [#4280] Fix migration scripts.

--- akvo/rsr/migrations/0182_auto_20200810_1250.py
+++ akvo/rsr/migrations/0182_auto_20200810_1250.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('rsr', '0180_projecthierarchy_is_master'),
+        ('rsr', '0181_program_reports'),
     ]
 
     operations = [
