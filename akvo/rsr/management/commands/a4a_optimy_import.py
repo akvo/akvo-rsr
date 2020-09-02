52609adb779590b3966a67784b75ed8ea22cfa89
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-09-02 17:09:23 +0530

    [#4261] Add command to import Optimy contracts

---
+++ akvo/rsr/management/commands/a4a_optimy_import.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+
+# Akvo Reporting is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+""" Import projects from Optimy for Aqua for All
+
+Usage:
+
+    python manage.py a4a_optimy_import [--project-id <optimy_project_id>]
+
+"""
+
+from itertools import groupby
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+import requests
+
+from akvo.rsr.iso3166 import ISO_3166_COUNTRIES
+from akvo.rsr.models import (
+    BudgetItem,
+    BudgetItemLabel,
+    Organisation,
+    Project,
+    ProjectCustomField,
+    ProjectLocation,
+)
+from akvo.utils import custom_get_or_create_country
+
+BASE_URL = "https://api.optimytool.com/v1.3"
+USERNAME = settings.OPTIMY_USERNAME
+PASSWORD = settings.OPTIMY_PASSWORD
+COUNTRY_NAME_TO_ISO_MAP = {name: code for code, name in ISO_3166_COUNTRIES}
+
+
+def get_projects(contracts_only=True):
+    CONTRACT_STATUS = "d30a945f-e524-53fe-8b2f-0c65b27be1ea"
+    response = requests.get(f"{BASE_URL}/projects", auth=(USERNAME, PASSWORD))
+    content = response.json()
+    projects = content["data"]
+    if contracts_only:
+        projects = [
+            project for project in projects if project["status_id"] == CONTRACT_STATUS
+        ]
+    return projects
+
+
+def get_submission_versions(project_id):
+    response = requests.get(
+        f"{BASE_URL}/projects/{project_id}/versions", auth=(USERNAME, PASSWORD)
+    )
+    data = response.json()["data"]
+    versions = [
+        list(versions)[-1]["version_id"]
+        for form_part_id, versions in groupby(data, key=lambda x: x["form_part_id"])
+    ]
+    return versions
+
+
+def get_project_answers(project_id):
+    version_ids = get_submission_versions(project_id)
+
+    answers = []
+    for version_id in version_ids:
+        print(f"Fetching answers for {project_id} - {version_id}...")
+        response = requests.get(
+            f"{BASE_URL}/projects/{project_id}/versions/{version_id}/answers",
+            auth=(USERNAME, PASSWORD),
+        )
+        data = response.json()["data"]
+        answers.extend(data)
+
+    return answers
+
+
+def create_project(project_id, answers):
+    question_mapping = {
+        "title": "9900586f-3c4b-5e3e-a9e6-a209eb8cb8e3",
+        # FIXME: subtitle?
+        "total-budget": "322932f0-e294-5621-a37b-fd57fec9937a",
+        "aqua-for-all-budget": "b0268b0c-d7e9-513a-bb27-1de7c0ec593a",
+        "co-financing-budget": "6c05de7b-4031-5809-a692-a45beadf7cec",
+        "start-date": "b785b97e-64f7-5149-a07b-7216497aa39f",
+        "end-date": "d3c4132c-1e55-5177-943e-3afa25b092ab",
+        "project-number": "683c31bc-d1d3-57f2-bf57-2e4c54894181",
+        "country": "913bec17-7f11-540a-8cb5-c5803e32a98b",
+        "summary": "02f1316c-4d5c-5989-8183-e392a634d23e",
+    }
+    answers_by_id = {ans["question_id"]: ans for ans in answers}
+
+    def get_answer(key):
+        ans_key = "answer_name" if key == "country" else "value"
+        answer = answers_by_id.get(question_mapping[key], {}).get(ans_key)
+        if not answer:
+            print(f"Could not find answer for {key}")
+        return answer
+
+    optimy_project_id_field = "Optimy Project ID"
+    custom_field = ProjectCustomField.objects.filter(
+        name=optimy_project_id_field, value=project_id
+    ).first()
+    if custom_field is not None:
+        project = custom_field.project
+
+    else:
+        title = get_answer("title")
+        project = Project.objects.create(title=title)
+        ProjectCustomField.objects.get_or_create(
+            project=project,
+            name="Optimy Project ID",
+            defaults=dict(value=project_id, section="1", order="1"),
+        )
+
+    # Add Aqua for All project Number
+    answer_project_number = answers_by_id.get(question_mapping["project-number"])
+    if answer_project_number:
+        ProjectCustomField.objects.get_or_create(
+            project=project,
+            name=answer_project_number["question_name"],
+            defaults=dict(value=answer_project_number["value"], section="1", order="1"),
+        )
+
+    # Update project attributes
+    data = dict(
+        date_start_actual=get_answer("start-date"),
+        date_end_actual=get_answer("end-date"),
+        is_public=False,
+        project_plan_summary=get_answer("summary"),
+        iati_status="2",  # Implementation status
+    )
+    for key, value in data.items():
+        if value is not None:
+            setattr(project, key, value)
+    project.save(update_fields=data.keys())
+
+    # Add reporting organisation
+    a4a = Organisation.objects.get(name="Aqua for All")
+    project.set_reporting_org(a4a)
+
+    # Create budget objects
+    BudgetItem.objects.filter(project=project).delete()
+    total = BudgetItemLabel.objects.get(label="Total")
+    BudgetItem.objects.create(
+        project=project, label=total, amount=get_answer("total-budget")
+    )
+
+    # Create location objects
+    ProjectLocation.objects.filter(location_target=project).delete()
+    project.primary_location = None
+    name = get_answer("country")
+    iso_code = COUNTRY_NAME_TO_ISO_MAP.get(name)
+    if iso_code:
+        country = custom_get_or_create_country(iso_code)
+        ProjectLocation.objects.create(location_target=project, country=country)
+    else:
+        print(f"Could not find iso code for {name}")
+
+    # Publish the project
+    project.publish()
+
+    return project
+
+
+class Command(BaseCommand):
+    help = "Import projects from Optimy for Aqua for All"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--project-id", type=str, help="ID of the project to import"
+        )
+
+    def handle(self, *args, **options):
+        project_id = options["project_id"]
+        if not project_id:
+            print("Fetching projects from Optimy")
+            projects = get_projects()
+        else:
+            projects = [dict(id=project_id)]
+
+        for project in projects:
+            project_id = project["id"]
+            answers = get_project_answers(project_id)
+            project = create_project(project_id, answers)
+            print(f"Imported {project_id} as {project.id} - {project.title}")
