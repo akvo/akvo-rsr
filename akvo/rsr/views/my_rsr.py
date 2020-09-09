akvo/akvo-rsr

commit 5624da962ba41f7ba3be91d88aea91fd69209bde

[#3238] results ui: Allow org admin to perform ME Manager actions
Closes #3238

akvo/rsr/views/my_rsr.py
@@ -545,7 +545,7 @@ def my_results(request, project_id, template='myrsr/my_results.html'):
     me_managers = project.publishing_orgs.employments().approved().\
         filter(group__in=[admins_group, me_managers_group])
     # Can we unlock and approve?
-    user_is_me_manager = user.is_superuser or user.is_admin or user.me_manager_for_project(project)
+    user_is_me_manager = user.has_perm('rsr.do_me_manager_actions')
     show_narrative_reports = project.partners.filter(
         id__in=settings.NARRATIVE_REPORTS_BETA_ORGS
     ).exists()

akvo/rsr/models/__init__.py
@@ -212,6 +212,7 @@ rules.add_perm('rsr.delete_indicatorlabel', is_rsr_admin | is_org_admin | is_org
 rules.add_perm('rsr.add_indicatorperiod', is_rsr_admin | is_org_admin | is_org_project_editor)
 rules.add_perm('rsr.change_indicatorperiod', is_rsr_admin | is_org_admin | is_org_project_editor)
 rules.add_perm('rsr.delete_indicatorperiod', is_rsr_admin | is_org_admin | is_org_project_editor)
+rules.add_perm('rsr.do_me_manager_actions', is_rsr_admin | is_org_admin | is_org_me_manager)
 
 rules.add_perm('rsr.view_indicatorperioddata', is_rsr_admin | is_org_admin | is_org_me_manager)
 rules.add_perm('rsr.add_indicatorperioddata', is_rsr_admin | is_org_admin | is_org_project_editor)

commit 4fe468668b16f729e5e273104e35c2616b378cb5

[#3810] Fix buggy is ME manager check for project
The bug was introduced in 5624da962ba41f7ba3be91d88aea91fd69209bde. The
call for `has_perm` was incorrect and didn't pass in the project as an
argument. So, any user who had M&E manager permissions on any project
was marked as M&E manager on every project they had access to. This
incorrectly gave the user M&E manager permissions on projects where they
are not supposed to have such permissions.

This bug was introduced in a commit that is quite old, but it didn't
manifest as a user visible problem until now, since it was not possible
to have different roles for users on projects, until the FAC feature was
built. Given that only Nuffic is using FAC currently, and they have
reported the problem, there shouldn't have been many instances of this
bug causing problems in the wild, yet.

akvo/rsr/views/my_rsr.py
@@ -199,7 +199,7 @@ def my_project(request, project_id, template='myrsr/my_project.html'):
     me_managers = project.publishing_orgs.employments().approved().\
         filter(group__in=[admins_group, me_managers_group])
     # Can we unlock and approve?
-    user_is_me_manager = user.has_perm('rsr.do_me_manager_actions')
+    user_is_me_manager = user.has_perm('rsr.do_me_manager_actions', project)
     show_narrative_reports = project.partners.filter(
         id__in=settings.NARRATIVE_REPORTS_BETA_ORGS
     ).exists() and user.has_perm('rsr.add_narrativereport', project)

akvo/rsr/tests/test_permissions.py
@@ -352,6 +352,7 @@ class PermissionsTestCase(BaseTestCase):
         for i, project in enumerate(self.projects):
             test = self.assertTrue if i == 0 else self.assertFalse
             test(user.has_perm('rsr.change_project', project))
+            test(user.has_perm('rsr.do_me_manager_actions', project))
 
             # Project Status
             self.assertFalse(user.has_perm('rsr.change_publishingstatus', project.publishingstatus))
@@ -561,6 +562,7 @@ class PermissionsTestCase(BaseTestCase):
             test = self.assertTrue if i == 0 else self.assertFalse
             test(user.has_perm('rsr.view_project', project))
             self.assertFalse(user.has_perm('rsr.change_project', project))
+            self.assertFalse(user.has_perm('rsr.do_me_manager_actions', project))
 
         # Can add indicator
         project = self.projects[0]