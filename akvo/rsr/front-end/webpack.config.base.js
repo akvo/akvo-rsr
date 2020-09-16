bd77f2d40e99a277fadf05daa717e039e530929b
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-09-16 17:09:10 +0530

    [#4280] Remove old user projects access page

--- akvo/rsr/front-end/webpack.config.base.js
+++ akvo/rsr/front-end/webpack.config.base.js
@@ -29,7 +29,6 @@ class MiniCssExtractPluginCleanup {
 const entry = {
     // "New React"
     results: "./scripts-src/my-results/app.js",
-    userProjects: "./scripts-src/user-projects-access/app.js",
     vendors: [
         // NOTE: babel-polyfill always needs to be loaded before react and redux
         // https://github.com/facebook/react/issues/8379#issuecomment-316346239
