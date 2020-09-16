832a4b02f2247d91b2352f77800b51a746eec856
Martin Christov <martin.christov@gmail.com>
2020-09-16 17:28:20 +0300

    [#4311] layout & basic ui

--- akvo/rsr/spa/app/modules/results/enumerator.jsx
+++ akvo/rsr/spa/app/modules/results/enumerator.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import './enumerator.scss'
+import { Collapse, Button, Icon, Form, Input } from 'antd'
+
+const { Panel } = Collapse
 
 const Enumerator = ({ results, id }) => {
   const [indicators, setIndicators] = useState([])
@@ -27,26 +30,60 @@ const Enumerator = ({ results, id }) => {
       <ul>
         {indicators.map(indicator =>
           <li>
-            <div className="check" />
+            <div className="check-holder">
+              <div className="check" />
+            </div>
             <h5>{indicator.title}</h5>
           </li>
         )}
       </ul>
       <div className="content">
-        {selected && (
-          <div className="indicator">
-            <AddUpdate period={selected.periods[0]} />
-          </div>
-        )}
+        {selected && [
+          selected.description &&
+          <p className="desc">
+            {selected.description}
+          </p>,
+          <Collapse destroyInactivePanel accordion>
+            {selected.periods.map(period =>
+              <Panel header={[<div><b>{period.periodStart}</b> - <b>{period.periodEnd}</b></div>, <Button type="primary" disabled>Submit</Button>]}>
+                <AddUpdate period={period} indicator={selected} />
+              </Panel>
+            )}
+          </Collapse>
+        ]}
       </div>
     </div>
   )
 }
 
-const AddUpdate = ({period}) => {
+const AddUpdate = ({period, indicator}) => {
   return (
     <div className="add-update">
-      <p>Add update for period: <b>{period.periodStart} - {period.periodEnd}</b></p>
+      <header>
+        {indicator.ascending ? [
+          <Icon type="rise" />, <b>Ascending</b>
+        ] : [
+          <Icon type="fall" />, <b>Descending</b>
+        ]}
+      </header>
+      <div className="inputs-container">
+        <div className="inputs">
+          {/* <h5>Value percentage</h5> */}
+          <Form aria-orientation="vertical">
+            <Form.Item label="Numerator">
+              <Input />
+            </Form.Item>
+            <Form.Item label="Denumerator">
+              <Input />
+            </Form.Item>
+          </Form>
+        </div>
+        <div className="prev-value">
+          <h5>your previous value update</h5>
+          <div className="date">3 sep 2020</div>
+          <div className="author">John Snow</div>
+        </div>
+      </div>
     </div>
   )
 }
