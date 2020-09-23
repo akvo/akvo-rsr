c9ecfa7177e6a277f07c38573bb31081c8c7c8dc
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-09-23 14:03:39 +0530

    Remove password prompt on test servers

--- akvo/rsr/front-end/scripts-src/classic/jsx/cookie.jsx
+++ akvo/rsr/front-end/scripts-src/classic/jsx/cookie.jsx
@@ -7,11 +7,7 @@ var i18nCookie,
     dropCookie,
     cookieDuration,
     cookieName,
-    cookieValue,
-    protectCookieDuration,
-    protectCookieName,
-    protectPassword,
-    testEnvironments;
+    cookieValue;
 
 function createDiv() {
     var cookie_text = i18nCookie.cookie_text;
@@ -34,169 +30,11 @@ function createDiv() {
     Cookies.set(cookieName, cookieValue, { expires: cookieDuration }); // Create the cookie
 }
 
-function createModal() {
-    var Modal = ReactBootstrap.Modal;
-    var Button = ReactBootstrap.Button;
-
-    var CookieModal = React.createClass({
-        getInitialState: function() {
-            return {
-                showModal: true,
-                passwordField: "",
-                showError: false
-            };
-        },
-
-        close: function() {
-            this.setState({
-                showModal: false
-            });
-        },
-
-        createProtectionCookie: function() {
-            Cookies.set(protectCookieName, cookieValue, { expires: protectCookieDuration });
-        },
-
-        checkPassword: function() {
-            this.setState({ showError: false });
-
-            if (this.state.passwordField === protectPassword) {
-                this.createProtectionCookie();
-                this.close();
-            } else {
-                this.setState({ showError: true });
-            }
-        },
-
-        handleChange: function(event) {
-            this.setState({ passwordField: event.target.value });
-        },
-
-        render: function() {
-            var errorNode,
-                formGroupClass = "form-group";
-
-            if (this.state.showError) {
-                errorNode = React.createElement(
-                    "span",
-                    {
-                        className: "help-block-error"
-                    },
-                    i18nCookie.incorrect_password
-                );
-                formGroupClass += " has-error";
-            } else {
-                errorNode = React.createElement("span");
-            }
-
-            return React.createElement(
-                Modal,
-                {
-                    autoFocus: true,
-                    backdrop: "static",
-                    bsSize: "large",
-                    keyboard: false,
-                    show: this.state.showModal,
-                    onHide: this.close
-                },
-                React.createElement(
-                    Modal.Header,
-                    {
-                        closeButton: false
-                    },
-                    React.createElement(Modal.Title, null, i18nCookie.password_environment)
-                ),
-                React.createElement(
-                    Modal.Body,
-                    null,
-                    i18nCookie.password_environment_desc,
-                    React.createElement("br"),
-                    React.createElement("br"),
-                    React.createElement(
-                        "form",
-                        {
-                            className: formGroupClass,
-                            onSubmit: this.checkPassword
-                        },
-                        React.createElement("input", {
-                            type: "password",
-                            value: this.state.passwordField,
-                            onChange: this.handleChange,
-                            className: "form-control",
-                            autoFocus: true
-                        })
-                    ),
-                    errorNode,
-                    React.createElement("br"),
-                    i18nCookie.password_environment_avail
-                ),
-                React.createElement(
-                    Modal.Footer,
-                    null,
-                    React.createElement(
-                        Button,
-                        {
-                            onClick: this.checkPassword,
-                            bsStyle: "success"
-                        },
-                        i18nCookie.continue_to_rsr
-                    )
-                )
-            );
-        }
-    });
-
-    ReactDOM.render(React.createElement(CookieModal), document.getElementById("cookieModal"));
-}
-
 function removeMe() {
     var element = document.getElementById("cookie-law");
     element.parentNode.removeChild(element);
 }
 window.removeMe = removeMe;
-
-var loadJS = function(url, implementationCode, location) {
-    // url is URL of external file, implementationCode is the code to be called from the file,
-    // location is the location to insert the <script> element
-
-    var scriptTag = document.createElement("script");
-    scriptTag.src = url;
-
-    scriptTag.onload = implementationCode;
-    scriptTag.onreadystatechange = implementationCode;
-
-    location.appendChild(scriptTag);
-};
-
-function loadAndRenderReact() {
-    function loadReactBootstrap() {
-        var reactBootstrapSrc = document.getElementById("react-bootstrap").src;
-        loadJS(reactBootstrapSrc, createModal, document.body);
-    }
-
-    function loadReactDOM() {
-        var reactDOMSrc = document.getElementById("react-dom").src;
-        loadJS(reactDOMSrc, loadReactBootstrap, document.body);
-    }
-
-    console.log("No React, load again.");
-    var reactSrc = document.getElementById("react").src;
-    loadJS(reactSrc, loadReactDOM, document.body);
-}
-
-function loadReactForModal() {
-    // Check if React is loaded
-    if (
-        typeof React !== "undefined" &&
-        typeof ReactDOM !== "undefined" &&
-        typeof ReactBootstrap !== "undefined"
-    ) {
-        // Render React components
-        createModal();
-    } else {
-        loadAndRenderReact();
-    }
-}
 
 document.addEventListener("DOMContentLoaded", function() {
     // Load global settings
@@ -205,21 +43,6 @@ document.addEventListener("DOMContentLoaded", function() {
     cookieDuration = 14; // Number of days before the cookie expires, and the banner reappears
     cookieName = "complianceCookie"; // Name of our cookie
     cookieValue = "on"; // Value of cookie
-    protectCookieDuration = 7; // Number of days before the protection cookie expires, and the modal reappears
-    protectCookieName = "protectCookie"; // Name of our protection cookie
-    protectPassword = "TesTing!"; // No need to encrypt this password, it's not vital
-    testEnvironments = ["test", "uat"]; // Set the test environments that need a password
-
-    // Check for a protection cookie on Test or UAT
-    if (Cookies.get(protectCookieName) !== cookieValue) {
-        var hostnameArray = window.location.hostname.split(".");
-        for (var i = 0; i < testEnvironments.length; i++) {
-            if (hostnameArray.indexOf(testEnvironments[i]) > -1) {
-                loadReactForModal();
-                break;
-            }
-        }
-    }
 
     // Check for general cookie
     if (Cookies.get(cookieName) !== cookieValue) {
