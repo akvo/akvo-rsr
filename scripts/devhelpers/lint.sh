c3ed997fa1720bebf894ec1ba8e30bbbf5b8a56a
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-09-23 11:16:32 +0530

    [#4326] Make it easy to lint only FE/BE code

--- scripts/devhelpers/lint.sh
+++ scripts/devhelpers/lint.sh
@@ -1,22 +1,50 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+MODE="${1---all}"
+LINT_BACKEND=
+LINT_FRONTEND=
+
+if [[ "${MODE}" == "--all" || "${MODE}" == "--backend" ]];
+then
+    LINT_BACKEND=1
+fi
+
+if [[ "${MODE}" == "--all" || "${MODE}" == "--frontend" ]];
+then
+    LINT_FRONTEND=1
+fi
+
 # Backend lint
-echo "Running backend lint"
-flake8 --ignore=E501,W503 --exclude=wsgi.py,scripts,migrations,__init__.py,node_modules,akvo/rsr/views/translations.py akvo/
-# Need environ to be set before other imports, etc. So, ignore E402
-flake8 --ignore=E501,E402,W503 akvo/scripts/
-flake8 --ignore=E501,E402,W503 akvo/wsgi.py
+function lint_backend {
+    echo "Running backend lint"
+    flake8 --ignore=E501,W503 --exclude=wsgi.py,scripts,migrations,__init__.py,node_modules,akvo/rsr/views/translations.py akvo/
+    # Need environ to be set before other imports, etc. So, ignore E402
+    flake8 --ignore=E501,E402,W503 akvo/scripts/
+    flake8 --ignore=E501,E402,W503 akvo/wsgi.py
+}
 
-# Frontend lint
-echo "Running frontend lint"
+function lint_frontend {
+    # Frontend lint
+    echo "Running frontend lint"
 
-pushd akvo/rsr/dir
-npm install
-npm run lint
-popd
+    pushd akvo/rsr/dir
+    npm install
+    npm run lint
+    popd
 
-pushd akvo/rsr/spa
-npm install
-npm run lint
-popd
+    pushd akvo/rsr/spa
+    npm install
+    npm run lint
+    popd
+}
+
+if [[ $LINT_BACKEND ]];
+then
+    lint_backend
+fi
+
+if [ $LINT_FRONTEND ];
+then
+    lint_frontend
+fi
