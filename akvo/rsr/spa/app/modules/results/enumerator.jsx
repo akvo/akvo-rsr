1cfced3db3f93ebe27751f07fac1e554eb65ecf3
Martin Christov <martin.christov@gmail.com>
2020-10-05 16:13:51 +0300

    [#4311] update with disaggregations

--- akvo/rsr/spa/app/modules/results/enumerator.jsx
+++ akvo/rsr/spa/app/modules/results/enumerator.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import './enumerator.scss'
-import { Collapse, Button, Icon, Form, Input, Divider, Upload } from 'antd'
+import { Collapse, Button, Icon, Form, Input, Divider, Upload, InputNumber } from 'antd'
 import { useTranslation } from 'react-i18next'
 import moment from 'moment'
 
 const { Panel } = Collapse
+const { Item } = Form
 
 const Enumerator = ({ results, id }) => {
   const [indicators, setIndicators] = useState([])
@@ -64,6 +65,16 @@ const Enumerator = ({ results, id }) => {
 const AddUpdate = ({period, indicator}) => {
   const { t } = useTranslation()
   console.log(indicator)
+  const dsgGroups = {}
+  period.disaggregationTargets.forEach(item => {
+    if (!dsgGroups[item.category]) dsgGroups[item.category] = []
+    dsgGroups[item.category].push(item)
+    // if (period.disaggregationTargets.length > 0) {
+    //   const target = period.disaggregationTargets.find(it => it.typeId === item.typeId)
+    //   if (target != null) dsgGroups[item.category][dsgGroups[item.category].length - 1].targetValue = target.value
+    // }
+  })
+  const dsgKeys = Object.keys(dsgGroups)
   return (
     <div className="add-update">
       <header>
@@ -77,6 +88,28 @@ const AddUpdate = ({period, indicator}) => {
       <div className="inputs-container">
         <div className="inputs">
           {/* <h5>Value percentage</h5> */}
+          {dsgKeys.map(dsgKey =>
+            <div className="dsg-group">
+              <div className="h-holder">
+                <h5>{dsgKey}</h5>
+              </div>
+              {dsgGroups[dsgKey].map(dsg => {
+                // const dsgIndex = update.disaggregations.findIndex(it => it.category === dsgKey && it.type === dsg.type)
+                // const value = dsgIndex > -1 ? update.disaggregations[dsgIndex].value : ''
+                return (
+                  <Item label={dsg.type}>
+                    <InputNumber
+                      formatter={val => String(val).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                      parser={val => val.replace(/(,*)/g, '')}
+                      // onChange={handleDsgValueChange(dsgKey, dsg.type)}
+                      // value={value}
+                    />
+                  </Item>
+                )
+              }
+              )}
+            </div>
+          )}
           <Form.Item label={indicator.measure === '2' ? 'Numerator' : 'Value'}>
             <Input />
           </Form.Item>
@@ -99,43 +132,74 @@ const AddUpdate = ({period, indicator}) => {
           ]}
         </div>
         {period.updates.length > 0 &&
-        ((update) => (
-        <div className="prev-value-holder">
-          <div className="prev-value">
-            <h5>previous value update</h5>
-            <div className="date">{moment(update.createdAt).format('DD MMM YYYY')}</div>
-            <div className="author">{update.userDetails.firstName} {update.userDetails.lastName}</div>
-            {indicator.measure === '1' &&
-            <div>
-              <div className="value">
-                {update.value}
-              </div>
-              {period.targetValue && [
-                <div className="target-cap">{(Math.round(((period.updates.reduce((acc, val) => acc + val.value, 0)) / period.targetValue) * 100 * 10) / 10)}% of target reached</div>
-              ]}
-            </div>
-            }
-            {indicator.measure === '2' &&
-            [
-            <div className="value-holder">
-              <div>
-                <div className="value">
-                  {(Math.round((update.numerator / update.denominator) * 100 * 10) / 10)}%
+        ((update) => {
+            const dsgGroups = {}
+            update.disaggregations.forEach(item => {
+              if (!dsgGroups[item.category]) dsgGroups[item.category] = []
+              dsgGroups[item.category].push(item)
+              if(period.disaggregationTargets.length > 0){
+                const target = period.disaggregationTargets.find(it => it.typeId === item.typeId)
+                if (target != null) dsgGroups[item.category][dsgGroups[item.category].length - 1].targetValue = target.value
+              }
+            })
+            const dsgKeys = Object.keys(dsgGroups)
+            // console.log(dsgGroups, period.disaggregationTargets)
+            return (
+              <div className="prev-value-holder">
+                <div className="prev-value">
+                  <h5>previous value update</h5>
+                  <div className="date">{moment(update.createdAt).format('DD MMM YYYY')}</div>
+                  <div className="author">{update.userDetails.firstName} {update.userDetails.lastName}</div>
+                  {indicator.measure === '1' &&
+                    <div>
+                      <div className="value">
+                        {update.value}
+                      </div>
+                      {(period.targetValue && dsgKeys.length === 0) && [
+                        <div className="target-cap">{(Math.round(((period.updates.reduce((acc, val) => acc + val.value, 0)) / period.targetValue) * 100 * 10) / 10)}% of target reached</div>
+                      ]}
+                      {dsgKeys.map(dsgKey => [
+                        <div className="dsg-group">
+                          <div className="h-holder">
+                            <h5>{dsgKey}</h5>
+                          </div>
+                          <ul>
+                          {dsgGroups[dsgKey].map((dsg) => [
+                            <li>
+                              <div className="label">{dsg.type}</div>
+                              <div>
+                                <b>{dsg.value}</b>
+                                {dsg.targetValue && <b> ({Math.round(((dsg.value / dsg.targetValue) * 100 * 10) / 10)}%)</b>}
+                              </div>
+                            </li>
+                          ])}
+                          </ul>
+                        </div>
+                      ])}
+                    </div>
+                  }
+                  {indicator.measure === '2' &&
+                    [
+                      <div className="value-holder">
+                        <div>
+                          <div className="value">
+                            {(Math.round((update.numerator / update.denominator) * 100 * 10) / 10)}%
+                          </div>
+                          <div className="target-cap">{(Math.round((update.value / period.targetValue) * 100 * 10) / 10)}% of target</div>
+                        </div>
+                        <div className="breakdown">
+                          <div className="cap">Numerator</div>
+                          <b>{update.numerator}</b>
+                          <div className="cap num">Denominator</div>
+                          <b>{update.denominator}</b>
+                        </div>
+                      </div>,
+                    ]
+                  }
                 </div>
-                <div className="target-cap">{(Math.round((update.value / period.targetValue) * 100 * 10) / 10)}% of target</div>
               </div>
-              <div className="breakdown">
-                <div className="cap">Numerator</div>
-                <b>{update.numerator}</b>
-                <div className="cap num">Denominator</div>
-                <b>{update.denominator}</b>
-              </div>
-            </div>,
-            ]
-            }
-          </div>
-        </div>
-        ))(period.updates[period.updates.length - 1])
+            )
+        })(period.updates.sort((a, b) => a.id - b.id)[period.updates.length - 1])
         }
       </div>
       <Divider />
