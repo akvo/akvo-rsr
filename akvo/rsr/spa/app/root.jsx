3ccfa89dcd5730f6e94caff568334ffe622b204b
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-10-13 19:28:56 +0530

    [#4311] Show stripped down header in enumerator form
    We hide the links to tabs and the back arrow to go back to the list of
    projects, in the enumerator web form view.

--- akvo/rsr/spa/app/root.jsx
+++ akvo/rsr/spa/app/root.jsx
@@ -59,7 +59,7 @@ const Root = ({ dispatch }) => {
             <Redirect to="/" />
           </Route>
           <Route path="/hierarchy/:projectId?" component={Hierarchy} />
-          <Route path="/projects/:id" component={Editor} />
+          <Route path="/projects/:id" render={({ match }) => <Editor simpleHeader={jwtView} match={match} />} />
           <Route path="/programs/:projectId" component={Program} />
           <Route path="/users" component={Users} />
           <Route path="/reports" component={Reports} />
