b3c806ec99da993c231bedb470c6de86946bd3d8
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-10-19 15:01:52 +0530

    [#4311] Allow enumerators to create period data comments

--- akvo/rsr/models/__init__.py
+++ akvo/rsr/models/__init__.py
@@ -260,7 +260,7 @@ rules.add_perm('rsr.change_disaggregation', is_rsr_admin | is_org_admin | is_org
 rules.add_perm('rsr.delete_disaggregation', is_rsr_admin | is_org_admin | is_org_me_manager_or_project_editor)
 rules.add_perm('rsr.view_disaggregation', is_org_enumerator)
 
-rules.add_perm('rsr.add_indicatorperioddatacomment', is_rsr_admin | is_org_admin | is_org_me_manager_or_project_editor)
+rules.add_perm('rsr.add_indicatorperioddatacomment', is_rsr_admin | is_org_admin | is_org_me_manager_or_project_editor | is_org_enumerator)
 rules.add_perm('rsr.change_indicatorperioddatacomment', is_rsr_admin | is_org_admin | is_org_me_manager_or_project_editor)
 rules.add_perm('rsr.delete_indicatorperioddatacomment', is_rsr_admin | is_org_admin | is_org_me_manager_or_project_editor)
 
