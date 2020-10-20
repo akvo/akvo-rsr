563330e5dddcdea1cbf4239d870e739802f9e759
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-10-20 13:26:56 +0530

    [#4348] Switch to using global exception handler

--- akvo/rest/views/related_project.py
+++ akvo/rest/views/related_project.py
@@ -4,12 +4,7 @@
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
-from rest_framework.response import Response
-from rest_framework import status
-
-
 from akvo.rsr.models import RelatedProject
-from akvo.rsr.models.related_project import ParentChangeDisallowed
 from ..serializers import RelatedProjectSerializer
 from ..viewsets import PublicProjectViewSet
 
@@ -19,15 +14,3 @@ class RelatedProjectViewSet(PublicProjectViewSet):
     """
     queryset = RelatedProject.objects.all()
     serializer_class = RelatedProjectSerializer
-
-    def update(self, request, *args, **kwargs):
-        try:
-            return super().destroy(request, *args, **kwargs)
-        except ParentChangeDisallowed as e:
-            return Response(str(e), status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
-    def destroy(self, request, *args, **kwargs):
-        try:
-            return super().destroy(request, *args, **kwargs)
-        except ParentChangeDisallowed as e:
-            return Response(str(e), status=status.HTTP_405_METHOD_NOT_ALLOWED)
