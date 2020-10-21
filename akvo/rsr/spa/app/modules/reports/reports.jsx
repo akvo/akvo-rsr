akvo/akvo-rsr

commit 3fd8fbf0d1adc8c0fba64d7eb3b11ca4ba9cb8a6

babel enable optional chaining + fix enumerator bug at reports

akvo/rsr/spa/app/modules/reports/reports.jsx
@@ -39,9 +39,7 @@ const Reports = ({programId, projectId, userRdr}) => {
   const [downloading, setDownloading] = useState(false)
   const [{ results: reports = [] }, loading] = useFetch(programId ? `/program_reports/${programId}/` : projectId ? `/project/${projectId}/reports/` : '/organisation_reports/')
   useEffect(() => {
-    if (userRdr && userRdr.organisations && userRdr.organisations.length > 0) {
-      setCurrentOrg(userRdr.organisations[0].id)
-    }
+    setCurrentOrg(userRdr?.organisations[0]?.id)
   }, [userRdr])
   const orgs = userRdr && userRdr.organisations ? userRdr.organisations : []
   if (!programId && !projectId) {

akvo/rsr/spa/.babelrc
@@ -7,6 +7,7 @@
   "plugins": [
     "react-hot-loader/babel",
     "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-syntax-dynamic-import"
+    "@babel/plugin-syntax-dynamic-import",
+    "@babel/plugin-proposal-optional-chaining"
   ]
 }

commit 2207de8a07af6159267084c495556751ec8a5c55

reports.jsx: Fix broken optional chaining rule
The buggy optional chaining rule was introduced in
3fd8fbf0d1adc8c0fba64d7eb3b11ca4ba9cb8a6. This commit fixes it.

akvo/rsr/spa/app/modules/reports/reports.jsx
@@ -39,7 +39,7 @@ const Reports = ({programId, projectId, userRdr}) => {
   const [downloading, setDownloading] = useState(false)
   const [{ results: reports = [] }, loading] = useFetch(programId ? `/program_reports/${programId}/` : projectId ? `/project/${projectId}/reports/` : '/organisation_reports/')
   useEffect(() => {
-    setCurrentOrg(userRdr?.organisations[0]?.id)
+    setCurrentOrg(userRdr?.organisations?.[0]?.id)
   }, [userRdr])
   const orgs = userRdr && userRdr.organisations ? userRdr.organisations : []
   if (!programId && !projectId) {