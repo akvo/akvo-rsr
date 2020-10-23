6e1c9b0a59df3a4c8f99214fa45f892d93803ff3
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-10-23 12:39:35 +0200

    [#4357] Update deploy script to send Zulip notifications

--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -3,4 +3,4 @@
 git fetch
 
 docker run --rm -e ZULIP_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
-  -it akvo/akvo-devops:20200810.101253.a1dbc6a promote-test-to-prod.sh rsr rsr-version akvo-rsr zulip
+  -it akvo/akvo-devops:20201023.101935.7dacd92 promote-test-to-prod.sh rsr rsr-version akvo-rsr zulip rsr
