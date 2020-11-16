ec12878b8d9ae1ccccabafd47272ef030574b6dd
Martin Christov <martin.christov@gmail.com>
2020-11-16 12:37:42 +0200

    [#4375] api remove projectEditorChange

--- akvo/rsr/spa/app/utils/api.js
+++ akvo/rsr/spa/app/utils/api.js
@@ -37,7 +37,7 @@ const getConfigWithTransform = (transform) => {
 
 export default {
   get: (url, params, transform, cancelToken) => axios({url, ...{...getConfigWithTransform(transform), params}, cancelToken}),
-  post: (url, data, transform, cancelToken, projectEditorChange) => axios({ url, method: 'POST', data: typeof data === 'object' ? { ...data, projectEditorChange } : data, ...getConfigWithTransform(transform), cancelToken}),
-  patch: (url, data, transform, cancelToken, projectEditorChange) => axios({ url, method: 'PATCH', data: { ...data, projectEditorChange }, ...getConfigWithTransform(transform), cancelToken}),
-  delete: (url, projectEditorChange) => axios({ url, method: 'DELETE', data: {projectEditorChange}, ...config })
+  post: (url, data, transform, cancelToken) => axios({ url, method: 'POST', data: typeof data === 'object' ? data : data, ...getConfigWithTransform(transform), cancelToken}),
+  patch: (url, data, transform, cancelToken) => axios({ url, method: 'PATCH', data, ...getConfigWithTransform(transform), cancelToken}),
+  delete: (url) => axios({ url, method: 'DELETE', ...config })
 }
