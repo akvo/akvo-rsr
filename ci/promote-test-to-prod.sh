6521d9229f9a05530e8f058d17f9c75a60aa3b69
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-11-18 13:57:23 +0100

    Fix promote script by adding Zulip channel to arg list

--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -3,4 +3,4 @@
 git fetch
 
 docker run --rm -e ZULIP_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
-  -it akvo/akvo-devops:20201110.135154.8d39c9c promote-test-to-prod.sh rsr rsr-version akvo-rsr zulip
+  -it akvo/akvo-devops:20201110.135154.8d39c9c promote-test-to-prod.sh rsr rsr-version akvo-rsr zulip rsr
