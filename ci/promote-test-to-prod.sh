df53b6c7decc7693841d748951d6fb6832b0d9bc
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-12-03 15:00:11 +0530

    Use latest devops image
    [ci skip]

--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -3,4 +3,4 @@
 git fetch
 
 docker run --rm -e ZULIP_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
-  -it akvo/akvo-devops:20201110.135154.8d39c9c promote-test-to-prod.sh rsr rsr-version akvo-rsr zulip rsr
+  -it akvo/akvo-devops:20201203.085214.79bec73 promote-test-to-prod.sh rsr rsr-version akvo-rsr zulip rsr
