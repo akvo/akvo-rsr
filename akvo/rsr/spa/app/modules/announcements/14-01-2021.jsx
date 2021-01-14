6aa08ec555fab651cf1f8709dc9dc7c9c0b7d500
Martin Christov <martin.christov@gmail.com>
2021-01-14 11:46:19 +0300

    new Results tab announcement improvement

--- akvo/rsr/spa/app/modules/announcements/14-01-2021.jsx
+++ akvo/rsr/spa/app/modules/announcements/14-01-2021.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Divider } from 'antd'
 import { Link } from 'react-router-dom'
+import api from '../../utils/api'
 
-export default ({ close }) => (
-  <div>
-    <h3>Improved Results tab</h3>
-    <p>An easier way to manage your results data and report on your indicators in now available!</p>
-    <Link to="/"><Button type="primary" onClick={close}>Check it out in a published project</Button></Link>
-  </div>
-)
+export default ({ close }) => {
+  const [project, setProject] = useState(null)
+  useEffect(() => {
+    api.get('my_projects/?limit=1&filter={"publishingstatus__status":"published"}')
+    .then((d) => {
+      if(d?.data?.results.length > 0) {
+        setProject(d.data.results[0])
+      }
+    })
+  }, [])
+  return (
+    <div>
+      <h3>Improved Results tab</h3>
+      <p>An easier way to manage your results data and report on your indicators in now available!</p>
+      {project != null &&
+        <Link to={`/projects/${project.id}/results`}><Button type="primary" onClick={close}>Check it out</Button></Link>
+      }
+    </div>
+  )
+}
