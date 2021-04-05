16c5e111f5edb0f0dac697e0983b1c482c9720ae
zuhdil <zuhdil@gmail.com>
2021-04-05 17:44:32 +0700

    [#4476] program approvals endpoint

--- akvo/rest/urls.py
+++ akvo/rest/urls.py
@@ -275,6 +275,11 @@ urlpatterns += (
         views.add_project_to_program,
         name='add_project_to_program'),
 )
+urlpatterns += (
+    url(r'v1/program/(?P<program_pk>[0-9]+)/approvals/$',
+        views.get_program_period_updates_for_approvals,
+        name='get_program_period_updates_for_approvals'),
+)
 
 
 # Typeahead
