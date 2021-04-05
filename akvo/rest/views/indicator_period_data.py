8332eee915a3e242653b8412b904a0e3f73284ee
zuhdil <zuhdil@gmail.com>
2021-04-05 20:08:06 +0700

    [#4476] change the response data structure

--- akvo/rest/views/indicator_period_data.py
+++ akvo/rest/views/indicator_period_data.py
@@ -268,28 +268,12 @@ def get_program_period_updates_for_approvals(request, program_pk):
             'countries': [loc.country.iso_code for loc in project.locations.all() if loc.country]
         }
 
-    pending_updates_view = []
-    for update in pending_updates:
-        u = update.copy()
-        u.update({
-            'period': periods[u['period']],
-            'indicator': indicators[u['indicator']],
-            'result': results[u['result']],
-            'project': projects[u['project']],
-        })
-        pending_updates_view.append(u)
-
     data = {
-        'periods': [
-            {
-                'id': p['id'],
-                'period_start': p['period_start'],
-                'period_end': p['period_end'],
-                'locked': p['locked'],
-                'project': projects[p['project']]
-            }
-            for p in periods.values()
-        ],
-        'pending_updates': pending_updates_view
+        'pending_updates': pending_updates,
+        'periods': periods,
+        'indicator': indicators,
+        'results': results,
+        'projects': projects,
     }
+
     return Response(data)
