78ed28cbbf38346c3c7086c06ea20ec31831c0dc
Martin Christov <martin.christov@gmail.com>
2021-04-07 10:23:14 +0300

    [#4400] periods styling

--- akvo/rsr/spa/app/modules/approvals/approvals.jsx
+++ akvo/rsr/spa/app/modules/approvals/approvals.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { Icon } from 'antd'
+import React, { useEffect } from 'react'
+import api from '../../utils/api'
+import './styles.scss'
 
-const Approvals = () => {
+const Approvals = ({ params }) => {
+  useEffect(() => {
+    api.get(`/program/${params.id}/approvals`)
+    .then(({ data }) => {
+      //
+    })
+  }, [])
   return (
     <div className="approvals">
-      <h2>Approvals</h2>
+      <h4>Period locking</h4>
+      <div className="periods">
+        <ul>
+          <li>
+            <div className="label">period</div>
+            <b>1 Mar 2021 - 1 Aug 2021</b>
+            <div className="status unlocked">
+              <Icon type="unlock" />
+              unlocked for all projects
+            </div>
+          </li>
+          <li>
+            <div className="label">period</div>
+            <b>1 Mar 2021 - 1 Aug 2021</b>
+            <div className="status locked">
+              <Icon type="lock" />
+              locked for all projects
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="pending">
+        <h4>Pending approval</h4>
+        <div className="filters">
+          Author, Project name, project location
+        </div>
+      </div>
     </div>
   )
 }
