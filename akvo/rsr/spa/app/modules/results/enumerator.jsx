202c19401b164d4d6654603622adf2251b742e66
Martin Christov <martin.christov@gmail.com>
2021-04-07 12:19:18 +0300

    enum form fix indicator measure returned for revision

--- akvo/rsr/spa/app/modules/results/enumerator.jsx
+++ akvo/rsr/spa/app/modules/results/enumerator.jsx
@@ -208,7 +208,7 @@ const AddUpdate = ({ period, indicator, addUpdateToPeriod, patchUpdateInPeriod, 
     setFileSet([])
   }, [period])
   const draftUpdate = period.updates.find(it => it.status === 'D')
-  const pendingUpdate = (period.updates[0]?.status === 'P' || indicator.measure === '2'/* trick % measure update to show as "pending update" */) ? period.updates[0] : null
+  const pendingUpdate = (period.updates[0]?.status === 'P' || (indicator.measure === '2' && period.updates[0]?.status !== 'R')/* trick % measure update to show as "pending update" */) ? period.updates[0] : null
   const recentUpdate = /* in the last 12 hours AND NOT returned for revision */ period.updates.filter(it => it.status !== 'R').find(it => { const minDiff = (new Date().getTime() - new Date(it.lastModifiedAt).getTime()) / 60000; return minDiff < 720 })
   // the above is used for the M&E view bc their value updates skip the "pending" status
   const submittedUpdate = pendingUpdate || recentUpdate
