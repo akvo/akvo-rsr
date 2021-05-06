a451f52824de24bea0c656b11d99f3c2f781d1b2
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-05-06 16:23:54 +0530

    [#4524] Delete old vagrant helper scripts

--- scripts/devhelpers/cleanup_etc_hosts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TMP_FILE=`mktemp /tmp/tmphosts`
-sed '/# localdev_rsr/d' /etc/hosts > $TMP_FILE
-sudo mv $TMP_FILE /etc/hosts
--- scripts/devhelpers/setup_etc_hosts.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "This script will edit /etc/hosts and therefore requires your administrator password."
-sudo bash -c 'echo "192.168.50.101 rsr.localdev.akvo.org   # localdev_rsr" >> /etc/hosts'
