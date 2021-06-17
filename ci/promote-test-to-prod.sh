8cb08c8ce12e636394314269de99892a3a017d7a
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-06-17 17:52:57 +0530

    Change stream name to notify for releases

--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -3,4 +3,4 @@
 git fetch
 
 docker run --rm -e ZULIP_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
-  -it akvo/akvo-devops:20201203.085214.79bec73 promote-test-to-prod.sh rsr rsr-version akvo-rsr zulip "Everest Engine"
+  -it akvo/akvo-devops:20201203.085214.79bec73 promote-test-to-prod.sh rsr rsr-version akvo-rsr zulip "Akvo RSR Engine"
