779346ebdd97656492f051251fdf9bfff093876a
Michael <michael@akvo.org>
2021-10-11 15:47:13 +0200

    chore: Fix make-and-restore-production-dump.sh for clean envs
    Fresh environments (linux at least) don't have all required directories or rights for said directories.
    The default user had ID 1000 and the default gcloud image user has ID 100 --> no write rights
    It's thus impossible for the user to write to the mounted volumes.
    
    Additionally, access was given to **all** of `~/.config` which can be a security issue.
    It's one of the default XDG configuration directories so many applications
     will store their config there and some might store unencrypted secrets.
    
    Thus, this uses directories local to the project and keeps old backups, just in case.
    
    #4650 Cannot dump and restore prod on fresh install

--- scripts/data/helper/make-gcp-dump.sh
+++ scripts/data/helper/make-gcp-dump.sh
@@ -10,5 +10,9 @@ fi
 gcloud config set project akvo-lumen
 
 gcloud sql export sql rsr-prod-database "gs://akvo-rsr-db-dump/${dump_file}" --database=rsr_db
-gsutil cp "gs://akvo-rsr-db-dump/$dump_file" /data/db_dump.gz
+gsutil cp "gs://akvo-rsr-db-dump/$dump_file" /data/$dump_file
 gsutil rm "gs://akvo-rsr-db-dump/$dump_file"
+
+# Point db_dump to the last dump
+cd /data
+ln -nfs $dump_file db_dump.gz
