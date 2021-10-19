dcae7172d646e8f3a8dde1b347b572a4baa0b897
Michael <michael@akvo.org>
2021-10-19 15:52:01 +0200

    #4660 Split web service for local environment
    Before, the web service started 3 different services (2 FE and 1BE).
    That's now split for the dev environment, which allows:
    
     - starting or restarting services separately
     - reading the logs of each separate service
    
    In order to do so:
    
     - `start-node-app.sh` was refactored out of `start-django.sh`
     - `docker-compose.yml`s user 3.7 in order to make use of `x-...` YAML blocks
       to reuse blocks of YAML

--- scripts/docker/dev/start-django.sh
+++ scripts/docker/dev/start-django.sh
@@ -1,57 +1,21 @@
 #!/usr/bin/env bash
-
+# Prepare and start the django server
 set -eu
-
-_term() {
-  echo "Caught SIGTERM signal!"
-  kill -TERM "$child" 2>/dev/null
-}
-
-trap _term SIGTERM
 
 ./scripts/docker/dev/wait-for-dependencies.sh
 
-if [ -z "${IS_REPORTS_CONTAINER:-}" ]; then
-  pushd akvo/rsr/front-end
+pushd akvo/rsr/front-end
   if [[ ! -d "node_modules" ]]; then
     npm install
   fi
   if [[ ! -f "static/rsr/dist/vendors.js" ]]; then
     npm run dev
   fi
-  popd
+popd
 
-  pushd akvo/rsr/spa
-  (
-      npm install
-      npm start
-  ) &
-  popd
+SKIP_REQUIRED_AUTH_GROUPS=true python manage.py migrate --noinput
+SKIP_REQUIRED_AUTH_GROUPS=true python manage.py createcachetable || true
 
-  pushd akvo/rsr/dir
-  (
-      npm install
-      npm start
-  ) &
-  popd
-fi
+python manage.py populate_local_db
 
-if [ -z "${IS_REPORTS_CONTAINER:-}" ]; then
-    SKIP_REQUIRED_AUTH_GROUPS=true python manage.py migrate --noinput
-    SKIP_REQUIRED_AUTH_GROUPS=true python manage.py createcachetable || true
-fi
-#python manage.py collectstatic
-
-## Not running cron jobs in dev
-#python manage.py crontab add
-#env >> /etc/environment
-#/usr/sbin/cron
-
-if [ -z "${IS_REPORTS_CONTAINER:-}" ]; then
-    python manage.py populate_local_db
-fi
-
-python manage.py runserver 0.0.0.0:${DJANGO_PORT:-8000} &
-
-child=$!
-wait "$child"
+python manage.py runserver 0.0.0.0:${DJANGO_PORT:-8000}
