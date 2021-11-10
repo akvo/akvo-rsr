a9f9bb132fc90317d9efd7c5c5b3b5d89586915c
Michael <michael@akvo.org>
2021-11-10 10:47:47 +0100

    chore(upgrade): fix reset password url generation
    The second part of the token was longer (probably more bits are being used now).
    
    #4658 Upgrade to django 3.2

--- akvo/urls.py
+++ akvo/urls.py
@@ -102,7 +102,7 @@ urlpatterns = i18n_patterns(
     url(r'^sign_out/$',
         account.sign_out, name='sign_out'),
 
-    url(r'^reset_password/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    url(r'^reset_password/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})/$',
         auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm',
         kwargs={'set_password_form': RSRSetPasswordForm}),
 
