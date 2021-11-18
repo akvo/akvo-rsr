1b13b583faddf907aafcae00c2327df43c0ad6fd
zuhdil <zuhdil@gmail.com>
2021-11-18 08:10:57 +0700

    [#2] [WIP] Run puppeteer E2E tests on CI

---
+++ akvo/rsr/e2e/scripts/wait-for-host.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const isReachable = require('is-reachable');
+const { exit } = require('process')
+
+const RSR_DOMAIN = process.env.RSR_DOMAIN || 'http://localhost';
+
+(async () => {
+  const isReady = await isReachable(RSR_DOMAIN, {timeout: 90 * 1000});
+  if (isReady) {
+    console.log('Site reachable');
+  } else {
+    console.error('Site unreachable');
+    exit(1)
+  }
+})();
