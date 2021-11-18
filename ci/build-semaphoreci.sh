6f710d0b031dc061fdbd31a861dbea8d062078eb
zuhdil <zuhdil@gmail.com>
2021-11-18 08:10:57 +0700

    [WIP] Run puppeteer E2E tests on CI

--- ci/build-semaphoreci.sh
+++ ci/build-semaphoreci.sh
@@ -38,28 +38,29 @@ if [ -z "$CI_COMMIT" ]; then
     export CI_COMMIT=local
 fi
 
-log Login to DockerHub
-docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+if [[ ! "${SKIP_DOCKER_PUSH:-}" = yes ]]; then
+  log Login to DockerHub
+  docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+fi
 
 docker_build akvo/rsr-backend-dev -t rsr-backend:dev -f Dockerfile-dev .
 
-log Starting docker-compose
-docker-compose -p rsrci -f docker-compose.yaml -f docker-compose.ci.yaml up -d --build
 
 if [[ ! "${SKIP_BACKEND_TESTS:-}" = yes ]]; then
+  log Starting docker-compose
+  docker-compose -p rsrci -f docker-compose.yaml -f docker-compose.ci.yaml up -d --build
+
   log Running tests
-  docker-compose -p rsrci -f docker-compose.yaml -f docker-compose.ci.yaml run web scripts/docker/dev/run-as-user.sh scripts/docker/ci/build.sh
+  docker-compose -p rsrci -f docker-compose.yaml -f docker-compose.ci.yaml exec -T web scripts/docker/dev/run-as-user.sh scripts/docker/ci/build.sh
 fi
 
-
-#log Stopping docker-compose
-#docker-compose -p rsrci -f docker-compose.yaml -f docker-compose.ci.yaml down
 
 log Preparing deploy info file
 echo "DEPLOY_COMMIT_FULL_ID = '`git rev-parse HEAD`'" > ._66_deploy_info.conf
 echo "DEPLOY_COMMIT_ID = '`git rev-parse --short HEAD`'" >> ._66_deploy_info.conf
 echo "DEPLOY_BRANCH = '$CI_BRANCH'" >> ._66_deploy_info.conf
 
+docker_build akvo/rsr-puppeteer-dev -t rsr-puppeteer:dev -f Dockerfile-e2e .
 docker_build akvo/rsr-backend-prod-no-code -f Dockerfile-prod-no-code .
 
 log Creating Production Backend image with code
@@ -76,8 +77,10 @@ docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/rsr-nginx:${CI_COMMIT} -f D
 log Starting docker-compose for end to end tests
 touch "log_docker_compose_ci_prod"
 docker-compose -p rsrciprod -f docker-compose.yaml -f docker-compose.ci.yaml -f docker-compose.ci.prod.images.yaml up -d --build
+docker-compose -p rsrciprod -f docker-compose.yaml -f docker-compose.ci.yaml -f docker-compose.ci.prod.images.yaml ps
 log Running end to end tests
-docker-compose -p rsrciprod -f docker-compose.yaml -f docker-compose.ci.yaml -f docker-compose.ci.prod.images.yaml run --no-deps web scripts/docker/dev/run-as-user.sh scripts/docker/ci/end-to-end.sh
+docker-compose -p rsrciprod -f docker-compose.yaml -f docker-compose.ci.yaml -f docker-compose.ci.prod.images.yaml exec -T web scripts/docker/dev/run-as-user.sh scripts/docker/ci/end-to-end.sh
+docker-compose -p rsrciprod -f docker-compose.yaml -f docker-compose.ci.yaml -f docker-compose.ci.prod.images.yaml exec -T puppeteer npm run test -- --ci
 rm "log_docker_compose_ci_prod"
 
 log Done
