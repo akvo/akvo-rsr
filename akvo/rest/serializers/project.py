2eb439a3a99a8c7ff46baeeaa16ea2d97fafa56e
Michael <michael@akvo.org>
2021-12-07 13:24:22 +0100

    test: fix ProjectPostTestCase.test_custom_fields_added
    The serializer was failing because it was expecting path input, but that's generated depending on the UUID (which is readonly too)
    
    #4710: Improve project hierarchies

--- akvo/rest/serializers/project.py
+++ akvo/rest/serializers/project.py
@@ -78,6 +78,8 @@ class ProjectSerializer(BaseRSRSerializer):
     program = serializers.SerializerMethodField()
     targets_at = TargetsAtField(choices=Project.TARGETS_AT_OPTION, required=False)
     iati_profile_url = serializers.SerializerMethodField()
+    path = serializers.SerializerMethodField()
+    uuid = serializers.ReadOnlyField()
 
     class Meta:
         model = Project
@@ -129,6 +131,9 @@ class ProjectSerializer(BaseRSRSerializer):
 
     def get_iati_profile_url(self, obj):
         return obj.get_iati_profile_url()
+
+    def get_path(self, project: Project):
+        return str(project.path)
 
 
 class ProjectDirectorySerializer(serializers.ModelSerializer):
