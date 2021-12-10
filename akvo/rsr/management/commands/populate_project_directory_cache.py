25b91ded76dd29781e07413852493b4ceb71dcf1
zuhdil <zuhdil@gmail.com>
2021-12-10 11:16:20 +0700

    Quick fix project directory

--- akvo/rsr/management/commands/populate_project_directory_cache.py
+++ akvo/rsr/management/commands/populate_project_directory_cache.py
@@ -12,11 +12,10 @@ Usage:
     python manage.py populate_project_directory_cache
 
 """
-
+from time import time
 from django.core.management.base import BaseCommand
 
-from akvo.rest.views.project import serialized_project
-from akvo.rest.cache import delete_project_from_project_directory_cache
+from akvo.rest.cache import serialized_project, delete_project_from_project_directory_cache
 from akvo.rsr.models import Project
 
 
@@ -26,6 +25,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         projects = Project.objects.published().values_list('pk', flat=True)
 
+        start = time()
+        print(f"Populating project directory cache with {projects.count()} projects")
         for project_id in projects:
+            print(f"project {project_id}", end=' ', flush=True)
+            project_start = time()
             delete_project_from_project_directory_cache(project_id)
+            print(f"{time() - project_start}", end=' ', flush=True)
             serialized_project(project_id)
+            print(f"{time() - project_start}")
+        print(f"total: {time() - start}")
