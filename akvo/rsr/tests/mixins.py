ce9610da8810ad42f8a8e0611f2d1682337aa450
Michael <michael@akvo.org>
2021-12-16 17:24:54 +0100

    feat: Add AkvoTreeModel.descendants(max_depth=...)
    In order to test the abstract Model, stackoverflow provided a solution of creating a temporary table.
    This uncovered a problem in test_results_framework.py where a child project should still have existed.
    
    Subclasses of AkvoTreeModel now also don't have to set the path manually as a signal does it for all subclasses.
    
    #4710: Improve project hierarchies

---
+++ akvo/rsr/tests/mixins.py
@@ -0,0 +1,39 @@
+from django.db import ProgrammingError, connection
+from django.db.models.base import ModelBase
+from django.test import TestCase
+
+
+class ModelMixinTestCase(TestCase):
+    """
+    Base class for tests of model mixins. To use, subclass and specify the
+    mixin class variable. A model using the mixin will be made available in
+    self.model
+
+    Adapted from from https://stackoverflow.com/a/51146819
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        # Create a dummy model which extends the mixin
+        cls.model = ModelBase(
+            '__TestModel__' + cls.mixin.__name__,
+            (cls.mixin,),
+            {'__module__': cls.mixin.__module__}
+        )
+
+        # Create the schema for  our test model
+        try:
+            with connection.schema_editor() as schema_editor:
+                schema_editor.create_model(cls.model)
+        except ProgrammingError as error:
+            # --keepdb leaves the existing table intact
+            if "already exists" not in str(error):
+                raise
+        super(ModelMixinTestCase, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        # Delete the schema for the test model
+        with connection.schema_editor() as schema_editor:
+            schema_editor.delete_model(cls.model)
+        super(ModelMixinTestCase, cls).tearDownClass()
