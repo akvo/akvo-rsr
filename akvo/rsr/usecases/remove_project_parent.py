9c67bbf0e7e2ea87a2ade6737b03ad99a0d1587c
Michael <michael@akvo.org>
2021-12-16 09:17:54 +0100

    refactor: extract common TreeModel code from Project
    More hierarchies exist that would benefit from the AkvoTreeModel (organisations for example).
    
    #4710: Improve project hierarchies

--- akvo/rsr/usecases/remove_project_parent.py
+++ akvo/rsr/usecases/remove_project_parent.py
@@ -3,11 +3,9 @@
 # Akvo Reporting is covered by the GNU Affero General Public License.
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
-from uuid import UUID
 
-from akvo.rsr.models import Project, Result
-from akvo.rsr.models.related_project import ParentChangeDisallowed
-from akvo.rsr.usecases.utils import RF_MODELS_CONFIG
+from akvo.rsr.models import Project
+from akvo.rsr.usecases.change_project_parent import RF_MODELS_CONFIG
 
 
 def remove_parent(project: Project):
@@ -18,27 +16,4 @@ def remove_parent(project: Project):
     for (model, parent_attr, project_relation, _) in RF_MODELS_CONFIG.values():
         model.objects.filter(**{project_relation: project}).update(**{parent_attr: None})
 
-    check_child_results(project.uuid, project.get_parent_uuid())
-
-    # Append project to list in order to modify known reference / current object in memory
-    # Otherwise it would be a new object
-    sub_tree = list(project.descendants(with_self=False)) + [project]
-    old_path = project.path
-
-    # Remove old parent from subtree paths
-    path_to_cut = len(old_path[:-1])
-    for node in sub_tree:
-        node.path = node.path[path_to_cut:]
-
-    Project.objects.bulk_update(sub_tree, ["path"])
-
-
-def check_child_results(project_uuid: UUID, parent_project_uuid: UUID):
-    """
-    Ensure that a project doesn't have results which were imported from a parent
-    """
-    project_results = Result.objects.filter(project__uuid=project_uuid)
-    child_results = project_results.filter(parent_result__project__uuid=parent_project_uuid)
-    if child_results.exists():
-        raise ParentChangeDisallowed()
-
+    project.delete_parent(force=True)
