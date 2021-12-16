f8d6091f58cb31f2392f8beea0eddf78f02ffae1
Michael <michael@akvo.org>
2021-12-16 09:17:54 +0100

    refactor: extract common TreeModel code from Project
    More hierarchies exist that would benefit from the AkvoTreeModel (organisations for example).
    
    #4710: Improve project hierarchies

--- akvo/rsr/tests/models/test_project.py
+++ akvo/rsr/tests/models/test_project.py
@@ -11,10 +11,12 @@ from django.db import models
 from django.test import TestCase
 
 from akvo.rsr.factories.project import ProjectFactory
-from akvo.rsr.models.project import TreeWillBreak
+from akvo.rsr.models import (
+    BudgetItem, Organisation, OrganisationCodelist, OrganisationIndicatorLabel, Partnership,
+    Project, ProjectUpdate,
+)
+from akvo.rsr.models.tree.errors import NodesWillBeOrphaned
 from akvo.rsr.tests.base import BaseTestCase
-from akvo.rsr.models import BudgetItem, Partnership, Project, ProjectUpdate, Organisation, \
-    OrganisationIndicatorLabel, RelatedProject, OrganisationCodelist, IatiExport
 
 
 class ProjectModelTestCase(BaseTestCase):
@@ -186,7 +188,7 @@ class ProjectModelTestCase(BaseTestCase):
         self.assertFalse(org2.use_project_roles)
         self.assertTrue(project.use_project_roles)
 
-    def test_reset_path_with_children(self):
+    def test_delete_parent_with_children(self):
         """
         In a project hierarchy with children, it shouldn't be possible to just remove a parent
 
@@ -199,8 +201,8 @@ class ProjectModelTestCase(BaseTestCase):
         subchild = ProjectFactory(title="subchild")
         subchild.set_parent(child, True).save()
 
-        with self.assertRaises(TreeWillBreak):
-            child.reset_path()
+        with self.assertRaises(NodesWillBeOrphaned):
+            child.delete_parent()
 
 
 class ProjectDeleteTestCase(BaseTestCase):
