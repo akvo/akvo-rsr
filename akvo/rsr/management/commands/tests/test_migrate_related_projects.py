2c570f37ffd9f6d1ce469543832c58e631c4687c
Michael <michael@akvo.org>
2022-01-05 15:55:17 +0100

    test: migrate siblings in multiple groups
    The usecase was possible, but it was difficult to trigger, so the RelatedProjects are now sorted by ID.
    We need to know that the usecase is possible.
    
    It was also refactored to make the code easier to read. Hopefully.
    
    #4710: Improve project hierarchies

--- akvo/rsr/management/commands/tests/test_migrate_related_projects.py
+++ akvo/rsr/management/commands/tests/test_migrate_related_projects.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import factory
 from django.test import TestCase
 
@@ -7,7 +9,7 @@ from akvo.rsr.models import Project, RelatedProject
 
 
 class MigrateSiblingsTest(TestCase):
-    def create_siblings(self, sibling_count):
+    def create_siblings(self, sibling_count) -> List[Project]:
         siblings = ProjectFactory.create_batch(sibling_count, title=factory.Sequence(lambda n: 'child%d' % n))
         sibling_rp = []
         # iterate i, i+1
@@ -37,7 +39,7 @@ class MigrateSiblingsTest(TestCase):
 
     def test_multiple_parents(self):
         """
-        A group of sibling have multiple parents, which a problem, shouldn't get new parents
+        A group of sibling have multiple parents, which is a problem, shouldn't get new parents
         """
         siblings = self.create_siblings(10)
         # Two parents in the group
@@ -54,4 +56,35 @@ class MigrateSiblingsTest(TestCase):
                     for project in Project.objects.filter(path__match="*{2,}"))
                 ),
             2
+        )
+
+    def test_multiple_groups(self):
+        """
+        Two groups of siblings have one sibling that is in both groups
+
+        The two groups should be merged as they are all siblings.
+        """
+        siblings1 = self.create_siblings(5)
+        siblings2 = self.create_siblings(5)
+
+        siblings1[0].set_parent(Project.objects.create(title="Group 1 Parent")).save()
+
+        RelatedProject.objects.create(
+            project=siblings1[-1],
+            related_project=siblings2[-1],
+            relation=RelatedProject.PROJECT_RELATION_SIBLING
+        )
+
+        migrate_siblings()
+
+        # Only one parent should exist
+        parents = Project.objects.filter(path__depth=1)
+        self.assertEqual(parents.count(), 1)
+
+        # All siblings should have the same parent
+        children = parents.first().children()
+        self.assertEqual(children.count(), len(siblings1 + siblings2))
+        self.assertEqual(
+            set(child.uuid for child in children),
+            set(child.uuid for child in siblings1) | set(child.uuid for child in siblings2)
         )
