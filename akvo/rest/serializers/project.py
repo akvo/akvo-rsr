8721d5cc5b720fa17756be897d67bd706d112daa
Michael <michael@akvo.org>
2022-01-17 16:16:23 +0100

    refactor: exclude path and uuid from ProjectSerializer
    `fields` and `exclude` can't be used together, which introduced a mental block and the "readonly" fields solution.
    But the doc says that `exclude` alone is exactly what we want: all fields minus the excluded ones
    
    #4710: Improve project hierarchies

--- akvo/rest/serializers/project.py
+++ akvo/rest/serializers/project.py
@@ -74,12 +74,13 @@ class ProjectSerializer(BaseRSRSerializer):
     program = serializers.SerializerMethodField()
     targets_at = TargetsAtField(choices=Project.TARGETS_AT_OPTION, required=False)
     iati_profile_url = serializers.SerializerMethodField()
-    path = serializers.SerializerMethodField()
-    uuid = serializers.ReadOnlyField()
 
     class Meta:
         model = Project
-        fields = '__all__'
+        exclude = [
+            "path",
+            "uuid",
+        ]
 
     def get_editable(self, obj):
         """Method used by the editable SerializerMethodField"""
@@ -127,9 +128,6 @@ class ProjectSerializer(BaseRSRSerializer):
 
     def get_iati_profile_url(self, obj):
         return obj.get_iati_profile_url()
-
-    def get_path(self, project: Project):
-        return str(project.path)
 
 
 class ProjectDirectorySerializer(serializers.ModelSerializer):
