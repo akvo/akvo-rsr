8af99fd7d3725d54bcb92454fbd5f3d9a8c7cc9b
Michael <michael@akvo.org>
2022-01-19 17:29:51 +0100

    refactor: Move update_descendants into a usecase
    The model methods only do very basic checks to make sure the usecases have been handled correctly.
    They are a failsafe for the usecases, but mostly there for plumbing.
    Usecases do the hard work of moving other data around.
    
    #4710: Improve project hierarchies

--- akvo/rsr/tests/models/test_tree.py
+++ akvo/rsr/tests/models/test_tree.py
@@ -1,8 +1,8 @@
-import copy
 from typing import List
 
-from akvo.rsr.models.tree.errors import NodesWillBeOrphaned, TreeWillBreak
+from akvo.rsr.models.tree.errors import TreeWillBreak
 from akvo.rsr.models.tree.model import AkvoTreeModel
+from akvo.rsr.models.tree.usecases import check_set_parent, set_parent
 from akvo.rsr.tests.mixins import ModelMixinTestCase
 
 
@@ -14,7 +14,7 @@ class DescendantsTestCase(ModelMixinTestCase[AkvoTreeModel]):
         for i in range(depth):
             nodes.append(self.model.objects.create())
         for parent, child in zip(nodes, nodes[1:]):
-            child.set_parent(parent, force=True, update_descendants=False)
+            child.set_parent(parent)
         self.model.objects.bulk_update(nodes, ["path"])
 
         return nodes
@@ -50,43 +50,33 @@ class ChangeParentTestCase(ModelMixinTestCase[AkvoTreeModel]):
         self.right_child.set_parent(self.root).save()
         self.right_descendant1.set_parent(self.right_child).save()
         self.right_descendant2.set_parent(self.right_child).save()
-
+    
+    @staticmethod
+    def set_parent(project: AkvoTreeModel, new_parent: AkvoTreeModel):
+        check_set_parent(project, new_parent)
+        return set_parent(project, new_parent)
+    
     def test_self(self):
         """
-        Passing the same object should not have any effect on object
+        Passing the same object should fail
         """
-        old_right_child = copy.deepcopy(self.right_child)
-
-        self.right_child.set_parent(self.right_child)
-
-        self.assertEqual(old_right_child, self.right_child)
+        with self.assertRaisesRegex(ValueError, "Nodes are the same"):
+            self.set_parent(self.right_child, self.right_child)
 
     def test_self_from_db(self):
         """
-        Passing the same object retrieved from the DB should not have any effect on the object
+        Passing the same object retrieved from the DB should also fail
         """
         right_child = self.model.objects.get(id=self.right_child.id)
-        old_right_child = copy.deepcopy(right_child)
 
-        self.right_child.set_parent(right_child)
-
-        self.assertEqual(old_right_child, right_child)
-
-    def test_unforced_subtree_move(self):
-        """
-        Setting the parent of a node with descendants (aka moving a subtree)
-        is a dangerous operation that can mess up the tree
-
-        Ensure that we aren't allowed to do it without forcing
-        """
-        with self.assertRaises(NodesWillBeOrphaned):
-            self.right_child.set_parent(self.left_child)
+        with self.assertRaisesRegex(ValueError, "Nodes are the same"):
+            self.set_parent(self.right_child, right_child)
 
     def test_forced_subtree_move(self):
         """
-        Force moving a subtree and check that the new descendants are at the right place
+        Movea subtree and check that the new descendants are at the right place
         """
-        self.right_child.set_parent(self.left_child, force=True)
+        self.set_parent(self.right_child, self.left_child)
 
         left_children_uuids = self.left_child.descendants(with_self=False).values_list("uuid", flat=True)
         right_children_uuids = self.right_child.descendants(with_self=False).values_list("uuid", flat=True)
@@ -95,7 +85,7 @@ class ChangeParentTestCase(ModelMixinTestCase[AkvoTreeModel]):
 
     def test_forced_bad_subtree_move(self):
         """
-        Attempt forcing a subtree move to on of its own descendants, which shouldn't work
+        Attempt a subtree move to on of its own descendants, which shouldn't work
         """
         with self.assertRaises(TreeWillBreak):
-            self.right_child.set_parent(self.right_descendant2, force=True)
+            self.set_parent(self.right_child, self.right_descendant2)
--- akvo/rsr/usecases/remove_project_parent.py
+++ akvo/rsr/usecases/remove_project_parent.py
@@ -3,11 +3,14 @@
 # Akvo Reporting is covered by the GNU Affero General Public License.
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+from django.db import transaction
 
 from akvo.rsr.models import Project
+from akvo.rsr.models.tree.usecases import delete_parent
 from akvo.rsr.usecases.change_project_parent import RF_MODELS_CONFIG
 
 
+@transaction.atomic()
 def remove_parent(project: Project):
     if not project.has_ancestors:
         return
@@ -16,4 +19,4 @@ def remove_parent(project: Project):
     for (model, parent_attr, project_relation, _) in RF_MODELS_CONFIG.values():
         model.objects.filter(**{project_relation: project}).update(**{parent_attr: None})
 
-    project.delete_parent(force=True).save()
+    delete_parent(project)
