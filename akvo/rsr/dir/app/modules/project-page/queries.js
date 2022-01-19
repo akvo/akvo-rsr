c1ef78c9b24b408272c9193629ed549a5852c5d9
ifirmawan <firmawaneiwan@gmail.com>
2022-01-19 16:01:54 +0700

    wip infite swr

--- akvo/rsr/dir/app/modules/project-page/queries.js
+++ akvo/rsr/dir/app/modules/project-page/queries.js
@@ -1,6 +1,16 @@
 import useSWR from 'swr'
+import useSWRInfinite from 'swr/infinite'
 import humps from 'humps'
 import api from '../../utils/api'
+
+const getKeyData = (url, pageIndex, previousPageData) => {
+  // reached the end
+  if (previousPageData && !previousPageData.data) return null
+  // first page, we don't have `previousPageData`
+  if (pageIndex === 0) return url
+  // add the cursor to the API endpoint
+  return `${url}&page=${pageIndex + 1}`
+}
 
 export const queryUser = () => useSWR('/me/', (url) => api.get(url).then((res) => res.data))
 export const queryProject = (projectId) =>
@@ -33,8 +43,37 @@ export const queryPartnershipLinks = (projectId) =>
 export const queryPartnershipFunds = (projectId) =>
   useSWR(`/partnership/?format=json&project=${projectId}`, (url) => api.get(url).then((res) => res.data))
 
+export const queryBudget = (projectId) =>
+  useSWR(`/budget_item/?format=json&project=${projectId}`, (url) => api.get(url).then((res) => res.data))
+
 export const queryIndicators = (projectId) =>
   useSWR(`/indicator/?format=json&limit=100&result__project=${projectId}`, (url) => api.get(url).then((res) => res.data))
 
-export const queryBudget = (projectId) =>
-  useSWR(`/budget_item/?format=json&project=${projectId}`, (url) => api.get(url).then((res) => res.data))
+export const queryIndicatorPeriod = (projectId) =>
+  useSWRInfinite(
+    (pageIndex, previousPageData) => {
+      if (previousPageData && !previousPageData.next) {
+        return null
+      }
+      return `/indicator_period/?format=json&limit=10&indicator__result__project=${projectId}&page=${pageIndex + 1}`
+    },
+    (url) => api.get(url).then((res) => res.data)
+  )
+
+export const queryIndicatorPeriodData = (projectId) =>
+  useSWRInfinite(
+    (index) => `/indicator_period_data/?format=json&limit=100&period__indicator__result__project=${projectId}&page=${index + 1}`,
+    (url) => api.get(url).then((res) => res.data)
+  )
+
+export const queryPeriodDataComments = (projectId) =>
+  useSWRInfinite(
+    (index) => `/indicator_period_data_comment/?format=json&limit=100&data__period__indicator__result__project=${projectId}&page=${index + 1}`,
+    (url) => api.get(url).then((res) => res.data)
+  )
+
+export const queryDimensionNames = (projectId) =>
+  useSWR(`/dimension_name/?format=json&limit=100&project=${projectId}`, (url) => api.get(url).then((res) => res.data))
+
+export const queryDimensionValues = (projectId) =>
+  useSWR(`/dimension_value/?format=json&limit=100&name__project=${projectId}`, (url) => api.get(url).then((res) => res.data))
