c67fb245ec7c37fcc12c77d6a180c7701ee306e1
Michael <michael@akvo.org>
2022-01-26 11:44:59 +0100

    fix: Don't crash during errors in migrate_siblings
    Migrate siblings was crashing at `check_set_parent` and `set_parent`
     due to data inconsistencies.
    Now they are ignored, but printed.
    
    Additionally, the function was refactored to improve command output
     and code readability (hopefully). The functions had gotten a little big.
    
    #4710: Improve project hierarchies

--- akvo/rsr/management/commands/migrate_related_projects.py
+++ akvo/rsr/management/commands/migrate_related_projects.py
@@ -7,7 +7,7 @@ import dataclasses
 import operator
 from argparse import ArgumentParser
 from functools import reduce
-from typing import Dict, List, Set, Hashable, Generic, TypeVar
+from typing import Dict, Generic, Hashable, List, Set, TypeVar
 from uuid import UUID
 
 from django.core.management.base import BaseCommand
@@ -15,6 +15,7 @@ from django.db import transaction
 from django.db.models import F
 
 from akvo.rsr.models import Project, RelatedProject
+from akvo.rsr.models.tree.errors import ParentIsSame
 from akvo.rsr.models.tree.usecases import check_set_parent, set_parent
 
 
@@ -53,26 +54,27 @@ class Command(BaseCommand):
             # this happened in production for some reason
             project__exact=F("related_project")
         )
-
+        self.out("===Setting parents")
         for rp in parent_child_related_projects:
             # Refresh from DB in order to get the new paths
             rp.refresh_from_db(fields=["project", "related_project"])
             if rp.relation == RelatedProject.PROJECT_RELATION_CHILD:
-                node, parent = rp.related_project, rp.project
+                child, parent = rp.related_project, rp.project
             else:
-                node, parent = rp.project, rp.related_project
+                child, parent = rp.project, rp.related_project
             try:
-                check_set_parent(node, parent)
-                set_parent(node, parent)
+                check_set_parent(child, parent)
+                set_parent(child, parent)
             except ParentIsSame:
                 pass
             except Exception as e:
                 self.err(
-                    "Ignoring RP(%s): project(%s) related_project(%s) relation(%s)" % (
-                        rp.id, rp.project.id, rp.related_project.id, rp.get_relation_display()
-                    )
+                    "Ignoring RP(%s): parent(%s) child(%s)" % (rp.id, parent.id, child.id)
                 )
                 self.err(f"Cause: {e}")
+            else:
+                # self.out(f"parent({parent.id}|child({child.id})")
+                pass
 
         # handle siblings
         self.migrate_siblings()
@@ -91,6 +93,7 @@ class Command(BaseCommand):
         Siblings have to be migrated once the parent+child RelatedProjects have been treated.
         :return:
         """
+        self.out("====Migrating siblings")
         related_siblings_query = RelatedProject.objects.filter(
             relation__in=[RelatedProject.PROJECT_RELATION_SIBLING],
             related_project__isnull=False,
@@ -101,8 +104,9 @@ class Command(BaseCommand):
         sibling_groups = self.resolve_sibling_groups(related_siblings, sibling_uuid_groups)
 
         # Try to set parents of the groups
-        modified_projects = self.set_sibling_parents(sibling_groups)
-        print(f"Set parents for {len(modified_projects)} siblings")
+        parent_sibling_dict = self.aggregate_sibling_parents(sibling_groups)
+        modified_projects = self.set_group_parents(parent_sibling_dict)
+        self.out(f"Set parents for {len(modified_projects)} siblings")
         Project.objects.bulk_update(modified_projects, ["path"])
 
     def group_siblings(self, related_projects: List[RelatedProject]) -> List[Set[UUID]]:
@@ -131,58 +135,48 @@ class Command(BaseCommand):
                 first_group.update(*groups_to_merge)
                 # Remove merged groups
                 for group in groups_to_merge:
-                    groups.remove(group)
+                    sibling_uuid_groups.remove(group)
         return sibling_uuid_groups
 
     def resolve_sibling_groups(
             self,
             related_siblings: List[RelatedProject],
             sibling_uuid_groups: List[Set[UUID]]
-            ) -> List[List[Project]]:
+    ) -> List[Set[Project]]:
         """Use cache to resolve project UUIDs in the groups to projects"""
         sibling_projects_cache: Dict[UUID, Project] = {}
         for related_sibling in related_siblings:
             sibling_projects_cache[related_sibling.project.uuid] = related_sibling.project
             sibling_projects_cache[related_sibling.related_project.uuid] = related_sibling.related_project
-        sibling_groups: List[List[Project]] = [
-            [sibling_projects_cache[sibling_id] for sibling_id in sibling_id_group]
+        sibling_groups: List[Set[Project]] = [
+            set([sibling_projects_cache[sibling_id] for sibling_id in sibling_id_group])
             for sibling_id_group in sibling_uuid_groups
         ]
         return sibling_groups
 
-    def set_sibling_parents(self, sibling_groups: List[List[Project]]) -> List[Project]:
-        """
-        Attempts to sets the parent of each group of sibling projects
-
-        Not all groups will have the same parent (or a parent at all), which is of course a problem.
-
-        :return: All the projects that now have a parent
-        """
-        modified_projects = []
+    def aggregate_sibling_parents(self, sibling_groups: List[Set[Project]]) -> Dict[Project, Set[Project]]:
+        """Aggregates sibling groups that one parent in a dict"""
         orphaned_siblings = []
+        parent_sibling_dict: Dict[Project, Set[Project]] = {}
         for sibling_group in sibling_groups:
             # Find parents
-            parents = {}
-            for sibling in sibling_group:
-                parent_uuid = sibling.get_parent_uuid()
-                if parent_uuid:
-                    parents.setdefault(parent_uuid, []).append(sibling)
+            parents = self.get_group_parents(sibling_group)
 
-            #
             parent_count = len(parents)
             if parent_count == 0:
                 # print(f"{sibling_group} are all orphans")
                 orphaned_siblings.append(sibling_group)
+
             elif parent_count == 1:
                 parent_uuid, _ = parents.popitem()
                 parent = Project.objects.get(uuid=parent_uuid)
-                for sibling in sibling_group:
-                    if not sibling.get_parent_uuid():
-                        set_parent(sibling, parent)
-                        modified_projects.append(sibling)
-                self.out(f"{parent_uuid} is the parent of {sibling_group}")
+                parent_sibling_dict.setdefault(parent, set([])).update(sibling_group)
+
             else:
-                self.out(f"{sibling_group} has multiple parents!")
+                self.err(f"multiple parents:")
+                for project in sorted(sibling_group, key=lambda p: p.id):
+                    self.err(f"\t{project.id}: {project}")
+                    self.stderr.flush()
 
         orphan_count = reduce(
             operator.add,
@@ -191,6 +185,41 @@ class Command(BaseCommand):
         )
         self.out(f"Orphaned siblings: {len(orphaned_siblings)}")
         self.out(f"Total orphans: {orphan_count}")
+
+        return parent_sibling_dict
+
+    def get_group_parents(self, sibling_group: Set[Project]) -> Dict[UUID, Set[Project]]:
+        """The parents of the sibling group"""
+        parents = {}
+        for sibling in sibling_group:
+            parent_uuid = sibling.get_parent_uuid()
+            if parent_uuid:
+                parents.setdefault(parent_uuid, []).append(sibling)
+        return parents
+
+    def set_group_parents(self, parent_sibling_dict: Dict[Project, Set[Project]]) -> List[Project]:
+        """
+        Attempts to sets the parent of each group of sibling projects
+
+        Not all groups will have the same parent (or a parent at all), which is of course a problem.
+
+        :return: All the projects that now have a parent
+        """
+        modified_projects = []
+
+        # Set parent of every sibling in the group
+        for parent, sibling_group in sorted(parent_sibling_dict.items(), key=lambda p, _: p.id):
+            self.out(f"Parent {parent.id}: {parent}")
+            for sibling in sibling_group:
+                self.out(f"\tSibling {sibling.id}: {sibling}")
+                try:
+                    check_set_parent(sibling, parent)
+                    set_parent(sibling, parent)
+                    modified_projects.append(sibling)
+                except ParentIsSame:
+                    pass
+                except Exception as e:
+                    self.err(f"\tCouldn't set parent({parent.id}) for sibling({sibling.id}|{sibling}): {e}")
 
         return modified_projects
 
