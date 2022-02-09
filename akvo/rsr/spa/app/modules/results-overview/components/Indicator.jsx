bb62b0a150cd7e0f587535f3c3061488085c37d5
ifirmawan <firmawaneiwan@gmail.com>
2022-02-09 08:12:48 +0700

    [#4828] Fix Misplaced chart visual in the 'Description' section
    [#4828] Create empty has no periods.

--- akvo/rsr/spa/app/modules/results-overview/components/Indicator.jsx
+++ akvo/rsr/spa/app/modules/results-overview/components/Indicator.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react'
-import { Row, Col, Collapse, Icon, Checkbox } from 'antd'
+import { Card, Collapse, Empty, Icon, Row, Col } from 'antd'
 import { useTranslation } from 'react-i18next'
 import moment from 'moment'
-import { setNumberFormat } from '../../../utils/misc'
 import { UpdatePeriod } from './UpdatePeriod'
-import TargetCharts from '../../../utils/target-charts'
 
 const { Panel } = Collapse
 const Aux = node => node.children
@@ -19,29 +17,18 @@ export const Indicator = ({
   const [activeKey, setActiveKey] = useState(-1)
 
   const { t } = useTranslation()
-  const sumActualValue = indicator?.periods.reduce((total, currentValue) => total + currentValue.actualValue, 0)
-
   return indicator.periods.length === 0
-    ? <div className="no-periods">{t('This indicator has no periods')}</div>
+    ? (
+      <Row>
+        <Col style={{ padding: 16 }}>
+          <Card>
+            <Empty description={t('This indicator has no periods')} />
+          </Card>
+        </Col>
+      </Row>
+    )
     : (
       <Aux>
-        {targetsAt && targetsAt === 'indicator' && indicator?.targetValue && (
-          <Row>
-            <Col span={16} />
-            <Col span={4} className="stats-indicator">
-              <div className="stat value">
-                <div className="label">aggregated actual value</div>
-                <b>{setNumberFormat(sumActualValue)}</b><br />
-                <span>
-                  of <b>{indicator?.targetValue}</b> target
-                </span>
-              </div>
-            </Col>
-            <Col span={4}>
-              {<TargetCharts targetValue={indicator?.targetValue} actualValue={sumActualValue} />}
-            </Col>
-          </Row>
-        )}
         <Collapse accordion className="periods" bordered={false} activeKey={activeKey} onChange={key => { setActiveKey(key) }}>
           {indicator.periods.map(period => {
             const updates = period?.updates?.sort((a, b) => b.id - a.id)
