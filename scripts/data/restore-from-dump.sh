7146243ce6fb92bfe08565e064be0bd463c475ec
Michael <michael@akvo.org>
2022-02-22 10:15:04 +0100

    fix: restore DB dump in dev env as postgres user
    This allows us to execute privileged queries including creating extensions.
    
    #4856: Bug: Restoring DB dump with ltree extension doesn't work

--- scripts/data/restore-from-dump.sh
+++ scripts/data/restore-from-dump.sh
@@ -7,15 +7,9 @@ RSR_DB_NAME="${2}"
 RSR_USER="${3}"
 DUMP_FILE="${4}"
 
-if [[ -z "${RSR_PASSWORD:-}" ]]; then
-    read -r -s -p "Password for user ${RSR_USER} for the new DB ${RSR_DB_NAME}@${DB_HOST}: " RSR_PASSWORD
-fi
-
-export PGPASSWORD="${RSR_PASSWORD}"
-
 echo ""
 
-psql_settings=("--username=${RSR_USER}" "--host=${DB_HOST}" "--dbname=${RSR_DB_NAME}" "--set" "ON_ERROR_STOP=on")
+psql_settings=("--username=postgres" "--host=${DB_HOST}" "--dbname=${RSR_DB_NAME}" "--set" "ON_ERROR_STOP=on")
 
 psql "${psql_settings[@]}" --command="DROP SCHEMA public CASCADE"
 psql "${psql_settings[@]}" --command="CREATE SCHEMA public"
@@ -25,3 +19,27 @@ gunzip --stdout "${DUMP_FILE}" \
   | sed -e "/rsr_reportserver_db_user/d" \
   | sed -e "/ALTER DEFAULT PRIVILEGES FOR ROLE postgres/d" \
   | psql "${psql_settings[@]}"
+
+echo "Setting the owner of public tables to ${RSR_USER}"
+echo "
+  select
+    'Alter table '||t.schemaname||'.'||t.tablename ||' owner to ${RSR_USER};'
+  from pg_tables t
+  where schemaname='public';
+" | psql "${psql_settings[@]}" \
+  | grep Alter \
+  | psql "${psql_settings[@]}"
+
+echo "Setting the owner of public views to ${RSR_USER}"
+echo "
+  select
+    'Alter view '||v.schemaname||'.'||v.viewname ||' owner to ${RSR_USER};'
+  from pg_views v
+  where schemaname='public';
+" | psql "${psql_settings[@]}" \
+  | grep Alter \
+  | psql "${psql_settings[@]}"
+
+echo "Granting access to all current and future public tables for user ${RSR_USER}"
+echo "GRANT ALL ON schema public TO ${RSR_USER};" | psql "${psql_settings[@]}"
+echo "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO ${RSR_USER};" | psql "${psql_settings[@]}"
