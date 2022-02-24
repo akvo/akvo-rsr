0ddd142fb6f5c5e48539cb972201cbf13f24c42a
ifirmawan <firmawaneiwan@gmail.com>
2022-02-24 20:36:33 +0700

    [#4582] Fix typo confirm deletion message

--- akvo/rsr/spa/app/modules/results-admin/TobeReported.jsx
+++ akvo/rsr/spa/app/modules/results-admin/TobeReported.jsx
@@ -54,7 +54,7 @@ const TobeReported = ({
     Modal.confirm({
       icon: <Icon type="close-circle" style={{ color: '#f5222d' }} />,
       title: 'Do you want to delete this update?',
-      content: 'You’ll lose this update if when you click OK',
+      content: 'You’ll lose this update when you click OK',
       onOk() {
         api.delete(`/indicator_period_data_framework/${update.id}/`)
         const _results = results.map((pa) => ({
