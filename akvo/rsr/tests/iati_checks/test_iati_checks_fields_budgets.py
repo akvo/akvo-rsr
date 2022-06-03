8ec2c5d23bce3a3051b17937c297daa8883c8a22
zuhdil <zuhdil@gmail.com>
2022-06-03 14:24:39 +0700

    [#4923] Adjust validation for DGIS and MWC

--- akvo/rsr/tests/iati_checks/test_iati_checks_fields_budgets.py
+++ akvo/rsr/tests/iati_checks/test_iati_checks_fields_budgets.py
@@ -9,18 +9,18 @@ For additional details on the GNU license please see < http://www.gnu.org/licens
 
 import datetime
 
+from django.conf import settings
 from akvo.rsr.tests.base import BaseTestCase
 from akvo.iati.checks.fields import budgets as budgets_checks
-from akvo.rsr.models import BudgetItem, BudgetItemLabel
+from akvo.rsr.models import BudgetItem, BudgetItemLabel, ProjectEditorValidationSet
 
 
-class IatiCheckFieldsBudgetsTestCase(BaseTestCase):
+class IatiCheckBudgetPeriodNotLongerThanOneYearTestCase(BaseTestCase):
 
     def setUp(self):
         self.project = self.create_project("Test project")
         self.label = BudgetItemLabel.objects.create(label="Test")
 
-    def test_budget_period_not_longer_than_one_year(self):
         BudgetItem.objects.create(
             project=self.project,
             label=self.label,
@@ -29,6 +29,24 @@ class IatiCheckFieldsBudgetsTestCase(BaseTestCase):
             period_end=datetime.date.today() + datetime.timedelta(days=366),
             value_date=datetime.date.today(),
         )
+
+    def test_dgis_validation_set(self):
+        validation, _ = ProjectEditorValidationSet.objects.get_or_create(name=settings.VALIDATION_SET['DGIS'])
+        self.project.validations.add(validation)
+        all_checks_passed, checks = budgets_checks(self.project)
+        self.assertFalse(all_checks_passed)
+        self.assertEqual('warning', checks[0][0])
+        self.assertIn('period must not be longer than one year', checks[0][1])
+
+    def test_dgis_modified_validation_set(self):
+        validation, _ = ProjectEditorValidationSet.objects.get_or_create(name=settings.VALIDATION_SET['DGIS_MODIFIED'])
+        self.project.validations.add(validation)
+        all_checks_passed, checks = budgets_checks(self.project)
+        self.assertFalse(all_checks_passed)
+        self.assertEqual('warning', checks[0][0])
+        self.assertIn('period must not be longer than one year', checks[0][1])
+
+    def test_other_validation_set(self):
         all_checks_passed, checks = budgets_checks(self.project)
         self.assertFalse(all_checks_passed)
         self.assertEqual('error', checks[0][0])
