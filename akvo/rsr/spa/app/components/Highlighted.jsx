52d0fa72339c6ee042cc2fa22e3201d6b209f51e
ifirmawan <firmawaneiwan@gmail.com>
2022-06-06 22:13:56 +0700

    [#4642] Create program advance filter
    [#4642] Coloring tag periods and fix total matches
    [#4642] Set tag colour for countributors and countries
    [#4642] Fix filtering sub contributors and undefined contributor countries

--- akvo/rsr/spa/app/components/Highlighted.jsx
+++ akvo/rsr/spa/app/components/Highlighted.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
-import { Typography } from 'antd'
-import { escapeRegExp } from 'lodash'
-
-const { Text } = Typography
+import escapeRegExp from 'lodash/escapeRegExp'
 
 const Highlighted = ({ text = '', highlight = '' }) => {
   if (!highlight || (highlight && !highlight.trim())) {
-    return <Text>{text}</Text>
+    return <>{text}</>
   }
   const regex = new RegExp(`(${escapeRegExp(highlight)})`, 'gi')
   const parts = text.split(regex)
   return (
-    <Text>
+    <>
       {parts.filter(part => part).map((part, i) => (
-        regex.test(part) ? <mark key={i}>{part}</mark> : <span key={i}>{part}</span>
+        regex.test(part) ? <mark key={i}>{part}</mark> : <React.Fragment key={i}>{part}</React.Fragment>
       ))}
-    </Text>
+    </>
   )
 }
 
