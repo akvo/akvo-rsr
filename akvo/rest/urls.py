96f6e35c1e7735edf8f3129a69e83cc4451c14f1
zuhdil <zuhdil@gmail.com>
2022-06-22 09:38:29 +0700

    [#4642] Create program results endpoint
    `/rest/v1/program/<program_id>/results/`
    
    returns the result frameworks of the program and its contribution hierarchy

--- akvo/rest/urls.py
+++ akvo/rest/urls.py
@@ -311,6 +311,9 @@ urlpatterns += (
     url(r'v1/program/(?P<program_pk>[0-9]+)/updates/$',
         views.program_updates,
         name='program_updates'),
+    url(r'v1/program/(?P<program_pk>[0-9]+)/results/$',
+        views.get_program_results,
+        name='get_program_results'),
 )
 
 
