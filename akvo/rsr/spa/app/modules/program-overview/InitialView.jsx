0fef322522cec92cae473e50692fc1a43fa8dccd
ifirmawan <firmawaneiwan@gmail.com>
2022-06-23 20:29:29 +0700

    [#4642] Create advanced program filtering
    [#4642] Coloring tag periods and fix total matches
    [#4642] Set tag colour for countributors and countries
    [#4642] Fix filtering sub contributors and undefined contributor countries
    
    [#4642] Create program results endpoint
    
    `/rest/v1/program/<program_id>/results/`
    
    returns the result frameworks of the program and its contribution hierarchy
    
    [#4642] Implement indicator_updates_by_period_id
    [#4642] Adjust program results endpoint
    
    Add attributes:
    - indicator.target_values
    - indicator.score_options
    - period.target_values attributes
    
    [#4642] Implement indicator_updates_by_period_id end point
    
    Send request to backend after periods pane is open and
    calculate aggregations value after contributor pane is open.
    
    [#4642] Fix request contributors after filtering
    [#4642] Fix bug select all partners not working
    
    [#4642] Fix bug subproject percentage of update value

---
+++ akvo/rsr/spa/app/modules/program-overview/InitialView.jsx
@@ -0,0 +1,75 @@
+/* global */
+import React from 'react'
+import { Collapse, Icon, Spin } from 'antd'
+import { useTranslation } from 'react-i18next'
+import classNames from 'classnames'
+import moment from 'moment'
+
+import StickyClass from '../program/sticky-class'
+import ExpandIcon from '../program/ExpandIcon'
+import Highlighted from '../../components/Highlighted'
+
+const { Panel } = Collapse
+
+const InitialView = ({ results, search }) => {
+  const { t } = useTranslation()
+  return (
+    <Collapse defaultActiveKey={results.map((_, ix) => ix)} bordered={false} expandIcon={({ isActive }) => <ExpandIcon isActive={isActive} />}>
+      {results.map((result, rx) => (
+        <Panel
+          key={rx}
+          header={(
+            <StickyClass offset={20}>
+              <h1><Highlighted text={result.title} highlight={search} /></h1>
+              <div><i>{result.type}</i><span>{t('nindicators', { count: result.indicatorTitles.length })}</span></div>
+            </StickyClass>
+          )}
+        >
+          <Collapse defaultActiveKey={result.indicatorTitles.map((_, tx) => tx)} expandIcon={({ isActive }) => <ExpandIcon isActive={isActive} />}>
+            {result.indicatorTitles.map((title, tx) => (
+              <Panel
+                key={tx}
+                header={
+                  <StickyClass top={40}>
+                    <h3><Highlighted text={title} highlight={search} /></h3>
+                    <div><span className="type">{result.type}</span> <span className={classNames('periods', { 'color-periods': false })}>{t('nperiods', { count: result.periods.length })}</span></div>
+                  </StickyClass>}
+              >
+                <div className="indicator">
+                  <Collapse defaultActiveKey={result.periods.map((_, jx) => jx)} expandIcon={({ isActive }) => <ExpandIcon isActive={isActive} />}>
+                    {
+                      result
+                        .periods
+                        .filter((p) => (p[0] && p[1]))
+                        .sort((a, b) => moment(b[0], 'DD/MM/YYYY').unix() - moment(a[0], 'DD/MM/YYYY').unix())
+                        .map((p, px) => {
+                          const { 0: periodStart, 1: periodEnd } = p
+                          return (
+                            <Panel
+                              key={px}
+                              header={(
+                                <div>
+                                  <h5 className={classNames({ 'color-periods': false })}>{moment(periodStart, 'YYYY-MM-DD').format('DD MMM YYYY')} - {moment(periodEnd, 'YYYY-MM-DD').format('DD MMM YYYY')}</h5>
+                                  <ul className="small-stats">
+                                    <li>Loading...</li>
+                                  </ul>
+                                </div>
+                              )}
+                            >
+                              <div className="loading-container"><Spin indicator={<Icon type="loading" style={{ fontSize: 32 }} spin />} /></div>
+                            </Panel>
+                          )
+                        })
+                    }
+                  </Collapse>
+                </div>
+              </Panel>
+            ))}
+          </Collapse>
+        </Panel>
+      ))}
+    </Collapse>
+  )
+}
+
+export default InitialView
