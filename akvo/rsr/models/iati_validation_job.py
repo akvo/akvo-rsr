d98f73bd8c4a66e9fe4ca2479ce0315618316cb0
zuhdil <zuhdil@gmail.com>
2022-07-19 03:25:25 +0700

    [#4951] Implement IATI validator scheduler

---
+++ akvo/rsr/models/iati_validation_job.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Akvo RSR is covered by the GNU Affero General Public License.
+# See more details in the license.txt file located at the root folder of the Akvo RSR module.
+# For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
+
+from django.db import models
+
+
+class IatiValidationJobMixin(models.Model):
+    scheduled_at = models.DateTimeField(help_text='Date and time after which the job is expected to run')
+    started_at = models.DateTimeField(null=True, help_text='Date and time when the job is runned')
+    finished_at = models.DateTimeField(null=True, help_text='Date and time when the job is done')
+    attempts = models.IntegerField(default=0)
+
+    class Meta:
+        abstract = True
+        ordering = ('scheduled_at',)
+
+
+class IatiActivityValidationJob(IatiValidationJobMixin):
+    project = models.ForeignKey('Project', on_delete=models.CASCADE)
+
+
+class IatiOrganisationValidationJob(IatiValidationJobMixin):
+    organisation = models.ForeignKey('Organisation', on_delete=models.CASCADE)
