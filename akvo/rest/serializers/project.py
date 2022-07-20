0e7d7ddaba8d8e644455ff107157c283b16649db
Michael <michael@akvo.org>
2022-07-20 11:23:29 +0200

    Revert "refactor: exclude path and uuid from ProjectSerializer"
    This reverts commit 7a7e9f52cb5ac78c403d121bbf98a5f0bbf9d895.
    
    The frontend requires access to the path in order to know its parents.
    It also needs to determine if the project in a hierarchy.

--- akvo/rest/serializers/project.py
+++ akvo/rest/serializers/project.py
@@ -78,13 +78,12 @@ class ProjectSerializer(BaseRSRSerializer):
     program = serializers.SerializerMethodField()
     targets_at = TargetsAtField(choices=Project.TARGETS_AT_OPTION, required=False)
     iati_profile_url = serializers.SerializerMethodField()
+    path = serializers.SerializerMethodField()
+    uuid = serializers.ReadOnlyField()
 
     class Meta:
         model = Project
-        exclude = [
-            "path",
-            "uuid",
-        ]
+        fields = '__all__'
 
     def get_editable(self, obj):
         """Method used by the editable SerializerMethodField"""
@@ -132,6 +131,9 @@ class ProjectSerializer(BaseRSRSerializer):
 
     def get_iati_profile_url(self, obj):
         return obj.get_iati_profile_url()
+
+    def get_path(self, project: Project):
+        return str(project.path)
 
 
 class ProjectDirectorySerializer(serializers.ModelSerializer):
