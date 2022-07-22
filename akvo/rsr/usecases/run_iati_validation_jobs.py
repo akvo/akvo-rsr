ea6baf2326fc97b22dd485000edd740bf99f6915
zuhdil <zuhdil@gmail.com>
2022-07-22 14:17:19 +0700

    [#4951] Complete email templates and code clean-up

--- akvo/rsr/usecases/run_iati_validation_jobs.py
+++ akvo/rsr/usecases/run_iati_validation_jobs.py
@@ -3,6 +3,9 @@
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
+import json
+import logging
+
 from datetime import datetime, timedelta
 from dataclasses import dataclass
 from lxml import etree
@@ -20,6 +23,8 @@ from akvo.iati.exports.iati_org_export import IatiOrgXML
 from akvo.rsr.models import Project, Organisation, IatiActivityValidationJob, IatiOrganisationValidationJob, IatiCheck
 from akvo.utils import rsr_send_mail
 
+logger = logging.getLogger(__name__)
+
 VALIDATOR_TIMEOUT = 30  # seconds
 VALIDATOR_MAX_ATTEMPTS = getattr(settings, 'IATI_VALIDATOR_MAX_ATTEMPTS', 3)
 validator = IATIValidatorAPI(getattr(settings, 'IATI_VALIDATOR_SUBSCRIPTION_KEY', ''), VALIDATOR_TIMEOUT)
@@ -31,7 +36,7 @@ class RequestRate:
     period: timedelta
 
 
-class IATIValidatorThrottling:
+class IATIValidatorRateLimiter:
 
     def __init__(self, short_term: RequestRate, long_term: RequestRate):
         self.short_term = short_term
@@ -63,16 +68,15 @@ class IATIValidatorThrottling:
         activity_jobs = IatiActivityValidationJob.objects.filter(finished_at__gt=finished_at)
         organisation_jobs = IatiOrganisationValidationJob.objects.filter(finished_at__gt=finished_at)
         job_count = activity_jobs.count() + organisation_jobs.count()
-        latest_jobs = [j for j in [
+        datetimes = [j.finished_at for j in [
             activity_jobs.order_by('-finished_at').first(),
             organisation_jobs.order_by('-finished_at').first()
         ] if j]
-        datetimes = [j.finished_at for j in latest_jobs]
         latest = max(datetimes) if datetimes else None
         return (job_count, latest)
 
 
-rate_limiter = IATIValidatorThrottling(
+rate_limiter = IATIValidatorRateLimiter(
     short_term=RequestRate(
         count=getattr(settings, 'IATI_VALIDATOR_RATE_LIMIT_COUNT', 0),
         period=getattr(settings, 'IATI_VALIDATOR_RATE_LIMIT_PERIOD', timedelta(seconds=0))
@@ -100,13 +104,12 @@ def run_iati_activity_validation_job(scheduled_at: datetime = now()):
         process_activity_validation_results(project, validator_result, check_result)
     except IATIValidatorTimeoutException:
         pass  # retry on next round
-    except IATIValidatorException as e:
-        # TODO: log error
-        print(e)
+    except IATIValidatorException:
+        logger.exception(f'Failed to run IATI validator for project: {project.title}')
 
 
 def get_pending_activity_jobs(scheduled_at: datetime) -> QuerySet:
-    # Add 10 seconds for the failed jobs to avoid race conditions
+    # Add 10 seconds to avoid race conditions for the failed jobs
     timeout_at = now() - timedelta(seconds=VALIDATOR_TIMEOUT + 10)
     return IatiActivityValidationJob.objects\
         .select_related('project')\
@@ -150,14 +153,28 @@ def _persist_internal_project_validator_result(project: Project, result: CheckRe
 def process_activity_validation_results(project: Project, validator_result: IATIValidationResult, check_result: CheckResult):
     if check_result.error_count == validator_result.error_count:
         return
+    message = f'Inconsistent IATI activity validation results for project: {project.title}\n'\
+        f'IATI validator: {validator_result.error_count} errors, {validator_result.warning_count} warnings\n'\
+        f'Internal validator: {check_result.error_count} errors, {check_result.warning_count} warnings'
+    logger.info(message)
     email_recipients = getattr(settings, 'IATI_ACTIVITY_VALIDATION_ERROR_RECIPIENTS', [])
     if not email_recipients:
         return
-    # TODO: fill out subject, message and make error data as attachments
+    attachments = [{
+        'filename': 'iati-validator-api-result.json',
+        'content': json.dumps(validator_result.data, indent=2),
+        'mimetype': 'application/json'
+    }, {
+        'filename': 'rsr-custom-validator-result.json',
+        'content': json.dumps(check_result.data, indent=2),
+        'mimetype': 'application/json'
+    }]
     rsr_send_mail(
         email_recipients,
         subject='iati_validation/activity_error_subject.txt',
-        message='iati_validation/activity_error_message.txt'
+        message='iati_validation/activity_error_message.txt',
+        msg_context={'project': project, 'api_result': validator_result, 'rsr_result': check_result},
+        attachments=attachments
     )
 
 
@@ -180,13 +197,12 @@ def run_iati_organisation_validation_job(scheduled_at: datetime = now()):
         process_organisation_validation_results(organisation, validator_result)
     except IATIValidatorTimeoutException:
         pass  # retry on next round
-    except IATIValidatorException as e:
-        # TODO: log error
-        print(e)
+    except IATIValidatorException:
+        logger.exception(f'Failed to run IATI validator for organisation: {organisation.name}')
 
 
 def get_pending_organisation_jobs(scheduled_at: datetime = now()):
-    # Add 10 seconds for the failed jobs to avoid race conditions
+    # Add 10 seconds to avoid race conditions for the failed jobs
     timeout_at = now() - timedelta(seconds=VALIDATOR_TIMEOUT + 10)
     return IatiOrganisationValidationJob.objects\
         .select_related('organisation')\
@@ -197,14 +213,24 @@ def get_pending_organisation_jobs(scheduled_at: datetime = now()):
 def process_organisation_validation_results(organisation: Organisation, validator_result: IATIValidationResult):
     if validator_result.error_count == 0 and validator_result.warning_count == 0:
         return
+    message = f'IATI validation error for organisation: {organisation.name}\n'\
+        f'errors: {validator_result.error_count}\n'\
+        f'warnings: {validator_result.warning_count}'
+    logger.info(message)
     email_recipients = getattr(settings, 'IATI_ORGANISATION_VALIDATION_ERROR_RECIPIENTS', [])
     if not email_recipients:
         return
-    # TODO: fill out subject, message and make error data as attachments
+    attachments = [{
+        'filename': 'iati-validator-api-result.json',
+        'content': json.dumps(validator_result.data, indent=2),
+        'mimetype': 'application/json'
+    }]
     rsr_send_mail(
         email_recipients,
         subject='iati_validation/organisation_error_subject.txt',
-        message='iati_validation/organisation_error_message.txt'
+        message='iati_validation/organisation_error_message.txt',
+        msg_context={'organisation': organisation, 'result': validator_result},
+        attachments=attachments
     )
 
 
