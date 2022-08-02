135c0058564c3bf76092cfd7440402eea0a22962
ifirmawan <firmawaneiwan@gmail.com>
2022-08-02 14:30:42 +0700

    [#5057] Add redux persist to store important user props

--- akvo/rsr/spa/app/store/root-reducer.js
+++ akvo/rsr/spa/app/store/root-reducer.js
@@ -1,11 +1,26 @@
 import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import editorRdr from '../modules/editor/reducer'
 import userRdr from './user-reducer'
 
+const userPersistConfig = {
+  key: 'userRdr',
+  storage,
+  whitelist: [
+    'id',
+    'lang',
+    'programs',
+    'firstName',
+    'lastName',
+    'canManageUsers',
+    'seenAnnouncements'
+  ]
+}
 const rootReducer = combineReducers({
   editorRdr,
-  userRdr
+  userRdr: persistReducer(userPersistConfig, userRdr),
 })
 
 export default rootReducer
