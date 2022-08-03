877a6c4c461da393abac38b5f8c3f193fc81f8b7
ifirmawan <firmawaneiwan@gmail.com>
2022-08-03 16:08:35 +0700

    [#5051] Freeze time column in audit trail
    [#5051] Make audit trail components responsive in mobile view

--- akvo/rsr/spa/app/modules/results-overview/components/UpdateItems.jsx
+++ akvo/rsr/spa/app/modules/results-overview/components/UpdateItems.jsx
@@ -1,20 +1,16 @@
 import React, { useState, useRef } from 'react'
 import moment from 'moment'
-import { Button, Card, Collapse, Row, Col } from 'antd'
+import { Card, Collapse, Row, Col } from 'antd'
 import classNames from 'classnames'
 import { useTranslation } from 'react-i18next'
-import SVGInline from 'react-svg-inline'
 import orderBy from 'lodash/orderBy'
 
 import Update from '../../results/update'
-import DsgOverview from '../../results/dsg-overview'
-import { StatusPeriod } from '../../../components/StatusPeriod'
-import editButton from '../../../images/edit-button.svg'
 import ProgressBar from '../../../components/ProgressBar'
 import LineChart from '../../../components/LineChart'
+import UpdateHeader from './UpdateHeader'
 
 const { Panel } = Collapse
-const Aux = node => node.children
 
 const UpdateItems = ({
   role,
@@ -22,8 +18,6 @@ const UpdateItems = ({
   indicator,
   updates,
   targetsAt,
-  editPeriod,
-  disaggregations,
   setEditing
 }) => {
   const { t } = useTranslation()
@@ -119,34 +113,19 @@ const UpdateItems = ({
                   'draft-update': update?.id && update.status === 'D'
                 })}
                 header={
-                  <Aux>
-                    <div className="label">
-                      <p style={{ lineHeight: '14px' }}>
-                        <small>created at</small><br />
-                        <strong>{moment(update.createdAt).format('DD MMM YYYY')}</strong>
-                      </p>
-                    </div>
-                    <div className="label author">
-                      {update.userDetails && `${update.userDetails.firstName} ${update.userDetails.lastName}`}
-                    </div>
-                    <div className="value-container">
-                      {indicator.type === 1 &&
-                        <div className={classNames('value', { hovered: hover === updates.length - 1 - index || Number(pinned) === index })}>
-                          {String(update.value).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                          {indicator.measure === '2' && <small>%</small>}
-                        </div>
-                      }
-                    </div>
-                    <StatusPeriod {...{ update, pinned, index, t }} />
-                    {['m&e', 'admin'].includes(role) && (
-                      <Button
-                        type="link"
-                        onClick={() => handleOnEdit(update)}
-                      >
-                        <SVGInline svg={editButton} className="edit-button" />
-                      </Button>
-                    )}
-                  </Aux>
+                  <UpdateHeader
+                    {...{
+                      isPeriod,
+                      update,
+                      indicator,
+                      pinned,
+                      index,
+                      hover,
+                      role,
+                      handleOnEdit
+                    }}
+                    updateLength={updates.length}
+                  />
                 }
               >
                 <Update
