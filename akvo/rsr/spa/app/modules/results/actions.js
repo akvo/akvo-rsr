fa9e06aefc4845a737960bf928353c6a96245ae8
ifirmawan <firmawaneiwan@gmail.com>
2022-08-04 19:05:37 +0700

    [#5057] Create results reducer to reduce reloading page

---
+++ akvo/rsr/spa/app/modules/results/actions.js
@@ -0,0 +1,9 @@
+import actionTypes from './action-types'
+
+export const setResultState = (payload) => (dispatch) => {
+  dispatch({ type: actionTypes.SET_RESULTS, payload })
+}
+
+export const updateItemState = (payload) => (dispatch) => {
+  dispatch({ type: actionTypes.UPDATE_ITEM, payload })
+}
