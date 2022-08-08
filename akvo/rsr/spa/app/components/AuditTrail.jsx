69665722637038142417adf2328dd78b23426a47
ifirmawan <firmawaneiwan@gmail.com>
2022-08-08 15:03:19 +0700

    [#5051] Make audit trail components responsive in mobile view

--- akvo/rsr/spa/app/components/AuditTrail.jsx
+++ akvo/rsr/spa/app/components/AuditTrail.jsx
@@ -5,72 +5,99 @@ import { setNumberFormat } from '../utils/misc'
 
 const { Text } = Typography
 
-const DetailsValue = ({ data, disaggregations, scores }) => {
-  const status = {
-    D: 'Draft',
-    P: 'Pending Approval',
-    R: 'Declined',
-    A: 'Approved'
-  }
-  return (
-    <>
-      <Row style={{ marginBottom: '16px' }}>
-        <Col span={8}><Text type="secondary">Value</Text></Col>
-        <Col span={16}>
-          {data.value && <Text strong>{setNumberFormat(data.value)}</Text>}
-          {data?.disaggregations && (
-            <details>
-              <summary>Disaggregations</summary>
-              <br />
-              <ul>
-                {disaggregations.length > 0 && disaggregations.map((ds) => (
-                  <li key={ds.id}>
-                    <Text strong>{ds?.type} :</Text><br />
-                    {ds?.value}
-                  </li>
-                ))}
-              </ul>
-            </details>
-          )}
-          {data?.scoreIndices?.length > 0 && (
-            <details>
-              <summary>Score</summary>
-              <br />
-              <ul>
-                {data.scoreIndices.map(sx => <li key={sx}>{scores[sx - 1]}</li>)}
-              </ul>
-            </details>
-          )}
-        </Col>
-      </Row>
-      <Row style={{ marginBottom: '16px' }}>
-        <Col span={8}><Text type="secondary">Status</Text></Col>
-        <Col span={16}>
-          <Text strong>{status[data.status] || data.status}</Text>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={8}><Text type="secondary">Notes</Text></Col>
-        <Col span={16}>
-          {data.reviewNote || '-'}
-        </Col>
-      </Row>
-    </>
-  )
+const statusDescription = {
+  D: 'Draft',
+  P: 'Pending Approval',
+  R: 'Declined',
+  A: 'Approved'
 }
 
-const ActionFlagValue = ({ data, status }) => {
-  switch (data) {
-    case 'ADDITION':
-      return <Text>VALUE ADDED</Text>
-    default:
-      return status
-        ? status === 'R' ? <Text>VALUE REVIEWED</Text> : status === 'P' ? <Text>VALUE SUBMITTED</Text> : data
-        : <Text>VALUE MODIFIED</Text>
-  }
+const flagDescription = {
+  ADDITION: 'VALUE ADDED',
+  R: 'VALUE REVIEWED',
+  P: 'VALUE SUBMITTED',
 }
 
-export const AuditTrail = ({ audits, textReport, disaggregations, scores }) => {
+const DataValue = ({ value, disaggregations, scoreIndices, scores }) => (
+  <>
+    {value && <Text strong>{setNumberFormat(value)}</Text>}
+    {(disaggregations.length > 0) && (
+      <details>
+        <summary>Disaggregations</summary>
+        <br />
+        <ul>
+          {disaggregations.map((ds) => (
+            <li key={ds.id}>
+              <Text strong>{ds?.type} :</Text><br />
+              {ds?.value}
+            </li>
+          ))}
+        </ul>
+      </details>
+    )}
+    {(scoreIndices?.length > 0) && (
+      <details>
+        <summary>Score</summary>
+        <br />
+        <ul>
+          {scoreIndices.map(sx => <li key={sx}>{scores[sx - 1]}</li>)}
+        </ul>
+      </details>
+    )}
+  </>
+)
+
+const DetailsValue = ({ data, disaggregations, scores }) => (
+  <>
+    <Row style={{ marginBottom: '16px' }}>
+      <Col span={8}><Text type="secondary">Value</Text></Col>
+      <Col span={16}>
+        <DataValue {...{ ...data, disaggregations, scores }} />
+      </Col>
+    </Row>
+    <Row style={{ marginBottom: '16px' }}>
+      <Col span={8}><Text type="secondary">Status</Text></Col>
+      <Col span={16}>
+        <Text strong>{statusDescription[data.status] || data.status}</Text>
+      </Col>
+    </Row>
+    <Row>
+      <Col span={8}><Text type="secondary">Notes</Text></Col>
+      <Col span={16}>
+        {data.reviewNote || '-'}
+      </Col>
+    </Row>
+  </>
+)
+
+const ActionFlagValue = ({ data, status }) => <Text>{flagDescription[data] ? flagDescription[data] : status ? flagDescription[status] || data : 'VALUE MODIFIED'}</Text>
+
+const DescriptionValue = ({ user, actionFlag, data, disaggregations, scores }) => (
+  <ul>
+    <li>
+      <strong>Action</strong><br />
+      <ActionFlagValue data={actionFlag} status={data?.status} />
+    </li>
+    <li>
+      <strong>User</strong><br />
+      {`${user.firstName} ${user.lastName}`}
+    </li>
+    <li>
+      <strong>Value</strong><br />
+      <DataValue {...{ ...data, disaggregations, scores }} />
+    </li>
+    <li>
+      <strong>Status</strong><br />
+      {statusDescription[data.status] || data.status}
+    </li>
+    <li>
+      <strong>Notes</strong><br />
+      {data.reviewNote || '-'}
+    </li>
+  </ul>
+)
+
+export const AuditTrail = ({ audits, disaggregations, scores, mobileView = false }) => {
   const columns = [
     {
       title: 'TIME',
@@ -96,12 +123,26 @@ export const AuditTrail = ({ audits, textReport, disaggregations, scores }) => {
   ]
 
   const data = audits
-    .map((a) => ({
-      ...a,
+    .map((a, ax) => ({
+      id: ax,
       name: (a?.user?.firstName?.length || a?.user?.lastName?.length) ? `${a.user.firstName} ${a.user.lastName}` : a.user.email,
-      data: <DetailsValue {...{ ...a, textReport, disaggregations, scores }} />,
+      data: <DetailsValue {...{ ...a, disaggregations, scores }} />,
       actionFlag: <ActionFlagValue data={a.actionFlag} status={a?.data?.status} />,
-      actionTime: moment(a.actionTime).format('DD MMM YYYY HH:mm')
+      actionTime: moment(a.actionTime).format('DD MMM YYYY HH:mm'),
+      description: <DescriptionValue {...{ ...a, disaggregations, scores }} />,
     }))
-  return <Table columns={columns} dataSource={data} pagination={{ pageSize: 50 }} scroll={{ y: 240, x: 800 }} />
+
+  let tableProps = {
+    columns,
+    dataSource: data,
+    pagination: { pageSize: 50 },
+    scroll: { y: 240, x: 800 }
+  }
+  if (mobileView) {
+    tableProps = {
+      ...tableProps,
+      expandedRowRender: record => <span style={{ margin: 0 }}>{record.description}</span>
+    }
+  }
+  return <Table {...tableProps} rowKey="id" />
 }
---
+++ akvo/rsr/spa/app/modules/results-overview/components/UpdateHeader.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { Button, Col, Row } from 'antd'
+import { useTranslation } from 'react-i18next'
+import moment from 'moment'
+import classNames from 'classnames'
+import SVGInline from 'react-svg-inline'
+
+import { StatusPeriod } from '../../../components/StatusPeriod'
+import editButton from '../../../images/edit-button.svg'
+import { setNumberFormat } from '../../../utils/misc'
+
+const UpdateHeader = ({
+  isPeriod,
+  update,
+  indicator,
+  pinned,
+  index,
+  hover,
+  role,
+  updateLength,
+  handleOnEdit
+}) => {
+  const { t } = useTranslation()
+  const createdAt = isPeriod ? 4 : 2
+  const xlColumns = {
+    createdAt,
+    author: 4,
+    value: 10,
+    status: 3,
+    action: 2
+  }
+  return (
+    <Row type="flex" justify="space-between" align="middle" className="w-full">
+      <Col xs={12} md={4} lg={4} xl={xlColumns.createdAt}>
+        <div className="label">
+          <p>
+            <small>created at</small><br />
+            <strong>{moment(update.createdAt).format('DD MMM YYYY')}</strong>
+          </p>
+        </div>
+      </Col>
+      <Col xs={12} md={4} lg={5} xl={xlColumns.author}>
+        <div className="label author">
+          {update.userDetails && `${update.userDetails.firstName} ${update.userDetails.lastName}`}
+        </div>
+      </Col>
+      {indicator.type === 1 &&
+        (
+          <Col xs={12} md={6} lg={7} xl={xlColumns.value}>
+            <div className="value-container">
+              <div className={classNames('value', { hovered: hover === updateLength - 1 - index || Number(pinned) === index })}>
+                {setNumberFormat(update.value)}
+                {indicator.measure === '2' && <small>%</small>}
+              </div>
+            </div>
+          </Col>
+        )
+      }
+      <Col xs={12} md={5} lg={5} xl={xlColumns.status}>
+        <StatusPeriod {...{ update, pinned, index, t }} />
+      </Col>
+      {['m&e', 'admin'].includes(role) && (
+        <Col xs={24} md={2} lg={2} xl={xlColumns.action} className="text-right">
+          <Button type="link" onClick={() => handleOnEdit(update)}>
+            <SVGInline svg={editButton} className="edit-button" />
+          </Button>
+        </Col>
+      )}
+    </Row>
+  )
+}
+
+export default UpdateHeader
