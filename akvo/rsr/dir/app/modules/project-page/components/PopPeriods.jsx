52e6859e4044b5e2edc8b85e401859bdebc2f4fd
ifirmawan <firmawaneiwan@gmail.com>
2022-08-22 22:41:56 +0700

    [#5081] Fix disaggregation in public project overview
    In order to fix looping/repeated requests from the front side i've added a redux package to manage results state
    which can update and show that fetch is executed more efficiently without sending state through nested components.
    
    Separate endpoint requests per component and call them after the collapse pane is opened.

--- akvo/rsr/dir/app/modules/project-page/components/PopPeriods.jsx
+++ akvo/rsr/dir/app/modules/project-page/components/PopPeriods.jsx
@@ -7,12 +7,18 @@ import {
   Collapse,
   Select,
 } from 'antd'
+import { useSelector, useDispatch } from 'react-redux'
+import { selectPeriod } from '../../../features/periods/periodSlice'
 
 const { Text } = Typography
 const { Option } = Select
 
-const PopPeriods = ({ periods, onChange }) => {
+const PopPeriods = () => {
   const [open, setOpen] = useState(false)
+
+  const { options, fetched } = useSelector((state) => state.periods)
+  const dispatch = useDispatch()
+
   return (
     <Collapse
       bordered={false}
@@ -24,16 +30,18 @@ const PopPeriods = ({ periods, onChange }) => {
         <Row gutter={[8, 16]}>
           <Col>
             <Select
-              open={open}
-              onChange={onChange}
+              open={(open && fetched)}
+              onChange={(value) => dispatch(selectPeriod(value))}
               onMouseEnter={() => setOpen(!open)}
               onSelect={() => setOpen(false)}
+              loading={!fetched}
+              disabled={!fetched}
               mode="multiple"
               className="w-full"
               placeholder="Select Period"
               allowClear
             >
-              {periods.map((op, ox) => <Option value={op} key={ox}>{op}</Option>)}
+              {options.map((op, ox) => <Option value={op} key={ox}>{op}</Option>)}
             </Select>
           </Col>
         </Row>
