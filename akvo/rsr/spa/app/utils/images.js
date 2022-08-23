6e9e9f5f589912a597b7080c404ac9d04ab1100f
ifirmawan <firmawaneiwan@gmail.com>
2022-08-23 23:12:58 +0700

    [#5080] Added active state to fix indicator approval workflow
    Active status helps MnE managers to identify updates of indicators that have an unlocked period still in progress.
    the following what has to be done in order to finalize  this feature :
    1. Update status description.
    2. Add new icon history.
    3. Create audit trail modal
    4. Call /indicator_period_data_framework/ by period
       Pulling data audit trails from endpoint /indicator_period_data_framework/ by period ID of selected update.

---
+++ akvo/rsr/spa/app/utils/images.js
@@ -0,0 +1,13 @@
+import clockHistory from '../images/clock-history.svg'
+import editPencil from '../images/edit-pencil.svg'
+import editButton from '../images/edit-button.svg'
+
+export const icons = {
+  clock: {
+    history: clockHistory
+  },
+  edit: {
+    pencil: editPencil,
+    button: editButton
+  }
+}
