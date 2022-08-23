a7bbf22d85c28e2dd7a42de997fcac233637e0c6
Michael <michael@akvo.org>
2022-08-23 09:50:45 +0200

    reorder migrations after rebase from master
    Migrations with the same number were introduced on master
    
    #4710 Improve project hierarchies

--- akvo/rsr/migrations/0220_project_tree.py
+++ akvo/rsr/migrations/0220_project_tree.py
@@ -34,7 +34,7 @@ def migrate_contributing_projects(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('rsr', '0218_project_thumbnail_cache'),
+        ('rsr', '0219_iatiactivityvalidationjob_iatiorganisationvalidationjob'),
     ]
 
     operations = [
