831c0875c6fae630011161a5a283f8dcfa656a1c
zuhdil <zuhdil@gmail.com>
2022-09-19 10:43:56 +0700

    [#5105] Save the ongoing cumulative indicator period disaggregations

--- akvo/rsr/tests/results_framework/test_cumulative_updates.py
+++ akvo/rsr/tests/results_framework/test_cumulative_updates.py
@@ -8,8 +8,11 @@ from akvo.rsr.models.result.utils import PERCENTAGE_MEASURE
 class CumulativeTestMixin:
     PERIOD_1_START = date(2020, 1, 1)
     PERIOD_2_START = date(2021, 1, 1)
+    DISAGGREGATION_CATEGORY = 'Gender'
+    DISAGGREGATION_TYPE_1 = 'Male'
+    DISAGGREGATION_TYPE_2 = 'Female'
 
-    def setup_project(self, percentage=False, contributors=None):
+    def setup_project(self, percentage=False, disaggregations=False, contributors=None):
         builder = ProjectFixtureBuilder().with_results([{
             'title': 'Result #1',
             'indicators': [{
@@ -22,6 +25,10 @@ class CumulativeTestMixin:
                 ]
             }]
         }])
+        if disaggregations:
+            builder.with_disaggregations({
+                self.DISAGGREGATION_CATEGORY: [self.DISAGGREGATION_TYPE_1, self.DISAGGREGATION_TYPE_2]
+            })
         if contributors:
             builder.with_contributors(contributors)
         return builder.build()
@@ -32,23 +39,46 @@ class SingleUserCumulativeUnitUpdatesTestCase(BaseTestCase, CumulativeTestMixin)
     def setUp(self):
         super().setUp()
         user = self.create_user('test@akvo.org', 'password', is_admin=True)
-        self.project = self.setup_project()
+        self.project = self.setup_project(disaggregations=True)
 
         self.period1 = self.project.get_period(period_start=self.PERIOD_1_START)
-        self.period1.add_update(user=user, value=1)
-        self.period1.add_update(user=user, value=2)
+        self.period1.add_update(user=user, value=1, disaggregations={
+            self.DISAGGREGATION_CATEGORY: {
+                self.DISAGGREGATION_TYPE_1: {'value': 1},
+            }
+        })
+        self.period1.add_update(user=user, value=2, disaggregations={
+            self.DISAGGREGATION_CATEGORY: {
+                self.DISAGGREGATION_TYPE_1: {'value': 1},
+                self.DISAGGREGATION_TYPE_2: {'value': 1},
+            }
+        })
 
         self.period2 = self.project.get_period(period_start=self.PERIOD_2_START)
-        self.period2.add_update(user=user, value=3)
-        self.period2.add_update(user=user, value=5)
+        self.period2.add_update(user=user, value=3, disaggregations={
+            self.DISAGGREGATION_CATEGORY: {
+                self.DISAGGREGATION_TYPE_1: {'value': 2},
+                self.DISAGGREGATION_TYPE_2: {'value': 1},
+            }
+        })
+        self.period2.add_update(user=user, value=5, disaggregations={
+            self.DISAGGREGATION_CATEGORY: {
+                self.DISAGGREGATION_TYPE_1: {'value': 3},
+                self.DISAGGREGATION_TYPE_2: {'value': 2},
+            }
+        })
 
     def test_period1(self):
         period1 = self.project.periods.get(id=self.period1.id)
         self.assertEqual(2, Decimal(period1.actual_value))
+        self.assertEqual(1, period1.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_1).value)
+        self.assertEqual(1, period1.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_2).value)
 
     def test_period2(self):
         period2 = self.project.periods.get(id=self.period2.id)
         self.assertEqual(5, Decimal(period2.actual_value))
+        self.assertEqual(3, period2.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_1).value)
+        self.assertEqual(2, period2.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_2).value)
 
 
 class MultiUserCumulativeUnitUpdatesTestCase(BaseTestCase, CumulativeTestMixin):
@@ -57,24 +87,51 @@ class MultiUserCumulativeUnitUpdatesTestCase(BaseTestCase, CumulativeTestMixin):
         super().setUp()
         user1 = self.create_user('test1@akvo.org', 'password', is_admin=True)
         user2 = self.create_user('test2@akvo.org', 'password', is_admin=True)
-        self.project = self.setup_project()
+        self.project = self.setup_project(disaggregations=True)
 
         self.period1 = self.project.get_period(period_start=self.PERIOD_1_START)
-        self.period1.add_update(user=user1, value=1)
-        self.period1.add_update(user=user1, value=2)
-        self.period1.add_update(user=user2, value=2)
+        self.period1.add_update(user=user1, value=1, disaggregations={
+            self.DISAGGREGATION_CATEGORY: {
+                self.DISAGGREGATION_TYPE_1: {'value': 1},
+            }
+        })
+        self.period1.add_update(user=user1, value=2, disaggregations={
+            self.DISAGGREGATION_CATEGORY: {
+                self.DISAGGREGATION_TYPE_1: {'value': 1},
+                self.DISAGGREGATION_TYPE_2: {'value': 1},
+            }
+        })
+        self.period1.add_update(user=user2, value=2, disaggregations={
+            self.DISAGGREGATION_CATEGORY: {
+                self.DISAGGREGATION_TYPE_2: {'value': 2},
+            }
+        })
 
         self.period2 = self.project.get_period(period_start=self.PERIOD_2_START)
-        self.period2.add_update(user=user2, value=3)
-        self.period2.add_update(user=user2, value=4)
+        self.period2.add_update(user=user2, value=3, disaggregations={
+            self.DISAGGREGATION_CATEGORY: {
+                self.DISAGGREGATION_TYPE_1: {'value': 1},
+                self.DISAGGREGATION_TYPE_2: {'value': 2},
+            }
+        })
+        self.period2.add_update(user=user2, value=4, disaggregations={
+            self.DISAGGREGATION_CATEGORY: {
+                self.DISAGGREGATION_TYPE_1: {'value': 2},
+                self.DISAGGREGATION_TYPE_2: {'value': 2},
+            }
+        })
 
     def test_period1(self):
         period1 = self.project.periods.get(id=self.period1.id)
         self.assertEqual(4, Decimal(period1.actual_value))
+        self.assertEqual(1, period1.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_1).value)
+        self.assertEqual(3, period1.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_2).value)
 
     def test_period2(self):
         period2 = self.project.periods.get(id=self.period2.id)
         self.assertEqual(6, Decimal(period2.actual_value))
+        self.assertEqual(3, period2.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_1).value)
+        self.assertEqual(3, period2.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_2).value)
 
 
 class CumulativePercentageUpdatesTestCase(BaseTestCase, CumulativeTestMixin):
@@ -88,25 +145,40 @@ class CumulativePercentageUpdatesTestCase(BaseTestCase, CumulativeTestMixin):
         super().setUp()
         user1 = self.create_user('test1@akvo.org', 'password', is_admin=True)
         user2 = self.create_user('test2@akvo.org', 'password', is_admin=True)
-        self.project = self.setup_project(percentage=True)
+        self.project = self.setup_project(percentage=True, disaggregations=True)
 
         self.period1 = self.project.get_period(period_start=self.PERIOD_1_START)
-        self.period1.add_update(user=user1, numerator=1, denominator=4)
+        self.period1.add_update(user=user1, numerator=1, denominator=4, disaggregations={
+            self.DISAGGREGATION_CATEGORY: {
+                self.DISAGGREGATION_TYPE_1: {'numerator': 1, 'denominator': 4},
+            }
+        })
 
         self.period2 = self.project.get_period(period_start=self.PERIOD_2_START)
-        self.period2.add_update(user=user2, numerator=2, denominator=4)
+        self.period2.add_update(user=user2, numerator=2, denominator=4, disaggregations={
+            self.DISAGGREGATION_CATEGORY: {
+                self.DISAGGREGATION_TYPE_1: {'numerator': 1, 'denominator': 4},
+                self.DISAGGREGATION_TYPE_2: {'numerator': 1, 'denominator': 4},
+            }
+        })
 
     def test_period1(self):
         period1 = self.project.periods.get(id=self.period1.id)
         self.assertEqual(25, Decimal(period1.actual_value))
         self.assertEqual(1, Decimal(period1.numerator))
         self.assertEqual(4, Decimal(period1.denominator))
+        self.assertEqual(1, period1.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_1).numerator)
+        self.assertEqual(4, period1.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_1).denominator)
 
     def test_period2(self):
         period2 = self.project.periods.get(id=self.period2.id)
         self.assertEqual(50, Decimal(period2.actual_value))
         self.assertEqual(2, Decimal(period2.numerator))
         self.assertEqual(4, Decimal(period2.denominator))
+        self.assertEqual(1, period2.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_1).numerator)
+        self.assertEqual(4, period2.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_1).denominator)
+        self.assertEqual(1, period2.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_2).numerator)
+        self.assertEqual(4, period2.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_2).denominator)
 
 
 class CumulativeAggregationTestCase(BaseTestCase, CumulativeTestMixin):
@@ -115,7 +187,7 @@ class CumulativeAggregationTestCase(BaseTestCase, CumulativeTestMixin):
         super().setUp()
         user1 = self.create_user('test1@akvo.org', 'password', is_admin=True)
         user2 = self.create_user('test2@akvo.org', 'password', is_admin=True)
-        self.lead_project = self.setup_project(contributors=[
+        self.lead_project = self.setup_project(disaggregations=True, contributors=[
             {'title': 'Contrib #1'},
             {'title': 'Contrib #2'}
         ])
@@ -127,16 +199,40 @@ class CumulativeAggregationTestCase(BaseTestCase, CumulativeTestMixin):
         contrib2_period1 = contrib2_project.get_period(period_start=self.PERIOD_1_START)
         contrib2_period2 = contrib2_project.get_period(period_start=self.PERIOD_2_START)
 
-        contrib1_period1.add_update(user=user1, value=1)
-        contrib1_period2.add_update(user=user2, value=1)
+        contrib1_period1.add_update(user=user1, value=1, disaggregations={
+            self.DISAGGREGATION_CATEGORY: {
+                self.DISAGGREGATION_TYPE_1: {'value': 1},
+                self.DISAGGREGATION_TYPE_2: {'value': 0},
+            }
+        })
+        contrib1_period2.add_update(user=user2, value=1, disaggregations={
+            self.DISAGGREGATION_CATEGORY: {
+                self.DISAGGREGATION_TYPE_1: {'value': 0},
+                self.DISAGGREGATION_TYPE_2: {'value': 1},
+            }
+        })
 
-        contrib2_period1.add_update(user=user1, value=1)
-        contrib2_period2.add_update(user=user1, value=2)
+        contrib2_period1.add_update(user=user1, value=1, disaggregations={
+            self.DISAGGREGATION_CATEGORY: {
+                self.DISAGGREGATION_TYPE_1: {'value': 0},
+                self.DISAGGREGATION_TYPE_2: {'value': 1},
+            }
+        })
+        contrib2_period2.add_update(user=user1, value=2, disaggregations={
+            self.DISAGGREGATION_CATEGORY: {
+                self.DISAGGREGATION_TYPE_1: {'value': 1},
+                self.DISAGGREGATION_TYPE_2: {'value': 1},
+            }
+        })
 
     def test_lead_period1(self):
         lead_period1 = self.lead_project.periods.get(period_start=self.PERIOD_1_START)
         self.assertEqual(2, Decimal(lead_period1.actual_value))
+        self.assertEqual(1, lead_period1.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_1).value)
+        self.assertEqual(1, lead_period1.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_2).value)
 
     def test_lead_period2(self):
         lead_period2 = self.lead_project.periods.get(period_start=self.PERIOD_2_START)
         self.assertEqual(4, Decimal(lead_period2.actual_value))
+        self.assertEqual(2, lead_period2.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_1).value)
+        self.assertEqual(2, lead_period2.disaggregations.get(dimension_value__value=self.DISAGGREGATION_TYPE_2).value)
