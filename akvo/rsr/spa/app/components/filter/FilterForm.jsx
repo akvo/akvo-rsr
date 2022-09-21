5c8c6b3ce85335e55d2608306cc47b58895ecc74
ifirmawan <firmawaneiwan@gmail.com>
2022-09-21 13:35:03 +0700

    [#4642] Create sticky filter panel and split up some codes

--- akvo/rsr/spa/app/components/filter/FilterForm.jsx
+++ akvo/rsr/spa/app/components/filter/FilterForm.jsx
@@ -1,14 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import {
   Typography,
   Input,
   Card,
-  Icon,
   Col,
   Row,
   Button,
   Checkbox,
-  Tooltip
 } from 'antd'
 
 const { Text } = Typography
@@ -20,34 +18,28 @@ const FilterForm = ({
   children,
   onCancel,
   onApply,
-  onUndo,
   onSearch,
   onSelectAll
 }) => {
-  const [toggle, setToggle] = useState(false)
-
   return (
     <Card id="rsr-filter-items-card">
-      <Row type="flex" justify="space-between" align="middle" className="row-header">
-        <Col lg={14}>{title && <Text strong>{title}</Text>}</Col>
-        <Col lg={2}>
-          <Button shape="circle" icon="search" size="small" onClick={() => setToggle(!toggle)} />
+      <Row type="flex" justify="space-between" align="middle" className="row-header" gutter={[8, 24]}>
+        <Col lg={16} md={24} sm={24} xs={24}>{title && <Text strong>{title}</Text>}</Col>
+        <Col lg={8} md={16} sm={16} xs={16} className="text-right">
+          Select All <Checkbox name="selectAll" onClick={onSelectAll} />
         </Col>
-        <Col lg={7}>Select All <Checkbox name="selectAll" onClick={onSelectAll} /></Col>
       </Row>
-      {toggle && (
-        <Row className="row-search-bar">
-          <Col>
-            <Search placeholder="Search for..." value={search} onChange={(e) => onSearch(e.target.value)} allowClear />
-          </Col>
-        </Row>
-      )}
+      <Row className="row-search-bar">
+        <Col>
+          <Search placeholder="Search for..." value={search} onChange={(e) => onSearch(e.target.value)} allowClear />
+        </Col>
+      </Row>
       {children}
       <Row type="flex" justify="end" align="middle" className="row-footer" gutter={[8, 8]}>
-        <Col lg={6}>
+        <Col span={6}>
           <Button type="link" size="small" onClick={onCancel} block>Cancel</Button>
         </Col>
-        <Col lg={6}>
+        <Col span={6}>
           <Button size="small" type="primary" onClick={onApply} block>Apply</Button>
         </Col>
       </Row>
