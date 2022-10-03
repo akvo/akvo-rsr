f4c7790f740305e0344561420049a10086c1f682
Michael <michael@akvo.org>
2022-10-03 11:58:00 +0200

    fix migrations after rebase from master
    #4710 Improve project hierarchies

--- akvo/rsr/migrations/0221_project_tree.py
+++ akvo/rsr/migrations/0221_project_tree.py
@@ -34,7 +34,7 @@ def migrate_contributing_projects(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('rsr', '0219_iatiactivityvalidationjob_iatiorganisationvalidationjob'),
+        ('rsr', '0220_emailreportjob'),
     ]
 
     operations = [
