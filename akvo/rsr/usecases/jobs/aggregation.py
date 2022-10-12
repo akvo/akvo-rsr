8f2ff76bb9ebde1221a62197390f74970bdad858
Michael <michael@akvo.org>
2022-10-12 16:36:13 +0200

    feat: Implement logic for handling aggregation tasks
    #5140: Stabilize period update aggregations

--- akvo/rsr/usecases/jobs/aggregation.py
+++ akvo/rsr/usecases/jobs/aggregation.py
@@ -1,8 +1,11 @@
 from django.db.models import QuerySet
+from django.db.transaction import atomic
+from typing import List
 
 from akvo.rsr.models import IndicatorPeriod
 from akvo.rsr.models.aggregation_job import IndicatorUpdateAggregationJob
 from akvo.rsr.models.cron_job import CronJobMixin
+from akvo.rsr.usecases.jobs.cron import is_job_dead
 
 
 def get_scheduled_jobs() -> QuerySet[IndicatorUpdateAggregationJob]:
@@ -33,3 +36,66 @@ def get_finished_jobs() -> QuerySet[IndicatorUpdateAggregationJob]:
     return IndicatorUpdateAggregationJob.objects.filter(
         status=CronJobMixin.Status.FINISHED,
     )
+
+
+def schedule_aggregation_job(period: IndicatorPeriod) -> IndicatorUpdateAggregationJob:
+    """
+    Schedule a job for the period to be aggregated upwards if no job exists
+    """
+    if existing_job := get_scheduled_jobs().filter(period=period).first():
+        return existing_job
+    return IndicatorUpdateAggregationJob.objects.create(period=period)
+
+
+def execute_aggregation_jobs():
+    """
+    Call the aggregation function for each aggregation job
+    """
+    handle_failed_jobs()
+
+    scheduled_jobs = get_scheduled_jobs()
+    for scheduled_job in scheduled_jobs:
+        with atomic():
+            scheduled_job.mark_running()
+            try:
+                run_aggregation(scheduled_job.period)
+                scheduled_job.mark_finished()
+            except Exception as e:
+                scheduled_job.mark_failed()
+                email_failed_job_owner(scheduled_job, str(e))
+
+
+@atomic
+def handle_failed_jobs():
+    """Identify failed jobs, notify owners, and reschedule them"""
+    identify_dead_jobs()
+    failed_jobs = get_failed_jobs()
+
+    for failed_job in failed_jobs:
+        email_failed_job_owner(failed_job, "Job died")
+
+        failed_job.mark_scheduled()
+
+
+@atomic
+def identify_dead_jobs() -> List[IndicatorUpdateAggregationJob]:
+    """
+    Find jobs that are supposed to be running, but with a dead process
+    """
+    dead_jobs = []
+    for running_job in get_running_jobs():
+        if not is_job_dead(running_job):
+            continue
+        running_job.mark_failed()
+        dead_jobs.append(running_job)
+
+    return dead_jobs
+
+
+def email_failed_job_owner(failed_job: IndicatorUpdateAggregationJob, reason: str):
+    raise NotImplementedError()
+
+
+def run_aggregation(period: IndicatorPeriod):
+    # TODO: Implement
+    raise NotImplementedError()
