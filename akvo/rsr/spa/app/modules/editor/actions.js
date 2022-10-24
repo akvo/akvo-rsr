3760a05af155f106ee368de9f064804ca938f047
ifirmawan <firmawaneiwan@gmail.com>
2022-10-24 10:32:56 +0700

    [#5075] Fix project editor nothing was returned from render.
    After reviewing each component in section5.jsx,
    I found the issue came from `editor/section5/indicators.jsx` file.
    Which is `allowIndicatorLabels` returning false so that component doesn't return from render.
    
    so I highly recommend in the future not to put logic inside component `<Condition />`.
    
    [#5073] Fix long delay in desc due to endpoint issue

--- akvo/rsr/spa/app/modules/editor/actions.js
+++ akvo/rsr/spa/app/modules/editor/actions.js
@@ -96,3 +96,4 @@ export const setProjectStatus = (publishingStatus, hasHierarchy, needsReportingT
   dispatch({ type: actionTypes.SAVE_FIELDS, fields: { publishingStatus, hasHierarchy, needsReportingTimeoutDays, pendingUpdateCount, canEditProject }, sectionIndex: 1, noSync: true })
 }
 export const setUser = (user) => ({ type: 'SET_USER', user })
+export const setFirstSectionID = projectId => ({ type: actionTypes.SET_FIRST_SECTION_ID, projectId })
