2bbc01f92a4ef25e6c310121903b132e207205c0
Iwan Firmawan <firmawaneiwan@gmail.com>
2022-12-06 08:58:04 +0700

    [#5169] Disable/enable cumulative switch on project editor
    Set disabled attribute value as true when the response from contribution_count API have more than zero number of updates and verse versa.

---
+++ akvo/rsr/spa/app/modules/editor/section5/cumulative/Toggle.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Form, Radio } from 'antd'
+import { useTranslation } from 'react-i18next'
+
+import InputLabel from '../../../../utils/input-label'
+import FinalField from '../../../../utils/final-field'
+import BlockToggleApi from './BlockToggleApi'
+
+const { Item } = Form
+
+const Toggle = ({
+  id: indicatorID,
+  name,
+  results,
+}) => {
+  const { t } = useTranslation()
+
+  const allIndicators = results?.flatMap((r) => r?.indicators)
+  const _indicator = allIndicators?.find((i) => i?.id === indicatorID)
+  const disabled = (_indicator?.contributionCount === undefined || (_indicator.contributionCount))
+  return (
+    <>
+      <BlockToggleApi id={indicatorID} name={name} indicator={_indicator} />
+      <Item
+        label={
+          <InputLabel tooltip={t('Select if indicators report a running total so that each reported actual includes the previously reported actual and adds any progress made since the last reporting period.')}>
+            {t('Cumulative')}
+          </InputLabel>
+        }
+      >
+        <FinalField
+          name={`${name}.cumulative`}
+          render={({ input }) => (
+            <Radio.Group {...input} disabled={disabled}>
+              <Radio.Button value={true}>{t('Yes')}</Radio.Button>
+              <Radio.Button value={false}>{t('No')}</Radio.Button>
+            </Radio.Group>
+          )}
+        />
+      </Item>
+    </>
+  )
+}
+
+export default connect(
+  ({
+    editorRdr: {
+      section5: {
+        fields: {
+          results,
+        },
+        errors
+      }
+    }
+  }) => ({ results, errors }),
+  null
+)(Toggle)
