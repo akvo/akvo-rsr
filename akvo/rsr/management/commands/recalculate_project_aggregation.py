43f7fdb3da98b982056776b64f4c57abac26f67f
zuhdil <zuhdil@gmail.com>
2023-02-03 13:30:15 +0700

    [#5212] Modify the `recalculate_project_aggregation` management command
    - Fix `--no-filter` arg and renamed to `--all`
    - Add `--quantitative` arg

--- akvo/rsr/management/commands/recalculate_project_aggregation.py
+++ akvo/rsr/management/commands/recalculate_project_aggregation.py
@@ -2,14 +2,16 @@ from django.core.management.base import BaseCommand
 from django.db.models import Count, Q
 
 from akvo.rsr.models import Project, IndicatorPeriod, IndicatorPeriodData
+from akvo.rsr.models.result.utils import QUANTITATIVE
 from akvo.rsr.usecases.jobs.aggregation import schedule_aggregation_job
 
 
 class Command(BaseCommand):
-    help = 'Script for recalculating periods aggregation of a project'
+    help = 'Script for recalculating periods aggregation of a project. By default will only apply to periods that have approved updates.'
 
     def add_arguments(self, parser):
-        parser.add_argument('--no-filter', action='store_true')
+        parser.add_argument('--all', action='store_true', help="Apply to all periods including periods with no approved updates")
+        parser.add_argument('--quantitative', action='store_true', help="Apply to quantitative indicator periods only. Will be ignored if --all is used")
         parser.add_argument('project_id', type=int)
 
     def handle(self, *args, **options):
@@ -21,12 +23,16 @@ class Command(BaseCommand):
 
         descendants = project.descendants()
         periods = IndicatorPeriod.objects.filter(indicator__result__project__in=descendants)
-        if options.get("no_filter") is None:
+        if not options.get("all"):
             periods = periods.annotate(
                 approved_count=Count('data', filter=Q(data__status=IndicatorPeriodData.STATUS_APPROVED_CODE))
             ).filter(approved_count__gte=1)
+            if options.get("quantitative"):
+                periods = periods.filter(indicator__type=QUANTITATIVE)
 
+        jobs = set()
         for period in periods:
-            schedule_aggregation_job(period)
+            new_jobs = schedule_aggregation_job(period)
+            jobs = jobs.union(set(new_jobs))
 
-        print(f"Scheduled period aggregation jobs: {periods.count()}, on project: {project.title}")
+        print(f"Scheduled period aggregation jobs: {len(jobs)}, on root project: {project.title}")
