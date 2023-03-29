0e56f2a6609ba8ffc6f57d7a86a7d23c255f5d0e
Michael <michael@akvo.org>
2023-03-29 15:29:57 +0200

    [#5258]doc: enable colon_fence for myst
    Allows markdown editors to highlight markdown syntax within myst directives
    
    https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#syntax-colon-fence

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,9 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+myst_enable_extensions = [
+    "colon_fence",  # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#syntax-colon-fence
+]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
