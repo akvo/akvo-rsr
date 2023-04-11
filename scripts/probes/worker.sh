e1385b044f9cfa710b447f8be8583f3d1781208c
zuhdil <zuhdil@gmail.com>
2023-04-11 15:22:47 +0700

    Adjust worker probe script

--- scripts/probes/worker.sh
+++ scripts/probes/worker.sh
@@ -2,6 +2,6 @@
 # The status is provided by a local server started by
 #  ./manage.py django_q_probettp
 
-status=$(curl -s localhost:8080)
-echo "returned status ${status}"
-[[ $status == "Working" ]] || [[ $status == "Idle" ]]
+status=$(curl -s -o /dev/null -w %{http_code} localhost:8080)
+echo "status code: ${status}"
+[[ $status == "400" ]]
