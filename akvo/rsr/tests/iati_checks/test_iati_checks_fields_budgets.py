60c62441fa4d7cee6770608e960bf4615d46f74e
zuhdil <zuhdil@gmail.com>
2023-05-22 17:09:07 +0700

    [#5280] Fix IATI budget checker and sector export

--- akvo/rsr/tests/iati_checks/test_iati_checks_fields_budgets.py
+++ akvo/rsr/tests/iati_checks/test_iati_checks_fields_budgets.py
@@ -9,10 +9,9 @@ For additional details on the GNU license please see < http://www.gnu.org/licens
 
 import datetime
 
-from django.conf import settings
 from akvo.rsr.tests.base import BaseTestCase
 from akvo.iati.checks.fields import budgets as budgets_checks
-from akvo.rsr.models import BudgetItem, BudgetItemLabel, ProjectEditorValidationSet
+from akvo.rsr.models import BudgetItem, BudgetItemLabel
 
 
 class IatiCheckBudgetPeriodNotLongerThanOneYearTestCase(BaseTestCase):
@@ -29,22 +28,6 @@ class IatiCheckBudgetPeriodNotLongerThanOneYearTestCase(BaseTestCase):
             period_end=datetime.date.today() + datetime.timedelta(days=366),
             value_date=datetime.date.today(),
         )
-
-    def test_dgis_validation_set(self):
-        validation, _ = ProjectEditorValidationSet.objects.get_or_create(name=settings.VALIDATION_SET['DGIS'])
-        self.project.validations.add(validation)
-        all_checks_passed, checks = budgets_checks(self.project)
-        self.assertFalse(all_checks_passed)
-        self.assertEqual('warning', checks[0][0])
-        self.assertIn('period must not be longer than one year', checks[0][1])
-
-    def test_dgis_modified_validation_set(self):
-        validation, _ = ProjectEditorValidationSet.objects.get_or_create(name=settings.VALIDATION_SET['DGIS_MODIFIED'])
-        self.project.validations.add(validation)
-        all_checks_passed, checks = budgets_checks(self.project)
-        self.assertFalse(all_checks_passed)
-        self.assertEqual('warning', checks[0][0])
-        self.assertIn('period must not be longer than one year', checks[0][1])
 
     def test_other_validation_set(self):
         all_checks_passed, checks = budgets_checks(self.project)
