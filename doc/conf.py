7fd13e7a2052e906a3ed654f023ed0bcaaf62120
zuhdil <zuhdil@gmail.com>
2023-10-05 10:24:27 +0700

    Use rtd_theme for the sphinx doc

--- doc/conf.py
+++ doc/conf.py
@@ -38,5 +38,5 @@ myst_enable_extensions = [
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'cloud'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
