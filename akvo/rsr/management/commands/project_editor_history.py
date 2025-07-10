2b4adeed145e1a0c05aa66eae8cca0cc6c85e928
zuhdil <zuhdil@gmail.com>
2025-07-10 11:35:15 +0700

    Change project editor history command output format

--- akvo/rsr/management/commands/project_editor_history.py
+++ akvo/rsr/management/commands/project_editor_history.py
@@ -5,9 +5,10 @@
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
 
-from django.contrib.admin.models import LogEntry, ADDITION, CHANGE, DELETION
+from django.contrib.admin.models import ADDITION, CHANGE, DELETION, LogEntry
 from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
+from tablib import Dataset
 
 from akvo.rsr.models import Project
 
@@ -30,9 +31,22 @@ class Command(BaseCommand):
             content_type_id=ContentType.objects.get_for_model(Project).pk,
             object_id=project_id,
         )
+        dataset = Dataset()
+        dataset.headers = [
+            "datetime",
+            "user",
+            "action",
+            "object",
+            "message",
+        ]
         for entry in entries:
-            action = ACTIONS[entry.action_flag]
-            user = entry.user.email
-            time = entry.action_time
-            message = entry.change_message
-            print(f"{time}\t{action}\t{user}\t{message}")
+            dataset.append(
+                [
+                    entry.action_time,
+                    entry.user.email,
+                    ACTIONS[entry.action_flag],
+                    entry.object_repr,
+                    entry.change_message,
+                ]
+            )
+        self.stdout.write(dataset.export("csv"))
