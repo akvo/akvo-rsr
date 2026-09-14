cbb4bdf275b6db3e98aadcf3d6ab221bf453b0a8
Zuhdil Herry Kurnia <zuhdil@gmail.com>
2026-09-14 11:44:56 +0700

    Realign two continuation lines after the assertEqual rename
    `assertEqual` is one character shorter than the removed `assertEquals`
    alias, which left the `msg=` arguments below it over-indented for their
    visual indent and tripped flake8 E127.

--- akvo/rsr/tests/views/py_reports/test_send_report_via_email.py
+++ akvo/rsr/tests/views/py_reports/test_send_report_via_email.py
@@ -74,7 +74,7 @@ class SendReportViaEmailTestCase(BaseTestCase):
         task_args = task_dict.get("args")
         params_arg = next(iter(task_args), {})
         self.assertEqual(self.program.id, params_arg.get("program_id"),
-                          msg="The expected program ID isn't present in the task's first argument")
+                         msg="The expected program ID isn't present in the task's first argument")
 
         # Emulate executing the task without going through django-q
         # There's currently no easy way to do so
@@ -126,7 +126,7 @@ class SendReportViaEmailTestCase(BaseTestCase):
         task_args = task_dict.get("args")
         params_arg = next(iter(task_args), {})
         self.assertEqual(self.org.id, params_arg.get("org_id"),
-                          msg="The expected organisation ID isn't present in the task's first argument")
+                         msg="The expected organisation ID isn't present in the task's first argument")
 
         # Emulate executing the task without going through django-q
         # There's currently no easy way to do so
